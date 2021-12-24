1fab508c88153bb1edc3496c149226c5b2c6dfa4
Purva Singh <singhpurva2906@gmail.com>
2021-12-24 09:04:59 -0500

    Time: 28 ms (22.56%), Space: 16.5 MB (11.06%) - LeetHub

---
+++ merge-intervals/merge-intervals.cpp
@@ -0,0 +1,38 @@
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>&b) {
+           if(a[0]!=b[0]) return a[0] < b[0];
+            return a[1] < b[1];
+        });
+        
+        vector<vector<int>> res;
+        
+        res.push_back(intervals[0]);
+        
+        for(int i=1;i<intervals.size();i++) {
+            
+            auto top = res.back();
+            
+            if(top[1] >= intervals[i][0]) {
+                
+                res.pop_back();
+                
+                vector<int> temp;
+                temp.push_back(top[0]);
+                
+                top[1] >= intervals[i][1]?temp.push_back(top[1]):temp.push_back(intervals[i][1]);
+                
+                res.push_back(temp);
+                
+            } else {
+                res.push_back(intervals[i]);
+            }
+            
+        }
+        
+        return res;
+    }
+};
