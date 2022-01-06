0ec32af3e8aab9d4613e3d088f72193652d716f5
Purva Singh <singhpurva2906@gmail.com>
2022-01-06 09:23:25 -0500

    Time: 8 ms (95.97%), Space: 10.2 MB (60.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
+        /*
+        TC: 
+        
+        */
         vector<int> total_passengers(1001, 0);
         
         for(auto x:trips) {
-            int start = x[1];
-            int end = x[2];
+            total_passengers[x[1]] += x[0];
+            total_passengers[x[2]] -= x[0];
+        }
+        
+        int cap=0;
+        
+        for(auto x:total_passengers) {
+            cap += x;
             
-            int passenger = x[0];
-            
-            for(int i=start;i<end;i++) {
-                total_passengers[i] += passenger;
-                
-                if(total_passengers[i] > capacity) return false;
-            }
+            if(cap > capacity) return false;
         }
         
         return true;
