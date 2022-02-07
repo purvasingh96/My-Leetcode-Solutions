1eeb515d3bc6d2009265caf8daeff696c572766f
Purva Singh <singhpurva2906@gmail.com>
2022-02-06 20:13:25 -0500

    Time: 10 ms (9.99%), Space: 6.8 MB (9.15%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+       map<int, int> m;
+        for(auto c:s){
+            m[c]+=1;
+        }
+        
+        for(auto c:t){
+            m[c]-=1;
+            if(m[c]==0) m.erase(c);
+        }
+        
+        return m.begin()->first;
+    }
+};
