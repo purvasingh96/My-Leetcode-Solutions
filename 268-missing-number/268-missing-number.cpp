558857934ccc0caaf4abf5e84943a606eb54f4f2
Purva Singh <singhpurva2906@gmail.com>
2022-02-07 07:54:59 -0500

    Time: 16 ms (90.86%), Space: 18.1 MB (19.85%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+       int missing = nums.size();
+       
+        for(int i=0;i<nums.size();i++) {
+            missing = missing ^ i ^ nums[i];
+        }
+        
+        return missing;
+        
+    }
+};
