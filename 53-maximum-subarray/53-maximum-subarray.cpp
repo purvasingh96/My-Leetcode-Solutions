61771eda07f46339d6e7b972af481b3c50d294e2
Purva Singh <singhpurva2906@gmail.com>
2022-02-13 11:41:57 -0500

    Time: 227 ms (9.81%), Space: 67.8 MB (54.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        vector<int> res(nums.size());
+        int prev = nums[0];
         int global_max = nums[0];
-        res[0] = nums[0];
         
         for(int i=1;i<nums.size();i++) {
             
-            res[i] = max(nums[i], (res[i-1]+nums[i]));
-            global_max = max(res[i], global_max);
+            prev = max(nums[i], (prev+nums[i]));
+            global_max = max(prev, global_max);
             
         }
         
