0fdd9d5f3a05cc549dd887b7219f4e1ea2be67a7
Purva Singh <singhpurva2906@gmail.com>
2022-03-05 09:55:50 -0500

    Time: 204 ms (45.75%), Space: 68.8 MB (70.34%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
         // monotonic decreasing queue
-        deque<int> q;
+        vector<int> q;
         
         q.push_back(0);
         
@@ -10,28 +10,22 @@ public:
             
             if(heights[q.back()] <= heights[i]) {
                 
-                while(!q.empty() && heights[q.back()] <= heights[i]) {
+                while(q.size()!=0 && heights[q.back()] <= heights[i]) {
                     q.pop_back();
                 }
                 
                 
             }
             
-            if(q.empty() || heights[q.back()] > heights[i]) {
+            if(q.size()==0 || heights[q.back()] > heights[i]) {
                 q.push_back(i);
             }
             
             
         }
         
-        vector<int> res;
         
-        while(!q.empty()) {
-            res.push_back(q.front());
-            q.pop_front();
-
-        }
         
-        return res;
+        return q;
     }
 };
