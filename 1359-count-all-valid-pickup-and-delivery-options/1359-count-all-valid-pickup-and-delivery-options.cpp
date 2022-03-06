ac06594204d094b111cdd9e7346e29e31c434e13
Purva Singh <singhpurva2906@gmail.com>
2022-03-06 08:30:28 -0500

    Time: 0 ms (100.00%), Space: 6 MB (31.45%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int countOrders(int n) {
         if(n==1) return 1;
-        vector<long> dp(n+1, 0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=6;
         
-        for(int i=3;i<=n;i++) {
+        
+        int ans = 1;
+        
+        for(int i=1;i<=n;i++) {
             
             int x = 2*i-1;
             int sum=0;
@@ -16,13 +15,13 @@ public:
                 sum+=j;
             }
             
-            long y = dp[i-1]%(1000000007);
+            long y = ans%(1000000007);
             
-            dp[i] = ((sum%1000000007)*y)%(1000000007);
+            ans = ((sum%1000000007)*y)%(1000000007);
             
         }
         
         
-        return dp[n]%(1000000007);
+        return ans;
     }
 };
