c5c8edfa894ff323834218c17a40b88e8416adb1
Purva Singh <singhpurva2906@gmail.com>
2022-03-11 15:54:15 -0500

    Time: 3 ms (97.42%), Space: 11.8 MB (55.94%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -10,40 +10,43 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        
-        int n = 1, i=1;
-        
-        if(!head || !head->next) return head;
-        
-        ListNode* curr = head;
-        
-        while(curr->next!=NULL) {
-            curr=curr->next;
-            n+=1;
+    
+    int nodeLength(ListNode* head) {
+        ListNode* temp = head;
+        int len=0;
+        while(temp) {
+            len+=1;
+            temp=temp->next;
         }
-        
-        curr->next = head;
-        
-        k = k%n;
-        
-        ListNode* new_tail = head;
-        
-        while(i!=(n-k)) {
-            
-            new_tail = new_tail->next;
-            i+=1;
-            
-        }
-        
-        ListNode* new_head = new_tail->next;
-        new_tail->next=NULL;
-        
-        return new_head;
-        
-        
-        
+        return len;
     }
     
-    
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next) return head;
+        
+        int len = nodeLength(head);
+        k = k%len;
+        if(k == 0) return head;
+        k = len - k;
+        //cout<<len<<" "<<k;
+        int i=1;
+        ListNode* temp = head;
+        
+        while(i!=k) {
+            i+=1;
+            temp = temp->next;
+        }
+        
+        ListNode* head_2 = temp->next;
+        temp->next=NULL;
+        ListNode* ans = head_2;
+        
+        while(head_2->next!=NULL) {
+            head_2 = head_2->next;
+        }
+        
+        head_2->next = head;
+        return ans;
+        
+    }
 };
