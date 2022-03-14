512cac4757de0c2da6f9f9f80afd5873536ed448
Purva Singh <singhpurva2906@gmail.com>
2022-03-14 08:11:59 -0400

    Time: 11 ms (38.47%), Space: 9.6 MB (45.10%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     string simplifyPath(string path) {
         vector<string> stack;
-        string res, temp;
         stringstream p(path);
         
-        while(getline(p, temp, '/')){
+        string temp;
+        
+        while(getline(p, temp, '/')) {
             
-            if(temp == "." || temp == "") continue;
-            else if(!stack.empty() && temp == "..") stack.pop_back();
+            if(temp == "." || temp =="") continue;
+            else if(stack.size()!=0 && temp == "..") stack.pop_back();
             else if(temp!="..") stack.push_back(temp);
             
         }
         
+        string res = "";
         
-        for(auto s:stack) {
-            res += ("/" + s);
+        for(auto c:stack) {
+            
+            res += ("/" + c);
+            
         }
         
         return stack.empty() ? "/":res;
-        
     }
 };
