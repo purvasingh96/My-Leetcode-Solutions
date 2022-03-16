79e4275047499ba89d92b7cc4d48e476a816fb5a
Purva Singh <singhpurva2906@gmail.com>
2022-03-16 10:51:29 -0400

    Time: 20 ms (17.80%), Space: 15.3 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -5,20 +5,9 @@ public:
         
         int left=0, right=0;
         
-        while(left<pushed.size() && right<popped.size()) {
+        for(int left=0;left<pushed.size();left++) {
             
-            while(left< pushed.size() && pushed[left]!=popped[right]) {
-                
-                st.push(pushed[left]);
-                left+=1;
-                
-            }
-            
-            if(left < pushed.size()) {
-                st.push(pushed[left]);
-                left+=1;
-            }
-            
+            st.push(pushed[left]);
             
             while(!st.empty() && right < popped.size() && st.top() == popped[right]) {
                 
@@ -26,12 +15,8 @@ public:
                 right+=1;
             }
             
-            if(left >= pushed.size() && right < popped.size()) return false;
-            
-            
-            
         }
         
-        return true;
+        return st.empty();
     }
 };
