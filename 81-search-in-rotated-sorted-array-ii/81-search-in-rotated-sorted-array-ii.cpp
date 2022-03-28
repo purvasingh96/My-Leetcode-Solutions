a187139e010a613cc09cf6698c642cc06a4a9770
Purva Singh <singhpurva2906@gmail.com>
2022-03-28 16:57:04 -0400

    Time: 7 ms (71.49%), Space: 14 MB (71.85%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    bool binary_search(vector<int>& nums, int start, int end, int target) {
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid] == target) return true;
-            
-            else if(target > nums[mid]) start = mid+1;
-            else end = mid-1;
-        }
-        return false;
-        
-    }
-    
-    
     bool search(vector<int>& nums, int target) {
-        int start=0, end=0;
-        if(nums.size()==1) return nums[0]==target;
+        int left=0;
+        int right = nums.size()-1;
         
-        for(int i=0;i<nums.size();i++) {
+        while(left<=right) {
+            int mid = left + (right-left)/2;
             
-            if(nums[i] == target) {
-                return true;
-            }
+            if(target == nums[mid]) return true;
             
-            if(i<nums.size()-1) {
-                if(nums[i] >= nums[i+1]) {
-                if(target >= nums[0] && target <= nums[i]) {
-                    return binary_search(nums, 0, i, target);
-                } else if(target >=nums[i+1] && target <= nums.back()) {
-                    return binary_search(nums, i+1, nums.size()-1, target);
+            if(nums[left] < nums[mid]) {
+                // first half is sorted
+                if(target >= nums[left] && target < nums[mid]) {
+                    right = mid-1;
+                } else {
+                    // second half is sorted
+                    left = mid+1;
+                }
+            } else if(nums[left] == nums[mid]) {
+                
+                left+=1;
+                
+            } else {
+                
+                if(target > nums[mid] && target <= nums[right]) {
+                    
+                    left = mid+1;
+                } else{
+                    right = mid-1;
                 }
                 
-                }
             }
             
-             
-         }
+        }
         
         return false;
-        
     }
 };
