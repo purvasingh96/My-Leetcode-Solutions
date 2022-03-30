88d870b740b45bd7e45e4573dc943d747aa6a3be
Purva Singh <singhpurva2906@gmail.com>
2022-03-30 08:04:00 -0400

    Time: 84 ms (82.10%), Space: 42.1 MB (9.38%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -8,33 +8,19 @@ public:
         
         int left=0, right = people.size()-1;
         
-        while(left<right) {
+        while(left<=right) {
             
-            
+            boat+=1;
             int sum = people[left] + people[right];
             
             if(sum <= limit) {
-                boat += 1;
+                
                 left += 1;
-                right -=1;
-            } else {
-                
-                if(people[left] > people[right]) {
-                    boat+=1;
-                    left+=1;
-                } else if(people[right] > people[left]) {
-                    boat+=1;
-                    right-=1;
-                } else {
-                    boat+=2;
-                    left+=1;
-                    right-=1;
-                }
-                
-            }
+            } 
+            
+            right-=1;
         } 
         
-        if(left==right) boat+=1;
         
         return boat;
         
