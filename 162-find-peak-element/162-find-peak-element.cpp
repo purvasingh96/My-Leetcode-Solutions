d0bf502a75b7288dc4652c52f406c4d840ebd0e6
Purva Singh <singhpurva2906@gmail.com>
2022-04-03 20:12:08 -0400

    Time: 12 ms (8.69%), Space: 8.8 MB (89.36%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -3,37 +3,33 @@ public:
     int findPeakElement(vector<int>& nums) {
         
         // O(logN) => binary search approach.
-        if(nums.size()==1) return 0;
+        int n = nums.size();
+        if(n==1) return 0;
+        if(n == 2) return nums[0] > nums[1] ? 0:1;
         
-        int low = 0, high = nums.size()-1;
+        int low = 1, high = nums.size()-2;
         
         while(low<=high) {
             
             int mid = low + (high-low)/2;
             
-            
-            if(mid-1>=0 && mid+1<nums.size()) {
                 if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
                     return mid;
                 }
                 
-                else if(nums[mid] <= nums[mid+1]) low = mid+1;
+                else if(nums[mid] < nums[mid+1]) low = mid+1;
             
                 else if(nums[mid] <= nums[mid-1]) high = mid-1;
-                
-            }  else if(mid-1 == -1) {
-                
-                if(nums[mid] > nums[mid+1]) return mid;
-                else if(nums[mid] <=nums[mid+1]) low = mid+1;
-                
-            } else if(mid+1 == nums.size()) {
-                
-                if(nums[mid] > nums[mid-1]) return mid;
-                else if(nums[mid]<=nums[mid-1]) high = mid-1;
-                
-            }
             
         }
+        
+        if(nums[0]>nums[1]) return 0;
+        
+        if(nums[n-1] > nums[n-2]) return n-1;
+        return -1;
+        
+        
+        
         
         return 0;
         
