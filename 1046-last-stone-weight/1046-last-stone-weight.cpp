7fdcc0aabac74730a8f2b28b67df0492f36f3e3e
Purva Singh <singhpurva2906@gmail.com>
2022-04-07 10:54:44 -0400

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        int max_weight = stones[0];
         
-        priority_queue<int> pq;
+        for(auto x:stones) max_weight = max(max_weight, x);
         
-        for(auto x:stones) {
-            pq.push(x);
+        vector<int> buckets(max_weight+1);
+        
+        for(auto weight:stones) {
+            buckets[weight] +=1;
         }
         
-        while(pq.size()>1) {
+        int biggest_weight = 0;
+        int curr_weight = max_weight;
+        
+        while(curr_weight > 0) {
             
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
+            if(buckets[curr_weight] == 0) curr_weight -= 1;
             
-            pq.push(a-b);
+            else if(biggest_weight == 0) {
+                
+                buckets[curr_weight] %= 2;
+                
+                if(buckets[curr_weight] == 1) biggest_weight = curr_weight;
+                
+                curr_weight -=1;
+                
+            } else {
+                
+                buckets[curr_weight] -= 1;
+                
+                if(biggest_weight - curr_weight <= curr_weight) {
+                    cout<<biggest_weight - curr_weight<<"\n";
+                    buckets[biggest_weight - curr_weight] += 1;
+                    biggest_weight = 0;
+                } else {
+                    biggest_weight -= curr_weight;
+                }
+                
+                
+                
+            }
+            
+            
             
         }
         
-        return pq.empty()?0:pq.top();
+        return biggest_weight;
         
     }
 };
