df3a16d2ab2664b9168dc53080f23010e4705a2f
Purva Singh <singhpurva2906@gmail.com>
2022-04-08 13:05:07 -0400

    Time: 18 ms (6.84%), Space: 7.3 MB (76.72%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -5,18 +5,18 @@ public:
         
         while(left<right) {
             
-            if(!isalnum(s[left])){
+            
                 while(left<right && !isalnum(s[left])) {
                     left += 1;
                 }
             
-            } 
             
-            if(!isalnum(s[right])) {
+            
+            
                 while(right>left && !isalnum(s[right])) {
                         right -= 1;
                     }
-            }
+            
             
             if(left == right) {
                 
@@ -24,11 +24,9 @@ public:
                 
                 
             }
+          
             
-            if(isalpha(s[left]) && isupper(s[left])) s[left] = tolower(s[left]);
-            if(isalpha(s[right]) && isupper(s[right])) s[right] = tolower(s[right]);
-            
-            if(s[left] != s[right]) return false;
+            if(tolower(s[left]) != tolower(s[right]) ) return false;
             
             left +=1;
             right -= 1;
