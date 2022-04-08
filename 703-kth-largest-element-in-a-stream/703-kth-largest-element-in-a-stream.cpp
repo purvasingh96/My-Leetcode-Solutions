55053e0d63eb374170ef0e91e4d57594035650ed
Purva Singh <singhpurva2906@gmail.com>
2022-04-08 10:02:12 -0400

    Time: 41 ms (64.44%), Space: 19.9 MB (17.78%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -3,14 +3,22 @@ private:
     priority_queue<int, vector<int>, greater<int>> pq;
     int kth;
 public:
+    // total time complexity = Nlogk + Mlogk
+    // space complexity = O(k)
+    
     KthLargest(int k, vector<int>& nums) {
         kth = k;
+        // nlogk
         for(auto x:nums) {
+            
             add(x);
         }
     }
     
+    // making M calls to add => Mlogk
+    
     int add(int val) {
+        // log k operation
         pq.push(val);
         if(pq.size()>kth) pq.pop();
         return pq.top();
