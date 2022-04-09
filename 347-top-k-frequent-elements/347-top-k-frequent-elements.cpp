3485f4106aa3e03983b9ec918bd148b621d404ba
Purva Singh <singhpurva2906@gmail.com>
2022-04-09 18:13:13 -0400

    Time: 16 ms (82.38%), Space: 13.9 MB (22.21%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,22 +1,21 @@
 class Solution {
 private:
     vector<int> unique;
-    map<int, int> count_map;
+    map<int, int> m;
 public:
-    // Quick Select
-    // partition
     int partition(int left, int right, int pivot_idx) {
         
-        int pivot_freq = count_map[unique[pivot_idx]];
+        int pivot_freq = m[unique[pivot_idx]];
         
         swap(unique[pivot_idx], unique[right]);
+        
         int store_idx = left;
         
         for(int i=left;i<=right;i++) {
             
-            if(count_map[unique[i]] < pivot_freq) {
+            if(m[unique[i]] < pivot_freq) {
                 
-                swap(unique[i], unique[store_idx]);
+                swap(unique[store_idx], unique[i]);
                 store_idx += 1;
                 
             }
@@ -32,37 +31,42 @@ public:
         
         if(left == right) return;
         
-        int pivot_idx = left + rand()%(right-left+1);
+        int pivot_idx = left + rand()%(right - left +1);
         
         pivot_idx = partition(left, right, pivot_idx);
         
         if(pivot_idx == k) return;
-        else if(k < pivot_idx) {
-            quickSelect(left, pivot_idx-1, k);
-        } else {
+        else if (pivot_idx < k) {
+            
             quickSelect(pivot_idx+1, right, k);
+            
+        } else {
+            quickSelect(left, pivot_idx-1, k);
         }
         
     }
     
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        for(auto x:nums) count_map[x]+=1;
         
-        for(auto x:count_map) {
-            unique.push_back(x.first);
+        for(auto x:nums) m[x] += 1;
+        for(auto it:m) {
+            unique.push_back(it.first);
         }
-        
-        int n = count_map.size();
+        cout<<m.size();
+        int n = m.size();
         
         quickSelect(0, n-1, n-k);
         
-        vector<int> topk(k);
         
-        copy(unique.begin() + n-k, unique.end(), topk.begin());
+        vector<int> top_k_frequent;
         
-        return topk;
+        for(int i=n-k;i<n;i++) {
+            top_k_frequent.push_back(unique[i]);
+        }
+        //copy(unique.begin() + n - k, unique.end(), top_k_frequent.begin());
         
+        return top_k_frequent;
         
     }
 };
