c5453fd85ef34383e8533f108ad280bd4d4cbcfd
Purva Singh <singhpurva2906@gmail.com>
2022-04-10 15:56:35 -0400

    Time: 24 ms (96.95%), Space: 17.8 MB (92.68%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.cpp
+++ 498-diagonal-traverse/498-diagonal-traverse.cpp
@@ -1,30 +1,79 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        vector<vector<int>> arr((m+n+1));
         
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
+        /*
+        moving_up_right= true
+        count = 1;
+        if(moving_up_right):
+            if j == n-1:
+                i++
+                moving_up_right = false
+            else if j==0:
+                j++;
+                moving_up_right = false
+            else:
+                i++
+                j--
+        
+        else:
+            if i== m-1:
+                j++;
+                moving_up_right = true;
+            else if j == 0:
+                i++;
+                moving_up_right = true;
+            else:
+                i++
+                j--
                 
-                arr[(i+j)].push_back(mat[i][j]);
+        res[count++] = mat[i][j]
+        */
+        
+        int m=mat.size(), n = mat[0].size();
+        
+        int i=0, j=0, count=1;
+        vector<int> res(m*n);
+        bool moveUpRight = true;
+        
+        res[0] = mat[0][0];
+        
+        while(i!=m-1 || j!=n-1) {
+            
+            if(moveUpRight) {
+                
+                if(j == n-1)  {
+                    i+=1;
+                    moveUpRight = false;
+                } else if(i == 0) {
+                    j+=1;
+                    moveUpRight = false;
+                } else {
+                    i-=1;
+                    j+=1;
+                }
+                 
+            } else {
+                
+                if(i == m-1) {
+                    j+=1;
+                    moveUpRight=true;
+                } else if(j == 0) {
+                    i+=1;
+                    moveUpRight = true;
+                } else {
+                    i+=1;
+                    j-=1;
+                }
                 
             }
-        }
-        
-        bool flip = true;
-        vector<int> res;
-        
-        for(int i=0;i<arr.size();i++) {
-            if(flip) {
-                reverse(arr[i].begin(), arr[i].end());
-            }
-            for(auto x:arr[i]) res.push_back(x);
-            flip = !flip;
+            
+            res[count++] = mat[i][j];
+         
+            
         }
         
         return res;
+        
     }
-    
-    
 };
