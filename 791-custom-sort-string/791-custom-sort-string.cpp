a241d9b3c66c61b0ff01ac2b43073aadc7619203
Purva Singh <singhpurva2906@gmail.com>
2022-04-11 11:47:07 -0400

    Time: 0 ms (100.00%), Space: 6.3 MB (58.23%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.cpp
+++ 791-custom-sort-string/791-custom-sort-string.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        map<char, int> m;
-        map<char, int> n;
+        
+        vector<int> freq(26, 0);
         string res="";
         //for(auto x:order) m[x] +=1;
         
-        for(auto x:s) n[x] +=1;
+        for(auto x:s) freq[x - 'a'] +=1;
         
         for(auto x:order) {
-            
-            if(n.find(x)!=n.end()) {
-                while(n[x]) {
+            if(freq[x-'a']!=0) {
+                while(freq[x - 'a']) {
                     res += x;
-                    n[x] -=1;
+                    freq[x-'a'] -=1;
                 }
             }
             
         }
         
-        for(auto it:n) {
-            if(it.second != 0) {
-                int size = it.second;
-                while(size--) {
-                    res += it.first;
-                }
-            }
-        }
+       for(int i=0;i<freq.size();i++) {
+           if(freq[i]!=0) {
+               
+               while(freq[i]!=0) {
+                   res += (i+'a');
+                   freq[i]-=1;
+               }
+               
+           }
+       }
         
         return res;
         
