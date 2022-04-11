ed46258a37e4406e0e4f989e02ce3b057d7442fa
Purva Singh <singhpurva2906@gmail.com>
2022-04-11 10:30:06 -0400

    Time: 52 ms (42.74%), Space: 14.3 MB (32.76%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -2,38 +2,28 @@ class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         
+        
         int m = grid.size(), n = grid[0].size();
+        k = k%(m*n);
         vector<int> temp_res(m*n);
         vector<vector<int>> res(m, vector<int>(n));
+        int row=0, col=0;
         
         for(int i=0;i<grid.size();i++) {
             
             for(int j=0;j<grid[i].size();j++) {
                 
-                int cur_idx = (n*i) + j;
-                int new_idx = (cur_idx+k)%(m*n);
-                temp_res[new_idx] = grid[i][j];
+                int new_col = (j+k)%n;
+                
+                int new_row = (i+(j+k)/n)%m;
+                
+                res[new_row][new_col] = grid[i][j];
+                
             }
             
         }
         
-        //for(auto x:temp_res) cout<<x<<" ";
-        
-        
-        int row=0, col=0;
-        
-        for(int i=0;i<temp_res.size();i++) {
-            
-            if(i%n==0 && i!=0) {
-                row+=1;
-                col=0;
-                res[row][col] = temp_res[i];
-                col+=1;
-            }   else{
-                res[row][col] = temp_res[i];
-                col+=1;
-            }
-        }
+
         
         return res;
         
