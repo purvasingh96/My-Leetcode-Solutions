ebe9b6745e02e0f60e5e99c1fc61a3452b3ab0c8
Purva Singh <singhpurva2906@gmail.com>
2022-04-13 22:36:19 -0400

    Time: 0 ms (100.00%), Space: 7.9 MB (89.37%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.cpp
+++ 249-group-shifted-strings/249-group-shifted-strings.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupStrings(vector<string>& strings) {
-        map<vector<int>,vector<string>> mp;
+        map<string,vector<string>> mp;
         for(string &st: strings)
         {
             vector<int> temp;
-            for(int i=1; i<st.size(); i++) temp.push_back((26+st[i]-st[i-1])%26);
-            mp[temp].push_back(st);
+            string t="";
+            for(int i=1; i<st.size(); i++) {
+                t += ((26 + st[i] - st[i-1])%26 + 'a') + ',';
+            }
+            mp[t].push_back(st);
         }
         vector<vector<string>> ans;
         cout<<('z' - 'a')<<"\n";
