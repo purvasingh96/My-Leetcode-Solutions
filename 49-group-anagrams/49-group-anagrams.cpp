ae9e2306f3d636ea7f8240397fce4658190a5805
Purva Singh <singhpurva2906@gmail.com>
2022-04-13 22:59:58 -0400

    Time: 36 ms (85.31%), Space: 24.3 MB (14.92%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    vector<int> createHash(string s) {
-        
+    string createHash(string s) {
+        // O(K)
         vector<int> count(26, 0);
         for(auto c:s) {
             count[c-'a'] += 1;
         }
         
-        return count;
+        string hash = "";
+        
+        for(int i=0;i<count.size();i++) {
+            if(count[i]>0) hash += string(count[i], i+'a');
+        }
+        
+        return hash;
     }
     
     
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-           map<vector<int>, vector<string>> m;
+           map<string, vector<string>> m;
             vector<vector<string>> res;
-        
+        //O(NK)
         for(auto c:strs) {
             m[createHash(c)].push_back(c);
         }
