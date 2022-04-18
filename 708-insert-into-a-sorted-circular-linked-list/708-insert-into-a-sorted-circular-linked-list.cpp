f326563ea194dfa3a635d34d9a609cf412f5062f
Purva Singh <singhpurva2906@gmail.com>
2022-04-18 11:36:11 -0400

    Time: 10 ms (56.10%), Space: 8.2 MB (9.88%) - LeetHub

---
+++ 708-insert-into-a-sorted-circular-linked-list/708-insert-into-a-sorted-circular-linked-list.cpp
@@ -0,0 +1,81 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+    }
+
+    Node(int _val, Node* _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* insert(Node* head, int insertVal) {
+        Node* newnode = new Node(insertVal);
+        
+        if(head==NULL) {
+            newnode->next=newnode;
+            return newnode;
+        }
+        
+        if(head->next==head) {
+            head->next=newnode;
+            newnode->next=head;
+            return head;
+        }
+        
+        Node* ans=head;
+        
+        while(head->val < head->next->val) head=head->next;
+        
+        head=head->next;
+        
+        cout<<head->next->val;
+        
+        Node* curr = head->next;
+        Node* prev=head;
+        
+        while(true) {
+            
+            int x = insertVal;
+            int y = prev->val;
+            int z = curr->val;
+            
+            if((y <= x && x<= z)) {
+                prev->next=newnode;
+                newnode->next=curr;
+                break;
+            }
+            
+            if(y>=z && (x<z || x>y)){
+                while(prev->val <= curr->val) {
+                    if(prev->next==head) break;
+                    prev=prev->next;
+                    curr=curr->next;
+                }
+                //cout<<prev->val<<" "<<curr->val;
+                prev->next=newnode;
+                newnode->next=curr;
+                break;
+                
+            }
+                
+            
+            prev=curr;
+            curr=curr->next;
+        }
+     
+        return ans;
+    }
+};
