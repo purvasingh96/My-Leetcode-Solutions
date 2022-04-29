40c6095627b854ed4823e741f90c637612301c6b
Purva Singh <singhpurva2906@gmail.com>
2022-04-29 14:02:17 -0400

    Time: 518 ms (32.53%), Space: 41.2 MB (5.19%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,57 @@
+class Solution {
+private:
+    map<int, int> rank;
+    map<int, int> root;
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()<2) return nums.size();
+        // rank.resize(1e5);
+        // root.resize(1e5);
+        //map<int, int> m;
+        
+        for(auto x:nums) {
+            rank[x] = 1;
+            root[x] = x;
+        }
+        
+        for(auto x:nums) {
+            if(rank.find(x+1)!=rank.end()) {
+                union1(x, x+1);
+            }
+            
+            if(rank.find(x-1)!=rank.end()) {
+                union1(x, x-1);
+            }
+        }
+        
+        int max_length = 0;
+        for(auto x:rank) {
+            max_length = max(max_length, x.second);
+        }
+        
+        return max_length;
+        
+    }
+    
+    int find(int x) {
+        if(root[x] == x) return x;
+        
+        return root[x] = find(root[x]);
+    }
+    
+    void union1(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+        
+        if(rx!=ry) {
+            
+            if(rank[rx] >= rank[ry]) {
+                root[ry] = rx;
+                rank[rx] += rank[ry];
+            } else {
+                root[rx] = ry;
+                rank[ry] += rank[rx];
+            }
+        }
+    }
+};
