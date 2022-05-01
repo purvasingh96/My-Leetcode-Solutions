de60aabd18f19f6d3ce07c446dc71ac57b474ecd
Purva Singh <singhpurva2906@gmail.com>
2022-05-01 19:12:52 -0400

    Time: 0 ms (100.00%), Space: 6.2 MB (68.77%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    void finalString(string s, stack<char>& st) {
-        for(auto x:s) {
-            
-            if(x!='#') {
-                st.push(x);
-            }
-            
-            else if(x=='#') {
-                if(st.empty()) continue;
-                else st.pop();
-            }
-            
-        }
-    }
-    
     bool backspaceCompare(string s, string t) {
-        stack<char> sts;
-        stack<char> stt;
-        
-        finalString(s, sts);
-        finalString(t, stt);
-        
-        if(sts.size()!=stt.size()) {
+        int k=0,p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='#')
+            {
+                k--;
+                 k=max(0,k);
+            }
+            
+           else
+           {
+               s[k]=s[i];
+               k++;
+           }
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=='#')
+            {
+                p--;
+                 p=max(0,p);
+            }
+            
+           else
+           {
+               t[p]=t[i];
+               p++;
+           }
+        }
+        if(k!=p)
             return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i])
+                    return false;
+            }
+            return true;
         }
         
-        while(!sts.empty()) {
-            if(sts.top()!=stt.top()) return false;
-            sts.pop();
-            stt.pop();
-        }
-        
-        return true;
     }
 };
