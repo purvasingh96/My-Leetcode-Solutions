e64061c99fb014bd8bb29fb911ecbe6995081263
Purva Singh <singhpurva2906@gmail.com>
2022-05-08 10:21:08 -0400

    Time: 19 ms (86.54%), Space: 10.3 MB (63.87%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,53 +1,57 @@
 class Solution {
 public:
+//     string removeDuplicates(string s, int k) {
+//         stack<pair<char, int>> st;
+        
+//         int i=0;
+        
+//         while(i<s.length()) {
+            
+//             if(st.empty() || s[i]!=st.top().first)  {
+//                 st.push(make_pair(s[i], 1));
+//                 i+=1;
+//             } else{
+                
+//                 auto curr = st.top();
+                
+//                 if(curr.second+1 == k) {
+//                     st.pop();
+//                 } else{
+//                     st.pop();
+//                     st.push(make_pair(curr.first, curr.second+1));
+                    
+                    
+//                 }
+                    
+//                 i+=1;
+                
+//             }
+            
+//         }
+        
+//         string res="";
+        
+//         while(!st.empty()) {
+//             auto x = st.top();
+//             st.pop();
+//             res += string(x.second, x.first);
+//         }
+        
+        
+//         reverse(res.begin(), res.end());
+//         return res;
+        
+        
+//     }
+
     string removeDuplicates(string s, int k) {
-        stack<pair<char, int>> st;
-        
-        int i=0;
-        
-        while(i<s.length()) {
-            
-            if(st.empty() || s[i]!=st.top().first)  {
-                st.push(make_pair(s[i], 1));
-                i+=1;
-            } else{
-                
-                auto curr = st.top();
-                
-                if(curr.second+1 == k) {
-                    st.pop();
-                } else{
-                    st.pop();
-                    st.push(make_pair(curr.first, curr.second+1));
-                    
-                    
-                }
-                    
-                i+=1;
-                
-            }
-            
-        }
-        
-        string res="";
-        
-        while(!st.empty()) {
-            
-            auto curr = st.top();
-            st.pop();
-            int f = curr.second;
-            
-            while(f!=0) {
-                res+= (curr.first);
-                f-=1;
-            }
-            
-            
-        }
-        
-        reverse(res.begin(), res.end());
-        return res;
-        
-        
-    }
+  vector<pair<char, short>> st;
+  string res;
+  for (auto ch : s) {
+    if (st.empty() || st.back().first != ch) st.push_back({ ch, 0 });
+    if (++st.back().second == k) st.pop_back();
+  }
+  for (auto& p : st) res += string(p.second, p.first);
+  return res;
+}
 };
