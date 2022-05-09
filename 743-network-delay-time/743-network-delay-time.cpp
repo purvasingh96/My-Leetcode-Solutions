17806d5c0a3b756fcafc11973380199c2a4b5042
Purva Singh <singhpurva2906@gmail.com>
2022-05-08 23:25:18 -0400

    Time: 154 ms (58.66%), Space: 43.9 MB (21.37%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,57 +1,49 @@
-struct Compare{
-  public:
+class MyCompare{
+public:
     bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
         return a.second > b.second;
     }
 };
+
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // single source + 
-        // weighted edges + 
-        // shortest path +
-        // +ve weights => dijkstra template
-        
-        vector<int> recieveTime(n+1, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
-        pq.push(make_pair(k, 0));
-        recieveTime[k]=0;
-        
+        int totalTime = INT_MIN;
+        vector<int> dist(n+1, INT_MAX);
+        dist[k] = 0;
         map<int, vector<pair<int, int>>> adj;
         
         for(auto x:times) {
-            adj[x[0]].push_back(make_pair(x[1], x[2]));
+            adj[x[0]].push_back({x[1], x[2]});
         }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, MyCompare> pq;
+        
+        pq.push({k, 0});
         
         while(!pq.empty()) {
             
-            auto f = pq.top();
+            auto top = pq.top();
             pq.pop();
             
-            int parent = f.first;
-            int time = f.second;
+            int parent = top.first, time = top.second;
             
-            for(auto c:adj[parent]) {
+            for(auto x:adj[parent]) {
+                int child = x.first;
+                int newTime = x.second;
                 
-                int child = c.first;
-                int new_time = c.second;
-                
-                if(recieveTime[child] > time + new_time) {
-                    
-                    recieveTime[child] = time + new_time;
-                    pq.push(make_pair(child, recieveTime[child]));
+                if(dist[child] > time + newTime) {
+                    dist[child] = time + newTime;
+                    pq.push({child, dist[child]});
                 }
-                
             }
             
         }
         
-        int ans = INT_MIN;
-        for(int i=1;i<=n;i++) {
-            ans = max(ans, recieveTime[i]);
+        for(int i=1;i<dist.size();i++) {
+            totalTime = max(totalTime, dist[i]);
         }
-        
-        return ans ==INT_MAX?-1:ans;
+        return totalTime==INT_MAX?-1:totalTime;
         
     }
 };
