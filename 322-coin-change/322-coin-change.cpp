9101542c10c084fbb3304ed1d36fe01b4bd3eea9
Purva Singh <singhpurva2906@gmail.com>
2022-05-21 10:10:58 -0400

    Time: 155 ms (29.73%), Space: 14.5 MB (54.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int dfs(vector<int>& coins, int remaining, vector<int>& count) {
-        if(remaining <0) return -1;
-        if(remaining == 0) return 0;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, amount+1);
+        dp[0]=0;
         
-        if(count[remaining-1]!=0) return count[remaining-1];
-        
-        int minval=INT_MAX;
-        
-        for(auto coin:coins){
-            int res  = dfs(coins, remaining-coin, count);
-            if(res>=0 && res<minval) {
-                minval = 1+res;
+        for(int i=0;i<=amount;i++) {
+            for(int j=0;j<coins.size();j++) {
+                
+                if(coins[j]<=i) {
+                    
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                    
+                }
+                
             }
         }
         
-        count[remaining-1] = minval==INT_MAX?-1:minval;
-        return count[remaining-1];
-        
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> count(amount, 0);
-        return dfs(coins, amount, count);
+        return dp[amount] == amount+1?-1:dp[amount];
     }
 };
