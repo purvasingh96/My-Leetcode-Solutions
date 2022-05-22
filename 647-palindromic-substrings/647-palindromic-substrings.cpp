64bfb7644a9eb43a10825d5b97cf6d2965314326
Purva Singh <singhpurva2906@gmail.com>
2022-05-22 09:53:42 -0700

    Time: 27 ms (37.17%), Space: 20.3 MB (12.05%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n+1, vector<int>(n, 0));
+        
+        // base case
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
+            dp[1][j]=1;
+        }
+        
+        int res = s.length();
+        
+        for(int i=2;i<n+1;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(j+i-1<n && s[j] == s[j+i-1]){
+                    
+                    if(dp[i-2][j+1]) {
+                        
+                        dp[i][j]+=1;
+                        res+=1;
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+//         for(auto x:dp){
+//             for(auto y:x){
+//                 cout<<y<<" ";
+//             }
+//             cout<<"\n";
+//         }
+        
+//         cout<<res;
+        
+        return res;
+            
+    }
+};
