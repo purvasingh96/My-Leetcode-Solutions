75a02a41973ccd66b07a7275ccc1bf1071c9629f
Purva Singh <singhpurva2906@gmail.com>
2022-05-22 16:27:57 -0700

    Time: 6 ms (27.51%), Space: 9.1 MB (73.04%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        
-        int p1 = m-1, p2=n-1, p=(m+n)-1;
-        
-        for(p = (m+n-1);p>=0;p--) {
+        /*
+        - copy nums2 to nums1 where 0s are present O(n)
+        - left=0, right=n;
+        - while(left<n && right<(m+n))
+            - if n[left] > n[right]: swap them and 
             
+        */
+        
+        int p1=m-1, p = (m+n-1), p2=n-1;
+        
+        for(p;p>=0;p--){
             if(p2<0) break;
-            
-            if(p1>=0 && nums1[p1] > nums2[p2]) {
+            if(p1>=0 && nums1[p1] > nums2[p2]){
+                nums1[p]=nums1[p1];
                 
-                nums1[p] = nums1[p1--];
+                p1--;
+            } else{
+                nums1[p]=nums2[p2];
                 
-            } else {
-                
-                nums1[p] = nums2[p2--];
-                
+                p2--;
             }
             
         }
         
+        
     }
+    
 };
