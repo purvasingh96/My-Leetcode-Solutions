990ff4440f33a5a468f09559ee671b61d7355929
Purva Singh <singhpurva2906@gmail.com>
2022-05-25 18:11:08 -0700

    Time: 49 ms (50.00%), Space: 13.9 MB (47.34%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        map<int, vector<pair<int, int>>> adj;
-        
-        for(auto x:flights) {
-            adj[x[0]].push_back({x[1], x[2]});
-        }
-        
+        // we know source, and asked to find cheapest route = BFS + dijkstra
         vector<int> dist(n, INT_MAX);
-        k+=1;
-        
         dist[src] = 0;
-        
         queue<pair<int, int>> q;
-        
         q.push({0, src});
         
-        while(!q.empty() && k--) {
+        map<int, vector<pair<int, int>>> adj;
+        
+        for(auto f:flights) {
+            adj[f[0]].push_back({f[1], f[2]});
+        }
+        
+        k+=1;
+        
+        while(!q.empty() && k--){
+            int sz = q.size();
             
-            int n = q.size();
-            
-            for(int i=0;i<n;i++) {
+            for(int i=0;i<sz;i++){
                 
                 auto f = q.front();
                 q.pop();
                 
                 int dU = f.first;
-                int p = f.second;
                 
-                for(auto c:adj[p]) {
+                for(auto ch:adj[f.second]){
                     
-                    int ch = c.first;
-                    int dV = c.second;
+                    int child = ch.first;
+                    int dV = ch.second;
                     
-                    if(dist[ch] > dU + dV) {
-                        
-                        dist[ch] = dU + dV;
-                        q.push({dist[ch], ch});
-                        
+                    if(dist[child] > dU + dV) {
+                        dist[child] = dU + dV;
+                        q.push({dist[child], child});
                     }
                     
-                    
                 }
-                
-                
             }
-            
         }
         
-        return dist[dst] ==INT_MAX?-1:dist[dst];
-        
+        return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
