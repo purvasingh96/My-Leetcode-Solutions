f0bcc758c221891b2d754b9aeb5ad9d52187b864
Purva Singh <singhpurva2906@gmail.com>
2022-06-03 11:58:49 -0700

    Time: 129 ms (8.77%), Space: 24.1 MB (25.90%) - LeetHub

--- 1245-tree-diameter/1245-tree-diameter.cpp
+++ 1245-tree-diameter/1245-tree-diameter.cpp
@@ -4,35 +4,41 @@ private:
     int dia=0;
     
 public:
-     int dfs(int node, int parent, unordered_map<int, vector<int>>& paths, int& ret) {
-        if (paths[node].size() == 1 && paths[node][0] == parent) return 1;
+    int dfs(int node, int parent, int& res, map<int, vector<int>>& paths){
+        if(paths[node].size()==1 && paths[node][0]==parent) return 1;
         
-        int first_max = 0, second_max = 0;
-        for (const auto& it : paths[node]) {
-            if (it == parent) continue;
+        int max1=0;
+        int max2=0;
+        
+        for(auto c:paths[node]){
             
-            int child_res = dfs(it, node, paths, ret);
+            if(c==parent) continue;
             
-            if (child_res > first_max) {
-                second_max = first_max;
-                first_max = child_res;
-            } else if (child_res > second_max) {
-                second_max = child_res;
+            int h = dfs(c, node, res, paths);
+            
+            if(h>max1){
+                max2=max1;
+                max1=h;
+            } 
+            
+            else if(h>max2){
+                max2=h;
             }
-            ret = max(ret, first_max + second_max);
+            
+            res = max(res, (max1+max2));
         }
-        return first_max + 1;
+        return max1+1;
+        
     }
-    
     int treeDiameter(vector<vector<int>>& edges) {
         if(edges.size()==0) return 0;
-        unordered_map<int, vector<int>> adj;
+        map<int, vector<int>> adj;
         for(auto e:edges){
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
         }
         int res = 0;
-        dfs(edges[0][0], INT_MAX, adj, res);
+        dfs(edges[0][0], INT_MAX, res, adj);
         return res;
     }
 };
