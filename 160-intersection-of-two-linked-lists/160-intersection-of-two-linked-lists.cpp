652f606effa91ddfd1fd46c68668fee969e1d62c
Purva Singh <singhpurva2906@gmail.com>
2022-06-06 07:03:37 -0700

    Time: 78 ms (30.54%), Space: 14.7 MB (31.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -7,48 +7,17 @@
  * };
  */
 class Solution {
-private:
-    int findLen(ListNode* node){
-        ListNode* temp = node;
-        int len=0;
-        
-        while(node!=NULL){
-            node=node->next;
-            len+=1;
-        }
-        
-        return len;
-    }
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)     {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* pa = headA;
+        ListNode* pb = headB;
         
-        int len_a = findLen(headA);
-        int len_b = findLen(headB);
-        
-        ListNode* minlen=headA;
-        ListNode* maxlen=headB;
-        
-        if(len_a>len_b){
-            minlen = headB;
-            maxlen = headA;
-        } 
-        
-        int diff = abs(len_a - len_b);
-        
-        while(maxlen!=NULL && diff!=0){
-            maxlen=maxlen->next; 
-            diff-=1;
+        while(pa!=pb){
+            pa = pa==NULL? headB: pa->next;
+            pb= pb==NULL? headA: pb->next;
+            
         }
         
-        while(minlen && maxlen){
-            if(minlen == maxlen) 
-                return minlen;
-            minlen=minlen->next;
-            maxlen=maxlen->next;
-        }
-        
-        
-        return NULL;
-        
+        return pa;
     }
 };
