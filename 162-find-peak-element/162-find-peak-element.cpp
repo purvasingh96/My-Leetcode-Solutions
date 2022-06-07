453dbf9c5c65883a237e26a56b64431519087535
Purva Singh <singhpurva2906@gmail.com>
2022-06-06 20:51:53 -0700

    Time: 9 ms (23.28%), Space: 8.8 MB (89.55%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -5,33 +5,30 @@ public:
         // O(logN) => binary search approach.
         int n = nums.size();
         if(n==1) return 0;
-        if(n == 2) return nums[0] > nums[1] ? 0:1;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
         
-        int low = 1, high = nums.size()-2;
         
-        while(low<=high) {
+        int low = 0, high = nums.size()-1;
+        
+        while(low<high){
             
             int mid = low + (high-low)/2;
             
-                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
-                    return mid;
-                }
-                
-                else if(nums[mid] < nums[mid+1]) low = mid+1;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+                return mid;
+            }
             
-                else if(nums[mid] <= nums[mid-1]) high = mid-1;
+            else if(nums[mid] < nums[mid+1]){
+                low = mid+1;
+            } else{
+                high=mid;
+            }
             
         }
+       
         
-        if(nums[0]>nums[1]) return 0;
-        
-        if(nums[n-1] > nums[n-2]) return n-1;
         return -1;
-        
-        
-        
-        
-        return 0;
         
     }
 };
