42929b92f4098c8e44ed79fef1cc7fe626f0cd36
Purva Singh <singhpurva2906@gmail.com>
2022-06-09 17:01:06 -0700

    Time: 8 ms (83.99%), Space: 9.6 MB (83.44%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -5,12 +5,12 @@ public:
         int n = cost.size();
         if(n==0) return 0;
         
-        for(int i=n-2;i>=0;i--){
-            cost[i][0] += min(cost[i+1][1], cost[i+1][2]);
-            cost[i][1] += min(cost[i+1][0], cost[i+1][2]);
-            cost[i][2] += min(cost[i+1][0], cost[i+1][1]);
+        for(int i=1;i<n;i++){
+            cost[i][0] += min(cost[i-1][1], cost[i-1][2]);
+            cost[i][1] += min(cost[i-1][0], cost[i-1][2]);
+            cost[i][2] += min(cost[i-1][0], cost[i-1][1]);
         }
         
-        return min(cost[0][0], min(cost[0][1], cost[0][2]));
+        return min(cost[n-1][0], min(cost[n-1][1], cost[n-1][2]));
     }
 };
