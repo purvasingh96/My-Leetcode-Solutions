a1289af06fa26ca8180fdd1412852a3ce5fb32b6
Purva Singh <singhpurva2906@gmail.com>
2022-06-10 05:50:45 -0700

    Time: 7 ms (91.93%), Space: 7.7 MB (83.21%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
         vector<int> m(128, -1);
+        int max_len = 0;
         int left=0;
-        int max_len=0;
-   
-        for(int right=0;right<s.length();right++) {
+        
+        for(int right=0;right<s.length();right++){
+            int asci = (int)s[right];
             
-            int asc = (int)s[right];
-            if(m[asc] != -1)  {
-                
-                left = max(m[asc], left);
-                
-                
+            if(m[asci]!=-1){
+                left = max(left, m[asci]);
             }
             
-            
-            m[asc] = right+1;
-            int cur_len = right-left+1;
-            max_len = max(max_len, cur_len);
-            
+            m[asci] = right+1;
+            max_len = max(max_len, right-left+1);
         }
         
         return max_len;
-        
     }
 };
