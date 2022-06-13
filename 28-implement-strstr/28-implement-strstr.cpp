8285e7147538150253d37115d4fe8899d8bb7dd4
Purva Singh <singhpurva2906@gmail.com>
2022-06-12 20:09:40 -0700

    Time: 0 ms (100.00%), Space: 6.3 MB (22.67%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,65 +1,51 @@
 class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        // KMP algorithm
-        int m = haystack.length();
-        int n = needle.length();
+private:
+    vector<int> generateLPS(string sub){
+        int n = sub.length();
+        vector<int> lps(n,0);
         
-        vector<int> table(n, 0);
-        
-        int j=0;
-        
-        for(int i=1;i<n;) {
-            
-            if(needle[i] == needle[j]) {
-                
-                table[i] = j+1;
+        for(int i=1,j=0;i<n;){
+            if(sub[i] == sub[j]){
+                lps[i]=j+1;
                 i+=1;
                 j+=1;
-                
             } else{
-                
                 if(j>0) {
-                    j = table[j-1];
+                    j = lps[j-1];
                 } else{
                     i+=1;
                 }
-                
             }
-            
         }
         
+        return lps;
+    }
+public:
+    int strStr(string haystack, string needle) {
+        auto lps = generateLPS(needle);
+        int n = haystack.length();
         
-        for(int i=0, match_pos=0;i<m;) {
+        for(int i=0,j=0;i<n;){
             
-            if(haystack[i] == needle[match_pos]) {
-                
-                if(match_pos == n-1) {
-                    
-                    return (i - (n - 1));
-                    
-                } else{
-                    
-                    i+=1;
-                    match_pos+=1;
-                    
-                }
-                
+            if(haystack[i] == needle[j]){
+                i+=1;
+                j+=1;
             } else{
                 
-                if(match_pos == 0) {
-                    i+=1;
+                if(j>0){
+                    j = lps[j-1];
                 } else{
-                    match_pos = table[match_pos-1];
+                    i+=1;
                 }
                 
             }
             
+            if(j == needle.length()){
+                return i-j;
+            }
             
         }
         
         return -1;
-        
-        
     }
 };
