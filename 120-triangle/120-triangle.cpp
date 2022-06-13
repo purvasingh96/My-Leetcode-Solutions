1e934cc18d028a604123da164460e1d4329df3a9
Purva Singh <singhpurva2906@gmail.com>
2022-06-13 06:41:03 -0700

    Time: 8 ms (61.32%), Space: 9.1 MB (9.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,42 +2,36 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& nums) {
         int m = nums.size();
-        vector<vector<int>> dp(m);
-        
-        dp[0].resize(1);
-        dp[0][0] = nums[0][0];
+        vector<int> dp_prev(1, nums[0][0]);
         
         for(int i=1;i<m;i++){
             int s = nums[i].size();
-            dp[i].resize(s, INT_MAX);
+            vector<int> dp_new(s, INT_MAX);
             
             for(int j=0;j<s;j++){
                 
                if(j==0){
                    
-                   dp[i][j] = dp[i-1][j] + nums[i][j];
+                   dp_new[j] = dp_prev[j] + nums[i][j];
                    
                } else if(j == nums[i].size()-1){
                    
-                   dp[i][j] = dp[i-1][j-1] + nums[i][j];
+                   dp_new[j] = dp_prev[j-1] + nums[i][j];
                    
                } else{
-                   dp[i][j] = min(dp[i-1][j] + nums[i][j], dp[i-1][j-1]+nums[i][j]);
+                   dp_new[j] = min(dp_prev[j] + nums[i][j], dp_prev[j-1]+nums[i][j]);
                }
                    
                 
             }
             
+            dp_prev = dp_new;
+            
         }
         
-//         for(auto x:dp){
-//             for(auto y:x){
-//                 cout<<y<<" ";
-//             }
-//             cout<<"\n";
-//         }
+
         
-        return *min_element(dp.back().begin(), dp.back().end());
+        return *min_element(dp_prev.begin(), dp_prev.end());
         
     }
 };
