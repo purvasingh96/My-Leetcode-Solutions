03927ce65348bd76d2ee93717d2f81a7ff169425
Purva Singh <singhpurva2906@gmail.com>
2022-06-19 22:18:16 -0700

    Time: 353 ms (25.87%), Space: 150.9 MB (18.17%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -0,0 +1,83 @@
+class TrieNode{
+public:
+    vector<TrieNode*> children;
+    vector<string> suggestions;
+    
+    TrieNode(){
+        children.resize(26, NULL);
+    }
+};
+
+class Trie{
+    TrieNode* root;
+public:
+    Trie(){
+        root = new TrieNode();
+    }
+    
+    void insert(string s){
+        TrieNode* node = root;
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(node->children[s[i]-'a']==NULL){
+                node->children[s[i]-'a']=new TrieNode();
+            }
+            
+            node = node->children[s[i]-'a'];
+            node->suggestions.push_back(s);            
+        }
+        
+    }
+    
+    vector<string> dfs(string s){
+        vector<string> ans;
+        TrieNode* node = root;
+        
+        for(int i=0;i<s.length();i++){
+            if(node->children[s[i]-'a']==NULL){
+                return ans;
+            }
+            node=node->children[s[i]-'a'];
+        }
+        
+        vector<string> sug = node->suggestions;
+        sort(sug.begin(), sug.end());
+        
+        if(sug.size()>3){
+            ans.insert(ans.end(), sug.begin(), sug.begin()+3);
+        } else{
+            ans = sug;
+        }
+        return ans;
+        
+    }
+    
+    
+};
+
+
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string s) {
+        Trie* trie = new Trie();
+        
+        vector<vector<string>> ans;
+        
+        for(auto product:products){
+            trie->insert(product);
+        }
+        
+        for(int i=1;i<=s.length();i++){
+            string t = s.substr(0, i);
+                
+                vector<string> res = trie->dfs(t);
+                ans.push_back(res);
+            
+            
+        }
+        
+        return ans;
+    }
+};
