f613e52e530653ce615c0c9a989e54ab455a03cc
Purva Singh <singhpurva2906@gmail.com>
2022-06-22 08:47:22 -0700

    Time: 3 ms (99.32%), Space: 9.9 MB (86.09%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,47 +1,59 @@
 class Solution {
+private:
+    void swap(int i, int j, vector<int>& nums){
+        int t = nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    
+    int partition(int left, int right, int pivot_idx, vector<int>& nums){
+        
+        int pivot = nums[pivot_idx];
+        int store_idx=left;
+        
+        swap(pivot_idx, right, nums);
+        
+        for(int i=left;i<=right;i++){
+            
+            if(nums[i] < pivot){
+                
+                swap(store_idx, i, nums);
+                store_idx+=1;
+                
+            }
+            
+        }
+        
+        swap(store_idx, right, nums);
+        
+        return store_idx;
+        
+    }
+    
+    int quickselect(int left, int right, int k_smallest, vector<int>& nums){
+        
+        if(left==right) return nums[left];
+        
+        int pivot_idx = left + rand()%(right-left+1);
+        
+        pivot_idx = partition(left, right, pivot_idx, nums);
+        
+        if(pivot_idx == k_smallest){
+            return nums[pivot_idx];
+        }
+        
+        else if(pivot_idx < k_smallest){
+            return quickselect(pivot_idx+1, right, k_smallest, nums);
+        }
+        
+        else return quickselect(left, pivot_idx-1, k_smallest, nums);
+        
+    }
+    
 public:
     int findKthLargest(vector<int>& nums, int k) {
         k = nums.size()-k;
-        
-        int left = 0, right=nums.size()-1;
-        
-        while(true) {
-            
-            int pidx = partition(nums, left, right);
-            
-            if(k == pidx) return nums[k];
-            
-            else if(k < pidx) {
-                right = pidx-1;
-            }
-             else left = pidx+1;
-            
-        }
+        return quickselect(0, nums.size()-1, k, nums);
         
     }
-    
-    int partition(vector<int>& nums, int left, int right) {
-        
-        int pidx = left + (rand()%(right - left + 1));
-        int pivot = nums[pidx];
-        
-        swap(nums[pidx], nums[right]);
-        
-        int l = left;
-        
-        for(int i=left;i<=right;i++) {
-            
-            if(nums[i] < pivot) {
-                swap(nums[l], nums[i]);
-                l+=1;
-            }
-            
-        }
-        
-        swap(nums[l], nums[right]);
-        return l;
-        
-    }
-    
-    
 };
