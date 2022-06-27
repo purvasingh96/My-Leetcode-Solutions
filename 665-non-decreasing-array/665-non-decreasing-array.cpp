8e04335a164542d4a8084b94a07e28467f9927ad
Purva Singh <singhpurva2906@gmail.com>
2022-06-27 11:36:02 -0700

    Time: 48 ms (26.27%), Space: 27 MB (53.57%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -3,27 +3,28 @@ public:
     bool checkPossibility(vector<int>& nums) {
         int c =0, i=0;
         
-        vector<int> q;
+        int prev=INT_MIN, prev_prev=INT_MIN;
         if(nums.size()<3) return true;
         nums.push_back(INT_MAX);
         
         while(i+1<nums.size()){
-            // cout<<i<<" ";
-            // if(q.size()>0) cout<<q.back()<<" ";
-            // cout<<nums[i]<<"\n";
-            if(q.empty() || q.back() <= nums[i]){
-                q.push_back(nums[i]);
+            if(prev==INT_MAX || prev <= nums[i]){
+                if(prev==INT_MAX){
+                    prev=nums[i];
+                } else{
+                    prev_prev=prev;
+                    prev=nums[i];
+                }
                 i+=1;
             } else{
-                //out<<"c: "<<c<<"\n";
                 if(c!=0) return false;
-                if(q.back() <= nums[i+1]){
+                if(prev <= nums[i+1]){
                     c+=1;
                     i+=1;
                     
                 } else if(nums[i] <= nums[i+1]){
                     c+=1;
-                    q.pop_back();
+                    prev=prev_prev;
                     
                 }
                 else return false;
