158183d9184578ad694212d263c7235fd8b44a7a
Purva Singh <singhpurva2906@gmail.com>
2022-07-02 12:28:39 -0700

    Time: 47 ms (25.40%), Space: 16.7 MB (56.85%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
     bool isValidPalindrome(string s, int k) {
-        
+      // len(s) - LCS between s and rev(s) should be <=k
         int n = s.length();
-        string original = s;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        string og = s;
         reverse(s.begin(), s.end());
-        int x = longestPalindromicSubsequence(original, s);
         
-        return (n-x<=k);
-        
-    }
-    
-    int longestPalindromicSubsequence(string s, string r) {
-        int n = s.length();
-        
-        vector<vector<int>> dp(n+1, vector<int>(n+1));
-        
-        for(int i=0;i<=n;i++) {
-            
-            for(int j=0;j<=n;j++) {
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
                 
                 if(i==0 || j==0) dp[i][j] = 0;
-                else if(s[i-1] == r[j-1]) dp[i][j] = 1+dp[i-1][j-1];
-                else {
+                
+                else if(og[i-1] == s[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } else{
+                    
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    
                 }
                 
             }
-            
         }
         
-        return dp[n][n];
+        int lcs = dp[n][n];
+        cout<<lcs;
         
+        return s.length()-lcs <= k;
     }
 };
