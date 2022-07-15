a46369b33babe29c59e276209b1fa9d9d22f12d4
Purva Singh <singhpurva2906@gmail.com>
2022-07-15 07:42:10 -0700

    Time: 2336 ms (5.02%), Space: 585.8 MB (5.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,52 +1,3 @@
-class UnionFind{
- private:
-    
-public:
-    vector<int> root;
-    vector<int> rank;
-    UnionFind(int n): root(n), rank(n) {
-        for(int i=0;i<n;i++) {
-            root[i]=i;
-            rank[i]=1;
-        }
-    }
-    
-    int find(int x) {
-        if(root[x]==x) return x;
-        return root[x] = find(root[x]);
-    }
-    
-    void merge(int x, int y) {
-        int rx = find(x);
-        int ry = find(y);
-        
-        if(rx!=ry) {
-            
-            if(rank[rx] >= rank[ry]) {
-                
-                root[ry] = rx;
-                rank[rx] += rank[ry];
-                
-            } else {
-                
-                root[rx] = ry;
-                rank[ry] += rank[rx];
-                
-            }
-            
-        }
-    }
-    
-    bool isConnected(int x, int y) {
-        return find(x) == find(y);
-    }
-    
-    int returnSize(int x) {
-        return rank[find(x)];
-    }
-    
-     
-};
 class Solution {
 private:
     int dx[4] = {0, 1, 0, -1};
@@ -56,45 +7,44 @@ private:
         return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
    }
     
+    int dfs(vector<vector<int>>& grid, int r, int c, vector<vector<bool>>& visited){
+       
+        if(!isValid(grid, r, c) || !grid[r][c] || visited[r][c]) return 0;
+        visited[r][c] = true;
+        int ans=0;
+        
+        for(int k=0;k<4;k++){
+            
+            int new_r = r+dx[k];
+            int new_c = c+dy[k];
+            
+            ans += dfs(grid, new_r, new_c, visited);
+            
+        }
+        
+        return 1+ans;
+    }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int max_area = INT_MIN;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        int m = grid.size(), n = grid[0].size();
-        UnionFind uf(m*n);
-        int max_area=INT_MIN;
-        
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 
-                if(grid[i][j]) {
-                    int p0 = i*n+j;
-                
-                for(int k=0;k<4;k++) {
+                if(grid[i][j] && !visited[i][j]){
+                    //visited[i][j] = true;
+                    int y=dfs(grid, i, j, visited);
                     
-                    int new_x = i+dx[k];
-                    int new_y = j + dy[k];
+                    max_area = max(max_area, y);
                     
-                    int p1 = new_x*n + new_y;
-                    
-                    if(isValid(grid, new_x, new_y) && grid[new_x][new_y]==1 && !uf.isConnected(p0, p1)) {
-                        
-                        
-                        uf.merge(p0, p1);
-                        
-                    }
-                    
-                }
-                
-                max_area = max(max_area, uf.returnSize(p0));
                 }
                 
             }
         }
         
-        
         return max_area==INT_MIN?0:max_area;
-        
-        
-        
     }
 };
