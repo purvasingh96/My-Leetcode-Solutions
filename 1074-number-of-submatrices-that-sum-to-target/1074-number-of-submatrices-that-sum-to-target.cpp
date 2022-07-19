8e07d5d4aa227e379a160e9b13054140f29924cd
Purva Singh <singhpurva2906@gmail.com>
2022-07-19 11:46:24 -0700

    Time: 1033 ms (65.49%), Space: 173.3 MB (35.82%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,26 +1,40 @@
 class Solution {
-
-public:
-    int numSubmatrixSumTarget(vector<vector<int>>& A, int target) {
-	int res = 0, m = size(A), n = size(A[0]);
-	for (int row = 0; row < m; row++)
-		for (int col = 1; col < n; col++)
-			A[row][col] += A[row][col - 1];
-	//         cursum, occurences
-	unordered_map<int, int> mp;
-	for (int colStart = 0; colStart < n; colStart++) {
-		for (int colEnd = colStart; colEnd < n; colEnd++) {
-			int cursum = 0;
-			mp = {{0, 1}};
-			for(int row = 0; row < m; row++){
-				cursum += A[row][colEnd] - (colStart ? A[row][colStart - 1] : 0);    
-				// mp[sum-target] will give number of submatrices found having sum as 'sum - target'
-				res += mp[cursum - target];
-				mp[cursum]++;
-			}
-		}
-	}
-	return res;    
-}
+private:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int, int> m;
+        m[0] = 1;
+        int sum=0, ans=0;
         
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(m.find((sum-k))!=m.end()){
+                ans += m[(sum-k)];
+            }
+            m[sum] += 1;
+        }
+        
+        return ans;
+    }
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int ans=0;
+        
+        vector<int> ssum(n);
+        
+        for(int i=0;i<m;i++){
+            
+            for(int p=0;p<n;p++) ssum[p]=0;
+            
+            for(int j=i;j<m;j++){
+                for(int k=0;k<n;k++){
+                    ssum[k] += matrix[j][k];
+                }   
+                ans += subarraySum(ssum, target);
+            }
+        }
+        
+        return ans;
+    }
 };
