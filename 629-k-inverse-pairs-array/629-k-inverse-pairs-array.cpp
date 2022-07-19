586060e4e67945f5681968977a63e11e82bc400c
Purva Singh <singhpurva2906@gmail.com>
2022-07-19 13:56:36 -0700

    Time: 2706 ms (5.13%), Space: 10.2 MB (87.18%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int kInversePairs(int n, int k) {
-        vector<int> dp(k+1, 0);
-        int mod = 1e9+7;
-        for(int i=1; i<=n; i++){
-            vector<int> tmp(k+1, 0);
-            tmp[0] = 1;
-            for(int j =1; j<=k; j++){
-                long long val = (dp[j] + mod - ((j-i) >=0 ? dp[j-i] : 0))%mod;
-                tmp[j] = (tmp[j-1] + val)%mod;
-            }
-            dp = tmp;
-        }
-        return (dp[k] + mod - (k>0 ? dp[k-1] : 0))%mod;
+        int dp[n+1][k+1];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i = 1; i <= n; i++)
+            for(int j = 0; j <= k; j++){
+                //when j = 0(k = 0) then sorted array is the only solution 
+                if(j == 0) dp[i][j] = 1;
+                else
+                    for(int p = 0; p <= min(j, i-1); p++)
+                        dp[i][j] = (dp[i][j] + dp[i-1][j-p])%1000000007;
+            }           
+    return dp[n][k];
     }
 };
