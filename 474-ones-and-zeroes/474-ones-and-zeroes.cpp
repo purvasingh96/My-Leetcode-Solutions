5cc6cfb1f078b56f45aecfbd84bdbb7f0784ad2d
Purva Singh <singhpurva2906@gmail.com>
2022-07-20 21:51:30 -0700

    Time: 164 ms (91.44%), Space: 32.4 MB (49.08%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        // subset + upper limit => knapsack
-        int l=strs.size();
-        int dp[l+1][m+1][n+1];
-        
-        memset(dp, 0, sizeof(dp));
-        
+        int sz=strs.size();
+        int dp[sz+1][m+1][n+1];
+        memset(dp,0,sizeof(dp));
         int res=0;
         
-        for(int i=1;i<=l;i++){
+        for(int i=1;i<=sz;i++){
             
-            int cz=0, co=0;
-            
-            for(auto x:strs[i-1]){
-                if(x=='0') cz+=1;
-                else co+=1;
-            }
-            
-            
+             int cnt1=count(strs[i-1].begin(),strs[i-1].end(),'1');  //count no of 1's in each substring
+            int cnt0=strs[i-1].size()-cnt1;
             
             for(int j=0;j<=m;j++){
-                
                 for(int k=0;k<=n;k++){
                     dp[i][j][k] = dp[i-1][j][k];
-                    if(j>=cz && k>=co){
-                        int y = 1+dp[i-1][j-cz][k-co];
-                        dp[i][j][k] = max(dp[i][j][k], y);
-                        
+                    
+                    if(j>=cnt0 && k>=cnt1) {
+                        dp[i][j][k] = max(dp[i][j][k], 1+dp[i-1][j-cnt0][k-cnt1]);
+                    
                     }
                     
                     res = max(res, dp[i][j][k]);
-                        
-                        
+                    
                 }
-                
             }
             
         }
