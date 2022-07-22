d99e06f2df5abe4469c8a9a727a7286bce5af22c
Purva Singh <singhpurva2906@gmail.com>
2022-07-22 05:53:44 -0700

    Time: 74 ms (22.88%), Space: 18.6 MB (26.42%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -13,11 +13,26 @@ private:
     }
 public:
     int change(int amount, vector<int>& coins) {
-        int ans=0;
         int m = coins.size();
-        vector<vector<int>> dp(m, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(m+1, vector<int>(amount+1, 0));
         
-        return dfs(0, amount, coins, dp);
+        for(int i=0;i<=m;i++){
+            dp[i][0] = 1;
+        }
         
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=amount;j++){
+                
+                if(coins[i-1] > j){
+                    dp[i][j] = dp[i-1][j];
+                } else{
+                    
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+                    
+                }
+                
+            }
+        }
+        return dp[m][amount];
     }
 };
