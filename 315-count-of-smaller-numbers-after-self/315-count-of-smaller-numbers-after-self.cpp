4183aace260238b401e31aec174e1650d18c44be
Purva Singh <singhpurva2906@gmail.com>
2022-07-23 18:10:29 -0700

    Time: 1709 ms (11.67%), Space: 178.5 MB (47.83%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,7 +1,6 @@
 class Solution {
 private:
-    void mergesort(int start, int end, vector<vector<int>>& nums, vector<int>& indices,
-                  vector<vector<int>>& temp){
+    void mergesort(int start, int end, vector<vector<int>>& nums, vector<int>& indices, vector<vector<int>>& temp){
         
         if(start>=end) return;
         
@@ -9,7 +8,6 @@ private:
         
         mergesort(start, mid, nums, indices, temp);
         mergesort(mid + 1, end, nums, indices, temp);
-        
         
         int left=start, right=mid+1, nsmall=0, idx=start;
         
@@ -27,37 +25,33 @@ private:
         }
         
         while(left<=mid){
-            
             indices[nums[left][1]] += nsmall;
             temp[idx++] = nums[left++];
         }
-    
         
         while(right<=end){
-            
             temp[idx++] = nums[right++];
         }
         
-        for(int k=start;k<=end;k++){
-            nums[k] = temp[k];
+        for(int i=start;i<=end;i++){
+            nums[i] = temp[i];
         }
         
     }
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> indices(n, 0);
         vector<vector<int>> temp1;
         vector<vector<int>> temp2;
-        int n = nums.size();
-        vector<int> ans(n, 0);
         
         for(int i=0;i<n;i++){
             temp1.push_back({nums[i], i});
             temp2.push_back({nums[i], i});
         }
         
-        mergesort(0, n-1, temp1, ans, temp2);
-        
-        return ans;
-        
+        int ans=0;
+        mergesort(0, n-1, temp1, indices, temp2);
+        return indices;
     }
 };
