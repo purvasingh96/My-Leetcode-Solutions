981bec46c5ff9bdf39d637925dee3ad672c8f058
Purva Singh <singhpurva2906@gmail.com>
2022-07-26 20:13:51 -0700

    Time: 1048 ms (68.70%), Space: 70.2 MB (68.70%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,47 +1,52 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
-        int n = nums.size(), res = 0, sum = 0;
-        sum = accumulate(nums.begin(), nums.end(),0);
-        
+        int n = nums.size();
         int N = n/2;
         vector<vector<int>> left(N+1), right(N+1);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         
-		//storing all possible sum in left and right part
-        for(int mask = 0; mask<(1<<N); ++mask){
-            int sz = 0, l = 0, r = 0;
-            for(int i=0; i<N; ++i){
-                if(mask&(1<<i)){
-                    sz++;
+        for(int mask=0;mask<(1<<N);mask++){
+            int sz=0, l=0, r=0;
+            
+            for(int i=0;i<N;i++){
+                // check if ith bit is set
+                if(mask& (1<<i)) {
+                    sz+=1;
                     l += nums[i];
                     r += nums[i+N];
                 }
             }
+            
             left[sz].push_back(l);
             right[sz].push_back(r);
         }
-
-        for(int sz=0; sz<=N; ++sz){
+        
+        for(int sz=0;sz<=N;sz++){
             sort(right[sz].begin(), right[sz].end());
         }
-
-        res = min(abs(sum-2*left[N][0]), abs(sum-2*right[N][0]));
-
-		//iterating over left part
-        for(int sz=1; sz<N; ++sz){
-            for(auto &a : left[sz]){
-                int b = (sum - 2*a)/2, rsz = N-sz;
-                auto &v = right[rsz];
-                auto itr = lower_bound(v.begin(), v.end(),b); //binary search over right part
+        
+        int res = INT_MAX;
+        
+        for(int sz=1;sz<=N;sz++){
+            for(auto &a:left[sz]){
+                int b =  (sum - 2*a)/2;
+                int rsz = N - sz;
                 
-                if(itr!=v.end()) res = min(res, abs(sum-2*(a+(*itr))));
-                if(itr!= v.begin()){
-                    auto it = itr; --it;
-                    res = min(res, abs(sum-2*(a+(*it))));
-                }                
+                auto &v = right[rsz];
+                
+                auto it = lower_bound(v.begin(), v.end(), b);
+                
+                if(it!=v.end()) res = min(res, abs(sum - 2*(a+(*it))));
+                if(it!=v.begin()){
+                    
+                    auto itr = it;
+                    --it;
+                    res = min(res, abs(sum - 2*(a+(*it))));
+                }
             }
         }
-        return res;
         
+        return res;
     }
 };
