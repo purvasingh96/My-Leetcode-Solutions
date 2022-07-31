6178c69b8a48e66a8229cb84eb0635909138b424
Purva Singh <singhpurva2906@gmail.com>
2022-07-30 19:17:57 -0700

    Time: 520 ms (80.64%), Space: 152.3 MB (42.98%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -3,73 +3,74 @@ private:
     int n;
     vector<int> seg;
     
-    void buildTree(vector<int>& nums, int ind, int left, int right) {
-		if (left == right) {
-			seg[ind] = nums[left];
-			return;
-		}
-		int mid = (left + right) / 2;
-		buildTree(nums, 2 * ind + 1, left, mid);
-		buildTree(nums, 2 * ind + 2, mid + 1, right);
-		seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
-	}
-    
-    int query(int root_idx, int low, int high, int qlow, int qhigh){
-        
-        //total overlap
-        if(low>=qlow && high<=qhigh){
-            return seg[root_idx];
+    void buildSegTree(vector<int>& nums, int idx, int left, int right) {
+        if(left==right){
+            seg[idx] = nums[left];
+            return;
         }
         
+        int mid = left + (right-left)/2;
+        buildSegTree(nums, 2*idx+1, left, mid);
+        buildSegTree(nums, 2*idx+2, mid+1, right);
+        
+        seg[idx] = seg[2*idx+1] + seg[2*idx+2];
+    }
+    
+    void updateUtil(int root_idx, int left, int right, int idx, int val){
+        
         // no overlap
-         if(qlow>high || qhigh <low){
+        if(idx>right || idx < left) return;
+        
+        // total overlap
+        if(left==right){
+            if(left==idx){
+                seg[root_idx] = val;
+            }
+            return;
+        }
+        
+        // partial overlap
+        int mid = left + (right-left)/2;
+        
+        updateUtil(2*root_idx+1, left, mid, idx, val);
+        updateUtil(2*root_idx+2, mid+1, right, idx, val);
+        
+        seg[root_idx] = seg[2*root_idx+1] + seg[2*root_idx+2];
+    }
+    
+    int queryUtil(int root_idx, int left, int right, int qleft, int qright){
+        
+        // total overlap
+        if(left >= qleft && right<=qright){
+            return seg[root_idx];
+        }
+        // no overlap
+        if(left>qright || right<qleft){
             return 0;
         }
         
         // partial overlap
+        int mid = left + (right-left)/2;
+        int l = queryUtil(2*root_idx+1, left, mid, qleft, qright);
+        int r = queryUtil(2*root_idx+2, mid+1, right, qleft, qright);
         
-            int mid = low + (high-low)/2;
-            
-            int left = query(2*root_idx+1, low, mid, qlow, qhigh);
-            int right = query(2*root_idx+2, mid+1, high,  qlow, qhigh);
-            
-            return left+right;
+        return l+r;
         
     }
-    
-    void updateUtil(int ind, int left, int right, int index, int val) {
-		// no overlap
-		if (index < left || index > right) return;
-
-		// total overlap
-		if (left == right) {
-			if (left == index)
-				seg[ind] = val;
-			return;
-		}
-
-		// partial overlap
-		int mid = (left + right) / 2;
-		updateUtil(2 * ind + 1, left, mid, index, val); // left child
-		updateUtil(2 * ind + 2, mid + 1, right, index, val); // right child
-		seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
-	}
     
 public:
     NumArray(vector<int>& nums) {
         n = nums.size();
-        seg.resize(4*n, 0);
-        buildTree(nums, 0, 0, n-1);
+        seg.resize(4*n);
+        buildSegTree(nums, 0, 0, n-1);
     }
     
     void update(int index, int val) {
-        if (n == 0) return;
         updateUtil(0, 0, n-1, index, val);
     }
     
     int sumRange(int left, int right) {
-        if (n == 0) return 0;
-        return query(0, 0, n-1, left, right);
+        return queryUtil(0, 0, n-1, left, right);
     }
 };
 
