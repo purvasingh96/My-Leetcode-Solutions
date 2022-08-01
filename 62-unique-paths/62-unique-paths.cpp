f51b90e088eab2d501516455120a3e9a1c5fd32d
Purva Singh <singhpurva2906@gmail.com>
2022-08-01 07:30:52 -0700

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,8 +3,10 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
         
+        dp[0][0]=1;
+        
         for(int i=0;i<m;i++){
-            dp[i][0] = 1;
+            dp[i][0]=1;
         }
         
         for(int j=0;j<n;j++){
@@ -13,7 +15,7 @@ public:
         
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
