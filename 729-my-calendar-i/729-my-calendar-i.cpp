bc7407c00f53b919c35c3eef3fd65e219b9f54d6
Purva Singh <singhpurva2906@gmail.com>
2022-08-03 06:34:15 -0700

    Time: 173 ms (47.75%), Space: 39.2 MB (27.98%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,47 +1,25 @@
 class MyCalendar {
 private:
-    vector<vector<int>> q;
+    map<int, int> m;
 public:
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        //cout<<start<<" "<<end<<"\n";
-        if(q.empty() || q.back()[1] <= start){
-            q.push_back({start, end});
-            //cout<<"true\n";
-            return true;
-        } else{
+        auto it = m.upper_bound(start);
+        if(it!=m.end()){
             
-            int left=0, right=q.size()-1;
+            if(it->first<end) return false;
             
-            while(left<right){
-                int mid = left + (right-left)/2;
-                if(q[mid][0]>start){
-                    right=mid;
-                } else {
-                    left=mid+1;
-                }
-            }
-            
-            if(left==0){
-                
-                if(start<q[left][0] && end<=q[left][0]){
-                    q.insert(q.begin()+left, {start, end});
-                    return true;
-                }
-                
-            } else if(left>=1){
-                if(start >= q[left-1][1] && end <= q[left][0]){
-                    q.insert(q.begin()+left, {start, end});
-                    return true;
-                }
-            }
-            
+        } 
+        
+        if(it!=m.begin()){
+            if(prev(it)->second > start) return false;
         }
-        //cout<<"false\n";
-        return false;
+        
+        m[start]=end;
+        return true;
     }
 };
 
