6020161524a11232fa6e4513d24fafdcedf6263d
Purva Singh <singhpurva2906@gmail.com>
2022-08-03 18:43:23 -0700

    Time: 45 ms (69.61%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,32 +1,48 @@
-class MyCompare{
-public:
-    // row, col, val
-    bool operator()(const vector<int>& a, const vector<int>& b){
-        return a[2] > b[2];
-    }
-};
 class Solution {
+private:
+    // count, smallest_larger_than_mid, largest_smaller_than_mid
+    vector<int> helper(vector<vector<int>>& matrix, int mid){
+        vector<int> res(3, 0);
+        res[1]=INT_MAX;
+        res[2]=INT_MIN;
+        int m=matrix.size(), n= matrix[0].size();
+        int r = m-1, c=0;
+        
+        while(r>=0 && c<n){
+            if(matrix[r][c] <= mid){
+                res[2] = max(res[2], matrix[r][c]);
+                res[0]+=(r+1);
+                c+=1;
+            } else {
+                res[1] = min(res[1], matrix[r][c]);
+                r-=1;
+            }
+        }
+        
+        return res;
+    }
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, MyCompare> pq;
-        int m = matrix.size(), n = matrix[0].size();
+        int m=matrix.size(), n= matrix[0].size();
+        int left=matrix[0][0], right=matrix[m-1][n-1];
         
-        for(int i=0;i<m;i++){
-            pq.push({i, 0, matrix[i][0]});
-        }
+        while(left<right)
+        {
+            int mid = left+(right-left)/2;
+            
+            auto res = helper(matrix, mid);
+            int count= res[0];
+            int mid_plus_1 = res[1];
+            int mid_minus_1 = res[2];
+            
+            if(count==k) return mid_minus_1;
+            if(count < k){
+                left = mid_plus_1;
+            } else {
+                right = mid_minus_1;
+            }
+        } 
         
-        
-        int count=0;
-        int ans=0;
-        while(count!=k){
-            auto f = pq.top();
-            int row = f[0], col=f[1], val=f[2];
-            ans = val;
-            pq.pop();
-            count+=1;
-            if(col+1<n) pq.push({row, col+1, matrix[row][col+1]});
-        }
-        
-        return ans;
+        return left;
     }
 };
