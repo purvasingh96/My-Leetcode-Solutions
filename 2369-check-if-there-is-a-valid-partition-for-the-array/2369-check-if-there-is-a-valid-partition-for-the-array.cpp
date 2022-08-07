6e13c6ad0959770730647c874604ef8e6fafe0dc
Purva Singh <singhpurva2906@gmail.com>
2022-08-07 07:38:13 -0700

    Time: 243 ms (55.56%), Space: 92.3 MB (11.11%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -6,7 +6,7 @@ private:
         
         if(dp[i]!=-1) return dp[i]==1;
         
-        bool x=false, y=false, z=false;
+        bool x=false, y=false;
         
         if(i+1<n) {
             if(nums[i] == nums[i+1]) {
@@ -16,17 +16,16 @@ private:
         
         if(i+2<n){
             
-            if((nums[i] == nums[i+1]) && (nums[i+1] == nums[i+2])){
-                y = dfs(i+3, nums, dp);
-            } 
+            bool cond_1 = ((nums[i] == nums[i+1]) && (nums[i+1] == nums[i+2]));
+            bool cond_2 = ((nums[i+2] == nums[i+1]+1) && (nums[i+1] == nums[i]+1));
             
-            else if((nums[i+2] == nums[i+1]+1) && (nums[i+1] == nums[i]+1)){
-                z = dfs(i+3, nums, dp);
+            if(cond_1 || cond_2){
+                y = dfs(i+3, nums, dp);
             }
             
         }
         
-        return dp[i] = (x||y||z);
+        return dp[i] = (x||y);
         
     }
 public:
