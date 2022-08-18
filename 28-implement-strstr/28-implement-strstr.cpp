d5f462781c77f5070c44a2cbfb1c1b4c4967fbad
Purva Singh <singhpurva2906@gmail.com>
2022-08-18 13:20:48 -0400

    Time: 6 ms (14.54%), Space: 6.2 MB (90.53%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        // rabin karp
-        if(haystack.length()==0 || needle.length()==0) return -1;
         
-        int base = 31, n = haystack.length(), m = needle.length();
-        int mod = 1e9+7;
+        int m = haystack.length(), n = needle.length(), base=26;
+        int mod = 1e9+7,j;
         
-        long long h = 1;
-        // abcd: h(abc) = a*31^2 + b*31^1 + c*31^0 => ()
-        for(int i=0;i<m-1;i++){
-            h = (h*base)%mod;
+        if(m==0 || n==0) return -1;
+        
+        long long h=1;
+        
+        for(int i=0;i<n-1;i++){
+            h = (h*base);
+            h = h%mod;
         }
         
-        long long sHash=0, pHash = 0;
-        int j=0;
-        // first iteration
-        for(int i=0;i<m;i++){
-            sHash = (sHash* base + haystack[i])%mod;
+        long long sHash=0, pHash=0;
+        
+        for(int i=0;i<n;i++){
+            sHash = (sHash*base + haystack[i])%mod;
             pHash = (pHash*base + needle[i])%mod;
         }
         
-        //cout<<sHash<<" "<<pHash;
-        
-        for(int i=0;i<=(n-m);i++){
+        for(int i=0;i<=(m-n);i++){
             if(sHash == pHash){
-                //cout<<"here";
-                for(j=0;j<m;j++){
+                for(j=0;j<n;j++){
                     if(haystack[i+j] != needle[j]) break;
                 }
+                
+                if(j==n) return i;
             }
             
-            if(j==m) return i;
-            
-            if(i < (n-m)){
-                sHash = ((sHash - h*haystack[i])*base + haystack[i+m])%mod;
+            if(i<(m-n)){
+                
+                sHash = ((sHash - h*haystack[i])*base + haystack[i+n])%mod;
                 if(sHash<0) sHash += mod;
+                
             }
         }
         
