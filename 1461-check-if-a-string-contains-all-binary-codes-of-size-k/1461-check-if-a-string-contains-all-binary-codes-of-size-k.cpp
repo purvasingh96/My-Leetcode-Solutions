72803b23383dd625493c2f0c4a33b40bdb774189
Purva Singh <singhpurva2906@gmail.com>
2022-08-18 14:06:56 -0400

    Time: 856 ms (56.00%), Space: 66.4 MB (69.56%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,22 +1,31 @@
 class Solution {
+// stoi(binaryNumber, 0, 2);
 public:
     bool hasAllCodes(string s, int k) {
-        int need = 1<<k;
-        int allOnes = need-1;
-        map<int, bool> got;
-        int hash=0;
+        int sz = pow(2, k);
+        set<long long> res;
+        int n = s.length();
         
-        for(int i=0;i<s.length();i++){
-            hash = ((hash<<1) & allOnes | (s[i]-'0'));
-            if(i>=k-1 && !got[hash]){
-                got[hash]=true;
-                need-=1;
-                if(need==0){
-                    return true;
-                }
-            }
+        long long h = 1;
+        int base = 2, mod=1e9+7;
+        
+        for(int i=0;i<k-1;i++){
+            h = h*base;
+            h=h%mod;
+        }
+    
+        
+        long long sHash = stoi(s.substr(0, k), 0, 2);
+        
+        for(int i=0;i<=(n - k);i++){
+            if(sHash < sz) res.insert(sHash);
+            
+            if(i<(n-k)){
+                sHash = ((sHash - h*(s[i]-'0'))*base)%mod + (s[i+k]-'0');
+                if(sHash < 0) sHash += mod;
+            }  
         }
         
-        return false;
+        return res.size() == sz;
     }
 };
