c25cdc6a21e7a307d396603507b109832d5cdf09
Purva Singh <singhpurva2906@gmail.com>
2022-08-20 15:24:24 -0400

    Time: 721 ms (47.20%), Space: 174.6 MB (8.50%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -2,67 +2,65 @@ class NumArray {
 private:
     int n;
     vector<int> seg;
-    
-    void buildSegTree(vector<int>& nums, int idx, int left, int right) {
-        if(left==right){
+    vector<int> nums;
+    void buildTree(int idx, int left, int right, vector<int>& seg, vector<int>& nums){
+        if(left == right){
             seg[idx] = nums[left];
             return;
         }
         
         int mid = left + (right-left)/2;
-        buildSegTree(nums, 2*idx+1, left, mid);
-        buildSegTree(nums, 2*idx+2, mid+1, right);
+        buildTree(2*idx+1, left, mid, seg, nums);
+        buildTree(2*idx+2, mid+1, right, seg, nums);
         
         seg[idx] = seg[2*idx+1] + seg[2*idx+2];
     }
     
+    int queryUtil(int root_idx, int left, int right, int qleft, int qright){
+        if(qleft<=left && qright>=right){
+            return seg[root_idx];
+        }
+        
+        if(qleft>right || qright<left) return 0;
+        
+        int mid = left + (right-left)/2;
+        
+        int l = queryUtil(2*root_idx+1, left, mid, qleft, qright);
+        int r = queryUtil(2*root_idx+2, mid+1, right, qleft, qright);
+        
+        return l+r;
+    }
+    
     void updateUtil(int root_idx, int left, int right, int idx, int val){
+        if(idx<left || idx>right){
+            return;
+        }
         
-        // no overlap
-        if(idx>right || idx < left) return;
-        
-        // total overlap
-        if(left==right){
-            if(left==idx){
+        if(left == right){
+            if(left == idx){
                 seg[root_idx] = val;
             }
             return;
         }
         
-        // partial overlap
         int mid = left + (right-left)/2;
         
         updateUtil(2*root_idx+1, left, mid, idx, val);
         updateUtil(2*root_idx+2, mid+1, right, idx, val);
         
         seg[root_idx] = seg[2*root_idx+1] + seg[2*root_idx+2];
+        
     }
     
-    int queryUtil(int root_idx, int left, int right, int qleft, int qright){
-        
-        // total overlap
-        if(left >= qleft && right<=qright){
-            return seg[root_idx];
-        }
-        // no overlap
-        if(left>qright || right<qleft){
-            return 0;
-        }
-        
-        // partial overlap
-        int mid = left + (right-left)/2;
-        int l = queryUtil(2*root_idx+1, left, mid, qleft, qright);
-        int r = queryUtil(2*root_idx+2, mid+1, right, qleft, qright);
-        
-        return l+r;
-        
-    }
+    
     
 public:
+    
     NumArray(vector<int>& nums) {
-        n = nums.size();
+        n=nums.size();
         seg.resize(4*n);
-        buildSegTree(nums, 0, 0, n-1);
+        nums = nums;
+        buildTree(0, 0, n-1, seg, nums);
     }
     
     void update(int index, int val) {
