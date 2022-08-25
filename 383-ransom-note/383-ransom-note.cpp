54fef2baf766b3c5d9eabaaeccad690d5be91a55
Purva Singh <singhpurva2906@gmail.com>
2022-08-25 00:49:32 -0400

    Time: 10 ms (95.81%), Space: 8.7 MB (89.81%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> r(26, 0);
-        vector<int> m(26, 0);
         
         for(auto x:ransomNote){
             r[x-'a']+=1;
         }
         
         for(auto x:magazine){
-            m[x-'a']+=1;
+            r[x-'a']-=1;
         }
         
         for(int i=0;i<26;i++){
-            if(r[i] > m[i]) return false;
+            if(r[i] >0) return false;
         }
         
         return true;
