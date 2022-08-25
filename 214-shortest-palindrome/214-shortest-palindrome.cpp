36f41a84fedeae1587ba2c224284229f3beb1b18
Purva Singh <singhpurva2906@gmail.com>
2022-08-25 14:34:22 -0400

    Time: 8 ms (40.50%), Space: 8.2 MB (6.68%) - LeetHub

--- 214-shortest-palindrome/214-shortest-palindrome.cpp
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -1,37 +1,51 @@
 class Solution {
 private:
-    int generateLPS(string s){
-        int j=0, n = s.length();
-        vector<int> lps(n, 0);
+    int longestPalindromicSubstring(string s){
+        int n = s.length();
+        vector<int> lps(n,0);
+        int maxlen = 0;
         
-        for(int i=1;i<s.length();){
-            if(s[i] == s[j]){
-                lps[i] = j+1;
+        for(int i=1, j=0;i<n;){
+            if(s[j] == s[i]){
+                lps[i]=j+1;
+                maxlen = max(maxlen, lps[i]);
                 i+=1;
                 j+=1;
-            } else{
+            } else {
                 
                 if(j>0){
-                    j = lps[j-1];
-                } else{
+                    j=lps[j-1];
+                } else {
                     i+=1;
                 }
                 
             }
-            
         }
- 
         
         return lps.back();
-    }
+        
+    } 
 public:
     string shortestPalindrome(string s) {
+        // find longest palindromic substring from begininng
+        // reverse rest of the string and append in front
         string og = s;
         reverse(s.begin(), s.end());
-        string new_s = og + "#" + s;
-        int lps = generateLPS(new_s);
+        string target = og + '#' + s;
         
-        //cout<<lps;
-        return s.substr(0, s.length()-lps) + og;
+        int len = longestPalindromicSubstring(target);
+        //cout<<len;
+        
+        string left = og.substr(0, len);
+        
+        
+        string right = og.substr(len);
+        //cout<<"left: "<<left<<" "<<"right: "<<right<<"\n";
+        reverse(right.begin(), right.end());
+        
+        return right+og;
+        
+        
+        
     }
 };
