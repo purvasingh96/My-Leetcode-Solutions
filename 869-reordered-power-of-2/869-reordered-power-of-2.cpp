c64ecaac2419c2f28cf5d074af3d3c0afd4e3a0d
Purva Singh <singhpurva2906@gmail.com>
2022-08-26 08:53:29 -0400

    Time: 10 ms (27.40%), Space: 6.2 MB (55.65%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,52 +1,30 @@
 class Solution {
 private:
     // 23456
-    vector<long> powersOfTwo(){
-        vector<long> res;
-        for(int i=0;pow(2,i)<=1e9;i++){
-            res.push_back(pow(2, i));
-        }
-        return res;
-    }
-    
-public:
-    bool reorderedPowerOf2(int n) {
-        auto res = powersOfTwo();
-        vector<int> og(10, -1);
+    vector<int> returnArray(int n){
+        vector<int> digits(10, -1);
         
         while(n!=0){
             int t = n%10;
-            if(og[t]==-1) {
-                og[t]=0;
+            if(digits[t]==-1) {
+                digits[t]=0;
             }
-            og[t]+=1;
+            digits[t]+=1;
             n=n/10;
         }
         
-        // 2256 , 625
+        return digits;
+    }
+
+public:
+    bool reorderedPowerOf2(int n) {
+        vector<int> digitsN = returnArray(n);
         
-        for(int i=0;i<res.size();i++){
-            if(res[i] == n) return true;
-            vector<int> ans(10, -1);
-            int temp = res[i];
-            
-            while(temp!=0){
-                int t = temp%10;
-                if(ans[t]==-1) ans[t]=0;
-                ans[t]+=1;
-                temp=temp/10;
-            }
-            
-            bool potential=true;
-            for(int i=0;i<10;i++){
-                if(ans[i] != og[i]){
-                    potential=false;
-                    break;
-                } 
-                
-            }
-            //cout<<res[i]<<" "<<potential<<"\n";
-            if(potential) return true;
+        for(int i=0;pow(2,i)<=1e9;i++){
+            int temp = pow(2, i);
+            if(temp == n) return true;
+            vector<int> digitsR = returnArray(temp);
+            if(digitsR == digitsN) return true;
         }
         
         return false;
