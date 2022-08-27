1628a08d51c0f59e7f79592c74bcf0ab1dc71df3
Purva Singh <singhpurva2906@gmail.com>
2022-08-27 10:53:22 -0400

    Time: 993 ms (76.21%), Space: 70.3 MB (56.93%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int minimumDifference(vector<int>& nums) {
+        
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
         int n = nums.size();
         int N = n/2;
-        vector<vector<int>> left(N+1), right(N+1);
-        int sum = accumulate(nums.begin(), nums.end(), 0);
         
-        for(int mask=0;mask<(1<<N);mask++){
+        vector<vector<int>> left(N+1), right(N+1);
+        
+        for(int mask=0;mask<(1<<N);mask++) {
             int sz=0, l=0, r=0;
-            
             for(int i=0;i<N;i++){
-                // check if ith bit is set
-                if(mask& (1<<i)) {
+                if(mask & (1<<i)){
                     sz+=1;
-                    l += nums[i];
-                    r += nums[i+N];
+                    l+=nums[i];
+                    r+=nums[i+N];
                 }
             }
             
@@ -22,31 +22,32 @@ public:
             right[sz].push_back(r);
         }
         
+        
         for(int sz=0;sz<=N;sz++){
             sort(right[sz].begin(), right[sz].end());
         }
         
-        int res = INT_MAX;
+        int mindiff = INT_MAX;
         
         for(int sz=1;sz<=N;sz++){
+            
             for(auto &a:left[sz]){
-                int b =  (sum - 2*a)/2;
-                int rsz = N - sz;
+                int rsz = N-sz;
+                int target = (total_sum - 2*a)/2;
                 
                 auto &v = right[rsz];
                 
-                auto it = lower_bound(v.begin(), v.end(), b);
-                
-                if(it!=v.end()) res = min(res, abs(sum - 2*(a+(*it))));
+                auto it = lower_bound(v.begin(), v.end(), target);
+                if(it!=v.end()){
+                    mindiff = min(mindiff, abs(total_sum - 2*(a + (*it))));
+                }
                 if(it!=v.begin()){
-                    
-                    auto itr = it;
-                    --it;
-                    res = min(res, abs(sum - 2*(a+(*it))));
+                    mindiff = min(mindiff, abs(total_sum - 2*(a + (*prev(it)))));
                 }
             }
         }
         
-        return res;
+        return mindiff;
+        
     }
 };
