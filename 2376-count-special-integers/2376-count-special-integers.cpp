78144362f81659b64cb0868823a43778b917d35c
Purva Singh <singhpurva2906@gmail.com>
2022-08-27 19:00:17 -0400

    Time: 9 ms (50.37%), Space: 6 MB (58.29%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -2,62 +2,46 @@ class Solution {
 private:
     int dp[11][2][1024];
     
-    int dfs(string& s, int pos, int tight, int mask){
-        
-        if(pos==s.length()){
+    int dfs(int pos, int tight, int mask, string& s){
+        if(pos == s.length()){
             return mask!=0;
         }
         
-        if(dp[pos][tight][mask]!=-1) {
-            return dp[pos][tight][mask];
-        }
+        if(dp[pos][tight][mask]!=-1) return dp[pos][tight][mask];
         
         int ans=0;
-        if(tight==1){
+        
+        if(tight){
             
-            // restriction case
-            for(int i=0;i<=(s[pos]-'0');i++){
-                
+            for(int i=0;i<=s[pos]-'0';i++){
                 if(mask & (1<<i)) continue;
                 
-                int new_mask = (mask == 0 && i==0 ? mask: (mask | 1<<i));
+                int new_mask = (mask==0 && i==0) ? mask : mask | (1<<i);
                 
-                if(i==(s[pos]-'0')){
-                    
-                    ans += dfs(s, pos+1, 1, new_mask);
-                    
-                } else {
-                    
-                    ans += dfs(s, pos+1, 0, new_mask);
-                    
-                }
-                
+                if(i==s[pos]-'0'){
+                   ans += dfs(pos+1, 1, new_mask, s);
+                } else{
+                    ans+= dfs(pos+1, 0, new_mask, s);
+                }   
             }
-            
-            
         } else {
             
-            for(int i=0;i<=9;i++){
-                
+            for(int i=0;i<10;i++){
                 if(mask & (1<<i)) continue;
                 
-                int new_mask = (mask == 0 && i==0 ? mask: (mask | 1<<i));
+                int new_mask = (mask==0 && i==0) ? mask : mask | (1<<i);
                 
-                ans += dfs(s, pos+1, 0, new_mask);
-                
+                ans += dfs(pos+1, 0, new_mask, s);
             }
-            
             
         }
         
         return dp[pos][tight][mask] = ans;
-        
     }
-    
 public:
     int countSpecialNumbers(int n) {
         string s = to_string(n);
         memset(dp, -1, sizeof(dp));
-        return dfs(s, 0, 1, 0);
+        return dfs(0, 1, 0, s);
     }
 };
