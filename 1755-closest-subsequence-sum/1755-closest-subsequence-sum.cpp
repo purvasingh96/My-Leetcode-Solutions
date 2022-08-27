2c348435d48b75baeaf9bc11ceb402be6cfb0229
Purva Singh <singhpurva2906@gmail.com>
2022-08-27 18:02:47 -0400

    Time: 2056 ms (23.35%), Space: 61.7 MB (72.18%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -1,63 +1,58 @@
 class Solution {
 public:
     int minAbsDifference(vector<int>& nums, int goal) {
-        int n = nums.size();
+        int n =nums.size();
         int N = n/2;
         int N_ = n-N;
-        int x = (1<<N)+1;
-        int y = (1<<N_)+1;
-        
-        vector<long long> left(x);
-        vector<long long> right(y);
+        int res = INT_MAX;
+        vector<int> left, right;
         
         for(int mask=0;mask<(1<<N);mask++){
-            long long sz=0, l=0;
-            for(int i=0;i<N;i++){
-                if(mask&(1<<i)) {
-                    sz+=1;
+            int l=0;
+            for(int i=0;i<=N;i++) {
+                if(mask & (1<<i)){
                     l+=nums[i];
                 }
             }
-            left[mask] = l;
+            
+            left.push_back(l);
         }
         
         for(int mask=0;mask<(1<<N_);mask++){
-            long long rsz=0, r=0;
-            for(int i=0;i<N_;i++){
+            int r=0;
+            for(int i=0;i<=N_;i++){
                 if(mask & (1<<i)){
-                    rsz+=1;
-                    r+= nums[i+N];
+                    r+=nums[i+N];
                 }
             }
-            right[mask] = r;
+            right.push_back(r);
         }
-        
-        
         
         sort(right.begin(), right.end());
         
-    //cout<<"working till here";
+        // for(auto x:left) cout<<x<<" ";
+        // cout<<"\n";
+        // for(auto x:right) cout<<x<<" ";
         
-        int res=INT_MAX;
+        for(int i=0;i<left.size();i++){
             
-            for(auto &a:left){
-                
-                 int target = (goal - a);
-                
-                auto it = lower_bound(right.begin(), right.end(), target);
-                
-                if(it!=right.end()) res = min(res, int(abs(goal - (a + (*it)))) );
-                
-                if(it!=right.begin()){
-                    //cout<<"it not v begin: "<<target<<"\n";
-                    --it;
-                    //cout<<"itr value: "<<*itr<<"\n";
-                    res = min(res, int(abs(goal - (a + (*it)))) );
-                    //cout<<res<<"\n";
-                }
-                
+            int a = left[i];
+            int target = goal-a;
+            
+            
+            auto it = lower_bound(right.begin(), right.end(), target);
+            if(it!=right.end()){
+                res = min(res, abs((a+*(it)) - goal));
             }
-         
+            
+            if(it!=right.begin()){
+                res = min(res, abs((a+*(prev(it))) - goal) );
+            }
+            
+            
+        }
+        
         return res;
+        
     }
 };
