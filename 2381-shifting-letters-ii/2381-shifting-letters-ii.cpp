2766ea18aa69adaaafd8029a95a359085eb7e288
Purva Singh <singhpurva2906@gmail.com>
2022-08-31 10:59:50 -0400

    Time: 440 ms (80.69%), Space: 103.9 MB (16.64%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        // line sweep
+        // linesweep
         int n = s.length();
+        
         vector<int> ps(n, 0);
         
-        for(int i=0;i<shifts.size();i++){
-            
-            int l = shifts[i][0], r = shifts[i][1], dir = shifts[i][2];
+        for(auto shift:shifts){
+            int l = shift[0], r = shift[1], dir = shift[2];
             
             if(dir==1){
-                ps[l] += 1;
+                ps[l]+=1;
                 if(r+1<n) ps[r+1]-=1;
-            } else {
+            } else{
                 ps[l]-=1;
                 if(r+1<n) ps[r+1]+=1;
             }
@@ -22,11 +22,15 @@ public:
         for(int i=1;i<n;i++){
             ps[i] += ps[i-1];
         }
+        
+        
         for(int i=0;i<n;i++){
-            int increaseBy = (s[i]-'a'+ps[i])%26;
-            if(increaseBy<0) increaseBy+=26;
-            increaseBy%=26;
-            s[i] = 'a'+increaseBy;
+            
+            int inc = (s[i]-'a'+ps[i])%26;
+            if(inc<0) inc+=26;
+            inc = inc%26;
+            s[i] = inc+'a';
+            
         }
         
         return s;
