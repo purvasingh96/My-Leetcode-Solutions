43dca82b1b4c00ad9e044396efc705dac0bc41ae
Purva Singh <singhpurva2906@gmail.com>
2022-09-01 14:28:52 -0400

    Time: 1316 ms (28.34%), Space: 237.5 MB (12.41%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,33 +1,28 @@
 class Solution {
 private:
-    void mergesort(int start, int end, vector<int>& nums, int& ans){
-        if(start>=end) return;
+    void merge(int left, int right, vector<int>& nums, int& ans){
+        if(left>=right) return;
         
-        int mid = start + (end-start)/2;
+        int mid = left + (right-left)/2;
         
-        mergesort(start, mid, nums, ans);
-        mergesort(mid+1, end, nums, ans);
+        merge(left, mid, nums, ans);
+        merge(mid+1, right, nums, ans);
         
-        int i=start, j=mid+1, n = nums.size();
-        //cout<<i<<" "<<j<<"\n";
+        int i=left, j=mid+1;
         
-        while(i<=mid && j<=end) {
-            //long target = 2*(long)nums[j];
-            
-            if(nums[i] > 2L * nums[j]){
-                ans += end-j+1;
+        while(i<=mid && j<=right){
+            if(nums[i] > 2L*nums[j]){
+                ans += right-j+1;
                 i+=1;
             } else {
                 j+=1;
             }
         }
         
-        //cout<<ans<<"\n";
-        
         vector<int> temp;
-        int i1=start, j1=mid+1;
+        int i1=left, j1=mid+1;
         
-        while(i1<=mid && j1<=end){
+        while(i1<=mid && j1<=right){
             if(nums[i1] > nums[j1]){
                 temp.push_back(nums[i1]);
                 i1+=1;
@@ -37,30 +32,27 @@ private:
             }
         }
         
-        //cout<<"main done\n";
-        
         while(i1<=mid){
             temp.push_back(nums[i1]);
             i1+=1;
         }
         
-        //cout<<"i1 done\n";
-        
-        while(j1<=end){
+        while(j1<=right){
             temp.push_back(nums[j1]);
             j1+=1;
         }
-        //cout<<"i2 done\n";
         
-        int idx=0;
-        for(int k=start;k<=end;k++){
-            nums[k] = temp[idx++];
+        int k=0;
+        for(int i=left;i<=right;i++){
+            nums[i]= temp[k++];
         }
+        
     }
 public:
     int reversePairs(vector<int>& nums) {
         int ans=0, n = nums.size();
-        mergesort(0, n-1, nums, ans);
+        merge(0, n-1, nums, ans);
         return ans;
+        
     }
 };
