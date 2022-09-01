64170757610f8f21db103aa8eaf41405c25e3307
Purva Singh <singhpurva2906@gmail.com>
2022-09-01 13:22:44 -0400

    Time: 56 ms (99.07%), Space: 16.7 MB (63.92%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,22 +1,60 @@
 class Solution {
-public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int res =0;
-        int m = nums1.size(), n = nums2.size();
+private:
+    bool check(vector<int>& nums1, vector<int>& nums2, int k){
+        unordered_set<int> st;
+        int n1 = nums1.size(), n2=nums2.size();
+        int base = 131, mod=1e9+9;
+        long long h=1;
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0;i<k-1;i++){
+            h = (h*base);
+            h = h%mod;
+        }
         
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                
-                if(nums1[i]==nums2[j]){
-                    dp[i][j] = 1+dp[i+1][j+1];
-                    res = max(res, dp[i][j]);
-                }
-                
+        long long shash=0;
+        for(int i=0;i<k;i++){
+            shash = (shash*base + nums1[i]);
+            shash = shash%mod;
+        }
+        
+        for(int i=0;i<=(n1-k);i++){
+            st.insert(shash);
+            if(i<(n1-k)){
+                shash = ((shash - nums1[i]*h)*base)%mod + nums1[i+k];
+                if(shash<0) shash+=mod;
             }
         }
         
-        return res;
+        shash=0;
+        for(int i=0;i<k;i++){
+            shash = (shash*base + nums2[i]);
+            shash = shash%mod;
+        }
+        
+        for(int i=0;i<=(n2-k);i++){
+            if(st.find(shash)!=st.end()) return true;
+            if(i<(n2-k)){
+                shash = ((shash - nums2[i]*h)*base)%mod + nums2[i+k];
+                if(shash<0) shash+=mod;
+            }
+        }
+        
+        return false;
+    }
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int left=1, right=min(nums1.size(), nums2.size());
+        int ans=0;
+        
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            
+            if(check(nums1, nums2, mid)){
+                ans=mid;
+                left=mid+1;
+            } else right=mid-1;
+        }
+        
+        return ans;
     }
 };
