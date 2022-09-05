e09621edf8d0c54f9fb7d6d65507fa46a5871960
Purva Singh <singhpurva2906@gmail.com>
2022-09-04 22:34:27 -0400

    Time: 257 ms (50.00%), Space: 398.6 MB (50.00%) - LeetHub

--- 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -1,13 +1,13 @@
 class Solution {
 private:
-    long long dfs(int cur_pos, int endpos, int k, unordered_map<string, long long>& dp){
+    long long dfs(int cur_pos, int endpos, int k, vector<vector<int>>& dp){
         int mod = 1e9+7;
         
         if((cur_pos==endpos && k==0)) return 1;
         if(k<=0) return 0;
         
-        string val = to_string(cur_pos) + "#" + to_string(k);
-        if(dp.find(val)!=dp.end()) return dp[val]%mod;
+        
+        if(dp[cur_pos+1000][k]!=-1) return dp[cur_pos+1000][k];
         
         // left
         long long left=0;
@@ -18,11 +18,11 @@ private:
         
         if(k>= endpos-cur_pos) left = dfs(cur_pos-1, endpos, k-1, dp);
         
-        return dp[val] = (left+right)%mod;
+        return dp[cur_pos+1000][k] = (left+right)%mod;
     }
 public:
     int numberOfWays(int startPos, int endPos, int k) {
-        unordered_map<string, long long> dp;
+        vector<vector<int>> dp(3100, vector<int>(1001, -1));
         int mod=1e9+7;
         return (int)dfs(startPos, endPos, k, dp)%mod;
     }
