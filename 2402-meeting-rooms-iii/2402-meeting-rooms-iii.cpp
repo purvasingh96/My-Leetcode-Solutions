199443c483782be8b120eadeb10787441961fadf
Purva Singh <singhpurva2906@gmail.com>
2022-09-05 02:06:37 -0400

    Time: 710 ms (100.00%), Space: 93.8 MB (50.00%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -2,41 +2,41 @@ class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         
-        vector<int> ans(n, 0);
-        vector<long long> times(n, 0);
+        vector<long long> total_meetings(n, 0);
+        vector<long long> ends(n, 0);
         sort(meetings.begin(), meetings.end());
         
         for(int i=0;i<meetings.size();i++){
             
-            int min_idx=-1;
-            long long val=1e18;
+            long long min_idx=-1;
+            long long earliest_ending=1e18;
             bool found_unused =false;
-            int start = meetings[i][0], end=meetings[i][1];
             
             for(int j=0;j<n;j++){
-                if(times[j] <= start) {
-                    times[j] = end;
+                if(ends[j] <= meetings[i][0]) {
+                    ends[j] = meetings[i][1];
                     found_unused = true;
-                    ans[j]+=1;
+                    total_meetings[j]+=1;
                     break;
                 } 
-                    if(times[j] < val) {
-                        val = times[j];
+                    if(ends[j] < earliest_ending) {
+                        earliest_ending = ends[j];
                         min_idx=j;
                     }
             
             }
             
             if(!found_unused) {
-                times[min_idx] += (1ll*(end - start));
-                ans[min_idx] += 1;
+                long long an = (1ll*(meetings[i][1] - meetings[i][0]));
+                ends[min_idx] += an;
+                total_meetings[min_idx] += 1;
             }
         }
         
         int res=-1, max_=0;
         for(int i=0;i<n;i++){
-            if(ans[i] > max_){
-                max_=ans[i];
+            if(total_meetings[i] > max_){
+                max_=total_meetings[i];
                 res=i;
             }
         }
