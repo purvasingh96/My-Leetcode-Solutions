191b90bd4e2ae0d860505f12e8311921123d6942
Purva Singh <singhpurva2906@gmail.com>
2022-09-06 13:02:12 -0400

    Time: 10 ms (32.85%), Space: 9.8 MB (22.90%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -5,8 +5,7 @@ public:
         
         while(left<right){
             int mid = left + (right-left)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) {
+             if(nums[mid] >= target) {
                 right=mid;
             } else {
                 left=mid+1;
