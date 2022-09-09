e4e220b497cecdea2573bb859f79ce81da5ed4a6
Purva Singh <singhpurva2906@gmail.com>
2022-09-09 07:27:51 -0400

    Time: 425 ms (44.74%), Space: 88.1 MB (80.57%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,6 +1,6 @@
 class SnapshotArray {
 private:
-    unordered_map<int, vector<pair<int, int>>> m;
+    vector<vector<pair<int, int>>> m;
     int snapshot;
     
     int binary_search(vector<pair<int, int>>& res, int idx){
@@ -22,36 +22,28 @@ private:
     
 public:
     SnapshotArray(int length) {
+        m.resize(length);
         snapshot=0;
     }
     
     void set(int index, int val) {
-        // find if we already have that element: binary search
-        // lower bound;
-        // m[snapshot] = {idx, val};
-        if(m[snapshot].size()==0 || m[snapshot].back().first < index){
-            m[snapshot].push_back({index, val});
-        } else{
-            int idx = binary_search(m[snapshot], index);
-            if(m[snapshot][idx].first == index){
-                m[snapshot][idx].second = val;
-            } else {
-                m[snapshot].insert(m[snapshot].begin()+idx, {index, val});
-            }
+        // m[index] => {snapId, val}
+        if(m[index].size()==0 || m[index].back().first <snapshot){
+            m[index].push_back({snapshot, val});
+        } else {
+            m[index].back().second=val;
         }
     }
     
     int snap() {
-        snapshot+=1;
-        m[snapshot] = m[snapshot-1];
-        return snapshot-1;
+        return snapshot++;
     }
     
     int get(int index, int snap_id) {
-        
-        int idx = binary_search(m[snap_id], index);
-        if(m[snap_id].size()==0 || m[snap_id][idx].first != index) return 0;
-        return m[snap_id][idx].second;
+        // O(logn)
+        auto it = upper_bound(m[index].begin(), m[index].end(), make_pair(snap_id, INT_MAX));
+        if(it==m[index].begin()) return 0;
+        return prev(it)->second;
     }
 };
 
