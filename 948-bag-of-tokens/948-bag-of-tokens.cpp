34c1846f8bc83111bbb2256ef1b844585a3681b9
Purva Singh <singhpurva2906@gmail.com>
2022-09-12 16:12:26 -0400

    Time: 16 ms (19.39%), Space: 10.6 MB (27.64%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
+        // O(NlogN)
         sort(tokens.begin(), tokens.end());
         int n = tokens.size();
         
@@ -11,23 +12,17 @@ public:
         
         int left=0, right=tokens.size()-1;
         
-        while(left<n && right>=0){
+        while(left<=right){
             
-            if(tokens[left]==INT_MAX && tokens[right]==INT_MAX) break;
-            
-            while(left<n && tokens[left]!=INT_MAX && power>=tokens[left]){
+            while(left<n && power>=tokens[left]){
                 score+=1;
-                power-=tokens[left];
-                tokens[left] =INT_MAX;
-                left+=1;
+                power-=tokens[left++];
             }
             
             maxscore= max(maxscore, score);
             
             if(score>=1) {
-                if(tokens[right]!=INT_MAX) power += tokens[right];
-                tokens[right]=INT_MAX;
-                right-=1;
+                power += tokens[right--];
                 score-=1;
             }
             
