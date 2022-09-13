b0ff411dce16728482a2d0bdb8703d9bdb1b9e63
Purva Singh <singhpurva2906@gmail.com>
2022-09-13 12:34:16 -0400

    Time: 8 ms (94.91%), Space: 12.6 MB (39.49%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-      
-        stable_sort(logs.begin(), logs.end(), [](const string& a, const string& b){
-            int i = a.find(" ");
-            int j = b.find(" ");
+       vector<string> letter, digit;
+        
+        for(auto s:logs){
+            if(s.back() >= 'a' && s.back() <= 'z') letter.push_back(s);
+            else digit.push_back(s);
+        }
+        
+        sort(letter.begin(), letter.end(), [](const string& a, const string& b){
+            int pos_a = a.find(" ");
+            string a_right = a.substr(pos_a+1);
+            string a_left = a.substr(0, pos_a);
             
-            if(isdigit(a[i+1])){
-                
-                if(isdigit(b[j+1])) return false;
-                else return false;
-                
-            } else {
-                
-                if(isdigit(b[j+1])){
-                    return true;
-                } else{
-                    
-                    if(a.substr(i+1) == b.substr(j+1)) {
-                        return a.substr(0, i) < b.substr(0, j);
-                    }
-                    
-                    return a.substr(i+1) < b.substr(j+1);
-                }
-                
-                
-                
-            }
+            int pos_b = b.find(" ");
+            string b_right = b.substr(pos_b+1);
+            string b_left= b.substr(0, pos_b);
             
-            
+            if(a_right!=b_right) return a_right < b_right;
+            return a_left < b_left;
         });
         
-        //letter.insert(letter.end(), digit.begin(), digit.end());
-        return logs;
+        letter.insert(letter.end(), digit.begin(), digit.end());
+        return letter;
         
     }
 };
