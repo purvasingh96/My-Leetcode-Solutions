891371f2d1cc3907464b41c0151553180738f167
Purva Singh <singhpurva2906@gmail.com>
2022-09-13 09:51:49 -0400

    Time: 18 ms (39.39%), Space: 11.1 MB (73.99%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -7,29 +7,7 @@ public:
             
             string sa = to_string(a);
             string sb = to_string(b);
-            int m = sa.length(), n=sb.length();
-
-            int i=0, j=0;
-
-            while(i<m && j<n && sa[i]-'0' == sb[i]-'0'){
-                i+=1;
-                j+=1;
-            }
-
-            if(i==m || j==n) {
-                string ab = sa+sb;
-                string ba = sb+sa;
-                
-                int k=0;
-                while(k<ab.length() && ab[k] == ba[k]){
-                    k+=1;
-                }
-                
-                return ab[k] > ba[k];
-            }
-            
-            return sa[i]-'0' > sb[j]-'0';
-           
+            return sa+sb > sb+sa;
         });
         
         //for(auto x:)
