ff89fb32a96e2c56c669bc80dd49214387cccdf3
Purva Singh <singhpurva2906@gmail.com>
2022-09-13 16:13:37 -0400

    Time: 78 ms (31.14%), Space: 21.4 MB (9.03%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,44 +1,30 @@
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //sort intervals based on 1st elem
-        // if x.second >= y.first => pop_back, merge new interval
-        // else push_back(y)
-        
-        // TC: O(nlogn), SC: O(logn)
-        vector<vector<int>> res;
-        
-        // O(nlogn), O(logn)
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
-            return a[0] < b[0];
-        });
-
-        res.push_back(intervals[0]);
-        // O(n), O(1)
-        for(int i=1;i<intervals.size();i++) {
-            
-            auto x = res.back();
-            auto y = intervals[i];
-            
-            if(x[1] < y[0]) {
-                res.push_back(y);
-            } else{
-                
-                vector<int> mergedInterval;
-                mergedInterval.push_back(x[0]);
-                if(y[1] < x[1]) mergedInterval.push_back(x[1]);
-                else mergedInterval.push_back(y[1]);
-                
-                if(!res.empty()) res.pop_back();
-                res.push_back(mergedInterval);
-                
-            }
-   
-            
+        // linesweep
+        map<int, int> m;
+        for(auto x:intervals){
+            m[x[0]]+=1;
+            m[x[1]]-=1;
         }
         
-        return res;
+        int res=0;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        for(auto x:m){
+            
+            if(res==0){
+                temp.push_back(x.first);
+            }
+            res+=x.second;
+            if(res==0){
+                temp.push_back(x.first);
+                ans.push_back(temp);
+                temp.clear();
+            }
+        }
+        
+        return ans;
         
     }
 };
