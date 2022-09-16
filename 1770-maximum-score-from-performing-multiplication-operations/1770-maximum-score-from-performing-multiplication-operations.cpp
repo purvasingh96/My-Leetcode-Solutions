b15949f759e7044ac0073bd809b114df682ae8c9
Purva Singh <singhpurva2906@gmail.com>
2022-09-16 13:41:40 -0400

    Time: 938 ms (21.50%), Space: 119.9 MB (71.96%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,28 @@
 class Solution {
-
-public:
-    int dp[1001][1001];
-    int helper(vector<int>& nums, vector<int>& muls, int start, int i){
-        if(i==muls.size()) return 0;
+private:
+    
+    int dfs(vector<int>& nums, vector<int>& muls, int i, int start,vector<vector<int>>& dp){
         
-        if(dp[i][start]!=-1) return dp[i][start];
+        if(i>=muls.size()) {
+            return 0;
+        }
         
-        int end = nums.size() - (i-start) -1;
+        int end = (nums.size()-1) - (i-start);
         
-        int op1 = nums[start] * muls[i] + helper(nums, muls, start+1, i+1);
-        int op2 = nums[end] * muls[i] + helper(nums, muls, start, i+1);
+        if(dp[i][start]!=INT_MIN) return dp[i][start];
         
-        return dp[i][start] = max(op1, op2);
+        int choose_front = muls[i]*nums[start] + dfs(nums, muls, i+1, start+1, dp);
+        int choose_back = muls[i]*nums[end] + dfs(nums, muls, i+1, start, dp);
+        
+        return dp[i][start] = max(choose_front, choose_back);
         
     }
+public:
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-       
-        memset(dp, -1, sizeof(dp));
-        return helper(nums, multipliers, 0, 0);
+        
+        int n = nums.size(), m=multipliers.size();
+        vector<vector<int>> dp(m,vector<int>(m, INT_MIN));
+        return dfs(nums, multipliers, 0, 0, dp);
         
     }
 };
