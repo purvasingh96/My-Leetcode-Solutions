a72e1613cfa3c1194bf2ce1b503595b7bcaa5a09
Purva Singh <singhpurva2906@gmail.com>
2022-09-20 08:27:17 -0400

    Time: 75 ms (98.54%), Space: 16.8 MB (65.54%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,58 +1,73 @@
 class Solution {
 private:
-    bool check(vector<int>& nums1, vector<int>& nums2, int k){
-        unordered_set<int> st;
-        int n1 = nums1.size(), n2=nums2.size();
-        int base = 131, mod=1e9+9;
+    bool isEnough(vector<int>& nums1, vector<int>& nums2, int k){
+        unordered_set<long long> st;
+        int base=131, mod=1e9+7;
         long long h=1;
         
+        long long hash1=0, hash2=0;
+        
         for(int i=0;i<k-1;i++){
-            h = (h*base);
-            h = h%mod;
+            h=h*base;
+            h=h%mod;
         }
         
-        long long shash=0;
+        
         for(int i=0;i<k;i++){
-            shash = (shash*base + nums1[i]);
-            shash = shash%mod;
+            hash1 = hash1*base + nums1[i];
+            hash1 = hash1%mod;
         }
         
-        for(int i=0;i<=(n1-k);i++){
-            st.insert(shash);
-            if(i<(n1-k)){
-                shash = ((shash - nums1[i]*h)*base)%mod + nums1[i+k];
-                if(shash<0) shash+=mod;
+        
+        
+        for(int i=0;i<=(nums1.size()-k);i++){
+            st.insert(hash1);
+            
+            if(i<(nums1.size()-k)){
+                hash1 = ((hash1 - h*nums1[i])*base)%mod + nums1[i+k];
+                if(hash1<0) hash1 += mod;
+            }
+            
+        }
+        
+        
+        
+        for(int i=0;i<k;i++){
+            hash2 = hash2*base + nums2[i];
+            hash2%=mod;
+        }
+        
+        for(int i=0;i<=(nums2.size()-k);i++){
+            if(st.find(hash2)!=st.end()){
+                return true;
+            }
+            
+            if(i<(nums2.size()-k)){
+                hash2 = ((hash2 - h*nums2[i])*base)%mod + nums2[i+k];
+                if(hash2<0) hash2 += mod;
+                
             }
         }
         
-        shash=0;
-        for(int i=0;i<k;i++){
-            shash = (shash*base + nums2[i]);
-            shash = shash%mod;
-        }
         
-        for(int i=0;i<=(n2-k);i++){
-            if(st.find(shash)!=st.end()) return true;
-            if(i<(n2-k)){
-                shash = ((shash - nums2[i]*h)*base)%mod + nums2[i+k];
-                if(shash<0) shash+=mod;
-            }
-        }
         
         return false;
     }
+    
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int left=1, right=min(nums1.size(), nums2.size());
+        
+        int l=0, r=min(nums1.size(), nums2.size());
         int ans=0;
         
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            
-            if(check(nums1, nums2, mid)){
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(isEnough(nums1, nums2, mid)){
                 ans=mid;
-                left=mid+1;
-            } else right=mid-1;
+                l=mid+1;
+            } else {
+                r=mid-1;
+            }
         }
         
         return ans;
