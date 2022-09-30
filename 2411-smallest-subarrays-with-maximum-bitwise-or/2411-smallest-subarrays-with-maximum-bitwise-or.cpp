037ab4292eae4a1ce8e240d9546a9ed152449509
Purva Singh <singhpurva2906@gmail.com>
2022-09-29 22:13:57 -0400

    Time: 1143 ms (16.34%), Space: 151.8 MB (9.33%) - LeetHub

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
-        vector<int> ans;
-        int n = nums.size();
         
+        // suffix array
+        int n = nums.size();
         vector<vector<int>> suffix(n, vector<int>(31, 0));
         
         for(int i=n-1;i>=0;i--){
-            
-            for(int j=30;j>=0;j--){
+            for(int j=0;j<31;j++) {
                 if(nums[i] & (1<<j)) suffix[i][j]+=1;
             }
             if(i+1<n){
-                for(int j=0;j<31;j++){
-                    suffix[i][j] += suffix[i+1][j];
-                }
+                for(int j=0;j<31;j++) suffix[i][j] += suffix[i+1][j];
             }
             
         }
         
-        int l=0;
-        vector<int> pre(31, 0);
         
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<31;j++) {
+        vector<int> pre(31, 0);
+        int l=0;
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){
+            
+            for(int j=0;j<31;j++){
                 if(nums[i] & (1<<j)) pre[j]+=1;
             }
             
@@ -31,46 +31,53 @@ public:
             
             if(i+1<n){
                 for(int j=0;j<31;j++){
-                    if(suffix[i+1][j] > 0 && pre[j]==0) {
+                    if(suffix[i+1][j] > 0 && pre[j]==0){
                         flag=false;
                         break;
                     }
                 }
+                
             }
             
-            if(flag) {
-                ans.push_back(i-l+1);
-            
-                while(1){
-
+            if(flag){
+                    
+                    ans.push_back(i-l+1);
+                    
+                    
+                    while(true){
+                        
                     for(int j=0;j<31;j++){
-                        if(nums[l] & 1<<j) pre[j]-=1;
+                        if(nums[l] & (1<<j)) {
+                            pre[j]-=1;
+                        }
+                    }
+                        
+                        bool flag=true;
+                        l+=1;
+                        if(l>i) break;
+                        
+                        if(i+1<n){
+                            
+                            for(int j=0;j<31;j++){
+                                if(suffix[i+1][j] > 0 && pre[j]==0) {
+                                    flag=false;
+                                    break;
+                                }
+                            }
+                            
+                            
+                        }
+                        
+                        if(flag) ans.push_back(i-l+1);
+                        else break;
                     }
                     
-                    bool flag=true;
-                    l+=1;
-                    if(l>i) break;
-                    if(i+1<n){
-
-                        for(int j=0;j<31;j++){
-                            if(suffix[i+1][j] > 0 && pre[j]==0) {
-                                flag=false;
-                                break;
-                            }
-                        }
-
-
-                    }
-
-                    if(flag) ans.push_back(i-l+1);
-                    else break;
-
+                    
                 }
-            
-            }
             
         }
         
         return ans;
+        
     }
 };
