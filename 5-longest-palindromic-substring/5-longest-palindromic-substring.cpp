90184c4562009f0ca0adcaa41d77743fa28d0100
Purva Singh <singhpurva2906@gmail.com>
2022-10-05 18:38:38 -0400

    Time: 311 ms (33.02%), Space: 35 MB (32.74%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -5,30 +5,23 @@ public:
         vector<vector<bool>> dp(n, vector<bool>(n, false));
         
         for(int i=0;i<n;i++){
-            dp[i][i] = true;
+            dp[i][i]=true;
         }
-        string ans="";
-        ans+=s[0];
+        
+        string c(1, s[0]);
+        string ans = c;
         
         for(int i=n-1;i>=0;i--){
             for(int j=i+1;j<n;j++){
-                
                 if(s[i] == s[j]){
-                    
                     if(j-i==1 || dp[i+1][j-1]){
                         
                         dp[i][j] = true;
-                        
                         if(ans.length() < j-i+1){
-                            
-                            ans = s.substr(i, j-i+1);
-                            
+                            ans=s.substr(i, j-i+1);
                         }
-                        
                     }
-                    
                 }
-                
             }
         }
         
