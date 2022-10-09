5d39b88702f3c87417d7e0e96e86acca88a29cd2
Purva Singh <singhpurva2906@gmail.com>
2022-10-09 09:28:18 -0400

    Time: 293 ms (79.27%), Space: 42.1 MB (81.88%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,71 +1,43 @@
-class Edge{
-public:
-    int u;
-    int v;
-    int w;
-    Edge(int u, int v, int w): u(u), v(v), w(w) {
-        
-    }
-};
-
-class MyCompare{
-public:
-    bool operator()(const Edge& a, const Edge& b){
-        return a.w > b.w;
-    }
-};
-
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
         int n = points.size();
-        priority_queue<Edge, vector<Edge>, MyCompare> pq;
-        vector<bool> visited(n, false);
         
-        int res=0, count=0;
+        int mstCost =0;
+        int edgesUsed = 0;
+        vector<bool> seen(n, false);
         
-        for(int j=0;j<n;j++) {
-            
-            int weight = abs(points[0][0] - points[j][0]) + abs(points[0][1] - points[j][1]);
-            
-            Edge edge(0, j, weight);
-            
-            pq.push(edge);
-            
-        }
-        
-        visited[0] = true;
-        
-        while(!pq.empty() && count!=n-1) {
+        while(edgesUsed < n){
             
             auto f = pq.top();
             pq.pop();
             
-            if(!visited[f.v]){
+            int weight = f.first;
+            int node = f.second;
+            
+            if(seen[node]) continue;
+            
+            seen[node] = true;
+            
+            mstCost += weight;
+            edgesUsed += 1;
+            
+            for(int nextNode=0;nextNode < n;nextNode++){
                 
-                visited[f.v] = true;
-                res += f.w;
-                
-                for(int j=0;j<n;j++) {
+                if(!seen[nextNode]){
+                    int w = abs(points[node][0] - points[nextNode][0]) + 
+                        abs(points[node][1] - points[nextNode][1]);
                     
-                    if(!visited[j]) {
-                        
-                        int dist = abs(points[f.v][0] - points[j][0]) + abs(points[f.v][1] - points[j][1]);
-                        Edge edge(f.v, j, dist);
-                        pq.push(edge);
-                        
-                    }
-                    
+                    pq.push({w, nextNode});
                 }
-                
-                count += 1;
                 
             }
             
-            
         }
         
-        return res;
+        return mstCost;
         
     }
 };
