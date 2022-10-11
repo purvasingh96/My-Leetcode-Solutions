d3e11af8287b74246bad51f33efbb725ffab9a71
Purva Singh <singhpurva2906@gmail.com>
2022-10-11 16:03:19 -0400

    Time: 100 ms (69.46%), Space: 61.6 MB (24.07%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -5,7 +5,10 @@ public:
         vector<int> res;
         
         for(int i=0;i<nums.size();i++){
-            if(res.empty() || (nums[i] > res.back() && res.size() < 3)) res.push_back(nums[i]);
+            if(res.empty() || (nums[i] > res.back() && res.size() < 3)) {
+                res.push_back(nums[i]);
+                if(res.size()==3) return true;
+            }
             else{
                 auto it = lower_bound(res.begin(), res.end(), nums[i]);
                 
