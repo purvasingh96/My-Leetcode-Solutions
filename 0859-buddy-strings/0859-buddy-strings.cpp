e14662a7da57a052f7adc1f3bddb4247ef646f63
Purva Singh <singhpurva2906@gmail.com>
2022-10-11 18:21:32 -0400

    Time: 12 ms (20.85%), Space: 7.1 MB (21.94%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.length()!=goal.length()) return false;
+        
+        vector<int> sc;
+        
+        for(int i=0;i<s.length();i++){
+                if(s[i]!=goal[i]) {
+                    if(sc.size()>2) return false;
+                    else{
+                    sc.push_back(i);
+                } 
+            }
+        }
+        
+        if(sc.size()==0)
+        {
+            vector<int> count(26, 0);
+            for(auto c:s){
+                count[c-'a']+=1;
+                if(count[c-'a']%2==0) return true;
+            }
+            return false;
+        }        
+            
+            
+            
+        if(sc.size()==2){
+            //cout<<sc[0]<<" "<<sc[1];
+            char char0 = s[sc[0]];
+            char char1 = s[sc[1]];
+            //cout<<char0<<" "<<char1<<"\n";
+            s[sc[0]] = char1;
+            s[sc[1]] = char0;  
+        }
+        
+        
+        return s == goal;
+        
+    }
+};
