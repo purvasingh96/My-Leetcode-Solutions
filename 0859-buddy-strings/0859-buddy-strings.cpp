8cd2899d814e1cee938010c17e175bff2b8d71ad
Purva Singh <singhpurva2906@gmail.com>
2022-10-21 15:56:12 -0400

    Time: 12 ms (26.19%), Space: 7 MB (64.99%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
+        
+        int s1=-1, s2=-1;
         if(s.length()!=goal.length()) return false;
+        int count=0;
         
-        vector<int> sc;
+        bool hasEvenCount=false;
+        vector<int> c(26, 0);
         
-        for(int i=0;i<s.length();i++){
-                if(s[i]!=goal[i]) {
-                    if(sc.size()>2) return false;
-                    else{
-                    sc.push_back(i);
-                } 
+        for(int i=0;i<s.length();i++){   
+            c[s[i]-'a'] +=1;
+            if(c[s[i]-'a']%2==0) hasEvenCount=true;
+            if(s[i]!=goal[i]){
+                if(s1==-1) s1=i;
+                else s2=i;
+                count+=1;
+                if(count > 2) return false;
             }
+            
         }
         
-        if(sc.size()==0)
-        {
-            vector<int> count(26, 0);
-            for(auto c:s){
-                count[c-'a']+=1;
-                if(count[c-'a']%2==0) return true;
-            }
-            return false;
-        }        
-            
-            
-            
-        else if(sc.size()==2){
-            //cout<<sc[0]<<" "<<sc[1];
-            char char0 = s[sc[0]];
-            char char1 = s[sc[1]];
-            //cout<<char0<<" "<<char1<<"\n";
-            s[sc[0]] = char1;
-            s[sc[1]] = char0;  
-        }
+        if(count==1) return false;
         
+        if(count==0) return hasEvenCount;
         
-        return s == goal;
+        return goal[s1]==s[s2] && goal[s2] == s[s1];
         
     }
 };
