e95d3ba03d6d0aa2e7a1c3b163ef85b6cfee9e58
Purva Singh <singhpurva2906@gmail.com>
2022-10-21 08:58:16 -0400

    Time: 866 ms (6.84%), Space: 134.6 MB (7.65%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.cpp
+++ 1166-design-file-system/1166-design-file-system.cpp
@@ -1,85 +1,83 @@
-class Node{
-public:
-    unordered_map<string, Node*> children;
+class TrieNode{
+ public:
     int val;
-    Node() {
+    unordered_map<string, TrieNode*> memo;
+    TrieNode(){
         val=-1;
     }
-    
 };
+
+
 class FileSystem {
 private:
-    Node* root;
-    vector<string> splitString(string path){
+    TrieNode* root;
+    
+    vector<string> splitString(string path) {
         vector<string> res;
         path = path.substr(1);
-        
         stringstream ss(path);
-        string s;
+        string temp;
         
-        while(getline(ss, s, '/')){
-            res.push_back(s);
+        while(getline(ss, temp, '/')){
+            res.push_back(temp);
         }
         
         return res;
     }
-    
 public:
     FileSystem() {
-        root = new Node();
+        root = new TrieNode();
     }
     
-    bool insertWord(Node* temp, string word, int val){
-        
-        if(temp->children.find(word)==temp->children.end()){
-            temp->children[word] = new Node();
+    bool searchPath(TrieNode* node, int start, int end, vector<string>& res){
+        for(int i=start;i<=end;i++){
+            if(node->memo.find(res[i])==node->memo.end()) return false;
+            node = node->memo[res[i]];
         }
-        //cout<<"insertion done";
-        if(temp->children[word]->val!=-1) return false;
-        temp->children[word]->val = val;
         return true;
     }
     
-    bool createPath(string path, int val) {
-        vector<string> words = splitString(path);
-
-        //cout<<words.size()<<"\n";
+    bool insert(TrieNode* node, string word, int val){
+        if(node->memo.find(word) == node->memo.end()){
+            node->memo[word] = new TrieNode();
+        }
+        node = node->memo[word];
+        if(node->val != -1) return false;
+        node->val = val;
+        return true;
+    }
+    
+    
+    bool createPath(string path, int value) {
+        vector<string> res = splitString(path);
         
-        if(words.size()==1) {
-            Node* temp =root;
-            return insertWord(temp, words[0], val);
+        if(res.size() == 1){
+            TrieNode* node = root;
+            return insert(node, res[0], value);
+        } else{
             
-        } else {
-            Node* temp = root;
-            string targetWord = words.back();
+            TrieNode* node = root;
+            string target = res.back();
             
-            for(int i=0;i<words.size()-1;i++){
-                
-                    if(temp->children.find(words[i]) == temp->children.end()) return false;
-                    else temp = temp->children[words[i]];
-                
+            for(int i=0;i<res.size()-1;i++){
+                if(node->memo.find(res[i]) == node->memo.end()) return false;
+                node =node->memo[res[i]];
             }
             
-            if(!insertWord(temp, targetWord, val)) return false;
+            if(!insert(node, target, value)) return false;
+            return true;
         }
-        return true;
     }
     
     int get(string path) {
-        Node* temp = root;
-        vector<string> words = splitString(path);
+        TrieNode* node = root;
+        vector<string> res = splitString(path);
         
-        
-        for(auto word:words){
-            //cout<<"word: "<<word;
-            
-                //cout<<c<<"\n";
-                if(temp->children.find(word) == temp->children.end()) return -1;
-                else temp = temp->children[word];
-            
+        for(int i=0;i<res.size();i++){
+            if(node->memo.find(res[i]) == node->memo.end()) return -1;
+            else node = node->memo[res[i]];
         }
-        //cout<<" temp->val: "<<temp->val<<"\n";
-        return temp->val;
+        return node->val;
     }
 };
 
