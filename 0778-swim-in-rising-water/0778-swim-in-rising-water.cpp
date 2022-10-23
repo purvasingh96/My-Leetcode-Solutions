fc5e6ac616484413e8e6b59edaef1798a4b3d583
Purva Singh <singhpurva2906@gmail.com>
2022-10-23 10:55:06 -0400

    Time: 1110 ms (5.01%), Space: 492.6 MB (5.02%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,49 +1,43 @@
+# define pp pair<int, pair<int,int>>
 class Solution {
 private:
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
     
-    bool isValid(int x, int y, vector<vector<int>>& grid, vector<vector<bool>>& seen, int time){
-        return (
-            x>=0 &&y>=0 && x<grid.size() && y<grid[0].size()
-            && !seen[x][y] && grid[x][y] <= time
-        
-        );
-    }
-    
-    bool dfs(int x, int y, vector<vector<int>>& grid, vector<vector<bool>>& seen, int time){
-        int n = grid.size();
-        
-        if(!isValid(x, y, grid, seen, time)) return false;
-        if(x==n-1 && y==n-1) return true;
-        
-        seen[x][y] = true;
-        
-        for(int i=0;i<4;i++){
-            int new_x = x+dx[i];
-            int new_y = y+dy[i];
-            
-            if(dfs(new_x, new_y, grid, seen, time)) return true;
-        }
-        
-        return false;
-    }
+    bool isValid(int x, int y, vector<vector<int>> grid){
+        return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
+   }
 public:
-    
     int swimInWater(vector<vector<int>>& grid) {
+        int ans=0;
         int n = grid.size();
-        int l = grid[0][0], r = n*n;
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        pq.push({grid[0][0], {0, 0}});
         
-        while(l<r){
-            int mid = l + (r-l)/2;
-            vector<vector<bool>> seen(n, vector<bool>(n, false));
-            if(dfs(0, 0, grid, seen, mid)){
-                r=mid;
-            } else{
-                l=mid+1;
+        while(!pq.empty()){
+            
+            auto f = pq.top();
+            pq.pop();
+            
+            ans = max(ans, f.first);
+            int x = f.second.first, y = f.second.second;
+            visited[x][y] = true;
+            
+            if(x==n-1 && y==n-1) return ans;
+            
+            for(int i=0;i<4;i++){
+                int new_x = x + dx[i];
+                int new_y = y + dy[i];
+                
+                if(isValid(new_x, new_y, grid) && !visited[new_x][new_y]){
+                    pq.push({grid[new_x][new_y], {new_x, new_y}});
+                    visited[new_x][new_y] = true;
+                }
             }
+            
+            
         }
-        
-        return l;
+          return ans;
     }
 };
