a9755e8f2bd9cb18595cd5b344d47f613a59e666
Purva Singh <singhpurva2906@gmail.com>
2022-10-30 21:33:55 -0400

    Time: 527 ms (68.90%), Space: 31.2 MB (79.50%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -1,7 +1,4 @@
-/*
-1. find path for which min score is N or for which all the values are greater than equal to N.
-2. BFS
-*/
+# define pp pair<int, pair<int, int>> 
 
 class Solution {
 private:
@@ -11,58 +8,46 @@ private:
     bool isValid(int x, int y, vector<vector<int>>& grid){
         return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    
-    bool check(vector<vector<int>>& grid, int mid){
-        int m = grid.size(), n = grid[0].size();
-        queue<pair<int, int>> q;
-        vector<vector<bool>> seen(m, vector<bool>(n, false));
-        q.push({0, 0});
-        if(grid[0][0] < mid) return false;
-        seen[0][0] = true;
+public:
+    int maximumMinimumPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), minVal = INT_MAX;
+        priority_queue<pp> pq;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                auto f = q.front();
-                q.pop();
+        pq.push({grid[0][0], {0, 0}});
+        visited[0][0] = true;
+        
+        
+        while(!pq.empty()){
+            
+            auto f = pq.top();
+            pq.pop();
+            
+            int val = f.first, x = f.second.first, y = f.second.second;
+            
+            minVal = min(minVal, val);
+            
+            if(x == m-1 && y == n-1) return minVal;
+            
+            
+            for(int i=0;i<4;i++){
                 
-                int x = f.first, y = f.second;
-                
-                if(x==m-1 && y==n-1) return true;
-                
+                int new_x = x + dx[i];
+                int new_y = y + dy[i];
                 
                 
-                for(int i=0;i<4;i++){
-                    int new_x = x + dx[i];
-                    int new_y = y + dy[i];
-                    
-                    if(isValid(new_x, new_y, grid) && !seen[new_x][new_y] && grid[new_x][new_y] >= mid){
-                        q.push({new_x, new_y});
-                        seen[new_x][new_y] = true;
-                    }
-                    
+                if(isValid(new_x, new_y, grid) && !visited[new_x][new_y]){
+                    pq.push({grid[new_x][new_y], {new_x, new_y}});
+                    visited[new_x][new_y] = true;
                 }
                 
             }
+            
+            
+            
         }
         
-        return false;
+        return minVal;
         
-    }
-public:
-    int maximumMinimumPath(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int left = 0, right = min(grid[0][0], grid[m-1][n-1]);
-        int ans=0;
-        
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(check(grid, mid)){
-                ans=mid;
-                left=mid+1;
-            } else right=mid-1;
-        }
-        
-        return ans;
     }
 };
