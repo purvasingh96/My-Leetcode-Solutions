d943042694957430ca63dced4bc718364a8d5e27
Purva Singh <singhpurva2906@gmail.com>
2022-11-01 21:48:08 -0400

    Time: 169 ms (53.81%), Space: 30.3 MB (63.52%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,45 +1,47 @@
 class Solution {
-public:
-    
-    
-    bool isvalid(int i,int j,int m,int n)
-    {
-        if(i==m||j==n||j<0||i<0)
-            return false;
-        return true;
+private:
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+
+    bool isValid(int x, int y, vector<vector<int>>& grid){
+            return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    
-    vector<vector<int>> dir={{1,0},{0,1},{0,-1},{-1,0}};
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) 
-    {
-        queue<pair<int,int>> q;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        vector<vector<int>> dis(m,vector<int>(n,-1));
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int, int>> q;
+        int m = mat.size(), n =mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)
             {
-                if(matrix[i][j]==0)
+                if(mat[i][j]==0)
                 { 
                     q.push({i,j});
-                    dis[i][j]=0;
+                    dist[i][j]=0;
                 }
             }
-        while(!q.empty())
-        {
-            pair<int,int> curr=q.front();
-            q.pop();
-            for(auto& x:dir)
-            {
-                int a=curr.first+x[0];
-                int b=curr.second+x[1];
-                if(isvalid(a,b,m,n)&&dis[a][b]==-1)
-                {
-                    q.push({a,b});
-                    dis[a][b]=dis[curr.first][curr.second]+1;
+        
+        while(!q.empty()){
+            
+                auto f = q.front();
+                q.pop();
+                int x = f.first, y = f.second;
+                
+                for(int i=0;i<4;i++){
+                    int new_x = x + dx[i];
+                    int new_y = y + dy[i];
+                    
+                    if(isValid(new_x, new_y, mat) && dist[new_x][new_y]==-1){
+                        q.push({new_x, new_y});
+                        dist[new_x][new_y] = dist[x][y] + 1;
+                    }
+                    
                 }
-            }
+                
+            
         }
-        return dis;
+        
+        return dist;
     }
 };
