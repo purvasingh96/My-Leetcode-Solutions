034137fc6369b5df9d96df8ad3ff6d3ea765c773
Purva Singh <singhpurva2906@gmail.com>
2022-11-04 20:53:32 -0400

    Time: 3 ms (86.98%), Space: 7 MB (48.73%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<string> ten = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    vector<string> one = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+    
+    
+    string helper(int n){
+        if(n >= 1000000000) return helper(n/1000000000) + " Billion" + helper(n%1000000000);
+        
+        if(n >= 1000000) return helper(n/1000000) + " Million" + helper(n%1000000);
+        
+        if(n >= 1000) return helper(n/1000) + " Thousand" + helper(n%1000);
+        
+        if(n >= 100) return helper(n/100) + " Hundred" + helper(n%100);
+        
+        if(n >= 20) return " " + ten[n/10 - 2] + helper(n%10);
+        
+        if(n>=1) return " "+one[n-1];
+        
+        return "";
+        
+    }
+public:
+    string numberToWords(int num) {
+        if(num==0) return "Zero";
+        return helper(num).substr(1);
+    }
+};
