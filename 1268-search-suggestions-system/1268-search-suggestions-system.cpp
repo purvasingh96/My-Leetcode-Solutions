93bca7f3e60081bac0b706ea8e5fecfb2231e476
Purva Singh <singhpurva2906@gmail.com>
2022-11-06 17:16:14 -0500

    Time: 2254 ms (6.09%), Space: 664.7 MB (5.06%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,23 +1,76 @@
+class Trie{
+public:
+    bool end;
+    vector<Trie*> children;
+        
+    Trie(){
+        children.resize(26, NULL);
+        end=false;
+    }
+    
+};
+
+
 class Solution {
+private:
+    Trie* root;
+    void insert(string& word){
+        Trie* node = root;
+        for(int i=0;i<word.length();i++){
+            int idx = word[i] - 'a';
+            if(node->children[idx]==NULL){
+                node->children[idx] = new Trie();
+            }
+            node = node->children[idx];
+        }
+        node->end=true;
+    }
+    
+    void search(Trie* node, string partial, vector<string>& res){
+        if(res.size() == 3) return;
+        
+        if(node->end) res.push_back(partial);
+        
+        for(int i=0;i<26;i++){
+            if(node->children[i]!=NULL){
+                char y = (i+'a');
+                string x(1, y);
+                search(node->children[i], partial+x, res);
+                
+            }
+        }
+    }
+    
+    
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string s) {
-        sort(products.begin(), products.end());
-        string prefix="";
-        int l=0;
-        int n = products.size(), m = s.length();
-        
-        vector<vector<string>> res(m, vector<string>());
+        root = new Trie();
+        for(auto x:products) insert(x);
+        string partial = "";
+        vector<vector<string>> ans;
         
         for(int i=0;i<s.length();i++){
-            
-            auto start = lower_bound(products.begin(), products.end(), s.substr(0, i+1));
-            auto end = lower_bound(start, min(start+3, products.end()), s.substr(0, i)+(char)(s[i]+1) );
-            
-            res[i] = vector<string>(start, end);
+            if(root->children[s[i]-'a']==NULL){
+                while(i<s.length()){
+                    ans.push_back({});
+                    i+=1;
+                } 
+                return ans;
+            } else{
+                root = root->children[s[i]-'a'];
+                partial += s[i];
+                vector<string> res;
+                search(root, partial, res);
+                ans.push_back(res);
+            }
             
         }
         
-        return res;
+        
+        return ans;
+        
+        
+        
         
     }
 };
