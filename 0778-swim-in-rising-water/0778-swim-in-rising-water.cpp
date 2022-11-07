7c936e8bce1cd20ea6f45527034ee12ee10d96a7
Purva Singh <singhpurva2906@gmail.com>
2022-11-07 17:01:27 -0500

    Time: 74 ms (34.20%), Space: 11.4 MB (30.28%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,44 +1,66 @@
-# define pp pair<int, pair<int,int>>
 class Solution {
 private:
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
+
+    bool isValid(int x, int y, vector<vector<int>>& grid){
+            return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
+    }
     
-    bool isValid(int x, int y, vector<vector<int>> grid){
-        return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
-   }
-public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int ans=0;
+    bool bfs(int mid, vector<vector<int>>& grid){
+        queue<pair<int, int>> q;
         int n = grid.size();
-        priority_queue<pp, vector<pp>, greater<pp>> pq;
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        pq.push({grid[0][0], {0, 0}});
-        visited[0][0] = true;
+        vector<vector<bool>> seen(n, vector<bool>(n, false));
         
-        while(!pq.empty()){
+        seen[0][0] = true;
+        
+        q.push({0, 0});
+        
+        
+        while(!q.empty()){
             
-            auto f = pq.top();
-            pq.pop();
-            
-            ans = max(ans, f.first);
-            int x = f.second.first, y = f.second.second;
-            
-            
-            if(x==n-1 && y==n-1) return ans;
-            
-            for(int i=0;i<4;i++){
-                int new_x = x + dx[i];
-                int new_y = y + dy[i];
+            int sz = q.size();
+            while(sz--){
                 
-                if(isValid(new_x, new_y, grid) && !visited[new_x][new_y]){
-                    pq.push({grid[new_x][new_y], {new_x, new_y}});
-                    visited[new_x][new_y] = true;
+                auto f = q.front();
+                q.pop();
+                
+                int x = f.first, y = f.second;
+                
+                if(x == n-1 && y==n-1) return true;
+                
+                for(int k=0;k<4;k++){
+                    int new_x = x +dx[k], new_y=y+dy[k];
+                    
+                    if(isValid(new_x, new_y, grid) && !seen[new_x][new_y] && grid[new_x][new_y] <= mid){
+                        q.push({new_x, new_y});
+                        seen[new_x][new_y] = true;
+                    }
                 }
+                
+                
             }
             
-            
         }
-          return ans;
+        
+        return false;
+    }
+    
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int l=grid[0][0], r=pow(n, 2);
+        
+        int ans=0;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            vector<vector<bool>> seen(n, vector<bool>(n, false));
+            if(bfs(mid, grid)){
+                ans = mid;
+                r = mid-1;
+            } else l = mid+1;
+        }
+        
+        return ans;
     }
 };
