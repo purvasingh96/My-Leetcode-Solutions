b9d4d8632f61d242208a448e982c98e7887366c7
Purva Singh <singhpurva2906@gmail.com>
2022-11-07 16:13:42 -0500

    Time: 132 ms (96.93%), Space: 49.9 MB (87.68%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,54 +1,57 @@
+# define pp pair<pair<int,int>, int>
 class Solution {
 private:
-    int binary_search(int start, vector<pair<pair<int, int>, int>>& nums, int target){
-        int l=start, r=nums.size()-1;
+    int binary_search(vector<pp>& res, int start, int target){
+        int l=start, r=res.size()-1;
+        int idx = -1;
         
-        while(l<r){
+        while(l<=r){
             int mid = l + (r-l)/2;
-            if(nums[mid].first.first >= target) r=mid;
-            else l=mid+1;
-        }
-        
-        if(nums[l].first.first < target) return -1;
-        return l;
-    }
-public:
-    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
-        vector<pair<pair<int, int>, int>> nums;
-        int n = p.size();
-        
-        for(int i=0;i<n;i++){
-            nums.push_back({{s[i], e[i]}, p[i]});
-        }
-        
-        
-        sort(nums.begin(), nums.end(), [](const pair<pair<int, int>, int>& a, const pair<pair<int, int>, int>&b ){
-            pair<int,int> ka = a.first;
-            pair<int,int> kb = b.first;
-            
-            if(ka.first == kb.first) return ka.second < kb.second;
-            return ka.first < kb.first;
-        });
-        
-        
-        vector<int> dp(n, 0);
-        dp[n-1] = nums[n-1].second;
-        
-        for(int i=n-2;i>=0;i--){
-            
-            int endTime = nums[i].first.second;
-            int profit = nums[i].second;
-                
-            int idx = binary_search(i+1, nums, endTime);
-            
-            if(idx!=-1){
-                dp[i] = profit + dp[idx];
-                dp[i] = max(dp[i], dp[i+1]);
+            if(res[mid].first.first >= target){
+                idx = mid;
+                r = mid-1;
             } else {
-                dp[i] = max(profit, dp[i+1]);
+                l = mid+1;
             }
         }
         
+        return idx;
+        
+    }
+public:
+    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
+        vector<pp> res;
+        int n = s.size();
+        vector<int> dp(n, 0);
+        
+        for(int i=0;i<s.size();i++){
+            res.push_back({{s[i], e[i]}, p[i]});
+        }
+        sort(res.begin(), res.end(), [](const pp& a, const pp& b){
+            auto x = a.first;
+            auto y = b.first;
+            
+            if(x.first == y.first) return x.second < y.second;
+            return x.first < y.first;
+            
+        });
+        
+        
+        dp[n-1] = res.back().second;
+        
+        for(int i=n-2;i>=0;i--){
+            
+            int end = res[i].first.second;
+            int idx = binary_search(res, i+1, end);
+            
+            if(idx!=-1){
+                dp[i] = res[i].second + dp[idx];
+                dp[i] = max(dp[i+1], dp[i]);
+            } else {
+                dp[i] += max(res[i].second, dp[i+1]);
+            }
+            
+        }
         
         return dp[0];
         
