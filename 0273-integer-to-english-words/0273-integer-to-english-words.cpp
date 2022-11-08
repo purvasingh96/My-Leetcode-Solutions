f2a9990d5a018cd96b09052e0cc36ab44adde393
Purva Singh <singhpurva2906@gmail.com>
2022-11-08 09:21:23 -0500

    Time: 6 ms (64.11%), Space: 7 MB (49.34%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,12 +1,14 @@
 class Solution {
 private:
+    vector<string> one ={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    
     vector<string> ten = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-    vector<string> one = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-
     
     
     string helper(int n){
-        if(n >= 1000000000) return helper(n/1000000000) + " Billion" + helper(n%1000000000);
+        
+        
+        if(n>= 1000000000) return  helper(n/1000000000) + " Billion" + helper(n%1000000000);
         
         if(n >= 1000000) return helper(n/1000000) + " Million" + helper(n%1000000);
         
@@ -14,9 +16,9 @@ private:
         
         if(n >= 100) return helper(n/100) + " Hundred" + helper(n%100);
         
-        if(n >= 20) return " " + ten[n/10 - 2] + helper(n%10);
+        if(n>=20) return " " + ten[n/10 - 2] + helper(n%10);
         
-        if(n>=1) return " "+one[n-1];
+        if(n>=1) return " " + one[n-1];
         
         return "";
         
