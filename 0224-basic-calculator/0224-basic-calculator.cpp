8ee17d59ecd236860c036802cec231782fc4ce07
Purva Singh <singhpurva2906@gmail.com>
2022-11-20 09:59:20 -0500

    Time: 30 ms (35.39%), Space: 11.5 MB (20.04%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,40 +1,44 @@
 class Solution {
 private:
     int parseNum(string& s, int& i){
+        int len = s.length();
         int temp=0;
         
         for(;i<s.length() && isdigit(s[i]);i++){
-            temp = temp*10 + (s[i]-'0');
+            temp = (temp*10 + (s[i]-'0'));
         }
+        
         return temp;
     }
     
-    int parseExp(string s, int& i){
-        
-        vector<int> nums;
+    int parseExp(string& s, int& i){
+        int n = s.length();
+        stack<int> st;
         char op = '+';
+        
         
         for(;i<s.length() && op!=')';i++){
             
-            int temp = 0;
-            if(s[i] == ' ') continue;
-            int n = s[i] == '(' ? parseExp(s, ++i) : parseNum(s, i);
-            
-            if(op == '+') nums.push_back(n);
-            else if(op == '-') nums.push_back(-n);
-            
-            op = s[i];
+                if(s[i] == ' ') continue;
+                int num = s[i] == '(' ? parseExp(s, ++i) : parseNum(s, i);
+                
+                if(op == '+') {st.push(num);}
+                else if(op == '-') st.push(-num);
+                
+                op = s[i];
             
         }
         
         int res=0;
-        while(nums.size()!=0) {
-            res += nums.back();
-            nums.pop_back();
-        }
         
+        while(st.size()!=0){
+            res+=st.top();
+            st.pop();
+        }
+        cout<<"res: "<<res<<"\n";
         return res;
     }
+    
     
 public:
     int calculate(string s) {
