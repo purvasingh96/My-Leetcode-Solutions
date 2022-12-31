c9774fb269811e18c6bd25a2c44bf0c34864af4a
Purva Singh <singhpurva2906@gmail.com>
2022-12-31 10:34:31 +0530

    Time: 4 ms (69.25%), Space: 7 MB (64.25%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -6,43 +6,49 @@ private:
     bool isValid(int x, int y, vector<vector<int>>& grid){
             return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    void backtrack(int i, int j, int emptyCells, int& ans, vector<vector<int>>& grid){
+    
+    void dfs(int i, int j, vector<vector<int>>& grid, int& count, int obstacleFreeCubes){
         
-        if(grid[i][j] == 2 && emptyCells == 1){
-            ans+=1;
+        if(obstacleFreeCubes == 1 && grid[i][j] == 2) {
+            count+=1;
             return;
         }
         
         int temp = grid[i][j];
-        grid[i][j] = -4;
+        grid[i][j] = -1;
         
         for(int k=0;k<4;k++){
-            int new_x = i + dx[k], new_y = j + dy[k];
-            if(isValid(new_x, new_y, grid) && grid[new_x][new_y]>=0){
-                backtrack(new_x, new_y, emptyCells-1, ans, grid);
+            int new_x = i + dx[k];
+            int new_y = j + dy[k];
+            
+            if(isValid(new_x, new_y, grid) && grid[new_x][new_y]!=-1){
+                
+                dfs(new_x, new_y, grid, count, obstacleFreeCubes-1);
             }
         }
         
         grid[i][j] = temp;
     }
+    
 public:
     int uniquePathsIII(vector<vector<int>>& grid) {
-        int ans=0;
-        int m = grid.size(), n = grid[0].size();
-        int si, sj, emptyCells=0;
+        // calculate 
+        int count=0, si, sj;
+        int obstacleFreeCubes=0;
         
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j] == 1){
-                    si=i, sj=j;
-                } 
-                if(grid[i][j] !=-1) emptyCells+=1;
+                if(grid[i][j] >=0) obstacleFreeCubes+=1;
+                 if(grid[i][j] == 1) {
+                    si=i;
+                    sj=j;
+                }
             }
         }
         
-        backtrack(si, sj, emptyCells, ans, grid);
+        cout<<"working";
         
-        
-        return ans;
+        dfs(si, sj, grid, count, obstacleFreeCubes);
+        return count;
     }
 };
