055b267619b54e2e528b94edbaf316d94fb289fa
Purva Singh <singhpurva2906@gmail.com>
2023-01-22 08:11:08 -0500

    Time: 0 ms (100.00%), Space: 6.5 MB (69.93%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -9,29 +9,6 @@ private:
         return false;
     }
     
-    void backtrack(string s, int len1, int len2, int len3, unordered_set<string>& res){
-        int n = s.length();
-        //cout<<"i1: "<<i1<<" i2: "<<i2<<" i3: "<<i3<<"\n"; 
-        string s1 = s.substr(0, len1);
-        string s2 = s.substr(len1, len2);
-        string s3 = s.substr(len1+len2, len3);
-        string s4 = s.substr(len1+len2+len3);
-        
-        //cout<<s1<<" "<<s2<<" "<<s3<<" "<<s4<<"\n";
-        if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
-            res.insert(s1+"."+s2+"."+s3+"."+s4);
-            return;
-        }
-        
-        for(int i=1;i<=3 && i<n;i++){
-            for(int j = 1;j<=3 && j<n;j++){
-                for(int k=1;k<=3 && k<n;k++){
-                    backtrack(s, i, j, k, res);
-                }
-            }
-        }
-        
-    }
 public:
     vector<string> restoreIpAddresses(string s) {
         unordered_set<string> res;
