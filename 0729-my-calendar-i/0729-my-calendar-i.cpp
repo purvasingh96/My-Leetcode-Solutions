a43c0e4c53d9bfc99706cd90313b2960ba572ee1
Purva Singh <singhpurva2906@gmail.com>
2023-01-23 14:58:31 -0500

    Time: 337 ms (14.64%), Space: 41.4 MB (10.06%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,80 @@
+class MyCalendar {
+private:
+    vector<vector<int>> res;
+    int bs(int target, vector<vector<int>>& res){
+        int l=0, r=res.size()-1;
+        int ans=-1;
+        
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            
+            if(res[mid][0]>=target){
+                ans=mid;
+                r=mid-1;
+            } else {
+                l = mid+1;
+            }   
+        }
+        
+        return ans;
+    }
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        /*
+            it is end: push_back
+            it is beginning: insert at 0th pos
+            it is bw 0-n: check
+        
+        */
+        
+        vector<int> target = {start, end};
+        
+        if(res.empty()){
+            res.push_back(target);
+            return true;
+        }
+        int it = bs(start, res);
+        // cout<<"id: "<<it<<"\n";
+        // for(auto x:res){
+        //     cout<<"("<<x[0]<<", "<<x[1]<<") ";
+        // }
+        // cout<<"\n";
+        
+        if(it==-1){
+            if(start >= res.back()[1]){
+                res.push_back(target);
+                return true;
+            } 
+            return false;
+            
+        } 
+        else if(it == 0){
+            if(end <= res[0][0]) {
+                res.insert(res.begin()+it, target);
+                return true;
+            }
+            return false;
+        } 
+        else{
+            
+            if(res[it][0] >= end && res[it-1][1]<=start){
+                res.insert(res.begin()+it, target);
+                return true;
+            } else return false;
+            
+        }
+        
+        
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
