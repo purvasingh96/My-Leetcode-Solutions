b79d946ccc341c5d96f20c0ae9176edaed4afe6c
Purva Singh <singhpurva2906@gmail.com>
2023-02-06 11:50:22 -0500

    Time: 39 ms (60.00%), Space: 9.6 MB (12.63%) - LeetHub

--- 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
@@ -1,41 +1,37 @@
 class Solution {
 private:
-    int dp[101][101], costs[101][101];
-    int n;
-    
-    int dfs(int housepos, int k){
+    long long dfs(int i, int k, vector<vector<int>>& cost, vector<vector<long long>>& dp, vector<int>& houses){
+        int n = houses.size();
+        if (i == houses.size() && k == 0) return 0;
+        if (i == houses.size() || k == 0) return INT_MAX;
         
-        if(housepos >= n) return 0;
-        if(k==1) return costs[housepos][n-1];
-        if(dp[housepos][k]!=-1) return dp[housepos][k];
         
-        int dist=INT_MAX;
-        for(int i=housepos;i<n;i++){
-            
-            dist = min(dist, costs[housepos][i] + dfs(i+1, k-1));
-            
+        if(dp[i][k]!=-1) return dp[i][k];
+        
+        long long ans=INT_MAX;
+        for(int j=i;j<n;j++){
+            long long temp = cost[i][j] + dfs(j+1, k-1, cost, dp, houses);
+            ans = min(ans, temp);
         }
         
-        return dp[housepos][k] = dist;
+        return dp[i][k] = ans;
     }
 public:
     int minDistance(vector<int>& houses, int k) {
-        n = houses.size();
+        int n = houses.size();
         sort(houses.begin(), houses.end());
+        vector<vector<int>> cost(n, vector<int>(n, 0));
+        vector<vector<long long>> dp(n, vector<long long>(k+1, -1));
         
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                costs[i][j]=0;
-                int median = houses[(i+j)/2];
-                
+                int mid = houses[(i+j)/2];
                 for(int k=i;k<=j;k++){
-                    costs[i][j] += abs(median - houses[k]);
+                    cost[i][j] += abs(houses[k] - mid);
                 }
-                
             }
         }
         
-        memset(dp, -1, sizeof(dp));
-        return dfs(0, k);
+        return (int)dfs(0, k, cost, dp, houses);
     }
 };
