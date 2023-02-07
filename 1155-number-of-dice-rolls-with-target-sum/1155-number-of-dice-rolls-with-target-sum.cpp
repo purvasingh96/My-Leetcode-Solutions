3951231b1a0ea7e9f36889fbe944c8ecfae649ab
Purva Singh <singhpurva2906@gmail.com>
2023-02-07 07:06:59 -0500

    Time: 31 ms (75.05%), Space: 8.5 MB (35.90%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,29 +1,23 @@
 class Solution {
 private:
-    // dp[num blanks][target sum] = how many ways
-    int mod=1e9+7;
-    long long dfs(int n, int k, int target, vector<vector<long long>>& dp){
-        if(target<0) return 0;
-        if(target==0 && n==0) return 1;
+    long long dfs(int n, int currSum, int k, int target, vector<vector<int>>& dp){
+        int mod = 1e9+7;
+        if(n==0 && currSum == target) return 1;
+        if(n==0 || currSum > target) return 0;
         
-        if(n==0) return 0;
+        if(dp[n][currSum]!=-1) return dp[n][currSum];
         
-        if(dp[n][target]!=-1) return dp[n][target];
-        
-        long long c=0;
+        long long count=0;
         for(int i=1;i<=k;i++){
-            c+= dfs(n-1, k, target-i, dp);
-            c=c%mod;
+            count += dfs(n-1, currSum+i, k, target, dp);
+            count = count%mod;
         }
         
-        return dp[n][target] = c;
-        
+        return dp[n][currSum] = count;
     }
-        
-        
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long>> dp(n+1, vector<long long>(target+1, -1));
-        return dfs(n, k, target, dp)%mod;
+        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        return (int)dfs(n, 0, k, target, dp);
     }
 };
