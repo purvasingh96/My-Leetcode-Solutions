fd3c8fdd4f73b2d88dfdd8b6c63ee02f07d309b3
Purva Singh <singhpurva2906@gmail.com>
2023-02-17 10:24:17 -0500

    Time: 3 ms (89.03%), Space: 10.2 MB (10.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,32 +1,69 @@
+class TrieNode{
+public:
+    vector<TrieNode*> dict;
+    string val;
+    int reps;
+    TrieNode(){
+        dict.resize(26, NULL);
+        reps=0;
+        val="";
+    }
+};
+
 class Solution {
 private:
-    bool isPossible(int len, vector<string>& strs){
-        string prefix = strs[0].substr(0, len);
-        for(auto s:strs){
-            if(s.rfind(prefix, 0)!=0) return false;
+    TrieNode* root;
+    
+    void insert(string s){
+        TrieNode* node = root;
+        
+        for(int i=0;i<s.length();i++){
+            
+            if(node->dict[s[i]-'a']==NULL){
+                node->dict[s[i]-'a'] = new TrieNode();
+            }
+            
+            
+            node = node->dict[s[i]-'a'];
+            node->reps+=1;
+            string temp(1, (s[i]));
+            node->val=temp;
         }
-        return true;
+        
     }
+    
+    void print(TrieNode* node){
+        for(int i=0;i<26;i++){
+            if(node->dict[i]!=NULL){
+                cout<<(char)(i+'a')<<" ";
+                print(node->dict[i]);
+            }
+        }
+    }
+    
+    void longestPrefix(int n, TrieNode* node, string& res){
+       for(int i=0;i<26;i++){
+           if(node->dict[i]){
+               if(node->dict[i]->reps==n){
+                   string temp(1, (i+'a'));
+                   res+=temp;
+                   longestPrefix(n, node->dict[i], res);
+               }
+           }
+       }
+    }
+    
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // BS : if f(x) is true and if f(x+1) is also true => binary search to divide search space in half
-        // if(isPrefix(of length x) == true) then isPrefix(of length x-1) is also true.
+        root = new TrieNode();
+        int n = strs.size();
+        string res="";
         
-        int minlen=INT_MAX;
-        for(auto s:strs) minlen = min(minlen, (int)s.length());
-        
-        int left=0, right=minlen;
-        
-        int ans=-1;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(isPossible(mid, strs)){
-                ans=mid;
-                left=mid+1;
-            } else right=mid-1;
-        }
-        
-        return ans==-1 ? "" : strs[0].substr(0, ans);
+        for(auto word:strs) insert(word);
+        TrieNode* node = root;
+        // print(node);
+        longestPrefix(n, node, res);
+        return res;
         
     }
 };
