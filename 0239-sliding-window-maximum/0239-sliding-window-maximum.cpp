cf87d147d7f99abb8a634ae085a37cbf49dc1be6
Purva Singh <singhpurva2906@gmail.com>
2023-02-19 18:31:05 -0500

    Time: 1239 ms (5.02%), Space: 224.6 MB (5.44%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,37 +1,23 @@
-class MyCompare{
-public:
-    bool operator()(const vector<int>& a, const vector<int>& b){
-        return a[1] < b[1];
-    }
-};
-
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // store idx in pq
-        // till pq.top is within (i to j) range: keep max as pq.top()
-        // pop pq.top
-        
-        priority_queue<vector<int>, vector<vector<int>>, MyCompare> pq;
+        priority_queue<vector<int>> pq;
         for(int i=0;i<k;i++){
-            pq.push({i, nums[i]});
+            pq.push({nums[i], i});
         }
-        
+        int right=k;
         vector<int> res;
-        res.push_back(pq.top()[1]);
-        int start = 1;
-        for(int end=k;end<nums.size();end++){
-            pq.push({end, nums[end]});
-                while(!pq.empty() && (pq.top()[0] <start)){
-                    pq.pop();
-                }
-            
-            res.push_back(pq.top()[1]);
-            start+=1;
+        res.push_back(pq.top()[0]);
+        
+        while(right<nums.size()){
+            while(!pq.empty() && right-pq.top()[1] >= k){
+                pq.pop();
+            }
+            pq.push({nums[right], right});
+            right+=1;
+            res.push_back(pq.top()[0]);
         }
         
         return res;
-        
     }
 };
