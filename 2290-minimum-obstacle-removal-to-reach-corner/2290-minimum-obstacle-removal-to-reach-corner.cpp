da7193c373f06f8cbaa017d894e6090d57a99bc6
Purva Singh <singhpurva2906@gmail.com>
2023-02-19 17:49:31 -0500

    Time: 2530 ms (8.20%), Space: 229.4 MB (18.86%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,53 +1,43 @@
-#define pi pair<int, int>
-#define pipi pair<int, pi>
-
 class Solution {
 private:
-    int dx[4] = {0, 0, -1, 1};
-    int dy[4] = {1, -1, 0, 0};
+    int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+bool isValid(int x, int y, vector<vector<int>>& grid){
+        return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
+}
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        priority_queue<pipi, vector<pipi>, greater<pipi>> pq;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        pq.push({0, {0,0}});
+        vector<vector<int>> visited(m, vector<int>(n, INT_MAX));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        visited[0][0]=0;
+        q.push({0,0, 0});
         
-        visited[0][0] = true;
-        
-        while(!pq.empty()){
-            
-            auto f = pq.top();
-            pq.pop();
-            int dist = f.first;
-            auto coords_info = f.second;
-            int x = coords_info.first;
-            int y = coords_info.second;
-            
-            
-            if(x==m-1 && y==n-1) return dist;
+        while(!q.empty()){
+            auto f = q.top();
+            q.pop();
+            int x = f[1], y = f[2], obstacles=f[0];
+            if(x==m-1 && y==n-1) return obstacles;
             
             for(int i=0;i<4;i++){
+                int new_x = x + dx[i];
+                int new_y = y + dy[i];
                 
-                int new_x = x+dx[i];
-                int new_y = y+dy[i];
-                
-                if(new_x>=0 && new_y>=0 && new_x<m && new_y<n){
-                    if(!visited[new_x][new_y]){
-                        visited[new_x][new_y] = true;
-                        if(grid[new_x][new_y]==0){
-                            pq.push({dist, {new_x, new_y}});
-                        } else{
-                            pq.push({dist+1, {new_x, new_y}});
-                        }
-                    }
+                if(isValid(new_x, new_y, grid)){
+                   if(visited[new_x][new_y] > grid[new_x][new_y]+obstacles){
+                       visited[new_x][new_y] = grid[new_x][new_y]+obstacles;
+                       q.push({grid[new_x][new_y]+obstacles, new_x, new_y});
+                   }
                 }
                 
                 
+                
+                
             }
-            
-            
-            
         }
+        
         return -1;
+        
     }
 };
