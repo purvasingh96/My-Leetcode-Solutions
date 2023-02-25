f1a23b3076af03d2cd54c7c4a93e5cd09bb8752f
Purva Singh <singhpurva2906@gmail.com>
2023-02-25 17:35:28 -0500

    Time: 69 ms (53.02%), Space: 55.5 MB (70.06%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,38 +1,22 @@
 class Solution {
-private:
-    bool enough(vector<int>& nums, int k, int mid){
-        vector<int> c(2, 0);
-        
-        for(int i=0;i<mid;i++){
-            c[nums[i]]+=1;
-        }
-        
-        if(c[1]+k >= mid) return true;
-        
-        int j=0;
-        for(int i=mid;i<nums.size();i++){
-            c[nums[i]]+=1;
-            c[nums[j]]-=1;
-            j+=1;
-            if(c[1]+k>=mid) return true;
-        }
-        
-        return false;
-    }
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int left = 0, right=nums.size();
-        int ans=0;
+        int left=0, right=0, ans=0;
+        vector<int> count(2, 0);
         
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            int f = enough(nums, k, mid);
-            //cout<<"mid: "<<mid<<" enough: "<<f<<"\n";
+        for(;right<nums.size();right++){
+            count[nums[right]]+=1;
             
-            if(f){
-                ans=mid;
-                left=mid+1;
-            } else right=mid-1;
+            if(count[0]>k){
+                
+                while(left<=right && count[0]>k){
+                    count[nums[left]]-=1;
+                    left+=1;
+                }
+                //cout<<ans<<" "; 
+            }
+            
+            ans = max(ans, right-left+1);
         }
         
         return ans;
