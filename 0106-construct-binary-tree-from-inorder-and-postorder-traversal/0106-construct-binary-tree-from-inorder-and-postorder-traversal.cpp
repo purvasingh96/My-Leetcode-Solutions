d9aec85076ac7d822372c4e40973c048e3165573
Purva Singh <singhpurva2906@gmail.com>
2023-02-27 09:58:35 -0500

    Time: 39 ms (27.23%), Space: 26 MB (91.04%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int postOrderIdx;
+    int inorderIdx(vector<int>& inorder, int val, int start, int end){
+        for(int i=0;i<inorder.size();i++){
+            if(inorder[i] == val) return i;
+        }
+        return -1;
+    }
+    
+    TreeNode* build(int left, int right, vector<int>& inorder, vector<int>& postorder){
+        //cout<<"left: "<<left<<" right: "<<right<<"\n";
+        if(left > right) return NULL;
+        
+        
+        int val = postorder[postOrderIdx];
+        TreeNode* root = new TreeNode(val);
+        int mid = inorderIdx(inorder, val, left, right);
+        postOrderIdx--;
+        root->right = build(mid+1, right, inorder, postorder);
+        root->left = build(left, mid-1, inorder, postorder);
+        
+        return root;
+    }
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = postorder.size()-1;
+        postOrderIdx = n;
+        return build(0, n, inorder, postorder);
+    }
+};
