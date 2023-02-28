8d9bd405bdcb240573b7351bfe317a5983648707
Purva Singh <singhpurva2906@gmail.com>
2023-02-28 13:02:42 -0500

    Time: 2453 ms (5.01%), Space: 465 MB (5.01%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,36 +1,57 @@
 class MyCalendarTwo {
 private:
-    map<int, int> m;
-public:
-    bool canAdd(){
-        int sum=0;
+     map<long long, long long> tree;
+    map<long long, long long> lazy;
+
+    int updateAdd(int rootIdx, int low, int high, int l, int r){
         
-        //for(auto x:m) cout<<x.first<<"=>"<<x.second<<"\n";
-        
-        for(auto it:m){
-            sum += it.second;
-            if(sum == 3) return false;
+         if(high<l || r<low)return 0 ; 
+        if(low>=l && high<=r)
+        {
+            tree[rootIdx]++;
+            lazy[rootIdx]++;
+            
+           return tree[rootIdx] ;
         }
-        return true;
+        
+            int mid = (low+high)/2 ;
+            updateAdd(1LL*2*rootIdx+1 , low ,mid ,l ,r ); 
+            updateAdd(1LL*2*rootIdx+2 , mid+1 ,high , l ,r ); 
+        return tree[rootIdx] =lazy[rootIdx]+ max(tree[1LL*2*rootIdx+1] , tree[1LL*2*rootIdx+2]); 
     }
     
+    int updateUndo(int rootIdx, int low, int high, int l, int r){
+        if(high<l || r<low)return 0 ; 
+        
+       if(l<=low && high<=r){
+           tree[rootIdx]-=1;
+           lazy[rootIdx]-=1;
+           return tree[rootIdx];
+       }
+
+        
+        int mid = low + (high-low)/2;
+        updateUndo(1LL*2*rootIdx+1, low, mid,  l,  r);
+         updateUndo(1LL*2*rootIdx+2, mid+1, high, l, r);
+        
+        return tree[rootIdx] = lazy[rootIdx]+max(tree[1LL*2*rootIdx+1], tree[1LL*2*rootIdx+2]);
+    }
+    
+    
+public:
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        m[start]+=1;
-        m[end]-=1;
-        bool ans = canAdd();
-        
-        if(!canAdd()) {
-            m[start]-=1;
-            m[end]+=1;
+       int res = updateAdd(0, 0, 1000000000, start, end-1);
+       
+        if(res==1||res==2) return true;
+        else {
+            updateUndo(0, 0, 1000000000, start, end-1);
+            return false;
         }
         
-//         cout<<"\nans: "<<ans;
-//         cout<<"\n=====\n";
-        return ans;
     }
 };
 
