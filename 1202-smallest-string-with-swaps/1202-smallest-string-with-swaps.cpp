4fea8d269715dbfa0e49e5fed2e0574bdce5ef1e
Purva Singh <singhpurva2906@gmail.com>
2023-03-02 10:49:21 -0500

    Time: 209 ms (55.75%), Space: 52.7 MB (65.98%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,72 +1,53 @@
-class UnionFind{
-  private:
-    vector<int> rank;
+class Solution {
+private:
     vector<int> root;
-public:
-    UnionFind(int n): rank(n), root(n) {
-        for(int i=0;i<n;i++) {
-            rank[i]=1;
-            root[i]=i;
-        }
+    vector<int> rank;
+    
+    int find(int x){
+        if(root[x]==x) return x;
+        return root[x]=find(root[x]);
     }
     
-    int find(int x) {
-        if(root[x] == x) return x;
-        return root[x] = find(root[x]);
-    }
-    
-    void disjointUnion(int x, int y) {
+    void merge(int x, int y){
         int rx = find(x);
         int ry = find(y);
         
-        if(rx!=ry) {
-            
-            if(rank[rx] >= rank[ry]) {
-                rank[rx]+=1;
+        if(rx!=ry){
+            if(rank[rx]>=rank[ry]){
                 root[ry]=rx;
+                rank[rx]+=rank[ry];
             } else{
-                rank[ry]+=1;
                 root[rx]=ry;
+                rank[ry]+=rank[rx];
             }
-            
-            
         }
     }
-};
-
-class Solution {
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
         int n = s.length();
-        UnionFind uf(n);
-        
-        for(auto p:pairs){
-            uf.disjointUnion(p[0], p[1]);
+        root.resize(n);
+        rank.resize(n, 1);
+        for(int i=0;i<n;i++){
+            root[i]=i;
         }
         
-        map<int, vector<int>> child;
-        
-        for(int i=0;i<n;i++) {
-            child[uf.find(i)].push_back(i);
+        for(auto x:pairs){
+            merge(x[0], x[1]);
         }
         
-        
-        //string s(n, '');
-        
-        for(auto p:child) {
-            string temp = "";
-            for(auto i:p.second) {
-                temp += s[i];
-            }
-            
-            sort(temp.begin(), temp.end());
-            int j=0;
-            for(auto k:p.second) {
-                s[k] = temp[j++];
-            }
+        unordered_map<int, priority_queue<char, vector<char>, greater<char>>> m;
+        for(int i=0;i<n;i++){
+            m[find(root[i])].push(s[i]);
         }
         
-        return s;
+        string res="";
+        for(int i=0;i<n;i++){
+            int rx = find(root[i]);
+            string temp(1, m[rx].top());
+            m[rx].pop();
+            res+=temp;
+        }
+        return res;
         
     }
 };
