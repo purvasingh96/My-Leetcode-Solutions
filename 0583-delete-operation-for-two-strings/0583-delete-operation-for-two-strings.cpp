c97631e177c73e4b968a5760e6d347dfae995c26
Purva Singh <singhpurva2906@gmail.com>
2023-03-08 11:00:47 -0500

    Time: 31 ms (38.08%), Space: 12.3 MB (42.55%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // return length of LCS
+        int m = word1.length(), n = word2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return (m+n) - 2*dp[m][n];
+    }
+};
