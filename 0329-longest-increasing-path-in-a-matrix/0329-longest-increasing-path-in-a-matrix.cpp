b9eef9e7f1be036025626b26f517ce6fc9b2e150
Purva Singh <singhpurva2906@gmail.com>
2023-03-09 17:57:05 -0500

    Time: 47 ms (76.86%), Space: 16 MB (55.48%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -6,29 +6,33 @@ private:
     bool isValid(int x, int y, vector<vector<int>>& grid){
             return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& memo) {
-        if(memo[i][j]!=0) return memo[i][j];
+    int dfs(int x, int y, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+        if(dp[x][y]!=-1) return dp[x][y];
         
-        int ans=0;
+        int ans=1;
         for(int k=0;k<4;k++){
-            int new_x = i + dx[k], new_y = j + dy[k];
-            if(isValid(new_x, new_y, grid) && grid[new_x][new_y] > grid[i][j]){
-                ans = max(ans, dfs(new_x, new_y, grid, memo));
+            int new_x = x + dx[k];
+            int new_y = y + dy[k];
+            
+            if(isValid(new_x, new_y, matrix) && 
+               matrix[new_x][new_y]<matrix[x][y]){
+               ans = max(ans, 1 + dfs(new_x, new_y, matrix, dp) );
             }
         }
         
-        return memo[i][j] = ans+1;
+        return dp[x][y] = ans;
+        
     }
-    
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> memo(m, vector<int>(n, 0));
-        int ans=0;
-        
+        int m = matrix.size(), n=matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        int ans=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ans = max(ans, dfs(i, j, matrix, memo));
+                if(dp[i][j]==-1){
+                    ans = max(ans, dfs(i, j, matrix, dp));
+                }
             }
         }
         
