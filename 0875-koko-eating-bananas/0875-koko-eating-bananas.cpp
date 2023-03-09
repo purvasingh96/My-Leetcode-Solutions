51c5b52d4877e10bc521a6d0b7773fca435786a4
Purva Singh <singhpurva2906@gmail.com>
2023-03-09 11:00:49 -0500

    Time: 27 ms (99.85%), Space: 19 MB (11.66%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,27 @@
 class Solution {
 private:
-    int isPossible(int mid, int h, vector<int>& piles){
-        long long total = 0;
-        
-        for(int i=0;i<piles.size();i++){
-            
-                total += piles[i]/mid;
-                if(piles[i]%mid!=0) total+=1;
-            
+    bool canEat(vector<int>& piles, int k, int h){
+        long long t=0;
+        for(auto p:piles){
+            t += p/k;
+            if(p%k!=0) t+=1;
         }
         
-        return total <= h;
+        return t<=h;
     }
+        
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right=*max_element(piles.begin(), piles.end());
-        int ans=0;
-        
-        
-        
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(isPossible(mid, h, piles)){
-                ans=mid;
-                right=mid-1;
-            } else left = mid+1;
+        int l=1;
+        int r=*max_element(piles.begin(), piles.end());
+        int ans=-1;
+        while(l<=r){
+            int m = (l + (r-l)/2);
+            if(canEat(piles, m, h)){
+                ans=m;
+                r=m-1;
+            } else l=m+1;
         }
-        
         return ans;
     }
 };
