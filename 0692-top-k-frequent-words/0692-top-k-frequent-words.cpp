60576cf7c0648574d803fa733d86a51c0406b348
Purva Singh <singhpurva2906@gmail.com>
2023-03-09 07:36:47 -0500

    Time: 38 ms (5.44%), Space: 46.7 MB (5.57%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,25 +1,64 @@
+class TrieNode{
+ public:
+    vector<TrieNode*> children;
+    bool isWord;
+    string word;
+    TrieNode(){
+        isWord = false;
+        children.resize(26, NULL);
+        word="";
+    }
+};
 class Solution {
+private:
+    void addWord(TrieNode* root, string s){
+        
+        TrieNode* curr = root;
+        for(int i=0;i<s.length();i++){
+            if(curr->children[s[i]-'a']==NULL){
+                curr->children[s[i]-'a'] = new TrieNode();
+            }
+            curr = curr->children[s[i]-'a'];
+        }
+        curr->isWord=true;
+        curr->word = s;
+    }
+    
+    void getWords(TrieNode* root, vector<string>& res, int& k){
+        if(k==0) return;
+        if(root->isWord) {
+            k--;
+            res.push_back(root->word);
+        }
+        
+        for(int i=0;i<26;i++){
+            if(root->children[i]!=NULL){
+                getWords(root->children[i], res,k);
+            }
+        }
+    }
+    
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map<string, int> m;
-        for(auto x:words) m[x]+=1;
-        vector<string> ans;
-        vector<pair<int, string>> res;
-        
-        priority_queue<pair<int, string>> pq;
+        vector<TrieNode*> res(500);
+        for(int i=0;i<res.size();i++){
+            res[i] = new TrieNode();
+        }
+        unordered_map<string, int> m;
+        int maxCount=0;
+        for(auto x:words){
+            m[x]+=1;
+            maxCount = max(maxCount, m[x]);
+        }
         
         for(auto x:m){
-            pq.push({-x.second, x.first});
-            if(pq.size() > k) pq.pop();
+            addWord(res[x.second], x.first);
         }
         
-        while(!pq.empty()) {
-            res.push_back(pq.top());
-            pq.pop();
+        vector<string> ans;
+        for(int i=maxCount;i>=0;i--){
+            getWords(res[i], ans, k);
         }
-        
-        sort(res.begin(), res.end());
-        for(auto x:res) ans.push_back(x.second);
         
         return ans;
         
