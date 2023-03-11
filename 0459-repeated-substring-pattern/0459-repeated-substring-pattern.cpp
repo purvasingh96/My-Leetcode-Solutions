121e20c0ef7118f6f977a0e509b62081f4eb1813
Purva Singh <singhpurva2906@gmail.com>
2023-03-11 14:06:50 -0500

    Time: 22 ms (57.18%), Space: 13.9 MB (46.51%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,40 +1,37 @@
 class Solution {
 private:
-    vector<int> generateLPS(string s){
-        int n = s.length();
+    vector<int> generateLPS(string& s){
+        int i=1, j=0, n = s.length();
+        cout<<"n: "<<n<<"\n";
         vector<int> lps(n, 0);
-        
-        int j=0, i=1;
-        while(i<n){
+        for(;i<s.length();i++){
             if(s[i] == s[j]){
-                lps[i] = j+1;
-                i+=1;
+                lps[i]=j+1;
                 j+=1;
             } else{
-                if(j>0){
-                    j = lps[j-1];
-                } else{
-                    i+=1;
+                while(j!=0 && s[i]!=s[j]){
+                    j =lps[j-1];
+                }
+                if(s[i]==s[j]){
+                    lps[i]=j+1;
+                    j+=1;
                 }
             }
         }
-        
         return lps;
     }
-    
 public:
     bool repeatedSubstringPattern(string s) {
-        // generate LPS
-        // k = length - lps.back
-        // n%k!=0 => false
-        // check for s.substr(0, k) * n/k  == s => true;
-        
         vector<int> lps = generateLPS(s);
         if(lps.back() == 0) return false;
         int n = s.length();
-        int k = n-lps.back();
+        int len = n-lps.back();
         
-        return n%k==0;
-        
+        string temp = s.substr(0, len);
+        int freq = n/(len);
+        if(n%len!=0) return false;
+        string ans="";
+        while(freq--) ans += temp;
+        return ans == s;
     }
 };
