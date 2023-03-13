dc971f4ad41ecf8020f4d954ea981066af4425ce
Purva Singh <singhpurva2906@gmail.com>
2023-03-13 08:33:48 -0400

    Time: 415 ms (31.65%), Space: 48.4 MB (15.31%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,55 +1,46 @@
 class Solution {
 private:
-    bool canReach(int x1, int x2, int y1, int y2, int r){
-        return pow((x1-x2),2) + pow((y1-y2), 2) <= pow(r,2);
+    int canReach(vector<int>& bomb1, vector<int>& bomb2){
+        int x1 = bomb1[0], y1 = bomb1[1], r1=bomb1[2];
+        int x2 = bomb2[0], y2=bomb2[1], r2 = bomb2[2];
+        
+        return pow((x1-x2), 2) + pow((y1-y2), 2) <= pow(r1, 2);
     }
-    
-    
 public:
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
         vector<vector<int>> m(n);
         
-        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                int x1 = bombs[i][0], y1=bombs[i][1];
-                int x2 = bombs[j][0], y2=bombs[j][1];
-                int r = bombs[i][2];
-                
-                if(canReach(x1, x2, y1, y2, r)){
+                if(canReach(bombs[i], bombs[j])){
                     m[i].push_back(j);
                 }
             }
         }
         
         int ans=0;
-        for(int i=0;i<m.size();i++){
+        for(int i=0;i<n;i++){
             unordered_set<int> detonated;
-            vector<int> bomb = bombs[i];
-            
             if(detonated.count(i)==0){
                 detonated.insert(i);
                 queue<int> q;
                 q.push(i);
                 
                 while(!q.empty()){
-                    int sz = q.size();
-                    while(sz--){
-                        int f = q.front();q.pop();
-                        
-                        for(auto x:m[f]){
-                            if(detonated.count(x)==0){
-                                detonated.insert(x);
-                                q.push(x);
-                            }
+                    auto f= q.front();
+                    q.pop();
+                    for(auto x:m[f]){
+                        if(detonated.count(x)==0){
+                            detonated.insert(x);
+                            q.push(x);
                         }
                     }
                 }
                 
+                ans = max(ans, (int)detonated.size());
+                
             }
-            ans = max(ans, (int)detonated.size());
-            
         }
         
         return ans;
