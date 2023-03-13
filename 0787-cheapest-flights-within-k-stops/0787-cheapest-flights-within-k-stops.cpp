1b31519e508bbbf45201e3457f40ac467836c5a5
Purva Singh <singhpurva2906@gmail.com>
2023-03-13 11:01:18 -0400

    Time: 53 ms (27.25%), Space: 17.5 MB (13.34%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         vector<int> dp(n, INT_MAX);
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto e:flights){
-            adj[e[0]].push_back({e[1], e[2]});
+        unordered_map<int, vector<vector<int>>> adj;
+        for(auto f:flights){
+            int u = f[0], v=f[1], d=f[2];
+            adj[u].push_back({v,d});
         }
         // dist, node, stops
+        
         pq.push({0, src, 0});
         
         while(!pq.empty()){
-            auto f = pq.top(); pq.pop();
-            int dist = f[0], node=f[1], stops=f[2];
+            auto f = pq.top();pq.pop();
+            int dist=f[0], u=f[1], stops=f[2];
             
-            if(dp[node] < stops || stops > k+1) continue;
-            dp[node]=stops;
-            if(node == dst) return dist;
+            if(dp[u] < stops || stops>k+1) continue;
+            dp[u]=stops;
+            if(u == dst) return dist;
             
-            for(auto neigh:adj[node]){
-                int v = neigh.first, price=neigh.second;
-                pq.push({dist+price, v, stops+1});
+            for(auto v:adj[u]){
+                int neigh = v[0], price = v[1];
+                int total = dist+price;
+                pq.push({total, neigh, stops+1});
             }
             
         }
         
         return -1;
+        
     }
 };
