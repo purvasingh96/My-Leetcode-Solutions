426ed0af6df2e20a8a75bf72dfb30a534347d843
Purva Singh <singhpurva2906@gmail.com>
2023-03-14 19:27:33 -0400

    Time: 14 ms (24.41%), Space: 10.5 MB (37.46%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.cpp
@@ -1,29 +1,26 @@
 class Solution {
-private:
     int dfs(int idx, int currWidth, int maxHeightSoFar, int shelfWidth, vector<vector<int>>& books, unordered_map<string, int>& dp){
-        
-        if(currWidth> shelfWidth) return INT_MAX;
-        if(idx >= books.size()) return maxHeightSoFar;
+        if(currWidth> shelfWidth || currWidth> shelfWidth && idx>=books.size()) return INT_MAX;
+        else if(idx >= books.size()) return maxHeightSoFar;
         string t=to_string(idx)+"#"+to_string(currWidth)+"#"+to_string(maxHeightSoFar);
+        // keep on same shelf
         
         if(dp.find(t)!=dp.end()) return dp[t];
         
-        // keep curr book in next shelf
         int nextShelf = maxHeightSoFar + dfs(idx+1, books[idx][0], books[idx][1], shelfWidth, books, dp);
-        
-        // includes curr book in the same shelf
         maxHeightSoFar = max(maxHeightSoFar, books[idx][1]);
+        
         int sameShelf = dfs(idx+1, currWidth+books[idx][0], maxHeightSoFar, shelfWidth, books, dp);
         
         
-        // minimum max height achievable.
-        return dp[t] = min(sameShelf, nextShelf); 
         
+        return dp[t] =  min(sameShelf, nextShelf);
+
     }
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
-        int n = books.size();
         unordered_map<string, int> dp;
+        
         return dfs(1, books[0][0], books[0][1], shelfWidth, books, dp);
     }
 };
