6a48bc746653ea847c6a919a17b5b0e9f99fd347
Purva Singh <singhpurva2906@gmail.com>
2023-03-14 11:12:45 -0400

    Time: 271 ms (81.18%), Space: 84 MB (81.53%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.cpp
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int maxSubArrayLen(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
-        long long prefixSum=0;
+        long long prefixSum =0;
         int ans=0;
-        
+        unordered_map<int, int> m;
         for(int i=0;i<nums.size();i++){
             prefixSum += nums[i];
-            if(prefixSum == k) ans=i+1;
+            if(prefixSum == k) ans = i+1;
             
-            if(m.find(prefixSum - k)!=m.end()){
-                ans = max(ans, (i - m[prefixSum - k]));
+            else {
+                if(m.find(prefixSum - k)!=m.end()){
+                    ans = max(ans, i-m[prefixSum - k]);
+                }
             }
             
-            if(m.find(prefixSum)==m.end()){
+            if(m.find(prefixSum) == m.end()){
                 m[prefixSum] = i;
             }
         }
