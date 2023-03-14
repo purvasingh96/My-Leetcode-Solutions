2be5ecc6550768aac1daeb0f7af66fc5dcdf3dd7
Purva Singh <singhpurva2906@gmail.com>
2023-03-14 12:54:46 -0400

    Time: 116 ms (94.12%), Space: 24 MB (93.35%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,67 +1,48 @@
 class Solution {
 private:
-    vector<vector<int>> hashMap(vector<int>& nums){
-        int n = nums.size();
-        unordered_map<int,int> seen;
-        vector<vector<int>> res;
-        for(int i=0;i<n;i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-            for(int j=i+1;j<n;j++){
-                
-                int complement = -nums[i]-nums[j];
-                
-                if(seen.find(complement)!=seen.end()){
-                    res.push_back({nums[i], complement, nums[j]});
-                    while(j+1<n && nums[j] == nums[j+1]){
-                        j+=1;
-                      }
-                    seen[nums[j]]+=1;
-                }
-                
-            }
-        }
-        }
-        
-        return res;
-        
-    }
-    
-    
-    
-    vector<vector<int>> twoPointers(vector<int>& nums){
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> res;
-        
-        for(int i=0;i<n;i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-                
-                    int low=i+1, high=n-1;
-                    
-                    while(low<high){
-                        int sum = nums[i] + nums[low]+nums[high];
-                        if(sum == 0){
-                            res.push_back({nums[i], nums[low], nums[high]});
-                            low+=1;
-                            high-=1;
-                            while(low<high && nums[low] == nums[low-1]){
-                                low+=1;
-                            }
-                        } else if(sum > 0){
-                            high-=1;
-                        } else {
-                            low+=1;
-                        }
+    void dfs(int n, int left, int right, int target, vector<vector<int>>& res, vector<int>& temp, vector<int>& nums){
+        if(n==2){
+            while(left<right){
+                int sum = nums[left] + nums[right];
+                if(sum == target){
+                    temp.push_back(nums[left]);
+                    temp.push_back(nums[right]);
+                    res.push_back(temp);
+                    temp.pop_back();
+                    temp.pop_back();
+                    left+=1;
+                    right-=1;
+                    while(left<right && nums[left]==nums[left-1]) {
+                        left+=1;
                     }
-                   
+                } else if(sum > target) {
+                    right-=1;
+                } else {
+                    left+=1;
+                }
             }
         }
         
-        return res;
+        
+        while(left<right){
+            temp.push_back(nums[left]);
+            dfs(n-1, left+1, right, target-nums[left], res, temp, nums);
+            temp.pop_back();
+            while(left<right && nums[left] == nums[left+1]){
+                left+=1;
+            }
+            left+=1;
+        }
+        
+        
     }
-    
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        return twoPointers(nums);
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        vector<vector<int>> res;
+        int n= nums.size();
+        dfs(3, 0, n-1, 0, res, temp, nums);
+        return res;
     }
 };
