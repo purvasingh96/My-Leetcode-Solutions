875f4da0acf0e74e66ef3d6b31ee4d9d09cceb81
Purva Singh <singhpurva2906@gmail.com>
2023-03-14 16:57:41 -0400

    Time: 131 ms (86.56%), Space: 24.1 MB (62.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,39 +1,41 @@
 class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        kSum(nums, 0, 3);
-        return ans;
-    }
-    void kSum(vector<int>& nums, int target, int k) {
-        vector<int> tmp;
-        dfs(nums, 0, (int)nums.size() - 1, k, target, tmp);
-    }
-    void dfs(vector<int>& nums, int l, int r, int k, int target, vector<int>& path) {
-        if (k == 2) {
-            while (l < r) {
-                if (nums[l] + nums[r] == target) {
-                    path.push_back(nums[l]);
-                    path.push_back(nums[r]);
-                    ans.push_back(path);
-                    path.pop_back();
-                    path.pop_back();
-                    while (l+1 < r && nums[l] == nums[l+1]) ++l; // Skip duplicate nums[l]
-                    ++l; --r;
-                } else if (nums[l] + nums[r] > target) {
-                    --r; // Decrease sum
-                } else {
-                    ++l; // Increase sum
-                }
+private:
+    void dfs(int k, int target, int l, int r, vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums){
+        if(k == 2){
+            while(l<r){
+                int sum = nums[l] + nums[r];
+                if(sum == target){
+                    temp.push_back(nums[l]);
+                    temp.push_back(nums[r]);
+                    ans.push_back(temp);
+                    temp.pop_back();
+                    temp.pop_back();
+                    while(l+1<r && nums[l]==nums[l+1]) {
+                        l+=1;
+                    }
+                    l+=1;
+                    r-=1;
+                } else if(sum > target) r-=1;
+                else l+=1;
             }
         }
-        while (l < r) {
-            path.push_back(nums[l]);
-            dfs(nums, l+1, r, k-1, target-nums[l], path);
-            path.pop_back();
-            while (l+1 < r && nums[l] == nums[l+1]) ++l; // Skip duplicate nums[l]
-            ++l;
+        
+        while(l<r){
+            temp.push_back(nums[l]);
+            dfs(k-1, target-nums[l], l+1, r, temp, ans, nums);
+            temp.pop_back();
+            while(l+1<r && nums[l] == nums[l+1]) l+=1;
+            l+=1;
         }
+        
+    }
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        vector<vector<int>> res;
+        int n = nums.size();
+        dfs(3, 0, 0, n-1, temp, res, nums);
+        return res;
     }
 };
