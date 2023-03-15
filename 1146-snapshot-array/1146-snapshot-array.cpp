47998c2a33fcf0072b8119a915a52ea0aaf4bc6a
Purva Singh <singhpurva2906@gmail.com>
2023-03-14 20:44:59 -0400

    Time: 350 ms (38.57%), Space: 138.4 MB (28.89%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,31 +1,32 @@
 class SnapshotArray {
+vector<vector<pair<int,int>>> m;
+int s;
 public:
-    // idx, snapid, val
-    vector<vector<pair<int, int>>> m;
-    int snapId;
-   
     SnapshotArray(int length) {
         m.resize(length);
-        snapId=0;
+        s=0;
     }
     
     void set(int index, int val) {
-        if(!m[index].empty() && m[index].back().first==snapId){
+        if(!m[index].empty() && m[index].back().first == s){
             m[index].pop_back();
-        } 
-            m[index].push_back({snapId, val});
-        
+        }
+        m[index].push_back({s, val});
     }
     
     int snap() {
-        snapId+=1;
-        return snapId-1;
+        s+=1;
+        return s-1;
     }
     
     int get(int index, int snap_id) {
-        auto it = upper_bound(m[index].begin(), m[index].end(), pair<int, int>(snap_id, 0), [](const pair<int, int>& a, const pair<int, int>& b){
+        // auto it = upper_bound(m[index].begin(), m[index].end(), pair<int, int>(snap_id, 0), [](const pair<int, int>& a, const pair<int, int>& b){
+        //     return a.first < b.first;
+        // });
+        auto it = upper_bound(m[index].begin(), m[index].end(), pair<int, int>(snap_id, INT_MAX), [](const pair<int, int>& a, const pair<int, int>& b){
             return a.first < b.first;
         });
+        
         if(it==m[index].begin()) return 0;
         return prev(it)->second;
     }
