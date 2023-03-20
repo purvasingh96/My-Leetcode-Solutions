7e1f6712da1833c78ea563aad4eecddbad8f79ff
Purva Singh <singhpurva2906@gmail.com>
2023-03-20 15:41:50 -0400

    Time: 23 ms (31.53%), Space: 22.1 MB (34.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,36 +1,21 @@
 class Solution {
-private:
-    
 public:
     bool canPlaceFlowers(vector<int>& nums, int n) {
-        if(nums.size()==1) {
-            if((nums[0]==0 && n==1) || n==0) return true;
-            else return false;
-        }
-        bool prev=(nums[0] == 1);
-        int i= (prev?2:0);
-        int s = nums.size();
+        vector<int> temp = {0};
+        temp.insert(temp.end(), nums.begin(), nums.end());
+        temp.push_back(0);
+        nums = temp;
         
-        while(i<s){
-            if(n==0) return true;
-            if(nums[i]==0){
-                if(i+1<s && nums[i+1]==0) {
-                    nums[i]=1;
-                    n-=1;
-                    i += 2;
-                } else {
-                    if(i+1<s && nums[i+1]==1){
-                        i+=3;
-                    } else if(i+1>=s && nums[i-1]==0){
-                        nums[i]=1;
-                        i+=2;
-                        n-=1;
-                    } else i+=1;
-                }
-            } else {
-                i+=2;
+        
+        for(int i=1;i<nums.size()-1;i++){
+            if(nums[i-1] == nums[i] && nums[i] == nums[i+1] && nums[i+1]==0){
+                n-=1;
+                nums[i]=1;
             }
+            if(n<=0) return true;
         }
-        return n==0;
+        
+        return n<=0;
+        
     }
 };
