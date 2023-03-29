18c02001dfa72fa17b9b1fe2183307ce5f62f576
Purva Singh <singhpurva2906@gmail.com>
2023-03-29 10:14:06 -0400

    Time: 10 ms (36.32%), Space: 10 MB (69.86%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,49 +1,15 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        vector<int> q;
-        bool c=false;
-        int v1, v2, index;
-        
-        for(int i=0;i<nums.size();i++){
-            int x = nums[i];
-            if(q.empty() || q.back()<x){
-                q.push_back(x);
-            } else {
-                if(c) return false;
-                v1=q.back();
-                q.pop_back();
-                v2 = x;
-                index=i;
-                c=true;
-                
+        int cnt=0;
+        for(int i=1;i<nums.size() && cnt<2;i++){
+            if(nums[i] <= nums[i-1]){
+                cnt+=1;
+                if(i>=2 && nums[i-2] >= nums[i]){
+                    nums[i] = nums[i-1];
+                }
             }
         }
-        
-        if(!c) return true;
-        
-        int idx1 = upper_bound(q.begin(), q.end(), v1) - q.begin();
-        
-        int idx2 = upper_bound(q.begin(), q.end(), v2) - q.begin();
-        
-        if(idx1==index-1){
-            if(idx1>=1) {
-                if( q[idx1-1]!=v1) return true;
-                else return false;
-            }
-            return true;
-            
-        }
-        
-        if(idx2==index-1){
-            if(idx2>=1) {
-                if(q[idx2-1]!=v2) return true;
-                else return false;
-            }
-            return true;
-            
-        }
-        
-        return false;
+        return cnt<2;
     }
 };
