f3a1beb1a2a15d3a23138b649b8885bade8cba58
Purva Singh <singhpurva2906@gmail.com>
2023-04-03 09:02:50 -0400

    Time: 239 ms (80.22%), Space: 99.8 MB (51.46%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,45 +1,25 @@
+#define ll long long
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         sort(potions.begin(), potions.end());
-        int m = potions.size();
-        int n = spells.size();
-        vector<int> ans;
+        vector<int> res;
+        int n = potions.size();
         
-        for(int i=0;i<n;i++){
-            
-            int left=0, right=m-1;
-            
-            
-            while(left<right){
-                
-                int mid = left + (right-left)/2;
-                
-                auto prod = (long)spells[i]* (long)potions[mid];
-                
-                if( prod >= success){
-                    right=mid;
-                }
-                
-                else if(prod < success){
-                    
-                    left = mid+1;
-                    
-                }
-                
+        for(auto spell:spells){
+            ll target = ceil((double)success/spell);
+            int index = lower_bound(potions.begin(), potions.end(), target) - potions.begin();
+            if(index == n) res.push_back(0);
+        
+            else if(index ==0){
+                long long prod = (ll)spell * (ll)potions[0];
+                if(prod < success) res.push_back(0);
+                else res.push_back(n);
+            } else {
+                res.push_back(n-index);
             }
-            
-            auto prod = (long)spells[i]* (long)potions[left];
-            if(prod < success) {
-                ans.push_back(0);
-            } else{
-                int len = (m-left);
-                ans.push_back(len);
-            }
-            
         }
         
-        return ans;
-        
+        return res;
     }
 };
