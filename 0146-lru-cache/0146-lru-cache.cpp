69555c9309a91fe629781262ade9bddbe0d07959
Purva Singh <singhpurva2906@gmail.com>
2023-04-07 00:36:24 -0400

    Time: 487 ms (48.91%), Space: 173.7 MB (53.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,77 +1,80 @@
-class CustomNode{
-public:
-    CustomNode* next;
-    CustomNode* prev;
-    int val, key;
-    CustomNode(int key, int val) {
-        this->val = val;
-        this->key = key;
-        this->next = NULL;
-        this->prev = NULL;
+class Node{
+ public:
+    Node* next;
+    Node* prev;
+    int key;
+    int val;
+    Node(int key, int val){
+        this->key=key;
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
     }
 };
 
 class LRUCache {
 private:
-    int n, cap;
-    CustomNode* head;
-    CustomNode* tail;
-    map<int, CustomNode*> m;
+    Node* h;
+    Node* t;
+    int c;
+    int capacity;
+    unordered_map<int, Node*> m;
 public:
     LRUCache(int capacity) {
-        this->cap = 0;
-        this->n = capacity;    
-        this->head = new CustomNode(0, 0);
-        this->tail = new CustomNode(0, 0);
-        this->tail->next = head;
-        this->head->prev = tail;
+        c=0;
+        this->capacity = capacity;
+        h = new Node(-1, -1);
+        t = new Node(-1, -1);
+        h->next=t;
+        t->prev=h;
     }
     
-    void remove(CustomNode* node){
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
+    void removeNode(Node* node){
+        Node* prev=node->prev;
+        prev->next=node->next;
+        node->next->prev=prev;
         m.erase(node->key);
+        c-=1;
     }
     
-    void addFront(CustomNode* node){
-        CustomNode* temp = head->prev;
-        temp->next = node;
-        node->prev = temp;
-        node->next = head;
-        head->prev = node;
+    void insertNodeInFront(Node* node){
+        Node* prev = t->prev;
+        prev->next=node;
+        node->prev=prev;
+        node->next=t;
+        t->prev=node;
         m[node->key] = node;
+        c+=1;
     }
     
-
     int get(int key) {
-        // for(auto x:m){
-        //     cout<<x.first<<"\n";
-        // }
-        //cout<<"\n====\n";
-        if(m.find(key) == m.end()) return -1;
-        CustomNode* k = m[key];
-        
-        remove(k);
-        addFront(k);
-        return m[key]->val;
+        //cout<<"getting: "<<key<<"\n";
+        if(m.find(key)==m.end()) return -1;
+        Node* node = m[key];
+        removeNode(node);
+        insertNodeInFront(node);
+        return node->val;
     }
     
     void put(int key, int value) {
         if(m.find(key)!=m.end()){
-            CustomNode* temp = m[key];
-            remove(temp);
-            temp->val = value;
-            addFront(temp);
-        } else{
-           if(m.size() == n){
-                remove(tail->next);
-            }
-
-            CustomNode* node = new CustomNode(key, value);
-            addFront(node); 
+            Node* node = m[key];
+            removeNode(node);
+            node->val = value;
+            insertNodeInFront(node);
+        } else {
+            //cout<<"cap before: "<<c<<" "<<capacity<<"\n";
+            if(c==capacity) removeNode(h->next);
+            //cout<<"cap after: "<<c<<"\n";
+            Node* node = new Node(key, value);
+            //cout<<"inserting: "<<node->key<<"\n";
+            
+            insertNodeInFront(node);
+            //cout<<"h->next: "<<h->next->key<<"\n";
+            //cout<<"t->prev: "<<t->prev->key<<"\n";
+            
+            m[key] = node;
         }
-        
-        
     }
 };
 
