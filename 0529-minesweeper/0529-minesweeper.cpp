33c7a8d923b7e927996434e735130afbe85208da
Purva Singh <singhpurva2906@gmail.com>
2023-04-06 20:46:07 -0400

    Time: 23 ms (44.53%), Space: 12.1 MB (48.75%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.cpp
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -1,64 +1,58 @@
 class Solution {
 private:
+    
     int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
     int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-    
+
     bool isValid(int x, int y, vector<vector<char>>& grid){
-        return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
+            return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    
-    int adjMines(int x, int y, vector<vector<char>>& grid){
+    char returnMines(int x,int y, vector<vector<char>>& board){
         int mines=0;
         for(int i=0;i<8;i++){
-            int new_x = x + dx[i];
-            int new_y = y + dy[i];
-            if(isValid(new_x, new_y, grid) && grid[new_x][new_y]=='M'){
+            int newX = x+dx[i];
+            int newY = y+dy[i];
+            if(isValid(newX, newY, board) && board[newX][newY]=='M'){
                 mines+=1;
             }
         }
-        
-        return mines;
+        return mines+'0';
     }
     
-    void bfs(vector<vector<char>>& board, int x, int y){
+    void dfs(int x, int y, vector<vector<char>>& board, vector<vector<bool>>& visited){
         
-        queue<pair<int, int>> q;
-        q.push({x,y});
-        
-        while(!q.empty()){
-                auto f = q.front();q.pop();
-                int x = f.first, y = f.second;
-                if(board[x][y]!='E') continue;
-                
-                int mines = adjMines(x, y, board);
-                if(mines!=0){
-                    board[x][y]=(mines+'0');
-                } else{
-                    board[x][y]='B';
-                    
-                    for(int i=0;i<8;i++){
-                        int new_x = x+dx[i];
-                        int new_y = y+dy[i];
-                        if(isValid(new_x, new_y, board)){
-                            q.push({new_x, new_y});
-                        }
+        if(board[x][y] == 'E'){
+            char mines = returnMines(x, y, board);
+            //cout<<"x: "<<x<<" y: "<<y<<"mines: "<<mines<<"\n";
+            if(mines=='0'){
+                board[x][y]='B';
+                // recrusve reveal  
+                for(int i=0;i<8;i++){
+                    int newX = x+dx[i];
+                    int newY = y+dy[i];
+                    if(isValid(newX, newY, board) && !visited[newX][newY] && board[newX][newY]!='B'){
+                        visited[newX][newY]=true;
+                        dfs(newX, newY, board, visited);
                     }
                 }
+            } else {
+               board[x][y] = mines; 
+            }
             
-        }
-        
-        
+        } 
     }
+    
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
-        int x = click[0], y = click[1];
-        if(board[x][y] == 'M'){
-            board[x][y] = 'X';
+        int x =click[0], y = click[1];
+        if(board[x][y]=='M'){
+            board[x][y]='X';
             return board;
         }
-        
-        //if(board[x][y]!='E') return board;
-        bfs(board, x, y);
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        visited[x][y]=true;
+        dfs(x, y, board, visited);
         return board;
     }
 };
