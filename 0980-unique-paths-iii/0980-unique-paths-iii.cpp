5fadd5c940bb5a029cbea6226d33010bca735868
Purva Singh <singhpurva2906@gmail.com>
2023-04-06 21:31:47 -0400

    Time: 4 ms (55.46%), Space: 7.1 MB (33.10%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,64 +1,53 @@
 class Solution {
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
+private:
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
 
-bool isValid(int x, int y, vector<vector<int>>& grid){
-        return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
-}
-    
-void backtrack(int x, int y, int& total, vector<vector<int>>& grid, vector<vector<bool>>& visited, int& ans, int nonObstacleSquares, int ex, int ey){
-    
-    if(x==ex && y==ey){
-        if(total == nonObstacleSquares){
+    bool isValid(int x, int y, vector<vector<int>>& grid){
+            return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
+    }
+    void backtrack(int x, int y, int& target, int tx, int ty, vector<vector<int>>& grid, vector<vector<bool>>& visited, int& ans){
+        if(x==tx && y==ty && target==-1){
             ans+=1;
+            return;
         }
-        return;
-    }
-    
-    visited[x][y]=true;
-    for(int i=0;i<4;i++){
-        int new_x = x + dx[i];
-        int new_y = y + dy[i];
         
-        if(isValid(new_x, new_y, grid) && !visited[new_x][new_y] && grid[new_x][new_y]!=-1){
-            total+=1;
-            backtrack(new_x, new_y, total, grid, visited, ans, nonObstacleSquares, ex, ey);
-            total-=1;
+        for(int i=0;i<4;i++){
+            int newx = x+dx[i];
+            int newy = y+dy[i];
+            
+            if(isValid(newx, newy, grid) && !visited[newx][newy] && grid[newx][newy]!=-1){
+                visited[newx][newy]=true;
+                target-=1;
+                backtrack(newx, newy, target, tx, ty, grid, visited, ans);
+                target+=1;
+                visited[newx][newy]=false;
+            }
         }
     }
-    visited[x][y]=false;
-}    
-    
-    
 public:
     int uniquePathsIII(vector<vector<int>>& grid) {
-        int nonObstacleSquares = 0;
-        int sx, sy, ex, ey;
-        int m = grid.size(), n = grid[0].size();
+        int target=0;
+        int sx, sy, tx, ty;
+        int m =grid.size(), n = grid[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    nonObstacleSquares+=1;
                     sx=i;
                     sy=j;
                 } else if(grid[i][j]==2){
-                    nonObstacleSquares+=1;
-                    ex=i;
-                    ey=j;
-                } else if(grid[i][j] == 0){
-                    nonObstacleSquares+=1;
+                    tx=i;
+                    ty=j;
+                } else if(grid[i][j]==0){
+                    target+=1;
                 }
             }
         }
-        
-        int ans=0, total=1;
-        
-        backtrack(sx, sy, total, grid, visited, ans, nonObstacleSquares, ex, ey);
-        
-        
+        visited[sx][sy]=true;
+        int ans=0;
+        backtrack(sx, sy, target, tx, ty, grid, visited, ans);
         return ans;
-        
     }
 };
