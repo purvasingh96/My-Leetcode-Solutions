005d84ec4b2efaa7b31e316a6a163378a688c99b
Purva Singh <singhpurva2906@gmail.com>
2023-04-08 19:30:25 -0400

    Time: 9 ms (42.89%), Space: 8.3 MB (51.51%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -1,36 +1,39 @@
 class Solution {
 private:
-    vector<vector<pair<int, int>>> flatten(vector<vector<int>>& mat){
-        int m = mat.size(), n=mat[0].size();
-        vector<vector<pair<int, int>>> res(m, vector<pair<int, int>>(n));
-        
+    unordered_map<int, vector<int>> flatten(vector<vector<int>>& mat){
+        unordered_map<int, vector<int>> m;
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
                 if(mat[i][j]!=0){
-                    res[i].push_back({mat[i][j], j});
+                    m[i].push_back(j);
                 }
             }
         }
-        return res;
+        return m;
     }
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
+        
         auto a = flatten(mat1);
         auto b = flatten(mat2);
-        int m = mat1.size(), n=mat1[0].size(), k=mat2[0].size();
+        int m = mat1.size(), n = mat1[0].size();
+        int k = mat2[0].size();
         vector<vector<int>> res(m, vector<int>(k, 0));
         
-        for(int row=0;row<a.size();row++){
-            for(auto r:a[row]){
-                int valA = r.first, col_ = r.second;
-                for(auto bc:b[col_]){
-                    int valB = bc.first, col = bc.second;
-                    res[row][col] += valA*valB; 
+        for(auto it: a){
+            int row1 = it.first;
+            for(auto col1:it.second){
+                for(auto it2:b){
+                    int row2 = it2.first;
+                    for(auto col2:it2.second){
+                        if(col1 == row2){
+                            res[row1][col2] += mat1[row1][col1] * mat2[row2][col2];
+                        }
+                    }
                 }
             }
         }
         
         return res;
-        
     }
 };
