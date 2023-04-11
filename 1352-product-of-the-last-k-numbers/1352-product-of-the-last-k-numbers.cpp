04b997f83108a5fb123bdb6d629aa80cb7c925b5
Purva Singh <singhpurva2906@gmail.com>
2023-04-10 23:53:01 -0400

    Time: 171 ms (46.85%), Space: 71.2 MB (7.88%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -12,11 +12,9 @@ public:
     }
     
     void add(int num) {
-        //res.push_back(num);
         if(num==0) {
             zeroPositions.push_back(n);
             prod.push_back(1);
-            //else prod.push_back(prod.back());
         }
         else {
             if(prod.size()==0) prod.push_back(num);
@@ -31,21 +29,15 @@ public:
     }
     
     int getProduct(int k) {
-        //cout<<"get Prod of last "<<k<<" digits: ";
-        //for(auto x: res) cout<<x<<" ";
-        //cout<<"\n";
         int target = n-k;
         int sz = zeroPositions.size();
         int idx = lower_bound(zeroPositions.begin(), zeroPositions.end(), target) - zeroPositions.begin();
         
-        if(idx < sz) return 0;
+        if(sz>0 && n-k <= zeroPositions.back()) return 0;
         if(k>=n) {
-            //cout<<"prod is: "<<prod.back()<<"\n";
             return prod.back();
         }
-        //cout<<"prod is: "<<prod.back()/prod[n-k-1]<<"\n";
         return prod.back()/prod[n-k-1];
-        //return k>=n?prod.back()%mod:(prod.back()%mod/prod[n-k-1]%mod)%mod;
     }
 };
 
