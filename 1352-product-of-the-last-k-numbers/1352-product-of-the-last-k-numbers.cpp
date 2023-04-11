3c76e2bf3b199616402aed18dff0d0c9e43c7776
Purva Singh <singhpurva2906@gmail.com>
2023-04-10 23:56:40 -0400

    Time: 165 ms (59.60%), Space: 71.2 MB (7.88%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -2,9 +2,8 @@ class ProductOfNumbers {
 private:
     vector<long long> prod;
     int n;
-    vector<int> zeroPositions;
-    int mod = 1e9+7;
-    vector<int> res;
+   int lastZeroPos;
+   int mod=1e9+7;
 
 public:
     ProductOfNumbers() {
@@ -12,11 +11,9 @@ public:
     }
     
     void add(int num) {
-        //res.push_back(num);
         if(num==0) {
-            zeroPositions.push_back(n);
+            lastZeroPos=n;
             prod.push_back(1);
-            //else prod.push_back(prod.back());
         }
         else {
             if(prod.size()==0) prod.push_back(num);
@@ -31,21 +28,13 @@ public:
     }
     
     int getProduct(int k) {
-        //cout<<"get Prod of last "<<k<<" digits: ";
-        //for(auto x: res) cout<<x<<" ";
-        //cout<<"\n";
-        int target = n-k;
-        int sz = zeroPositions.size();
-        int idx = lower_bound(zeroPositions.begin(), zeroPositions.end(), target) - zeroPositions.begin();
         
-        if(idx < sz) return 0;
+        if(n-k <= lastZeroPos) return 0;
+        
         if(k>=n) {
-            //cout<<"prod is: "<<prod.back()<<"\n";
             return prod.back();
         }
-        //cout<<"prod is: "<<prod.back()/prod[n-k-1]<<"\n";
         return prod.back()/prod[n-k-1];
-        //return k>=n?prod.back()%mod:(prod.back()%mod/prod[n-k-1]%mod)%mod;
     }
 };
 
