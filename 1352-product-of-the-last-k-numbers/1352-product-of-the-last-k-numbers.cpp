0658c78b0cd34e3f9c00147db6aa5343615b1860
Purva Singh <singhpurva2906@gmail.com>
2023-04-11 00:16:08 -0400

    Time: 155 ms (86.39%), Space: 70.9 MB (11.32%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.cpp
@@ -1,39 +1,31 @@
 class ProductOfNumbers {
 private:
+    int lastZeroPos;
     vector<long long> prod;
     int n;
-   int lastZeroPos;
-   int mod=1e9+7;
-
+    int mod=1e9+7;
 public:
     ProductOfNumbers() {
         n=0;
     }
     
     void add(int num) {
-        if(num==0) {
+        if(num == 0) {
             lastZeroPos=n;
             prod.push_back(1);
-        }
-        else {
+        } else {
             if(prod.size()==0) prod.push_back(num);
             else {
-                long long p = ((num)*(prod.back()))%mod;
-                prod.push_back(p);
+                long long p = prod.back()*num;
+                prod.push_back(p%mod);
             }
-            
         }
-        
         n+=1;
     }
     
     int getProduct(int k) {
-        
         if(n-k <= lastZeroPos) return 0;
-        
-        if(k>=n) {
-            return prod.back();
-        }
+        if(k>=n) return prod.back();
         return prod.back()/prod[n-k-1];
     }
 };
