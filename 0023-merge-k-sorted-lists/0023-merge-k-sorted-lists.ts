819cb4b0daf5f53a8dcf4bb406d54ba50262eb7a
Purva Singh <singhpurva2906@gmail.com>
2023-04-11 19:53:40 -0400

    Time: 136 ms (48.89%), Space: 51.9 MB (9.62%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -9,66 +9,112 @@
  *     }
  * }
  */
+
 type Comparator<T> = (a:T, b:T) => number;
 
-class PQ<T>{
-    private queue: T[] = [];
+// O(logN) based implementation
+class PQ<T> {
+    private pq: T[] = [];
     private comp: Comparator<T>;
-    constructor(comp:Comparator<T>){
+    constructor(comp: Comparator<T>){
         this.comp = comp;
-    }
-    
-    enqueue(x:T): void {
-        let insert: boolean = false;
-        
-        for(let i=0;i<this.queue.length;i++){
-            if(this.comp(x, this.queue[i]) < 0){
-                this.queue.splice(i, 0, x);
-                insert=true;
-                break;
-            }
-        }
-        
-        if(!insert){
-            this.queue.push(x);
-        }
-    }
-    
-    top() : T | undefined {
-        return this.queue[0];
-    }
-
-    pop() : T | undefined {
-        return this.queue.shift();
     }
 
     size(): number {
-        return this.queue.length;
+        return this.pq.length;
     }
     
-    isEmpty(): boolean {
-        return this.size()==0;
+    parent(i: number): number {
+        return Math.floor((i - 1)/2);
+    }
+    
+    left(i: number) : number {
+        return 2*i+1;
+    }
+    
+    right(i:number) : number {
+        return 2*i+2;
+    }    
+    
+    hasLeft(i: number) : boolean {
+        return this.left(i) < this.pq.length;
+    }
+    
+    hasRight(i:number) : boolean {
+        return this.right(i) < this.pq.length;
+    }
+
+    top() : T | undefined {
+        return this.pq[0];
+    }
+
+   
+
+    heapUp(i: number): void {
+        while(i>0){
+            let p: number = this.parent(i);
+            if(this.comp(this.pq[i], this.pq[p]) < 0){
+                [this.pq[i], this.pq[p]] = [this.pq[p], this.pq[i]];
+                i=p;
+            } else break;
+        }
+    }
+
+    heapDown(i: number): void {
+        while(true){
+            let leftChild: number = this.left(i);
+            let rightChild: number = this.right(i);
+            let minIndex = i;
+            
+            if(this.hasLeft(i) && this.comp(this.pq[leftChild], this.pq[minIndex])<0){
+                minIndex = leftChild;
+            } 
+            
+            if(this.hasRight(i) && this.comp(this.pq[rightChild], this.pq[minIndex])<0){
+                minIndex = rightChild;
+            }
+            
+            if(minIndex!=i){
+                [this.pq[minIndex], this.pq[i]] = [this.pq[i], this.pq[minIndex]];
+                i = minIndex;
+            } else break;
+        }
+    }
+
+    enqueue(val: T) : void {
+        this.pq.push(val);
+        this.heapUp(this.pq.length-1);
+    }
+    
+    dequeue() : T | undefined {
+        if(this.pq.length==0) return undefined;
+        const root = this.pq[0];
+        const last = this.pq.pop();
+        if(this.pq.length > 0){
+            this.pq[0] = last;
+            this.heapDown(0);
+        }
+        
+        return root;
     }
 }
 
-
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
     let pq = new PQ<ListNode>((a, b) => (a.val - b.val));
-    
     for(let node of lists){
-        if(node) pq.enqueue(node);
+       if(node) pq.enqueue(node);
     }
     
     let ans = new ListNode(0);
     let res = ans;
     
-    while(!pq.isEmpty()){
-        let f = pq.top(); pq.pop();
+    while(pq.size()!=0){
+        let f = pq.top();
+        pq.dequeue();
         ans.next = f;
-        ans = ans.next;
+        ans=ans.next;
         if(f.next) pq.enqueue(f.next);
     }
     
     return res.next;
-        
 };
