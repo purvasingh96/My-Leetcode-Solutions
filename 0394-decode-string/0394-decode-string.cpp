d4970ee14f3f94324d441801770283c71b03f4e4
Purva Singh <singhpurva2906@gmail.com>
2023-04-11 22:54:33 -0400

    Time: 0 ms (100.00%), Space: 6.6 MB (37.02%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,34 +1,46 @@
 class Solution {
 private:
-    string dfs(int& i, string& s, int freq){
-        
-        string d="";
-        string ans="";
-        
-        for(;i<s.length() && s[i]!=']';i++){
-            if(isdigit(s[i])){
-                d.push_back(s[i]);
-            } else{
-                if(s[i] == '['){
-                    int count = stoi(d);
-                    d="";
-                    i+=1;
-                    ans += dfs(i, s, count);
-                } else{
-                    ans.push_back(s[i]);
-                }
+    string dfs(int& idx, int t, int prev, string& temp, string soFar, string s){
+        //cout<<"idx: "<<idx<<" t: "<<t<<" prev: "<<prev<<" ";
+        while(idx<s.length() && s[idx]!=']'){
+            if(isdigit(s[idx])){
+                //temp="";
+                t = t*10 + s[idx]-'0';
+                idx+=1;
+            } else if(isalpha(s[idx])){
+                t=0;
+                temp.push_back(s[idx]);
+                idx+=1;
+            } else if(s[idx] == '['){
+                idx+=1;
+                soFar = temp;
+                temp = "";
+                string val = dfs(idx, 0, t, temp, soFar, s);
+                t=0;
+                temp = soFar + val;
+                //cout<<"idx: "<<idx<<" sofar: "<<soFar<<" val: "<<val<<"\n";
+                idx+=1;
             }
+            //cout<<"idx: "<<idx<<" temp: "<<temp<<"\n";
         }
         
-        string res="";
+        int tt = t==0?1:t;
+        int tp = prev==0?1:prev;
+        int freq = tt*tp;
+        
+        string ans="";
         while(freq--){
-            res+=ans;
+            ans += temp;
         }
-        return res;
+        
+        return ans;
     }
 public:
     string decodeString(string s) {
-        int i=0;
-        return dfs(i, s, 1);
+        s = "["+s+"]";
+        int idx=0;
+        string temp="";
+        string sofar="";
+        return dfs(idx, 0, 0, temp, sofar, s);
     }
 };
