e1834e3f69efa8f5d8363f5db9cc567173398422
Purva Singh <singhpurva2906@gmail.com>
2023-04-12 01:14:45 -0400

    Time: 97 ms (35.89%), Space: 40.7 MB (14.33%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -1,18 +1,21 @@
 class Solution {
+private:
+    vector<int> ps;
+    vector<int> w;
 public:
-    vector<int> res;
-    
     Solution(vector<int>& w) {
-        res.push_back(w[0]);
+        ps.push_back(w[0]);
         for(int i=1;i<w.size();i++){
-            res.push_back(w[i]+res.back());
+            ps.push_back(ps.back() + w[i]);
         }
+        this->w=w;
     }
     
     int pickIndex() {
-        //for(auto x:res) cout<<x<<" ";
-        int idx = rand()%(res.back());
-        return upper_bound(res.begin(), res.end(), idx) - res.begin();
+        int idx = rand()%(ps.back());
+        auto it = upper_bound(ps.begin(), ps.end(), idx) - ps.begin();
+        if(it == ps.size()) return ps.size()-1;
+        return it;
     }
 };
 
