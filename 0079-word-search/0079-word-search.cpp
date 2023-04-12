83746add64e6c88afe821b09c416bd31d05a79ab
Purva Singh <singhpurva2906@gmail.com>
2023-04-12 00:43:55 -0400

    Time: 711 ms (53.08%), Space: 8.1 MB (24.33%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,41 +2,40 @@ class Solution {
 private:
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
-
     bool isValid(int x, int y, vector<vector<char>>& grid){
             return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    bool backtrack(int i, int x, int y, string& word, vector<vector<char>>& board) {
-        if(i==word.length()) return true;
+    
+    bool dfs(int x, int y, int idx, vector<vector<char>>& board, string word){
+        if(idx >= word.length()) return true;
+        
         bool ans=false;
         
-        for(int k=0;k<4;k++){
-            int new_x = x + dx[k];
-            int new_y = y + dy[k];
-            
-            if(isValid(new_x, new_y, board) &&
-               board[new_x][new_y] == word[i]){
-                char t = board[new_x][new_y];
-                board[new_x][new_y] = '.';
-                ans = ans | backtrack(i+1, new_x, new_y, word, board);
-                board[new_x][new_y] = t;
+        for(int i=0;i<4;i++){
+            int new_x = x + dx[i];
+            int new_y =  y + dy[i];
+            if(isValid(new_x, new_y, board) && board[new_x][new_y]!='#' && board[new_x][new_y]==word[idx]){
+                char temp = board[new_x][new_y];
+                board[new_x][new_y] = '#';
+                bool val = dfs(new_x, new_y, idx+1, board, word);
+                //cout<<"val: "<<val<<"\n";
+                ans = ans or val;
+                board[new_x][new_y] = temp;
             }
         }
         
         return ans;
     }
-    
 public:
     bool exist(vector<vector<char>>& board, string word) {
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j] == word[0]){
-                    char t = board[i][j];
-                    board[i][j]='.';
-                    if(backtrack(1, i, j, word, board)) {
-                        return true;
-                    }
-                    board[i][j] = t;
+                    //cout<<"here";
+                    char temp = board[i][j];
+                    board[i][j] = '#';
+                    if(dfs(i, j, 1, board, word)) return true;
+                    board[i][j] = temp;
                 }
             }
         }
