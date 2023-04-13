6aee18427c77475abe1e29c434fefcf21247f89b
Purva Singh <singhpurva2906@gmail.com>
2023-04-13 00:08:55 -0400

    Time: 38 ms (64.23%), Space: 24.1 MB (16.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,13 +1,27 @@
 class Solution {
+private:
+    string strSort(string& s){
+        vector<int> count(26, 0);
+        for(auto c:s){
+            count[c-'a']+=1;
+        }
+        
+        string res="";
+        for(int i=0;i<26;i++){
+            if(count[i]>0){
+                string t(count[i], i+'a');
+                res+=t;
+            }
+        }
+        
+        return res;
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<vector<int>, vector<string>> m;
+        unordered_map<string, vector<string>> m;
         for(auto s:strs){
-            vector<int> count(26, 0);
-            for(auto c:s){
-                count[c-'a']+=1;
-            }
-            m[count].push_back(s);
+            
+            m[strSort(s)].push_back(s);
         }
         vector<vector<string>> res;
         for(auto it:m){
