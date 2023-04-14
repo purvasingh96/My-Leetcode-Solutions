dbd9f731064e615f125d8002e78fe71a333c0a1a
Purva Singh <singhpurva2906@gmail.com>
2023-04-13 23:41:39 -0400

    Time: 61 ms (78.46%), Space: 43 MB (72.07%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -0,0 +1,23 @@
+
+function uniquePaths(m: number, n: number): number {
+    let dp = [];
+    for(let i=0;i<m;i++){
+        dp.push(new Array<number>(n));
+    }
+    
+    for(let i=0;i<m;i++){
+        dp[i][0] = 1;
+    }
+    
+    for(let j=0;j<n;j++){
+        dp[0][j] = 1;
+    }
+    
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    
+    return dp[m-1][n-1];
+};
