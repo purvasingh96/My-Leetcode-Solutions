4d8b99fd69928300a49ef91264d33506753388d7
Purva Singh <singhpurva2906@gmail.com>
2023-04-15 20:20:51 -0400

    Time: 48 ms (38.57%), Space: 89.2 MB (92.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
-        int low=0, high=m;
+        int m = nums1.size();
+        int n = nums2.size();
+        int l =0, r=m;
         
         if(m>n){
             return findMedianSortedArrays(nums2, nums1);
         }
         
-        /*                      *(partition)
-         X = 23, 26(maxLeftX), |56(minRightX), 59
-         Y = 1,  2, 5(maxLeftY)| 8(minRightY), 90
-        
-        */
-        
-        while(low<=high){
-            int partitionX = low + (high-low)/2;
-            int partitionY = (m+n+1)/2 - partitionX;
+        while(l<=r){
+            int mid = l + (r-l)/2;
             
-            int maxLeftX = partitionX==0 ? INT_MIN : nums1[partitionX-1];
-            int minRightX = partitionX==m ? INT_MAX : nums1[partitionX];
+            int pX = mid;
+            int pY = (m+n+1)/2 - pX;
             
-            int maxLeftY = partitionY == 0 ? INT_MIN: nums2[partitionY-1];
-            int minRightY = partitionY == n ? INT_MAX: nums2[partitionY];
+            int maxLeftX = pX==0?INT_MIN:nums1[pX-1];
+            int minRightX = pX==m?INT_MAX:nums1[pX];
             
-            // found condition
+            int maxLeftY = pY==0?INT_MIN:nums2[pY-1];
+            int minRightY = pY==n?INT_MAX:nums2[pY];
+            
             if(maxLeftX <= minRightY && maxLeftY<=minRightX){
                 if((m+n)%2==0){
-                    return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2; 
-                } else{
-                    return (double)(max(maxLeftX, maxLeftY));
+                    // even
+                    return  (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2.00;
+                    
+                } else {
+                    return (double)max(maxLeftX, maxLeftY);
                 }
+            } else if(maxLeftX > minRightY){
+                r = pX-1;
+            } else {
+                l = pX+1;
             }
             
-            else if(maxLeftX > minRightY){
-                high = partitionX-1;
-            } else low = partitionX+1;
-    }
+        }
         
-        return -1;
+        return -1.000;
     }
-        
 };
