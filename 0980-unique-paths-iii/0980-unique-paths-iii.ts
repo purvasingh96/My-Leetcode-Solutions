1d4da4266e1470303c6bd96c94a4ae2ec64b7ccf
Purva Singh <singhpurva2906@gmail.com>
2023-04-18 11:37:11 -0400

    Time: 71 ms (60.00%), Space: 43.2 MB (80.00%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.ts
+++ 0980-unique-paths-iii/0980-unique-paths-iii.ts
@@ -5,54 +5,55 @@ function isValid(x:number, y:number, grid:number[][]): boolean{
     return x>=0 && y>=0 && x<grid.length && y<grid[0].length;
 }
 
-function backtrack(x:number, y:number, tx:number, ty:number, target: number, grid: number[][], ans: {val: number}){
-    if(x==tx && y==ty){
-        if(target==-1) ans.val+=1;
+function backtrack(x: number, y: number, tx: number, ty: number, grid:number[][], tnb: number, target: number, count:{val:number}) : void {
+    
+    if(x==tx && y==ty) {
+        if(tnb==target+1) count.val+=1;
         return;
     }
     
-    for(let i=0;i<4;i++){
-        let new_x = x+dx[i];
-        let new_y = y+dy[i];
+    let ans = 0;
+    for(let k=0;k<4;k++){
+        let newX = x+dx[k];
+        let newY = y+dy[k];
         
-        if(isValid(new_x, new_y, grid) &&  grid[new_x][new_y]!=-1){
-            let t: number = grid[new_x][new_y];
-            grid[new_x][new_y]=-1;
-            target-=1;
-            backtrack(new_x, new_y, tx, ty, target, grid, ans);
-            target+=1;
-            grid[new_x][new_y]=t;
+        if(isValid(newX,newY, grid) && grid[newX][newY]!=-1){
+            let temp = grid[newX][newY]
+            grid[newX][newY]=-1;
+            tnb+=1;
+            backtrack(newX, newY, tx, ty, grid, tnb, target, count);
+            tnb-=1;
+            grid[newX][newY]=temp;
         }
     }
+ 
 }
 
-
 function uniquePathsIII(grid: number[][]): number {
-    let target: number=0;
-    let sx, sy, tx, ty;
-    let visited=[];
-    let m = grid.length, n = grid[0].length;
-    for(let i=0;i<m;i++){
-        visited.push(new Array<boolean>(n));
-    }
+    let target=0;
+    let tx=0, ty=0, sx=0, sy=0;
+    
+    let m = grid.length;
+    let n = grid[0].length;
+    
     for(let i=0;i<m;i++){
         for(let j=0;j<n;j++){
-            
-            visited[i][j]=false;
-            
-            if(grid[i][j]==0) target+=1;
-            else if(grid[i][j]==1) {
+            if(grid[i][j]==1){
                 sx=i;
                 sy=j;
                 grid[i][j]=-1;
-            } else if(grid[i][j]==2){
+            } else if(grid[i][j]==0){
+                target+=1;
+            } else if(grid[i][j]==2) {
                 tx=i;
                 ty=j;
             }
         }
     }
     
-    let ans= {val: 0}
-    backtrack(sx, sy, tx, ty, target, grid, ans);
-    return ans.val;
+    let tnb=0;
+    let count = {val:0};
+    backtrack(sx, sy, tx, ty, grid, tnb, target, count);
+    return count.val;
+    
 };
