79c83758917a20c368ffd187f8ab403ee6be9fcb
Purva Singh <singhpurva2906@gmail.com>
2023-04-18 09:33:46 -0400

    Time: 99 ms (5.40%), Space: 46.8 MB (5.40%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.ts
+++ 0063-unique-paths-ii/0063-unique-paths-ii.ts
@@ -1,38 +1,32 @@
 function uniquePathsWithObstacles(g: number[][]): number {
     
-    let dp = [];
-    let m:number = g.length;
+    let m: number = g.length;
     let n: number = g[0].length;
+    if(g[m-1][n-1]==1) return 0;
+
     
-    for(let i=0;i<m;i++){
-        dp.push(new Array<number>(n));
-    }
+    let dp:number[][] = Array.from(Array(m), () => new Array(n).fill(0));
+    if (!g[0][0]) dp[0][0] = 1;
+
+      // First row path
+      for (let i = 1; i < dp.length; i++)
+        if (!g[i][0]) dp[i][0] = dp[i - 1][0];
+
+      // First column path
+      for (let j = 1; j < dp[0].length; j++)
+        if (!g[0][j]) dp[0][j] = dp[0][j - 1];
+  
     
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            dp[i][j]=0;
-        }
-    }
-    
-    
-    for(let i=0;i<m;i++){
-        if(g[i][0] == 1) break;
-        dp[i][0]=1;
-    }
-    
-    for(let j=0;j<n;j++){
-        if(g[0][j]==1) break;
-        dp[0][j]=1;
-    }
+    console.log(dp);
     
     
     for(let i=1;i<m;i++){
         for(let j=1;j<n;j++){
-            if(g[i][j]==1) continue;
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            if(g[i][j]==1) dp[i][j]=0;
+            else dp[i][j] = dp[i-1][j] + dp[i][j-1];
         }
     }
     
     return dp[m-1][n-1];
-        
+    
 };
