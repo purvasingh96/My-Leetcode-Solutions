8f6c97221b3f60b24db25a46af1fbf7e7b153d8c
Purva Singh <singhpurva2906@gmail.com>
2023-04-19 14:32:11 -0400

    Time: 54 ms (100.00%), Space: 45.9 MB (7.69%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.ts
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.ts
@@ -1,62 +1,34 @@
-function flatten(mat: number[][]){
+function flatten(mat: number[][]) : Map<number, number[]> {
     let m = new Map<number, number[]>();
-    let k: number = mat.length;
-    let n: number = mat[0].length;
     
-    for(let i=0;i<k;i++){
+    for(let i=0;i<mat.length;i++){
         m.set(i, new Array<number>());
-        for(let j=0;j<n;j++){
-            if(mat[i][j]!=0){
+        for(let j=0;j<mat[0].length;j++){
+            if(mat[i][j] != 0){
                 m.get(i)!.push(j);
             }
         }
     }
-    
+
     return m;
 }
 
-
 function multiply(mat1: number[][], mat2: number[][]): number[][] {
-
-    let res = [];
-    let m: number = mat1.length;
-    let n: number = mat1[0].length;
-    let k: number = mat2[0].length;
-    
-    // m X k
-    for(let i=0;i<m;i++){
-        res.push(new Array<number>(k));
-    }
+    let m1 = flatten(mat1);
+    let m2 = flatten(mat2);
+    let ans = Array.from(Array(mat1.length), ()=>(new Array(mat2[0].length).fill(0)));
     
     
-    
-    for(let i=0;i<m;i++){
-        for(let j=0;j<k;j++){
-            res[i][j] = 0;
-        }
-    }
-    
-    
-    let a = flatten(mat1);
-    let b = flatten(mat2);
-
-    
-    for(let row1 of a.keys()){
-        for(let col1 of a.get(row1)){
-            for(let row2 of b.keys()){
-                for(let col2 of b.get(row2)){
-                    
-                    if(col1 == row2){
-                        res[row1][col2] += mat1[row1][col1]*mat2[row2][col2];
+    for(let [r1, c] of m1.entries()){
+        for(let c1 of c){
+            for(let [r2, cs] of m2.entries()){
+                for(let c2 of cs){
+                    if(c1 == r2){
+                        ans[r1][c2] += mat1[r1][c1]*mat2[r2][c2];
                     }
-                    
                 }
             }
         }
     }
-    
-    return res;
-    
-    
-    
+    return ans;
 };
