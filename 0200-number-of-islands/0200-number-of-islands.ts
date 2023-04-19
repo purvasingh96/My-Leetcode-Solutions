61db0ad91d708b60fe7e3894bd315257f2c4f5f7
Purva Singh <singhpurva2906@gmail.com>
2023-04-19 17:37:11 -0400

    Time: 83 ms (74.81%), Space: 45.8 MB (69.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -5,79 +5,27 @@ function isValid(x: number, y:number, grid:string[][]): boolean{
     return x>=0 && y>=0 && x<grid.length && y<grid[0].length;
 }
 
-class queue<T>{
-private items: T[] = [];
-
- pop() : T | undefined {
-    return this.items.shift();
-}
-
- push(val: T) : void {
-    this.items.push(val);
-}
-
- front(): T {
-    return this.items[0];
-}
-
- isEmpty() : boolean {
-    return this.items.length == 0;
-}
-
- size(): number {
-    return this.items.length;
-}
-
-}
-
-
-class pair<T, U> {
-  constructor(public first: T, public second: U) {}
-}
-
-function bfs(i:number, j:number, grid:string[][]): void{
-    let q = new queue<pair<number, number>>(); 
-    let p = new pair<number, number>(i,j);
-    q.push(p);
-    
-    while(!q.isEmpty()){
-        let f = q.front();q.pop();
-        let x:number = f.first;
-        let y:number = f.second;
-        
-        for(let k=0;k<4;k++){
-            let new_x=x+dx[k];
-            let new_y = y + dy[k];
-            if(isValid(new_x, new_y, grid) && grid[new_x][new_y]=="1"){
-                grid[new_x][new_y]="0";
-                let p_new = new pair<number, number>(new_x, new_y);
-                q.push(p_new);
-            }
+function dfs(x: number, y:number, grid: string[][]){
+    for(let k=0;k<4;k++){
+        let new_x = x+dx[k];
+        let new_y = y + dy[k];
+        if(isValid(new_x, new_y, grid) && grid[new_x][new_y]=="1"){
+            grid[new_x][new_y]="0";
+            dfs(new_x, new_y, grid);
         }
     }
-
 }
-
 function numIslands(grid: string[][]): number {
-    let m:number = grid.length;
-    let n:number = grid[0].length;
-    let ans: number = 0;
-    
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
+    let ans=0;
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
             if(grid[i][j]=="1"){
-                ans+=1;
-                bfs(i, j, grid);
+                grid[i][j]="0";
+                ans += 1;
+                dfs(i, j, grid);
             }
         }
     }
-    
-    
     
     return ans;
-    
-    
-    
-    
-    
 };
