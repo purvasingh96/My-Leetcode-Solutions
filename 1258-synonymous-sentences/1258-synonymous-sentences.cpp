ab95ddb79732c84101295e42f8b9fa2d5919619e
Purva Singh <singhpurva2906@gmail.com>
2023-04-18 21:46:47 -0400

    Time: 0 ms (100.00%), Space: 8.4 MB (78.62%) - LeetHub

--- 1258-synonymous-sentences/1258-synonymous-sentences.cpp
+++ 1258-synonymous-sentences/1258-synonymous-sentences.cpp
@@ -1,59 +1,56 @@
 class Solution {
 private:
-    string buildSentence(vector<string>& str){
+    string build(vector<string>& s){
         string res="";
-        for(auto x:str){
-            res += x;
+        for(auto word:s){
+            res += word;
             res+=" ";
         }
+        
         res.pop_back();
         return res;
     }
-
 public:
     vector<string> generateSentences(vector<vector<string>>& synonyms, string text) {
         set<string> ans;
         ans.insert(text);
         unordered_map<string, vector<string>> m;
-        
         for(auto s:synonyms){
             m[s[0]].push_back(s[1]);
             m[s[1]].push_back(s[0]);
         }
         
-        vector<string> words;
-        queue<vector<string>> q;
-        
         stringstream ss(text);
-        string buff;
-        
-        while(getline(ss, buff, ' ')){
-            words.push_back(buff);
+        vector<string> res;
+        string t;
+        while(getline(ss, t, ' ')){
+            res.push_back(t);
         }
         
-        q.push(words);
-        
+        queue<vector<string>> q;
+        q.push(res);
         
         while(!q.empty()){
-            vector<string> f = q.front();
+            auto f = q.front();
             q.pop();
-            for(auto& wd : f){
-                if (m.count(wd) == 0) continue; // if word not in graph, skip
-                else{
-                    for (auto& syno : m[wd]){
-                        wd = syno; // replace by new synoymous word
-                        string newText = buildSentence(f);
-                        if (ans.count(newText) == 0){
-                            ans.insert(newText); // insert new text string to ans
-                            q.push(f); // push new word vector to que
+            
+            for(auto& word:f){
+                if(m.find(word)==m.end()) continue;
+                else {
+                    for(auto& synonym: m[word]){
+                        word = synonym;
+                        string temp = build(f);
+                        if(ans.count(temp)==0){
+                            ans.insert(temp);
+                            q.push(f);
                         }
                     }
                 }
-            }   
+            }
+            
         }
         
-        vector<string> result(ans.begin(), ans.end());
-        return result;
-        
+        vector<string> answer(ans.begin(), ans.end());
+        return answer;
     }
 };
