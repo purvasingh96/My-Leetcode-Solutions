e1f2e28a4839860ef4dccdedbba8b1a03e914994
Purva Singh <singhpurva2906@gmail.com>
2023-04-19 13:21:20 -0400

    Time: 139 ms (95.00%), Space: 55.7 MB (15.00%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.ts
+++ 0218-the-skyline-problem/0218-the-skyline-problem.ts
@@ -1,158 +1,138 @@
-type Comparator<T> = (a:T, b:T) => number;
+type Comparator<T> = (a: T, b:T) => number;
 
-class PQ<T> {
+class PQ<T>{
     private pq: T[] = [];
     private comp: Comparator<T>;
-    constructor(comp: Comparator<T>) {
+    constructor(comp: Comparator<T>){
         this.comp = comp;
     }
-    
-    hasLeft(i:number) : boolean{
-        return this.left(i) < this.size();
+
+    empty(): boolean {
+        return this.size()==0;
     }
 
-    hasRight(i: number): boolean{
-        return this.right(i) < this.size();
-    }
-    
-    top(): T | undefined {
-        return this.pq[0];
-    }
-    
-
-    left(i: number) : number {
+    left(i: number){
         return 2*i+1;
     }
     
-    right(i: number): number {
+    right(i: number){
         return 2*i+2;
     }
-    
-    parent(i: number) : number {
+
+    parent(i: number){
         return Math.floor((i-1)/2);
     }
 
-    size(): number {
+    size(): number{
         return this.pq.length;
     }
     
-    empty(): boolean {
-        return this.size()==0;
+    hasLeft(i) : boolean {
+        return this.left(i) < this.size();
+    }
+
+    hasRight(i) : boolean {
+        return this.right(i) < this.size();
+    }
+
+    top() : T| undefined{
+        if(this.size()==0) return undefined;
+        return this.pq[0];
+    }
+
+    dequeue() : T | undefined {
+        if(this.size()==0) return undefined;
+        
+        let root = this.pq[0];
+        let last = this.pq.pop();
+        if(this.size()>0){
+            this.pq[0] = last;
+            this.heapDown(0);
+        }
+        
+        return root;
     }
 
     enqueue(val: T) : void {
         this.pq.push(val);
         this.heapUp(this.size()-1);
     }
-
-    dequeue() : T | undefined {
-        if(this.size()==0) return undefined;
-        let root = this.pq[0];
-        let last = this.pq.pop();
-        if(this.size() > 0){
-            this.pq[0] = last;
-            this.heapDown(0);
-        }
-        return root;
-    }
-
-    heapUp(i: number): void {
-        while(i>0){
-            let p = this.parent(i);
-            if(this.comp(this.pq[i], this.pq[p]) < 0){
-                [this.pq[i], this.pq[p]] = [this.pq[p], this.pq[i]];
-                i =p;
-            } else break;
-        }
-    }
-
-    heapDown(i: number) : void {
-        
+    
+    heapDown(i: number) {
         while(true){
             let l = this.left(i);
             let r = this.right(i);
             let minIndex = i;
-
             if(this.hasLeft(i) && this.comp(this.pq[l], this.pq[minIndex]) < 0){
                 minIndex = l;
             }
 
             if(this.hasRight(i) && this.comp(this.pq[r], this.pq[minIndex]) < 0){
                 minIndex = r;
-            }
+            } 
             
-            if(minIndex!=i){
+            if(minIndex == i) break;
+            else {
                 [this.pq[i], this.pq[minIndex]] = [this.pq[minIndex], this.pq[i]];
                 i = minIndex;
-            } else break;
-            
+            }
         }
-        
+    }
+
+    heapUp(i: number) {
+        while(i>0){
+            let p = this.parent(i);
+            if(this.comp(this.pq[i], this.pq[p]) < 0){
+                [this.pq[i], this.pq[p]] = [this.pq[p], this.pq[i]];
+                i = p;
+            } else break;
+        }
     }
 }
-class Pair<T, U>{
-public constructor(public first:T, public second:U){};
-}
 
+class Pair<T, U>{
+    constructor(public first: T, public second:U){}
+}
 
 
 function getSkyline(buildings: number[][]): number[][] {
     let live = new PQ<number>((a, b) => (b-a));
-    let dead  = new PQ<number>((a, b) => (b-a));
+    let dead = new PQ<number>((a, b) =>(b-a));
+    let res: number[][] = [];
+    let temp = new Array<Pair<number, number>>();
     
-    let res = new Array<Pair<number,number>>();
+    
     for(let b of buildings){
-        let start = new Pair(b[0], b[2]);
-        let end = new Pair(b[1], -b[2]);
-        res.push(start);
-        res.push(end);
+        let ps = new Pair(b[0], b[2]);
+        let pe = new Pair(b[1], -b[2]);
+        temp.push(ps);
+        temp.push(pe);
     }
     
-    res.sort((a:Pair<number, number>, b:Pair<number, number>) => a.first==b.first?b.second -a.second:a.first-b.first);
+    temp.sort((a: Pair<number, number>, b: Pair<number, number>) => (a.first == b.first) ? (b.second-a.second) : (a.first - b.first));
     
-    let ans: number[][]=[];
-    
-    // for(let x of res){
-    //     console.log(x.first, ", ", x.second);
-    // }
-    
-    //let maxHeight: number=0;
-    for(let r of res){
-        let pos = r.first;
-        let ht = r.second;
-        
-        if(ht > 0){
-            live.enqueue(ht);
+    for(let x of temp){
+        if(x.second > 0){
+            live.enqueue(x.second);
         } else {
-            dead.enqueue(-ht);
+            dead.enqueue(-x.second);
         }
         
-        // if(live.size()>0) console.log("live top: ", live.top());
-        // if(dead.size()>0) console.log("dead top: ", dead.top());
-        
-        while(live.size()!=0 && dead.size()!=0 && live.top() == dead.top()){
+        while(!live.empty() && !dead.empty() && live.top() == dead.top()){
             live.dequeue();
             dead.dequeue();
         }
         
-        let maxHeight: number = live.size()==0?0:live.top();
+        let maxHeight = live.size()==0 ? 0: live.top();
         
-        if(ans.length == 0 || ans[ans.length-1][1]!=maxHeight){
-            let pNew = new Array<number>();
-            pNew.push(pos);
-            pNew.push(maxHeight);
-            ans.push(pNew);
-        } 
         
+        if(res.length==0 || res[res.length-1][1] != maxHeight){
+            res.push([x.first, maxHeight]);
+        }
         
     }
     
-    return ans;
-    
-    
-    
-    
-    
+    return res;
     
     
     
