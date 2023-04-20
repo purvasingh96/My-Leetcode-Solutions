ce2aba14d88be5dd16cb66423e94bb8a4fb6d4dd
Purva Singh <singhpurva2906@gmail.com>
2023-04-20 12:23:20 -0400

    Time: 108 ms (51.79%), Space: 52.3 MB (41.07%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.ts
+++ 0224-basic-calculator/0224-basic-calculator.ts
@@ -1,48 +1,48 @@
-let i: number=0;
-function dfs(s:string, temp:number, t:number[], op:string){
-    while(i<s.length && s[i]!=')'){
-        if(s[i]==" "){
+let i;
+function dfs(s: string, d: number, op: string) : number {
+    let st: number[] = [];
+    
+    while(i<s.length && s[i]!=")"){
+        //console.log(i, s[i]);
+        if(s[i] == " ") {
             i+=1;
             continue;
-        } else if(s[i]>="0" && s[i]<="9"){
-            
-            let k:number = (s.charCodeAt(i)-"0".charCodeAt(0));
-            temp = temp*10 + (k);
-            i+=1;
-        } else if(s[i]=="+" || s[i]=="-"){
-            if(op == "+") t.push(temp);
-            else t.push(-temp);
-            
-            op = s[i];
-            temp=0;
-            i+=1;
-        } else if(s[i] == "("){
-            let newT: number[] = [];
-            temp=0;
-            i+=1;
-            let val: number = dfs(s, temp, newT, "+");
-            if(op == "+") t.push(val);
-            else t.push(-val);
-            i+=1;
         }
+        else if(s[i] >="0" && s[i]<="9"){
+            //console.log("digit");
+            d=d*10 + (s[i].charCodeAt(0) - "0".charCodeAt(0));
+        } else if(s[i] == "("){
+            i+=1;
+            if(op == "+") st.push(d);
+            else st.push(-d);
+            let res = dfs(s, 0, "+");
+            if(op == "+") st.push(res);
+            else st.push(-res);
+            
+        } else {
+            // operator
+            if(op == "+") st.push(d);
+            else st.push(-d);
+            op = s[i];
+            d=0;
+        }
+        i+=1;
     }
     
-    if(op == "+") t.push(temp);
-    else t.push(-temp);
+    if(op == "+") st.push(d);
+    else st.push(-d);
     
-    let sum:number=0;
-    for(let num of t){
-        sum += num;
+    let ans =0;
+    for(let i=0;i<st.length;i++){
+        ans += st[i];
     }
     
-    return sum;
+    return ans;
+    
 }
 
 function calculate(s: string): number {
-    let temp: number=0;
-    let t: number[] = [];
-    i=0;
-    let newS = "(" + s + ")"
-    return dfs(newS, temp, t, "+");
-    
+    s = "("+s+")";
+    i = 0;
+    return dfs(s, 0, "+");
 };
