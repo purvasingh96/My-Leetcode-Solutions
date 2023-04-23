65976fded7e2f2fd05d114dff1b9f3111c07aeb9
Purva Singh <singhpurva2906@gmail.com>
2023-04-23 17:30:34 -0400

    Time: 218 ms (16.30%), Space: 66.8 MB (5.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -1,41 +1,36 @@
-function strSort(s: string){
-    let count = new Array<number>(26);
-    for(let i=0;i<26;i++) count[i]=0;
-    
-    for(let i=0;i<s.length;i++){
-        let idx: number = s[i].charCodeAt(0) - 'a'.charCodeAt(0);
+
+function strSort(s: string) : string{
+    let count = new Array(26).fill(0);
+    for(let c of s){
+        let idx = c.charCodeAt(0) - "a".charCodeAt(0);
         count[idx]+=1;
     }
-    
-    let res: string = "";
+    let res="";
     for(let i=0;i<26;i++){
-        if(count[i]>0){
-            let t: string="";
-            let p: string = String.fromCharCode(i+97);
-            t = t.padStart(count[i],p);
-            res+=t;
-        }
+        res+="#";
+        res+= count[i].toString();
     }
-    
+    //console.log(res);
     return res;
+    
 }
 
-
 function groupAnagrams(strs: string[]): string[][] {
+    let ans: string[][] = [];
     let m = new Map<string, string[]>();
     
     for(let s of strs){
-        let anagram = strSort(s);
-        if(m.get(anagram)==undefined){
-            m.set(anagram, new Array<string>());
+        let sortS = strSort(s);
+        if(m.get(sortS) == undefined){
+            m.set(sortS, new Array<string>());
         }
-        m.get(anagram)!.push(s);
+        m.get(sortS).push(s);
     }
     
-    let res: string[][] = [];
-    for(let values of m.values()){
-        res.push(values);
-    }
     
-    return res;
+    for(let v of m.values()){
+        ans.push(v);
+    }
+        
+    return ans;
 };
