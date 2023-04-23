58b10f7cf8a5e329675b947d0e0af91400007e31
Purva Singh <singhpurva2906@gmail.com>
2023-04-23 09:58:26 -0400

    Time: 64 ms (47.80%), Space: 43.4 MB (26.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.ts
+++ 0394-decode-string/0394-decode-string.ts
@@ -1,30 +1,34 @@
-let i: number=0;
-
-function dfs(s: string): string {
-    let res = "";
-    while (i < s.length && s[i] !== "]") {
-      if (!isNaN(parseInt(s[i]))) {
-        let k = 0;
-        while (!isNaN(parseInt(s[i]))) {
-          k = k * 10 + parseInt(s[i]) - parseInt("0");
-          i += 1;
+let i: number;
+let t: number;
+function dfs(s: string){
+    
+    let temp: string = "";
+    
+    while(i<s.length && s[i]!="]"){
+        // digit
+        if(!isNaN(parseInt(s[i]))){
+            let t=0;
+            while(!isNaN(parseInt(s[i]))){
+                t=t*10 + parseInt(s[i]) - parseInt("0");
+                i+=1;
+            }
+            
+            // skip ]
+            i+=1;
+            let res = dfs(s);
+            i+=1;
+            while(t--) temp+=res;
+        } else {
+            temp += s[i++];
         }
-        i += 1;
-        let decodedString = dfs(s);
-        i += 1;
-        while (k--) {
-          res += decodedString;
-        }
-      } else {
-        res += s[i++];
-      }
     }
-    return res;
-  }
+    
+    return temp;
+}
 
 function decodeString(s: string): string {
-    //s = "["+s+"]";
-    //let i: number=0;
     i=0;
+    //console.log(isDigit("["));
+    t=0;
     return dfs(s);
 };
