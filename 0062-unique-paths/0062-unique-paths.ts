63f2c056504009c72e74b2206ea336c0db731f4e
Purva Singh <singhpurva2906@gmail.com>
2023-04-23 18:22:57 -0400

    Time: 63 ms (70.05%), Space: 45 MB (17.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.ts
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -1,21 +1,8 @@
-
 function uniquePaths(m: number, n: number): number {
-    let dp = [];
-    for(let i=0;i<m;i++){
-        dp.push(new Array<number>(n));
-    }
-    
-    for(let i=0;i<m;i++){
-        dp[i][0] = 1;
-    }
-    
-    for(let j=0;j<n;j++){
-        dp[0][j] = 1;
-    }
-    
+    let dp = Array.from(Array(m), ()=>new Array(n).fill(1));
     for(let i=1;i<m;i++){
         for(let j=1;j<n;j++){
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            dp[i][j]= dp[i-1][j]+dp[i][j-1];
         }
     }
     
