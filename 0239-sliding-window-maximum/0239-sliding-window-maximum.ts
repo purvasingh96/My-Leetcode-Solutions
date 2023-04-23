0a2d0beeac6000716454372ca93ced2512cc74fc
Purva Singh <singhpurva2906@gmail.com>
2023-04-23 12:27:38 -0400

    Time: 842 ms (43.69%), Space: 77.9 MB (71.84%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.ts
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.ts
@@ -1,120 +1,28 @@
-type Comparator<T> = (a: T, b: T) => number;
-
-class PQ<T>{
-    private items: T[] = [];
-    private comp: Comparator<T>;
-
-    constructor(comp: Comparator<T>){
-        this.comp = comp;
-    }
-    
-    size() : number{
-        return this.items.length;
-    }
-    
-    top(): T | undefined{
-        if(this.size()==0) return undefined;
-        return this.items[0];
-    }
-    
-    parent(i: number): number{
-        return Math.floor((i-1)/2);
-    }
-    
-    left(i: number): number {
-        return 2*i+1;
-    }
-
-    right(i: number): number{
-        return 2*i+2;
-    }
-
-    hasLeft(i: number): boolean {
-        return this.left(i) < this.size();
-    }
-
-    hasRight(i: number): boolean {
-        return this.right(i) < this.size();
-    }
-
-    enqueue(val: T){
-        this.items.push(val);
-        this.heapUp(this.size()-1);
-    }
-
-    dequeue() : T|undefined {
-        if(this.size()==0) return undefined;
-        let root = this.items[0];
-        let last = this.items.pop();
-        if(this.size()>0){
-            this.items[0] = last;
-            this.heapDown(0);
-        }
-        return root;
-    }
-
-    heapUp(i: number) {
-        while(i>0){
-            let p = this.parent(i);
-            if(this.comp(this.items[i], this.items[p]) < 0){
-                [this.items[i], this.items[p]] = [this.items[p], this.items[i]];
-                i = p;
-            } else break;
-        }
-    }
-
-    heapDown(i: number){
-        while(true){
-            let l = this.left(i);
-            let r = this.right(i);
-            let minIndex = i;
-            
-            if(this.hasLeft(i) && this.comp(this.items[l], this.items[minIndex]) < 0){
-                minIndex = l;
-            }
-            
-            if(this.hasRight(i) && this.comp(this.items[r], this.items[minIndex]) < 0){
-                minIndex = r;
-            }
-            
-            if(minIndex==i) break;
-            else {
-                [this.items[i], this.items[minIndex]] = [this.items[minIndex], this.items[i]];
-                i = minIndex;
-            }
-            
-        }
-    }
-    
-}
-
-
-class pair<T, U> {
-    constructor(public first: T, public second:U){}
-}
-
 function maxSlidingWindow(nums: number[], k: number): number[] {
-    //let pq = new PQ<pair<number, number>>((a: pair<number, number>, b:pair<number, number>) => (b.second - a.second));
-    let pq = new PQ<pair<number, number>>((a: pair<number, number>, b:pair<number, number>) => (b.second - a.second));
-    
-    for(let i=0;i<k;i++){
-        let p = new pair(i, nums[i]);
-        pq.enqueue(p);
-    }
-    
+    let q: number[] = [];
     let res: number[] = [];
-    res.push(pq.top().second);
     
-    for(let i=k;i<nums.length;i++){
-        while(pq.size()!=0 && i - pq.top().first+1 > k){
-            pq.dequeue();
+    for(let i=0;i<nums.length;i++){
+        let cur = nums[i];
+        //console.log(i, q);
+        while(q.length!=0 && nums[q[q.length-1]]<cur){
+            q.pop();
         }
-        let p = new pair(i, nums[i]);
-        pq.enqueue(p);
-        res.push(pq.top().second);
+        
+        q.push(i);
+        //console.log(i, q);
+        // out of bounds
+        while(q.length!=0 && q[0] < (i-k+1)){
+            q.shift();
+        }
+        //console.log(i, q);
+        if(i>=k-1){
+            res.push(nums[q[0]]);
+        }
+        
+        
     }
     
     return res;
-    
     
 };
