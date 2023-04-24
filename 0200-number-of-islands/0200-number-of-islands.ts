0010a82ce18e13cf25e138af3d4e699bf23b70a3
Purva Singh <singhpurva2906@gmail.com>
2023-04-24 16:26:59 -0400

    Time: 95 ms (45.70%), Space: 45.7 MB (68.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -5,23 +5,26 @@ function isValid(x: number, y:number, grid:string[][]): boolean{
     return x>=0 && y>=0 && x<grid.length && y<grid[0].length;
 }
 
-function dfs(x: number, y:number, grid: string[][]){
+
+function dfs(x: number, y: number, grid:string[][]){
     for(let k=0;k<4;k++){
-        let new_x = x+dx[k];
+        let new_x = x + dx[k];
         let new_y = y + dy[k];
-        if(isValid(new_x, new_y, grid) && grid[new_x][new_y]=="1"){
-            grid[new_x][new_y]="0";
+        if(isValid(new_x, new_y, grid) && grid[new_x][new_y] == "1"){
+            grid[new_x][new_y] = "0";
             dfs(new_x, new_y, grid);
         }
     }
 }
+
+
 function numIslands(grid: string[][]): number {
     let ans=0;
+    
     for(let i=0;i<grid.length;i++){
         for(let j=0;j<grid[0].length;j++){
-            if(grid[i][j]=="1"){
-                grid[i][j]="0";
-                ans += 1;
+            if(grid[i][j] == "1"){
+                ans+=1;
                 dfs(i, j, grid);
             }
         }
