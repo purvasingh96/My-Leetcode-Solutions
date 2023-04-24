4476aa1885ae844d051c61766d19149d03ba374c
Purva Singh <singhpurva2906@gmail.com>
2023-04-24 15:44:01 -0400

    Time: 70 ms (50.00%), Space: 45.4 MB (16.67%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.ts
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.ts
@@ -1,34 +1,34 @@
 function flatten(mat: number[][]) : Map<number, number[]> {
-    let m = new Map<number, number[]>();
-    
-    for(let i=0;i<mat.length;i++){
-        m.set(i, new Array<number>());
-        for(let j=0;j<mat[0].length;j++){
-            if(mat[i][j] != 0){
-                m.get(i)!.push(j);
-            }
+    let flatMap = new Map<number, number[]>();
+    for(let row=0;row<mat.length;row++){
+        flatMap.set(row, new Array<number>());
+        for(let col=0;col<mat[0].length;col++){
+            if(mat[row][col]!=0) flatMap.get(row).push(col);
         }
     }
 
-    return m;
+    return flatMap;
 }
 
 function multiply(mat1: number[][], mat2: number[][]): number[][] {
-    let m1 = flatten(mat1);
-    let m2 = flatten(mat2);
-    let ans = Array.from(Array(mat1.length), ()=>(new Array(mat2[0].length).fill(0)));
+    let fm1 = flatten(mat1);
+    let fm2 = flatten(mat2);
+    let m1 = mat1.length, n2 = mat2[0].length;
     
+    let res = Array.from(Array(m1), ()=> new Array(n2).fill(0));
     
-    for(let [r1, c] of m1.entries()){
-        for(let c1 of c){
-            for(let [r2, cs] of m2.entries()){
-                for(let c2 of cs){
-                    if(c1 == r2){
-                        ans[r1][c2] += mat1[r1][c1]*mat2[r2][c2];
+    for(let [row1,cols1] of fm1.entries()){
+        for(let col1 of cols1){
+            for(let [row2, cols2] of fm2.entries()){
+                for(let col2 of cols2){
+                    if(col1 == row2){
+                        res[row1][col2] += (mat1[row1][col1] * mat2[row2][col2]);
                     }
                 }
             }
         }
     }
-    return ans;
+    
+    return res;
+    
 };
