0776b5798498662ec0e5dd6e8f4130e2f86fcbae
Purva Singh <singhpurva2906@gmail.com>
2023-04-24 11:41:46 -0400

    Time: 69 ms (80.00%), Space: 43 MB (80.00%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.ts
+++ 0980-unique-paths-iii/0980-unique-paths-iii.ts
@@ -5,55 +5,43 @@ function isValid(x:number, y:number, grid:number[][]): boolean{
     return x>=0 && y>=0 && x<grid.length && y<grid[0].length;
 }
 
-function backtrack(x: number, y: number, tx: number, ty: number, grid:number[][], tnb: number, target: number, count:{val:number}) : void {
+function dfs(x: number, y: number, tx: number, ty: number, grid:number[][], squares:number, target:number) : number {
+    if(x==tx && y==ty && squares == target+1) return 1;
     
-    if(x==tx && y==ty) {
-        if(tnb==target+1) count.val+=1;
-        return;
-    }
-    
-    let ans = 0;
+    let ans=0;
     for(let k=0;k<4;k++){
-        let newX = x+dx[k];
-        let newY = y+dy[k];
+        let new_x = x + dx[k];
+        let new_y = y + dy[k];
         
-        if(isValid(newX,newY, grid) && grid[newX][newY]!=-1){
-            let temp = grid[newX][newY]
-            grid[newX][newY]=-1;
-            tnb+=1;
-            backtrack(newX, newY, tx, ty, grid, tnb, target, count);
-            tnb-=1;
-            grid[newX][newY]=temp;
-        }
+        if(isValid(new_x, new_y, grid) && grid[new_x][new_y]!=-1){
+            let temp = grid[new_x][new_y]; 
+            grid[new_x][new_y] = -1;
+            squares+=1;
+            ans += dfs(new_x, new_y, tx, ty, grid, squares, target);
+            squares-=1;
+            grid[new_x][new_y] = temp;   
+        }  
     }
- 
+    return ans;
 }
 
 function uniquePathsIII(grid: number[][]): number {
-    let target=0;
-    let tx=0, ty=0, sx=0, sy=0;
+    let target=0, squares=0;
+    let sx, sy, tx, ty;
     
-    let m = grid.length;
-    let n = grid[0].length;
-    
-    for(let i=0;i<m;i++){
-        for(let j=0;j<n;j++){
-            if(grid[i][j]==1){
-                sx=i;
-                sy=j;
-                grid[i][j]=-1;
-            } else if(grid[i][j]==0){
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j] == 1){
+                [sx, sy] = [i, j];
+            } else if(grid[i][j] == 2){
+                [tx, ty] = [i, j];
+            } else if(grid[i][j] == 0){
                 target+=1;
-            } else if(grid[i][j]==2) {
-                tx=i;
-                ty=j;
             }
         }
     }
     
-    let tnb=0;
-    let count = {val:0};
-    backtrack(sx, sy, tx, ty, grid, tnb, target, count);
-    return count.val;
+    grid[sx][sy]=-1;
+    return dfs(sx, sy, tx, ty, grid, squares, target);
     
 };
