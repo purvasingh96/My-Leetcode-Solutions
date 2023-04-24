76eab35dfa922728aee1b7be0c8c9eecffd1301b
Purva Singh <singhpurva2906@gmail.com>
2023-04-24 11:27:56 -0400

    Time: 155 ms (16.67%), Space: 57.4 MB (50.00%) - LeetHub

--- 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.ts
+++ 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.ts
@@ -1,43 +1,45 @@
-function helper(temp: string, s:string, c: string, m:Map<string, number>, ans: {val: string[]}){
-    let valid=0;
-    for(let [k, v] of m.entries()){
+function dfs(temp: string, s: string, center: string, m: Map<string, number>, ans:{val: string[]}){
+    let valid=true;
+    for(let [k,v] of m.entries()){
         if(v>=2){
             m.set(k, m.get(k)-2);
             temp+=k;
-            helper(temp, s,c, m, ans);
+            dfs(temp, s, center, m, ans);
             temp = temp.slice(0, -1);
             m.set(k, m.get(k)+2);
-            valid+=1;
+            valid=false;
         }
     }
     
-    if(valid==0){
-        let rs = temp;
-        rs = rs.split("").reverse().join("");
-        ans.val.push(temp+c+rs);
+    if(valid){
+        let rev =temp;
+        rev = rev.split("").reverse().join("");
+        ans.val.push(temp+center+rev);
     }
+    
+    
 }
+
 
 function generatePalindromes(s: string): string[] {
     let m = new Map<string, number>();
-    for(let c of s) {
-        m.set(c, (m.get(c) ?? 0)+1);
-    }
-    
-    let answer : {val: string[]} = {val: []};
-    
     let odd=0;
-    let c : string="";
-    for(let [k,v] of m.entries()){
-        if(v%2!=0){
-            odd+=1;
-            c = k;
-        }
+    let center = "";
+    let ans : {val: string[]} = {val: []};
+    for(let c of s){
+        m.set(c, (m.get(c)??0)+1);
     }
     
-    if(odd>1) return answer.val;
-    let temp: string = "";
-    helper(temp, s, c, m, answer);
-    return answer.val;
+    for(let [k,v] of m.entries()){
+        if(v%2!=0) {
+            odd+=1;
+            center = k;
+        }
+        if(odd>1) return ans.val;
+        
+    }
+    let temp="";
+    dfs(temp, s, center, m, ans);
+    return ans.val;
     
 };
