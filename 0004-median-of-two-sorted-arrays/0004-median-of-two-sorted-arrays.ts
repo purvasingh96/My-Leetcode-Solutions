a60493e23fcee0e2b668ddc8623ed8b58d526b9b
Purva Singh <singhpurva2906@gmail.com>
2023-04-25 11:51:16 -0400

    Time: 110 ms (62.78%), Space: 46.7 MB (92.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.ts
@@ -10,24 +10,24 @@ function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
         let px = l + Math.floor((r-l)/2);
         let py = Math.floor((m+n+1)/2) - px;
         
-        let maxLeftX = px==0 ? Number.MIN_SAFE_INTEGER: nums1[px-1];
-        let minRightX = px==m? Number.MAX_SAFE_INTEGER : nums1[px];
+        let maxLeftX = px==0?Number.MIN_SAFE_INTEGER:nums1[px-1];
+        let minRightX = px==m?Number.MAX_SAFE_INTEGER:nums1[px];
         
-        let maxLeftY = py==0 ? Number.MIN_SAFE_INTEGER : nums2[py-1];
-        let minRightY = py==n ? Number.MAX_SAFE_INTEGER : nums2[py];
+        let maxLeftY = py==0?Number.MIN_SAFE_INTEGER:nums2[py-1];
+        let minRightY = py==n?Number.MAX_SAFE_INTEGER:nums2[py];
         
-        if(maxLeftX <=minRightY && maxLeftY<=minRightX){
+        if(maxLeftX<=minRightY && maxLeftY<=minRightX){
             if((m+n)%2==0){
-                return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2.000;
+                return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightY, minRightX))/2.000;
             } else {
                 return Math.max(maxLeftX, maxLeftY);
             }
-            
         } else if(maxLeftX > minRightY){
-            r = px-1;
-        } else l = px+1;
+            r=px-1;
+        } else l=px+1;
         
     }
+    
     return -1.0000;
     
 };
