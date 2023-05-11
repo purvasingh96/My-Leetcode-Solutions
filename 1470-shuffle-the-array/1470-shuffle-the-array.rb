a53ac6d93bef128c65c17a4a94abb45e0be977fa
Purva Singh <singhpurva2906@gmail.com>
2023-05-10 21:30:31 -0400

    Time: 92 ms (58.06%), Space: 211.5 MB (12.90%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.rb
+++ 1470-shuffle-the-array/1470-shuffle-the-array.rb
@@ -2,13 +2,13 @@
 # @param {Integer} n
 # @return {Integer[]}
 def shuffle(nums, n)
-    $i, $j, $k = 0, n, nums.size
+    i,j,k = 0, n, nums.size
     arr = Array.new
-    while $i<n && $j<$k do
-        arr.push(nums[$i])
-        arr.push(nums[$j])
-        $i+=1
-        $j+=1
+    while i<n && j<k do
+        arr.push(nums[i])
+        arr.push(nums[j])
+        i+=1
+        j+=1
     end
     return arr
 end
