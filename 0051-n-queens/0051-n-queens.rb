24891624cab5ed0b2712e7717ab667264d55575d
Purva Singh <singhpurva2906@gmail.com>
2023-05-11 16:51:37 -0400

    Time: 336 ms (5.26%), Space: 212.2 MB (15.79%) - LeetHub

--- 0051-n-queens/0051-n-queens.rb
+++ 0051-n-queens/0051-n-queens.rb
@@ -1,45 +1,53 @@
-class Solution {
-private:
-    bool isValid(int row, int col, vector<string>& board){
-        // col check
-        int  n = board[0].length();
-        for(int i=0;i<board.size();i++){
-            if(board[i][col] == 'Q') return false;
-            if(row-i >=0 && col-i >=0 && board[row-i][col-i]=='Q') return false;
-            if(row-i>=0 && col+i < n && board[row-i][col+i]=='Q') return false;
-            if(row+i<n && col-i >=0 && board[row+i][col-i] == 'Q') return false;
-            if(row+i<n && col+i<n && board[row+i][col+i] == 'Q') return false;       
-        }
-        
-        return true;
-    }
+# @param {Integer} n
+# @return {String[][]}
+
+def isValid(row, col, board)
+    n = board.length
+    (0..n-1).each do |i|
+        if board[i][col] == 'Q'
+            return false
+        end
+        if row-i>=0 && col-i>=0 && board[row-i][col-i]=='Q' 
+            return false
+        end
+        if row-i>=0 && col+i<n && board[row-i][col+i]=='Q'
+            return false
+        end
+        if row+i<n && col-i>=0 && board[row+i][col-i]=='Q'
+            return false
+        end
+        if row+i<n && col+i<n && board[row+i][col+i]=='Q'
+            return false
+        end 
+    end
+    return true
+end
+
+def backtrack(row, board, ans=[])
+    n = board.length
+    if row == board.size
+        ans << board.map(&:dup)
+        return
+    end
     
-    void backtrack(int row, vector<string>& board, vector<vector<string>>& ans){
-        
-        if(row == board.size()) {
-            ans.push_back(board);
-            return;
-        }
-        
-        for(int col = 0;col<board[0].length();col++){
-            if(isValid(row, col, board)){
-                board[row][col] = 'Q';
-                backtrack(row+1, board, ans);
-                board[row][col] = '.';
-            }
-        }
-        
-    }
-    
-public:
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> board;
-        for(int i=0;i<n;i++){
-            string s(n, '.');
-            board.push_back(s);
-        }
-        vector<vector<string>> ans;
-        backtrack(0, board, ans);
-        return ans;
-    }
-};
+    (0..n-1).each do |col|
+        if isValid(row, col, board)
+            print("yes")
+            board[row][col] = 'Q'
+            backtrack(row+1, board, ans)
+            board[row][col] = '.'
+        end
+    end
+end
+
+
+
+def solve_n_queens(n)
+    ans = []
+    board = []
+    (0..n-1).each do |i|
+        board << ('.'*n)
+    end
+    backtrack(0, board, ans)
+    ans
+end
