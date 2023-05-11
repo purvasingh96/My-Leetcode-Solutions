65623b9126e7a7879fc9cd669ea7b628bed01570
Purva Singh <singhpurva2906@gmail.com>
2023-05-11 14:56:17 -0400

    Time: 15 ms (63.08%), Space: 9 MB (56.41%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.cpp
@@ -3,28 +3,25 @@ public:
     int minimumKeypresses(string s) {
         if(s.length() <= 9) return s.length();
         int ans=0;
-        unordered_map<int, int> m;
+        vector<int> m(10, 0);
         vector<int> y(26, -1);
-        for(int i=1;i<=9;i++){
-            m[i] = 0;
-        }
+        vector<int> l(26, 0);
         
-        unordered_map<char, int> l;
-        vector<pair<char, int>> temp;
-        for(auto x:s) l[x]+=1;
+        vector<int> temp;
+        
+        for(auto x:s) l[x-'a']+=1;
         for(auto y:l) {
-            temp.push_back({y.first, y.second});
+            if(y!=0) temp.push_back(y);
         }
         
-        sort(temp.begin(), temp.end(), [](const pair<char, int>& a, const pair<char, int>& b){
-            return a.second > b.second;
+        sort(temp.begin(), temp.end(), [](const int& a, const int& b){
+            return a > b;
         });
         
         int pos=1;
         
         for(int i=0;i<temp.size();i++){
-            char c = temp[i].first;
-            int count = temp[i].second;
+            int count = temp[i];
             
             m[pos%9] +=1;
             ans += (m[pos%9]*count);
