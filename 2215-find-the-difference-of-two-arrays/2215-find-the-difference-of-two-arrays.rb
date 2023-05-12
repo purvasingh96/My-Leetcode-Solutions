67c0a4877f6ccc17e26bcdb043f5167b403d7013
Purva Singh <singhpurva2906@gmail.com>
2023-05-12 10:01:56 -0400

    Time: 168 ms (17.92%), Space: 213.2 MB (5.78%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.rb
@@ -0,0 +1,40 @@
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[][]}
+def find_difference(nums1, nums2)
+    m1, m2 = {}, {}
+    n1, n2 = nums1.size, nums2.size
+    
+    (0..n1-1).each do |i|
+        m1[nums1[i]] = 1
+        m2[nums1[i]] = -1
+    end
+    
+    (0..n2-1).each do |i|
+        m1[nums2[i]]=-1
+        if !m2.key?(nums2[i])
+            m2[nums2[i]] = 1
+        end
+    end
+    
+    v1, v2 = [], []
+    ans = []
+    
+    m1.each do |k, v|
+        if v==1
+            v1 << k
+        end
+    end
+    
+    m2.each do |k, v|
+        if v==1
+            v2 << k
+        end
+    end
+    
+    ans << v1
+    ans << v2
+    
+    ans
+    
+end
