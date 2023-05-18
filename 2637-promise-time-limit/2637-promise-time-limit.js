a4a62c9495a4ae39ee949e5f123d27cd974264f6
Purva Singh <singhpurva2906@gmail.com>
2023-05-18 12:30:55 -0400

    Time: 57 ms (76.27%), Space: 42.2 MB (32.19%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+  return async function(...args) {
+    const timeLimitPromise = new Promise((resolve, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t)
+    });
+    const returnedPromise = fn(...args);
+    return Promise.race([timeLimitPromise, returnedPromise]);
+  }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
