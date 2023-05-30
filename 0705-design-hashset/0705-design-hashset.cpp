360209547fcf03751e15d99920d0c6bd44313fc2
Purva Singh <singhpurva2906@gmail.com>
2023-05-30 11:14:41 -0400

    Time: 128 ms (39.68%), Space: 53.3 MB (28.50%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,37 +1,70 @@
 class MyHashSet {
-private:
-    vector<set<int>> res;
-    int hashkey;
+    TreeNode *root;
 public:
     MyHashSet() {
-        hashkey = 769;
-        res.resize(hashkey);
+        root=NULL;
     }
     
-    int hashindex(int key){
-        return key%hashkey;
+    TreeNode *insert(TreeNode *root,int key)
+    {
+        if(root==NULL)
+        {
+            return new TreeNode(key);
+        }
+        if(root->val>key)
+            root->left=insert(root->left,key);
+        else if(root->val<key)
+            root->right=insert(root->right,key);
+        else
+            root->val=key;
+        return root;
     }
-    
     void add(int key) {
-        int i = hashindex(key);
-        res[i].insert(key);
+        root=insert(root,key);
     }
     
+    TreeNode *del(TreeNode *root,int key)
+    {
+        if(root==NULL)
+            return NULL;
+        if(root->val>key)
+            root->left=del(root->left,key);
+        else if(root->val<key)
+            root->right=del(root->right,key);
+        else
+        {
+            if(!root->left && !root->right)
+                return NULL;
+            else if(root->left && !root->right)
+                return root->left;
+            else if(!root->left && root->right)
+                return root->right;
+            else
+            {
+                TreeNode *node=root->left;
+                while(node->right)
+                    node=node->right;
+                root->val=node->val;
+                root->left=del(root->left,root->val);
+            }
+        }
+        return root;
+    }
     void remove(int key) {
-        int i = hashindex(key);
-        res[i].erase(key);
+        root=del(root,key);
     }
     
     bool contains(int key) {
-        int i = hashindex(key);
-        return res[i].find(key)!=res[i].end();
+        TreeNode *node=root;
+        while(node)
+        {
+            if(node->val>key)
+                node=node->left;
+            else if(node->val<key)
+                node=node->right;
+            else
+                return true;
+        }
+        return false;
     }
 };
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
