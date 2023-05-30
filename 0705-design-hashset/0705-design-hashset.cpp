76c4d06451718e696b7d496f44acb652d745620c
Purva Singh <singhpurva2906@gmail.com>
2023-05-30 11:25:46 -0400

    Time: 94 ms (73.22%), Space: 53.4 MB (28.50%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,70 +1,78 @@
 class MyHashSet {
-    TreeNode *root;
+    
 public:
+    TreeNode* root;
     MyHashSet() {
-        root=NULL;
+        root = NULL;
     }
     
-    TreeNode *insert(TreeNode *root,int key)
-    {
-        if(root==NULL)
-        {
+    TreeNode* insert(TreeNode* root, int key){
+        if(!root){
             return new TreeNode(key);
+        } else {
+            if(root->val > key){
+                root->left = insert(root->left, key);
+            } else if(root->val < key){
+                root->right = insert(root->right, key);
+            } else {
+                root->val = key;
+            }
         }
-        if(root->val>key)
-            root->left=insert(root->left,key);
-        else if(root->val<key)
-            root->right=insert(root->right,key);
-        else
-            root->val=key;
+        
         return root;
     }
-    void add(int key) {
-        root=insert(root,key);
-    }
     
-    TreeNode *del(TreeNode *root,int key)
-    {
-        if(root==NULL)
-            return NULL;
-        if(root->val>key)
-            root->left=del(root->left,key);
-        else if(root->val<key)
-            root->right=del(root->right,key);
-        else
-        {
-            if(!root->left && !root->right)
-                return NULL;
-            else if(root->left && !root->right)
-                return root->left;
-            else if(!root->left && root->right)
+    TreeNode* deleteNode(TreeNode* root, int key){
+        if(!root) return NULL;
+        else if(root->val > key){
+            root->left = deleteNode(root->left, key);
+        } else if(root->val < key){
+            root->right = deleteNode(root->right, key);
+        } else {
+            if(!root->left && !root->right) return NULL;
+            else if(!root->left || !root->right) {
+                if(root->left) return root->left;
                 return root->right;
-            else
-            {
-                TreeNode *node=root->left;
-                while(node->right)
-                    node=node->right;
-                root->val=node->val;
-                root->left=del(root->left,root->val);
+            }
+            else if(root->left && root->right){
+                TreeNode* pred = root->left;
+                while(pred->right){
+                    pred = pred->right;
+                }
+                root->val = pred->val;
+                root->left = deleteNode(root->left, pred->val);
             }
         }
         return root;
     }
+    
+    
+    void add(int key) {
+        root = insert(root, key);
+    }
+    
     void remove(int key) {
-        root=del(root,key);
+        root = deleteNode(root, key);
     }
     
     bool contains(int key) {
-        TreeNode *node=root;
-        while(node)
-        {
-            if(node->val>key)
-                node=node->left;
-            else if(node->val<key)
-                node=node->right;
-            else
-                return true;
+        TreeNode* node = root;
+        while(node){
+            if(node->val > key){
+                node =node->left;
+            } else if(node->val < key){
+                node = node->right;
+            } else return true;
         }
+        
         return false;
     }
 };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
