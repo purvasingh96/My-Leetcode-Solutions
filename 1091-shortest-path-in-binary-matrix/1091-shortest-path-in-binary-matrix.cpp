32cdd98aa5af0a8d79216c20711dc740949e49ae
Purva Singh <singhpurva2906@gmail.com>
2023-06-01 10:08:06 -0400

    Time: 156 ms (20.76%), Space: 36.7 MB (17.38%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,57 +1,43 @@
 class Solution {
 private:
-    int offset_x[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-    int offset_y[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-public:
-    bool checkBoundry(int n, int x, int y) {
-        return (x>=0 && x<n && y>=0 && y<n);
+    int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+    int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+    bool isValid(int x, int y, vector<vector<int>>& grid){
+            return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    
+public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
-        if(grid[0][0] != 0) return -1;
-        //cout<<"here";
-        
-        int len=0, n = grid.size();
         queue<vector<int>> q;
+        if(grid[0][0]!=0) return -1;
+        grid[0][0]=1;
         
         q.push({0, 0, 1});
+        int ans = INT_MAX, m = grid.size(), n = grid[0].size();
         
-        while(!q.empty()) {
+        while(!q.empty()){
             
-            int s = q.size();
+            auto f = q.front();
+            q.pop();
             
-            for(int i=0;i<s;i++) {
+            int x = f[0], y = f[1], steps=f[2];
+            if(x == m-1 && y==n-1){
+                ans = min(ans, steps);
+            }
+            
+            for(int i=0;i<8;i++){
+                int new_x = x+dx[i];
+                int new_y = y+dy[i];
                 
-                auto f = q.front();
-                q.pop();
-                
-                int x = f[0], y = f[1], level=f[2];
-                
-                if(x == n-1 && y == n-1) return level;
-                
-                for(int j=0;j<8;j++) {
-                    
-                    int new_x = x + offset_x[j], new_y = y+offset_y[j];
-                    
-                    if(checkBoundry(n, new_x, new_y) && grid[new_x][new_y] == 0) {
-                        
-                        //cout<<"nx: "<<new_x<<" "<<new_y<<"\n";
-                        grid[new_x][new_y] = 1;
-                        
-                        //if(new_x == n-1 && new_y == n-1) return level+1;
-                        
-                        q.push({new_x, new_y, level+1});
-                        
-                    }
-                    
+                if(isValid(new_x, new_y, grid) && grid[new_x][new_y]==0){
+                    grid[new_x][new_y]=1;
+                    q.push({new_x, new_y, steps+1});
                 }
-                
-                
             }
             
         }
         
-        return -1;
+        return ans==INT_MAX?-1:ans;
+        
     }
 };
