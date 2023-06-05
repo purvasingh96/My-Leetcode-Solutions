7a5f9595b08c908f1e0dc13a1c500f5cdb86cbcd
Purva Singh <singhpurva2906@gmail.com>
2023-06-05 10:17:43 -0400

    Time: 8 ms (73.14%), Space: 10 MB (63.27%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    double slope(vector<int>& coord1, vector<int>& coord2){
+        int x1 = coord1[0], y1 = coord1[1], x2 = coord2[0], y2 = coord2[1];
+        if(x2 == x1) return INT_MAX;
+        return (double)(y2-y1)/(x2-x1);
+    }
+public:
+    bool checkStraightLine(vector<vector<int>>& coord) {
+        if(coord.size()==2) return true;
+        double m = slope(coord[0], coord[1]);
+        //cout<<"m: "<<m<<"\n";
+        for(int i=1;i<coord.size();i++){
+            //cout<<slope(coord[i], coord[i-1])<<"\n";
+            if(slope(coord[i], coord[i-1])!=m) return false;
+        }
+        
+        return true;
+    }
+};
