a1f608ece7e209437af1d4021084c543565dc0f1
Purva Singh <singhpurva2906@gmail.com>
2023-06-14 12:01:10 -0400

    Time: 48 ms (21.66%), Space: 8.5 MB (12.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,51 @@
 class Solution {
+private:
+    
 public:
     string minWindow(string s, string t) {
+        int left=0, right=0;
         unordered_map<char, int> m1;
         unordered_map<char, int> m2;
-        for(auto c:t) m2[c]+=1;
         
-        int count=0, len=INT_MAX;
-        string ans="";
+        for(auto c:t) m2[c] += 1;
         
-        int left=0, right=0;
         int n = s.length();
+        int len = INT_MAX;
+        string temp="";
+        string ans="";
+        int c=0;
         
-        while(right<n){
-            
+        while(right < n){
             if(m2.find(s[right])!=m2.end()){
                 m1[s[right]]+=1;
                 if(m1[s[right]] <= m2[s[right]]){
-                    count+=1;
+                    c+=1;
                 }
             }
             
-            if(count >= t.length()){
-                while(left < right && m2.find(s[left])==m2.end() || m1[s[left]] > m2[s[left]]){
-                    m1[s[left]]-=1;
-                    left+=1;
-                }
+            if(c >= t.length()){
+               
+            while(left < right &&
+                  (m2.find(s[left])==m2.end() || m1[s[left]] > m2[s[left]])){
                 
-                if(right-left+1 < len){
-                    len= right-left+1;
-                    ans = s.substr(left, len);
-                }
+                m1[s[left]]-=1;
+                
+                left+=1;
             }
+            if((right-left+1) < len){
+                len = (right-left+1);
+                ans = s.substr(left, len);
+            }
+            
+            
+            }
+            
+            
             
             right+=1;
-            
         }
         
-        return len==INT_MAX?"":ans;
+        return len==INT_MAX ? "":ans;
         
     }
 };
