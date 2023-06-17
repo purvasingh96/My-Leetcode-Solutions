48d4f55e5d69fe319136d51c4d531e7d78de35d8
Purva Singh <singhpurva2906@gmail.com>
2023-06-17 14:20:23 -0400

    Time: 8 ms (70.93%), Space: 7.2 MB (91.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,25 @@
 class Solution {
-private:
-    bool isPerm(vector<int>& res1, vector<int>& res2){
-        for(int i=0;i<26;i++){
-            if(res1[i]!=0){
-                if(res1[i] != res2[i]) return false;
-            }
-        }
-        
-        return true;
-    }
 public:
     bool checkInclusion(string s1, string s2) {
         if(s1.length() > s2.length()) return false;
-        vector<int> res1(26,0);
-        vector<int> res2(26, 0);
+        int l=0, r=0;
+        vector<int> count2(26, 0);
+        vector<int> count1(26, 0);
         
-        int m = s1.length(), n = s2.length();
-        for(auto c:s1) res1[c-'a']+=1;
+        for(auto c:s1) count1[c-'a']+=1;
         
-        for(int i=0;i<m;i++) res2[s2[i]-'a']+=1;
+        int n =s2.length();
+        int m = s1.length();
         
-        int l=0, r=m-1;
+        for(;r<m;r++) count2[s2[r]-'a']+=1;
+        if(count1 == count2) return true;
         
-        while(r<n){
-            if(isPerm(res1, res2)) return true;
-            else{
-                r+=1;
-                if(r<n){
-                    res2[s2[r]-'a'] +=1;
-                    res2[s2[l]-'a'] -=1;
-                    l+=1;
-                }
-            }
+        while(r<n){    
+            count2[s2[l]-'a']-=1;
+            count2[s2[r]-'a']+=1;
+            r+=1;
+            l+=1;
+            if(count1 == count2) return true;
         }
         
         return false;
