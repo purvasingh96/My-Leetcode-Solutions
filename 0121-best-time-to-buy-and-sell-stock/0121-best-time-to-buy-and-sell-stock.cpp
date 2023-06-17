3b30416485c7a2bd058e0f3c78d57adc8fd6cb57
Purva Singh <singhpurva2906@gmail.com>
2023-06-17 09:29:42 -0400

    Time: 147 ms (55.49%), Space: 93.3 MB (32.81%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // monotonic queue
-        deque<int> q;
+        int minPrice=INT_MAX;
         int profit=0;
         
         for(int i=0;i<prices.size();i++){
-            if(q.size()==0) q.push_back(prices[i]);
-            else {
-                while(q.size()!=0 && q.back() >= prices[i]){
-                    q.pop_back();
-                }
-                if(q.size()!=0){
-                    int diff = prices[i] - q.front();
-                    profit = max(profit, diff);
-                }
-                q.push_back(prices[i]);
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            } else if(prices[i] - minPrice > profit){
+                profit = prices[i] - minPrice;
             }
         }
         
