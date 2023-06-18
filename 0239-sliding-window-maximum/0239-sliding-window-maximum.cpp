e9cc50989c329640e652b175abc5dfbbc6254519
Purva Singh <singhpurva2906@gmail.com>
2023-06-18 12:31:15 -0400

    Time: 300 ms (84.55%), Space: 141.5 MB (33.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<pair<int, int>> q;
+        int n = nums.size();
+        vector<int> lft(n);
+        vector<int> rgt(n);
+        
+        lft[0] = nums[0];
+        rgt[n-1]=nums[n-1];
         int i=0;
+        
+        for (i = 0; i < n; i++)
+			{
+				lft[i] = nums[i];
+				 // If it start of block, then that element is max so far in the block
+				 // Else lft[i] is max so far in the block
+				if (i % k !=0) 
+					lft[i] = max(lft[i - 1], lft[i]);
+			}
+
+			rgt[n - 1] = nums[n - 1];
+			for (i = n - 2; i >= 0; i--)
+			{
+				rgt[i] = nums[i];
+				// same as for left, but now traversing from right
+				if (i % k != (k - 1))
+					rgt[i] = max(rgt[i], rgt[i + 1]);
+			}
+        
         vector<int> res;
-        
-        for(;i<k;i++){
-            while(!q.empty() && q.back().first < nums[i]){
-                    q.pop_back();
-                }
-            q.push_back({nums[i], i});
-        }
-        
-        res.push_back(q.front().first);
-        
-        for(;i<nums.size();i++){
-            // pop back to maintain decreasing monotonic queue
-            while(!q.empty() && q.back().first < nums[i]){
-                q.pop_back();
-            }
-            
-            q.push_back({nums[i], i});
-            
-            while(!q.empty() && (i - q.front().second) >= k){
-                q.pop_front();
-            }
-            
-            res.push_back(q.front().first);
-            
+        for(int i=0;i<=n-k;i++){
+            int ans = max(lft[i+k-1], rgt[i]);
+            res.push_back(ans);
         }
         
         return res;
