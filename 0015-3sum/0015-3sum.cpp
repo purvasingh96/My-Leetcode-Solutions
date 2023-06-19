704c1fe90a61c36f39f05d6401ffa64701972cab
Purva Singh <singhpurva2906@gmail.com>
2023-06-18 20:20:32 -0400

    Time: 115 ms (96.59%), Space: 25.9 MB (53.10%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,45 @@
 class Solution {
 private:
-    void dfs(int k, int target, int l, int r, vector<int>& temp, vector<vector<int>>& ans, vector<int>& nums){
-        if(k == 2){
-            while(l<r){
-                int sum = nums[l] + nums[r];
-                if(sum == target){
-                    temp.push_back(nums[l]);
-                    temp.push_back(nums[r]);
-                    ans.push_back(temp);
-                    temp.pop_back();
-                    temp.pop_back();
-                    while(l+1<r && nums[l]==nums[l+1]) {
-                        l+=1;
-                    }
-                    l+=1;
-                    r-=1;
-                } else if(sum > target) r-=1;
-                else l+=1;
-            }
-        }
-        
+    void twoSum(vector<int>& nums, vector<vector<int>>& res, int target, int i){
+        int l=i, r=nums.size()-1;
         while(l<r){
-            temp.push_back(nums[l]);
-            dfs(k-1, target-nums[l], l+1, r, temp, ans, nums);
-            temp.pop_back();
-            while(l+1<r && nums[l] == nums[l+1]) l+=1;
-            l+=1;
+            int sum = nums[l] + nums[r];
+            if(sum == target){
+                vector<int> temp = {-target, nums[l], nums[r]};
+                sort(temp.begin(), temp.end());
+                res.push_back(temp);
+                while(l<r && nums[l]==nums[l+1]){
+                    l+=1;
+                }
+                
+                while(r>l && nums[r] == nums[r-1]){
+                    r-=1;
+                }
+                
+                l+=1;
+                r-=1;
+            } else if(sum > target){
+                r-=1;
+            } else {
+                l+=1;
+            }
         }
         
     }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        vector<int> temp;
+        int left=0;
         vector<vector<int>> res;
-        int n = nums.size();
-        dfs(3, 0, 0, n-1, temp, res, nums);
+        
+        while(left<nums.size()){
+            twoSum(nums, res, -nums[left], left+1);
+            while(left < nums.size()-1 && nums[left] == nums[left+1]){
+                left+=1;
+            }
+            left+=1;
+        }
+        
         return res;
     }
 };
