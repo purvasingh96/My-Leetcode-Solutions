ffb0a52bd00c1e221040f8736fba7bd787728669
Purva Singh <singhpurva2906@gmail.com>
2023-06-20 11:04:06 -0400

    Time: 49 ms (21.70%), Space: 8.4 MB (16.16%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        if(t.length() > s.length()) return "";
         int c=0;
+        int l=0, r=0;
         unordered_map<char, int> ms;
         unordered_map<char, int> mt;
-        int l=0, r=0;
-        int minLen=INT_MAX;
         string ans="";
+        int len = INT_MAX;
         
         for(auto c:t) mt[c]+=1;
         
         for(;r<s.length();r++){
-            ms[s[r]]+=1;
-            
-            if(mt.find(s[r])!=mt.end() && ms[s[r]] <= mt[s[r]]){
-                c+=1;
+            if(mt.find(s[r])!=mt.end()){
+                ms[s[r]]+=1;
+                if(ms[s[r]] <= mt[s[r]]){
+                    c+=1;
+                }
             }
             
-            if(c == t.length()){
-                while(l<r && (mt.find(s[l]) == mt.end() || ms[s[l]] > mt[s[l]])){
+            if(c==t.length()){
+                
+                while(l<r && (mt.find(s[l]) == mt.end() || (ms[s[l]] > mt[s[l]])) ){
                     ms[s[l]]-=1;
                     l+=1;
                 }
-                if(r-l+1 < minLen){
-                    minLen = r-l+1;
-                    ans = s.substr(l, minLen);
+                
+                if(r-l+1 < len){
+                    len = r-l+1;
+                    ans = s.substr(l, len);
                 }
+                
             }
+            
         }
         
         return ans;
