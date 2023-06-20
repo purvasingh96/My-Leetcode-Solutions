0f9cbf17db6e2b02f4a24bcf349f5ea12936217b
Purva Singh <singhpurva2906@gmail.com>
2023-06-20 09:57:56 -0400

    Time: 331 ms (45.26%), Space: 137.1 MB (35.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> lft(n);
-        vector<int> rgt(n);
+        // TC: O(N), SC: O(K)
         
-        lft[0] = nums[0];
-        rgt[n-1]=nums[n-1];
-        int i=0;
-        
-        for (i = 0; i < n; i++)
-			{
-				lft[i] = nums[i];
-				 // If it start of block, then that element is max so far in the block
-				 // Else lft[i] is max so far in the block
-				if (i % k !=0) 
-					lft[i] = max(lft[i - 1], lft[i]);
-			}
-
-			rgt[n - 1] = nums[n - 1];
-			for (i = n - 2; i >= 0; i--)
-			{
-				rgt[i] = nums[i];
-				// same as for left, but now traversing from right
-				if (i % k != (k - 1))
-					rgt[i] = max(rgt[i], rgt[i + 1]);
-			}
+        deque<pair<int, int>> q;
         
         vector<int> res;
-        for(int i=0;i<=n-k;i++){
-            int ans = max(lft[i+k-1], rgt[i]);
-            res.push_back(ans);
+        int i=0;
+        
+       for(;i<k;i++){
+           while(!q.empty() && q.back().second < nums[i]){
+                   q.pop_back();
+               }
+               q.push_back({i, nums[i]});
+       }
+        
+        res.push_back(q.front().second);
+        
+        for(;i<nums.size();i++){
+            // pop_front: maintains current sliding window in deque
+            while(!q.empty() && (i-q.front().first+1) >k){
+                q.pop_front();
+            }
+            
+            // pop_back: maintains decreasing monotonic queue
+            while(!q.empty() && q.back().second < nums[i]){
+                q.pop_back();
+            }
+            
+            q.push_back({i, nums[i]});
+            
+            res.push_back(q.front().second);
         }
         
         return res;
+        
         
     }
 };
