5b48906a600142701ff24c12bf34ad9b67e47f3d
Purva Singh <singhpurva2906@gmail.com>
2023-06-20 10:14:19 -0400

    Time: 267 ms (97.91%), Space: 141.6 MB (33.65%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // TC: O(N), SC: O(K)
+        int n = nums.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
         
-        deque<pair<int, int>> q;
+        left[0] = nums[0];
+        right[n-1] = nums[n-1];
+        
+        
+        for(int i=1;i<n;i++){
+            if(i%k==0) left[i] = nums[i];
+            else left[i] = max(left[i-1], nums[i]);
+            
+            int idx = n-i-1;
+            if((idx+1)%k==0) right[idx] = nums[idx];
+            else right[idx] = max(right[idx+1], nums[idx]);
+        }
         
         vector<int> res;
-        int i=0;
         
-       for(;i<k;i++){
-           while(!q.empty() && q.back().second < nums[i]){
-                   q.pop_back();
-               }
-               q.push_back({i, nums[i]});
-       }
-        
-        res.push_back(q.front().second);
-        
-        for(;i<nums.size();i++){
-            // pop_front: maintains current sliding window in deque
-            while(!q.empty() && (i-q.front().first+1) >k){
-                q.pop_front();
-            }
-            
-            // pop_back: maintains decreasing monotonic queue
-            while(!q.empty() && q.back().second < nums[i]){
-                q.pop_back();
-            }
-            
-            q.push_back({i, nums[i]});
-            
-            res.push_back(q.front().second);
+        for(int i=0;i<=n-k;i++){
+            int ans = max(left[i+k-1], right[i]);
+            res.push_back(ans);
         }
         
         return res;
-        
-        
     }
 };
