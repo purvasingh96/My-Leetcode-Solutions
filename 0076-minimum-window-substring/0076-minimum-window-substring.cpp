b54f068e345ca037469c9ad65fe8a4264231b078
Purva Singh <singhpurva2906@gmail.com>
2023-06-20 14:44:25 -0400

    Time: 48 ms (22.28%), Space: 8.5 MB (12.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(t.length() > s.length()) return "";
         int c=0;
         int l=0, r=0;
-        unordered_map<char, int> ms;
-        unordered_map<char, int> mt;
-        string ans="";
+        string str="";
         int len = INT_MAX;
         
-        for(auto c:t) mt[c]+=1;
+        unordered_map<char, int> ms;
+        unordered_map<char, int> mt;
+        
+        for(auto c:t) mt[c] += 1;
         
         for(;r<s.length();r++){
+            
             if(mt.find(s[r])!=mt.end()){
                 ms[s[r]]+=1;
                 if(ms[s[r]] <= mt[s[r]]){
@@ -19,23 +20,26 @@ public:
                 }
             }
             
-            if(c==t.length()){
+            if(c == t.length()){
                 
-                while(l<r && (mt.find(s[l]) == mt.end() || (ms[s[l]] > mt[s[l]])) ){
+                while(l<r && (mt.find(s[l])==mt.end() || (ms[s[l]] > mt[s[l]]))){
                     ms[s[l]]-=1;
                     l+=1;
+                    
                 }
                 
                 if(r-l+1 < len){
                     len = r-l+1;
-                    ans = s.substr(l, len);
+                    str = s.substr(l,len);
                 }
                 
             }
             
+            
+            
         }
         
-        return ans;
+        return str;
         
     }
 };
