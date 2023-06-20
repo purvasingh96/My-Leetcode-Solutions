263a8cc402a4836b053d9af75629152c02b81933
Purva Singh <singhpurva2906@gmail.com>
2023-06-20 11:47:32 -0400

    Time: 0 ms (100.00%), Space: 6.1 MB (26.02%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,37 @@
 class Solution {
+private:
+    vector<vector<int>> p = {{1, 1}, {1, 0}};
+    vector<vector<int>> q = {{1, 1}, {1, 0}};
+    
 public:
     int fib(int n) {
         if(n<=1) return n;
         
-        vector<int> dp(n+1, 0);
-        dp[0] = 0;
-        dp[1] = 1;
+        multiply(p, n-1);
+        return p[0][0];
+    }
+    
+    void multiply(vector<vector<int>>& a, int n){
+        if(n<=1) return;
         
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        multiply(a, n/2);
+        helper(a, a);
+        
+        if(n%2!=0){
+            helper(a, q);
         }
         
-        return dp[n];
-            
     }
+    
+    void helper(vector<vector<int>>& x, vector<vector<int>>& y){
+        int a = x[0][0], b = x[0][1], c=x[1][0], d=x[1][1];
+        int e = y[0][0], f = y[0][1], g=y[1][0], h=y[1][1];
+        
+        x[0][0] = a*e + b*g;
+        x[0][1] = a*f + b*h;
+        x[1][0] = c*e + d*g;
+        x[1][1] = c*f + d*h;
+        
+    }
+    
 };
