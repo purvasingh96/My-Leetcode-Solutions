f14119618247365661994ac9182acc676ad2d3ef
Purva Singh <singhpurva2906@gmail.com>
2023-06-25 10:21:34 -0400

    Time: 0 ms (100.00%), Space: 6.8 MB (96.48%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -16,7 +16,7 @@ private:
             int new_y = y+dy[i];
             
             if(checkBounds(new_x, new_y, board)){
-                if(board[new_x][new_y] == 1 || board[new_x][new_y] == 4 || board[new_x][new_y] == 5){
+                if(board[new_x][new_y] == 1){
                     live+=1;
                 } else{
                     dead+=1;
@@ -28,42 +28,27 @@ private:
         return {live, dead};
     }
     
-    
-    void changeState(int x, int y, vector<vector<int>>& board){
-        auto p = countLiveDead(x, y, board);
-        int live = p.first, dead = p.second;
-        int state = board[x][y];
-        
-        if(state == 1){
-            if(live < 2) board[x][y] = 5;
-            else if(live >= 2 && live<=3) board[x][y] = 4;
-            else if(live > 3) board[x][y]=5;
-        }
-        
-        else if(state==0) {
-            if(live == 3) board[x][y]=2;
-            else board[x][y] = 3;
-        }
-        
-    }
+
         
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                changeState(i, j, board);
-            }
-        }
+        vector<vector<int>> boardCopy = board;
         
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(board[i][j] == 2 || board[i][j] == 4 || board[i][j]==1){
-                    board[i][j]=1;
-                } else if(board[i][j] == 3 || board[i][j] == 5 || board[i][j]==0){
-                    board[i][j] = 0;
+                auto p = countLiveDead(i, j, board);
+                int live = p.first, dead=p.second;
+                
+                if(board[i][j] == 1){
+                    if(live < 2 || live > 3) boardCopy[i][j] = 0;
+                    else if(live >=2 && live <= 3) boardCopy[i][j] = 1;
+                } else if(board[i][j] == 0){
+                    if(live == 3) boardCopy[i][j] = 1;
+                    else boardCopy[i][j] = 0;
                 }
             }
         }
         
+        board = boardCopy;
     }
 };
