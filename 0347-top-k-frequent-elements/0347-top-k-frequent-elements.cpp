b35e5107a6869e1420bed340d158fd868d21270b
Purva Singh <singhpurva2906@gmail.com>
2023-06-30 12:58:18 -0400

    Time: 16 ms (80.94%), Space: 13.5 MB (96.68%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    int partitionAndSort(int left, int right, int pivot_idx, unordered_map<int, int>& freq, vector<int>& res){
+        
+        int store_idx = left;
+        int pivot_freq = freq[res[pivot_idx]];
+        swap(res[pivot_idx], res[right]);
+        
+        for(int i=left;i<=right;i++){
+            if(freq[res[i]] < pivot_freq){
+                swap(res[i], res[store_idx]);
+                store_idx+=1;
+            }
+        }
+        
+        swap(res[store_idx], res[right]);
+        return store_idx;
+        
+    }
+    
+    
+    void quickSelect(int left, int right, int k_smallest, unordered_map<int, int>& freq, vector<int>& res){
+        
+        int pivot_idx = left + rand()%(right - left+1);
+        
+        pivot_idx = partitionAndSort(left, right, pivot_idx, freq, res);
+        
+        if(pivot_idx == k_smallest) return;
+        else if(pivot_idx > k_smallest){
+            quickSelect(left, pivot_idx-1, k_smallest, freq, res);
+        } else {
+            quickSelect(pivot_idx+1, right, k_smallest, freq, res);
+        }
+        
+    }
+
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for(auto x:nums) m[x]+=1;
+        
+        vector<int> res;
+        for(auto x:m) res.push_back(x.first);
+        
+        int n = res.size();
+        quickSelect(0, n-1, n-k, m, res);
+        
+        vector<int> ans;
+        for(int i=n-k;i<n;i++) ans.push_back(res[i]);
+        
+        return ans;
+        
+        
+    }
+};
