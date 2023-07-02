c0252dab05b30c4bd16cac64c07b94525a272122
Purva Singh <singhpurva2906@gmail.com>
2023-07-02 09:02:30 -0400

    Time: 3 ms (46.94%), Space: 8.2 MB (41.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,15 +1,25 @@
 class Solution {
+private:
+    void swap(int i, int j, vector<int>& nums){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> freq(3, 0);
-        for(auto x:nums) freq[x]+=1;
+        int l=0, r=nums.size()-1;
         int i=0;
-        int k=0;
-        while(i<nums.size()){
-            while(freq[k]--){
-                nums[i++]=k;        
-            }
-            k+=1;
+        
+        while(i<=r){
+            if(nums[i]==0){
+                swap(l, i, nums);
+                l+=1;
+            } else if(nums[i] == 2){
+                swap(r, i, nums);
+                r-=1;
+                i-=1;
+            } 
+            i+=1;
         }
         
         
