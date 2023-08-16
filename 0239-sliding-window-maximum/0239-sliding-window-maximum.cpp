606570d070d6f71678f343bebf1fb0cc2d75bb77
Purva Singh <singhpurva2906@gmail.com>
2023-08-16 06:30:28 -0400

    Time: 211 ms (84.35%), Space: 137 MB (33.15%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
+        // decreasing monotonic queue
+        deque<pair<int, int>> q;
+        vector<int> ans;
+        int i=0;
         
-        left[0] = nums[0];
-        right[n-1] = nums[n-1];
+        for(;i<k;i++){
+            while(!q.empty() && q.back().first < nums[i]){
+                q.pop_back();
+            }
+            q.push_back({nums[i], i});
+        }
         
+        ans.push_back(q.front().first);
         
-        for(int i=1;i<n;i++){
-            if(i%k==0) left[i] = nums[i];
-            else left[i] = max(left[i-1], nums[i]);
+        for(;i<nums.size();i++){
             
-            int idx = n-i-1;
-            if((idx+1)%k==0) right[idx] = nums[idx];
-            else right[idx] = max(right[idx+1], nums[idx]);
+            // decreasing mono queue
+            while(!q.empty() && q.back().first <nums[i]){
+                q.pop_back();
+            }
+            
+            // out of bounds
+            while(!q.empty() && i-q.front().second+1 > k){
+                q.pop_front();
+            }
+            
+            q.push_back({nums[i], i});
+            ans.push_back(q.front().first);
         }
         
-        vector<int> res;
-        
-        for(int i=0;i<=n-k;i++){
-            int ans = max(left[i+k-1], right[i]);
-            res.push_back(ans);
-        }
-        
-        return res;
+        return ans;
     }
 };
