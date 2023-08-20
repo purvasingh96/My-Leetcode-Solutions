6f92f8893a7e164d55d72007cc05fadca991d2ee
Purva Singh <singhpurva2906@gmail.com>
2023-08-20 11:14:42 -0400

    Time: 0 ms (100.00%), Space: 7.6 MB (60.48%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,31 @@
 class Solution {
 private:
-    void backtrack(vector<int>& nums, vector<int>& temp, vector<vector<int>>& res, unordered_set<int>& visited){
+    void backtrack(int i, vector<int>& nums, vector<int>& temp, unordered_map<int, bool>& visited, vector<vector<int>>& res){
+        
         if(temp.size()==nums.size()){
             res.push_back(temp);
             return;
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(visited.count(nums[i]) == 0){
-                visited.insert(nums[i]);
-                temp.push_back(nums[i]);
-                backtrack(nums, temp, res, visited);
+        
+        for(int k=0;k<nums.size();k++){
+            if(!visited[nums[k]]){
+                visited[nums[k]] = true;
+                temp.push_back(nums[k]);
+                backtrack(k, nums, temp, visited, res);
                 temp.pop_back();
-                visited.erase(nums[i]);
-            }    
+                visited[nums[k]]=false;
+            }
         }
         
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        //backtrack
-        //N!
-        vector<vector<int>> res;
+        unordered_map<int, bool> visited;
+        for(int i=-10;i<=10;i++) visited[i]=false;
         vector<int> temp;
-        unordered_set<int> visited;
-        backtrack(nums, temp, res, visited);
+        vector<vector<int>> res;
+        backtrack(0, nums, temp, visited, res);
         return res;
     }
 };
