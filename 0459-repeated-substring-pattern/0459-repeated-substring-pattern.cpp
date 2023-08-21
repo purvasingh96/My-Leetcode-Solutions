075135bb0698f20afc697742568ec4bcf68567e4
Purva Singh <singhpurva2906@gmail.com>
2023-08-21 06:30:41 -0400

    Time: 135 ms (20.29%), Space: 13.9 MB (41.41%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,32 +1,42 @@
 class Solution {
-private:
-    vector<int> generateLPS(string& s){
-        int i=1, j=0, n = s.length();
-        vector<int> lps(n, 0);
-        while(i<n){
-            if(s[i] == s[j]){
-                lps[i]=j+1;
-                i+=1;
-                j+=1;
-            } else{
-                if(j>0) j = lps[j-1];
-                else i+=1;
-            }
-        }
-        return lps;
-    }
 public:
     bool repeatedSubstringPattern(string s) {
-        vector<int> lps = generateLPS(s);
-        if(lps.back() == 0) return false;
-        int n = s.length();
-        int len = n-lps.back();
+        if(s.length()<=1) return false;
         
-        string temp = s.substr(0, len);
-        int freq = n/(len);
-        if(n%len!=0) return false;
-        string ans="";
-        while(freq--) ans += temp;
-        return ans == s;
+        int n = s.length();
+        //cout<<n<<"\n";
+        vector<int> lps(n, 0);
+        
+        int i=0, j=1;
+        
+        while(j<s.length()){
+            while(i>0 && s[i]!=s[j]){
+                i = lps[i-1];
+            }
+            if(s[i] == s[j]){
+                lps[j]=i+1;
+                i+=1;
+            }
+            j+=1;
+            //for(auto x:lps) cout<<x<<" ";
+            //cout<<"\n";
+        }
+        
+        if(lps.back() == 0) return false;
+        
+        for(auto x:lps) cout<<x<<" ";
+        int t = n/(n - lps.back()); // 3
+        int k = n - lps.back(); // 3
+        string temp = s.substr(0, k);
+        
+        string res = "";
+        while(t--){
+            res += temp;
+        }
+        
+        return res == s;
+        
+        
+        
     }
 };
