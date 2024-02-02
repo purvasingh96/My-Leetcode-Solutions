d9e7285b930cfd38f8dfe8e3bb597209f0e6bd34
Purva Singh <singhpurva2906@gmail.com>
2024-02-02 08:37:25 -0500

    Time: 101 ms (76.28%), Space: 71 MB (28.19%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
-        //decreasing monotonic stack
-        stack<int> st;
-        for(int i=0;i<heights.size();i++){
-            
-            while(!st.empty() && heights[st.top()] <= heights[i]){
-                st.pop();
-            }
-            
-            st.push(i);
-        }
-        
         vector<int> res;
-        while(!st.empty()) {
-            res.push_back(st.top());
-            st.pop();
+        for(int i=0;i<heights.size();i++){            
+            if(res.size() == 0 || heights[res.back()] > heights[i]){
+                res.push_back(i);
+            } else{
+                while(!res.empty() && heights[res.back()] <= heights[i]){
+                    res.pop_back();
+                }
+                res.push_back(i);
+            }
         }
         
-        reverse(res.begin(), res.end());
         return res;
     }
 };
