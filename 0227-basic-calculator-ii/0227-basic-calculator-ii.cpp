8556bb2a32e543b215d183072a7713baf88de0f1
Purva Singh <singhpurva2906@gmail.com>
2024-02-03 19:49:47 -0500

    Time: 17 ms (71.15%), Space: 13.6 MB (26.00%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,45 +1,54 @@
 class Solution {
+private:
+    bool isdigit(char c){
+        return c>='0' && c<='9';
+    }
 public:
     int calculate(string s) {
-        
         stack<int> st;
-        bool minus=false;
-        int num=0;
-        char op = '+';
+        char sign = '+';
         
+        // O(N);
         for(int i=0;i<s.length();i++){
-            char currChar = s[i];
-            if(isdigit(currChar)){
-                num = num*10 + (currChar - '0');
-            }
-            
-            if(!isdigit(currChar) && currChar!=' ' || i==s.length()-1){
-                if(op == '+'){
-                    st.push(num);
-                } else if(op == '-'){
-                    st.push(-num);
-                } else if(op == '*'){
-                    int x = st.top();
-                    st.pop();
-                    st.push(x*num);
-                } else if(op == '/'){
-                    int x = st.top();
-                    st.pop();
-                    st.push(x/num);
+            if(s[i] == ' '){
+                continue;
+            } else {
+                if(isdigit(s[i])){
+                    int d=0;
+                    while(i<s.length() && isdigit(s[i])){
+                        d = d*10 + (s[i]-'0');
+                        i+=1;
+                    }
+                    if(sign == '+'){
+                        st.push(d);
+                    } else if(sign == '-'){
+                        st.push(-1*d);
+                    } else if(sign == '/'){
+                        int top = st.top();
+                        st.pop();
+                        st.push(top/d);
+                    } else if(sign == '*'){
+                        int top = st.top();
+                        st.pop();
+                        st.push(top*d);
+                    }
+                    i-=1;
+                } else {
+                    sign = s[i];
                 }
-                op = currChar;
-                num=0;
             }
-            
         }
         
-        int res=0;
+        // O(L)
+        int ans=0;
         while(!st.empty()){
-            res += st.top();
+            int top = st.top();
             st.pop();
+            ans += top;
         }
         
-        return res;
+        // O(N+L), O(L)
         
+        return ans;
     }
 };
