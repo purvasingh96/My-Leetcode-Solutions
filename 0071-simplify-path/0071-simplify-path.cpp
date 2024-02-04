383181b6a8cd26b2b838a4b35103ea98784cc97e
Purva Singh <singhpurva2906@gmail.com>
2024-02-04 09:19:48 -0500

    Time: 0 ms (100.00%), Space: 10 MB (32.10%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,85 +1,40 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        if(path.back()!='/') path += "/";
-        stack<string> st;
-        int n = path.length();
-        int i=1;
-        // path will always start with /
-        st.push("/");
+        vector<string> st;
+        string res="/";
         
-        while(i<n){
-            if(st.top() == "/"){
-                if(path[i]=='/') {
-                    i+=1;
-                    continue;
-                }
-                
-                else if(path[i]=='.'){
-                    if(i+2<n && path.substr(i, 3)=="../"){
-                        if(!st.empty()) st.pop();
-                        if(!st.empty()) st.pop();
-                        i+=2;
-                    } 
-                    
-                    else if(i+1<n && path.substr(i, 2)=="./"){
-                        i+=1;
-                    }
-                    
-                    else {
-                        string temp="";
-                        while(path[i]!='/'){
-                            temp.push_back(path[i]);
-                            i+=1;
-                        }
-                            
-                        st.push(temp);
-                    }
-                    i+=1;
-                } 
-                
-                
-                else {
-                    string temp="";
-                    while(path[i]!='/'){
-                        temp.push_back(path[i]);
-                        i+=1;
-                    }
-                    i+=1;
-                    st.push(temp);
-                }
-                
-                if(st.empty() || st.top()!="/") st.push("/");
-            }
+        // split based on "/"
+        stringstream ss(path);
+        
+        string token;
+        
+        while(getline(ss, token, '/')){
+         if(token!=""){
+             if(token == ".."){
+                 // one directory up
+                 if(st.size()!=0){
+                     st.pop_back();
+                 }
+             } else if(token == ".") {
+                 // curr directory
+                 continue;
+             }
+             else {
+                 st.push_back(token);
+             }
+         }
         }
         
-        
-        stack<string> t;
-        while(!st.empty()){
-            t.push(st.top());
-            st.pop();
+        for(auto s:st){
+            res += s; 
+            res+="/";
         }
         
-        
-        
-        string ans="";
-        while(!t.empty()){
-            if(t.top() == "/" && ans.back()=='/') {
-                t.pop();
-                continue;
-            } else {
-                ans += t.top();
-                t.pop();
-            }
+        if(res.length()>1){
+            res.pop_back();
         }
         
-        
-        if(ans.back() == '/') ans.pop_back();
-        
-        if(ans[0]!='/') ans = "/"+ans;
-        //cout<<"ans: "<<ans;
-        
-        return ans;
-        
+        return res;
     }
 };
