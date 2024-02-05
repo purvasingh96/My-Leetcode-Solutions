77c5b4dee6c0ae4851c59fcf0d8c21eb60bcc724
Purva Singh <singhpurva2906@gmail.com>
2024-02-04 19:29:44 -0500

    Time: 4 ms (12.44%), Space: 7.8 MB (5.93%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.cpp
@@ -1,37 +1,37 @@
 class Solution {
-private:
-    bool isDigit(char c){
-        return c>='0' && c<='9';
-    }
 public:
     bool validWordAbbreviation(string word, string abbr) {
         stack<char> st;
+        
         for(int i=word.length()-1;i>=0;i--){
             st.push(word[i]);
         }
         
-        for(int j=0;j<abbr.length();j++){
-            if(st.empty() && j<abbr.length()){
-                return false;
-            }
-            if(st.top() == abbr[j]){
+        for(int i=0;i<abbr.length();i++){
+            if(st.empty()) return false;
+            if(abbr[i] == st.top()){
                 st.pop();
-            }
-            else {
-                if(isDigit(abbr[j])){
-                    // leading zero;
-                    if(abbr[j] == '0'){
+            } else {
+                if(isdigit(abbr[i])){
+                    
+                    if(abbr[i]=='0'){
+                        // leading zero;
                         return false;
-                    }
-                    int len=0;
-                    while(j<abbr.length() && isDigit(abbr[j])){
-                        len = len*10 + (abbr[j]-'0');
-                        j+=1;
-                    }
-                    j-=1;
-                    if(len > st.size()) return false;
-                    while(len--){
-                        st.pop();
+                    } else {
+                        int j=i;
+                        int digit=0;
+                        while(j<abbr.length() && isdigit(abbr[j])){
+                            digit = digit*10 + (abbr[j]-'0');
+                            j+=1;
+                        }
+                        if(digit > st.size()){
+                            return false;
+                        } else {
+                            while(digit--){
+                                st.pop();
+                            }
+                            i = j-1;
+                        }
                     }
                     
                 } else {
@@ -39,6 +39,7 @@ public:
                 }
             }
         }
+        
         return st.empty();
     }
 };
