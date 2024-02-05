06e1407888419604fda61c82404d74c711356e34
Purva Singh <singhpurva2906@gmail.com>
2024-02-04 20:26:33 -0500

    Time: 12 ms (98.80%), Space: 13.7 MB (16.15%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        int open=0, balance =0;
+        int balance =0;
+        vector<char> t;
+        
+        for(auto c:s){
+            if(c == ')'){
+                balance -=1;
+                if(balance <0){
+                    balance+=1;
+                    continue;
+                }
+            } else if(c == '('){
+                balance +=1;
+            }
+            t.push_back(c);
+        }
+        
+        if(balance <= 0){
+            // all invalid ) have been removed
+            string temp ="";
+            for(auto c:t) temp += c;
+            return temp;
+        }
+        
+        //for(auto x:t) cout<<x;
+        //cout<<"\n";
         string res="";
-        for(auto c:s) {
-            if(c == '('){
-                open+=1;
-                balance+=1;
-            } else if(c == ')'){
-                if(balance ==0) continue;
-                balance-=1;
+        //cout<<"balance:: "<<balance;
+        for(int i=t.size()-1;i>=0;i--){
+            //cout<<"t[i]:: "<<t[i]<<"\n";
+            if(t[i] == '('){
+                balance -=1;
+                if(balance >= 0){
+                    continue;
+                } else {
+                    res += t[i];
+                }
+            } else {
+                res += t[i];
             }
-            res+=c;
         }
-        
-        string ans="";
-        int openToKeep = open - balance;
-        for(int i=0;i<res.length();i++){
-            if(res[i] == '('){
-                openToKeep -=1;
-                if(openToKeep<0) continue;
-            }
-            ans +=res[i];
-        }
-        
-        return ans;
+        cout<<"res:: "<<res<<"\n";
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
