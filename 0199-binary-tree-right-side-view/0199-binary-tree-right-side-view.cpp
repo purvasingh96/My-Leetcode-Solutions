36e55f23254d6b184da9afc0b5b4b960145a80c4
Purva Singh <singhpurva2906@gmail.com>
2024-02-06 21:03:01 -0500

    Time: 0 ms (100.00%), Space: 14.1 MB (18.91%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,23 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
- 
-    vector<int> rightSideView(TreeNode *root) {
+    vector<int> rightSideView(TreeNode* root) {
         queue<TreeNode*> q;
-        
-        q.push(root);
         vector<int> res;
         if(!root) return res;
+        q.push(root);
+        res.push_back(root->val);
         
         while(!q.empty()){
             int sz = q.size();
+            int rightmost=true;
+            
             while(sz--){
-                auto f = q.front();
+                auto f = q.front(); 
                 q.pop();
-                if(f->left) q.push(f->left);
-                if(f->right) q.push(f->right);
-                if(sz==0) res.push_back(f->val);
+                if(f->right){
+                    q.push(f->right);
+                    if(rightmost){
+                        res.push_back(f->right->val);
+                        rightmost = false;
+                    } 
+                }
+                if(f->left){
+                    q.push(f->left);
+                    if(rightmost){
+                        res.push_back(f->left->val);
+                        rightmost = false;
+                    }
+                }
             }
+            
         }
+        
         return res;
     }
 };
