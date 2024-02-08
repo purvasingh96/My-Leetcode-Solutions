bfed59a67e7ca03a206c8e455b257c5a491ba1a6
Purva Singh <singhpurva2906@gmail.com>
2024-02-08 09:00:40 -0500

    Time: 7 ms (72.19%), Space: 19.4 MB (76.43%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.cpp
@@ -11,17 +11,24 @@
  */
 class Solution {
 private:
-    void dfs(TreeNode* root, double target, map<double, vector<int>>& res){
+    void dfs(TreeNode* root, vector<int>& res){
         if(!root) return;
-        
-        dfs(root->left, target, res);
-        res[abs(target - root->val)].push_back(root->val);
-        dfs(root->right, target, res);
+        dfs(root->left, res);
+        res.push_back(root->val);
+        dfs(root->right, res);
     }
 public:
     int closestValue(TreeNode* root, double target) {
-        map<double, vector<int>> res;
-        dfs(root, target, res);
-        return res.begin()->second[0];
+        vector<int> res;
+        dfs(root, res);
+        
+        sort(res.begin(), res.end(), [&](const int& a, const int& b){
+            if(abs(target - a) != abs(target -b)){
+                return abs(target - a) < abs(target-b);
+            }
+            return a<b;
+        });
+        
+        return res[0];
     }
 };
