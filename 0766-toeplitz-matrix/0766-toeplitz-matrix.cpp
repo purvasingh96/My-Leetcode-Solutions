d65f9547c20bb55548ba6d27c70aae375e909d4f
Purva Singh <singhpurva2906@gmail.com>
2024-02-10 16:47:43 -0500

    Time: 3 ms (99.36%), Space: 20.4 MB (15.73%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,34 +1,18 @@
 class Solution {
-private:
-    bool topHalf(vector<vector<int>>& matrix){
-        int r, c, num;
-        for(int j=0;j<matrix[0].size();j++){
-            r=0, c=j, num = matrix[r][c];
-            while(r<matrix.size() && c<matrix[0].size()){
-                if(matrix[r][c]!=num) return false;
-                r+=1;
-                c+=1;
-            }
-            
-        }
-        return true;
-    }
-    
-    bool bottomHalf(vector<vector<int>>& matrix){
-        int r, c, num;
-        for(int i=1;i<matrix.size();i++){
-            r = i, c=0, num = matrix[r][c];
-            while(r<matrix.size() && c<matrix[0].size()){
-                if(matrix[r][c]!=num) return false;
-                r+=1;
-                c+=1;
-            }
-        }
-        return true;
-    }
-    
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        return topHalf(matrix) && bottomHalf(matrix);
+        int m =matrix.size(), n = matrix[0].size();
+        vector<int> prev = matrix[0];
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=prev[j-1]){
+                    return false;
+                }
+            }
+            prev = matrix[i];
+        }
+        
+        return true;
     }
 };
