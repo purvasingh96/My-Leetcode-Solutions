1f48209ce674b1637d581dcb0aae4967afb9060a
Purva Singh <singhpurva2906@gmail.com>
2024-02-11 17:37:08 -0500

    Time: 265 ms (17.31%), Space: 49.3 MB (36.99%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,69 +1,60 @@
 class Solution {
 private:
-    bool isValid(string& s){
-        int balance=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i] == ')'){
-                balance -=1;
-                if(balance <0){
-                    return false;
+ 
+    int invalids(string s){
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push('(');
+            }
+            else if(s[i]==')'){
+                if(st.size()==0){
+                    st.push(')');
                 }
-            } else if(s[i] == '('){
-                balance +=1;
+                else if(st.top()==')'){
+                    st.push(')');
+                }
+                else if(st.top()=='('){
+                    st.pop();
+                }
             }
         }
         
-        return balance == 0;
+        int invalid=st.size(); //minimum removals
+        
+        return invalid;
+    }
+    
+    void dfs(string s, int invalid, unordered_set<string>& st, unordered_map<string, bool>& visited){
+        
+        if(invalid == 0){
+            if(!invalids(s)){
+                st.insert(s);
+                return;
+            }    
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i] == ')' || s[i] == '('){
+                string temp = s.substr(0, i) + s.substr(i+1);
+                if(visited[temp] == false){
+                    visited[temp]=true;
+                    dfs(temp, invalid-1, st, visited);
+                } 
+            }
+           
+        }
+        
+        
     }
 public:
     vector<string> removeInvalidParentheses(string s) {
-        queue<string> q;
-        q.push(s);
-        
-        
-        if(isValid(s)){
-            vector<string> ans;
-            ans.push_back(s);
-            return ans;
-        }
-        unordered_set<string> res;
+        int invalid = invalids(s);
         unordered_map<string, bool> visited;
-        while(!q.empty()){
-            int sz = q.size();
-            bool found=false;
-            while(sz--){
-                auto f=q.front();
-                q.pop();
-                
-                    for(int i=0;i<f.length();i++){
-                        while(i<f.length()-1 && f[i] == f[i+1]){
-                            i+=1;
-                        }
-                        if(f[i] == ')' || f[i] == '('){
-                            string t = f.substr(0, i) + f.substr(i+1);
-                            if(visited[t]==false){
-                                visited[t] = true;
-                                if(isValid(t)){
-                                    found=true;
-                                    res.insert(t);
-                                } else {
-                                    q.push(t);
-                                }
-                            }
-                            
-                            
-                        }
-                    }
-                
-            }
-             if(found){
-                    vector<string> ans(res.begin(), res.end());
-                    return ans;
-                }
-        }
+        unordered_set<string> st;
         
-        vector<string> ans(res.begin(), res.end());
-        
-        return ans;
+        dfs(s, invalid, st, visited);
+        vector<string> res(st.begin(), st.end());
+        return res;
     }
 };
