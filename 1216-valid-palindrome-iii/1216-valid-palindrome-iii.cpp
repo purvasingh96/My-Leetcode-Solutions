24458d3840c25e22f5d7ecd3f91fe232b254c07b
Purva Singh <singhpurva2906@gmail.com>
2024-02-11 18:13:39 -0500

    Time: 21 ms (61.55%), Space: 18.1 MB (12.68%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     bool isValidPalindrome(string s, int k) {
-      // len(s) - LCS between s and rev(s) should be <=k
         int n = s.length();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         string og = s;
         reverse(s.begin(), s.end());
+        string rev = s;
         
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                
-                if(i==0 || j==0) dp[i][j] = 0;
-                
-                else if(og[i-1] == s[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                } else{
-                    
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(og[i-1] == rev[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                    
                 }
-                
             }
         }
         
-        int lcs = dp[n][n];
-        cout<<lcs;
-        
-        return s.length()-lcs <= k;
+        int ans = dp[n][n];
+        return n - ans <= k;
     }
 };
