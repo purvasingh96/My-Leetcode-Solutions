d6466e46bc0d5d65a1b0700146866fd98028eed8
Purva Singh <singhpurva2906@gmail.com>
2024-02-14 22:49:18 -0500

    Time: 253 ms (97.14%), Space: 108 MB (81.49%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,39 +1,39 @@
-class DisjointSet{
+class UnionFind{
 public:
-    vector<int> root, size;
-    DisjointSet(int n){
-        root.resize(n+1);
-        size.resize(n+1, 1);
-        for(int i=0; i<=n;i++){
+    vector<int> root;
+    vector<int> size;
+    UnionFind(int n){
+        root.resize(n);
+        size.resize(n, 1);
+        for(int i=0;i<n;i++){
             root[i]=i;
         }
     }
     
-    int find(int i){
-        if(root[i] == i) return i;
-        return root[i] = find(root[i]);
+    int find(int x){
+        if(root[x] == x){
+            return x;
+        }
+        return root[x] = find(root[x]);
     }
     
-    void union_(int x, int y){
+    void merge(int x, int y){
         int rx = find(x);
         int ry = find(y);
-        
         
         if(rx!=ry){
             if(size[rx] >= size[ry]){
                 root[ry]=rx;
                 size[rx]+=size[ry];
             } else {
-                root[rx]=ry;
+                root[rx] = ry;
                 size[ry]+=size[rx];
             }
         }
-            
     }
 };
-
 class Solution {
-private:
+    private:
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
     
@@ -43,17 +43,17 @@ private:
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        DisjointSet ds(n*n);
+        UnionFind uf(n*n);
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]){
-                    for(int d=0;d<4;d++){
-                        int r = dx[d]+i;
-                        int c = dy[d]+j;
+                if(grid[i][j]==1){
+                    for(int k=0;k<4;k++){
+                        int new_x = i+dx[k];
+                        int new_y = j+dy[k];
                         
-                        if(isValid(r, c, n) && grid[r][c]){
-                            ds.union_(n*r+c, n*i+j);
+                        if(isValid(new_x, new_y, n) && grid[new_x][new_y]==1){
+                            uf.merge(n*i+j, new_x*n+new_y);
                         }
                     }
                 }
@@ -63,28 +63,31 @@ public:
         int ans=INT_MIN;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
+                
+                if(grid[i][j] == 0){
                     set<int> st;
-                    for(int d=0;d<4;d++){
-                        int r = dx[d]+i;
-                        int c = dy[d]+j;
+                    for(int k=0;k<4;k++){
+                        int new_x = i+dx[k];
+                        int new_y = j+dy[k];
                         
-                        if(isValid(r, c, n) && grid[r][c]){
-                            st.insert(ds.find(n*r+c));
+                        if(isValid(new_x, new_y, n) && grid[new_x][new_y]==1){
+                            st.insert(uf.find(new_x*n+new_y));
                         }
                     }
-                    
-                    int temp=1;
+                    int sum=1;
+                
                     for(auto x:st){
-                        temp += ds.size[x];
+                        sum += uf.size[x];
                     }
-                    
-                    ans = max(ans, temp);
+                    //cout<<sum<<"\n";
+                    ans = max(ans, sum);
                 }
+                
+                
             }
         }
         
+        return ans==INT_MIN? n*n : ans;
         
-        return ans==INT_MIN ? n*n : ans;
     }
 };
