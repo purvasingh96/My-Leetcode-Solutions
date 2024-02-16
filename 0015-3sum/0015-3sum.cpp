15efd8474ac4c542be800faaf8d22672965b83f3
Purva Singh <singhpurva2906@gmail.com>
2024-02-16 09:13:16 -0500

    Time: 129 ms (53.27%), Space: 37 MB (37.18%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,13 +1,28 @@
 class Solution {
 private:
     void twoSum(vector<int>& nums, int target, int start, set<vector<int>>& res){
-        //cout<<"target:: "<<target<<" start: "<<start<<"\n";
         int i=start, j=nums.size()-1;
+        
         while(i<j){
-            if(nums[i] + nums[j] == target){
-                //cout<<"here";
-                
-                res.insert({-target, nums[i], nums[j]});
+            int sum = nums[i] + nums[j];
+            
+            if(sum > target){
+                while(j>i && nums[j] == nums[j-1]){
+                    j-=1;
+                }
+                j-=1;
+            }
+            
+            else if(sum < target){
+                while(i<j && nums[i] == nums[i+1]){
+                    i+=1;
+                }
+                i+=1;
+            }
+            
+            else if(sum == target){
+                vector<int> temp = {-target, nums[i], nums[j]};
+                res.insert(temp);
                 while(j>i && nums[j] == nums[j-1]){
                     j-=1;
                 }
@@ -18,38 +33,18 @@ private:
                 i+=1;
                 j-=1;
             }
-            else {
-                if(nums[i] + nums[j] > target){
-                    while(j>i && nums[j] == nums[j-1]){
-                        j-=1;
-                    }
-                    j-=1;
-                    
-                }
-                else if(nums[i] + nums[j] < target){
-                    while(i<j && nums[i] == nums[i+1]){
-                        i+=1;
-                    }
-                    i+=1;
-                }
-            }
-            
-            
         }
     }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         int start=1;
         set<vector<int>> res;
-        
+        sort(nums.begin(), nums.end());
         while(start<nums.size()){
             twoSum(nums, -nums[start-1], start, res);
             start+=1;
         }
-        
         vector<vector<int>> ans(res.begin(), res.end());
-        
         return ans;
     }
 };
