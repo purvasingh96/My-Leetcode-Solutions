7bb3115832a1b19da4004e0977f8b7da241438d5
Purva Singh <singhpurva2906@gmail.com>
2024-02-17 21:54:57 -0500

    Time: 380 ms (84.37%), Space: 208 MB (44.14%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
@@ -3,36 +3,40 @@ public:
     vector<vector<int>> findRLEArray(vector<vector<int>>& e1, vector<vector<int>>& e2) {
         int i=0, j=0;
         vector<vector<int>> res;
+        
         while(i<e1.size() && j<e2.size()){
             int val, count;
             val = e1[i][0] * e2[j][0];
             
-            if(e2[j][1] > e1[i][1]){
-                e2[j][1] -= e1[i][1];                
+            if(e1[i][1] < e2[j][1]){
                 count = e1[i][1];
+                e2[j][1] -= e1[i][1];
+                if(e2[j][1]==0){
+                    j+=1;
+                }
                 i+=1;
-            } else if(e1[i][1] > e2[j][1]) {
+            } else if(e2[j][1] < e1[i][1]){
+                count = e2[j][1];
                 e1[i][1] -= e2[j][1];
-                count =e2[j][1];
+                if(e1[i][1] == 0){
+                    i+=1;
+                }
                 j+=1;
             } else if(e1[i][1] == e2[j][1]){
-                count = e1[i][1];
-                e1[i][1]=0;
-                e2[j][1]=0;
+                count = e2[j][1];
                 i+=1;
                 j+=1;
             }
             
             if(res.size()!=0 && res.back()[0] == val){
-                // combine
-                auto back = res.back();
+                count += res.back()[1];
                 res.pop_back();
-                count += back[1];
             }
-            
             res.push_back({val, count});
         }
         
+        
         return res;
+        
     }
 };
