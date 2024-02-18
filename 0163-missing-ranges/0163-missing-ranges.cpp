2b23cb47638920584f7d3691a4797a08e53cd20a
Purva Singh <singhpurva2906@gmail.com>
2024-02-18 13:35:02 -0500

    Time: 4 ms (5.25%), Space: 8.4 MB (5.95%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        int expected=lower;
+        vector<vector<int>> res;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(nums[i] != expected){
+                if(expected <= nums[i]-1){
+                    res.push_back({expected, nums[i]-1});    
+                }
+                
+            }
+            expected = nums[i]+1;
+        }
+        
+            if(expected <= upper){
+                res.push_back({expected, upper});    
+            }
+            
+        
+        
+        return res;
+    }
+};
