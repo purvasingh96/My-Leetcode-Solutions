9303f47afb034dc97d5c35262894ff379e1007e3
Purva Singh <singhpurva2906@gmail.com>
2024-02-19 11:21:09 -0500

    Time: 6 ms (38.43%), Space: 9.2 MB (31.76%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        cout<<"n: "<<n<<"\n";
-        if(n == 1){
+        if(n == 1) {
             return "1";
         }
         
         string t = countAndSay(n-1);
-        //cout<<"t:: "<<t<<"\n";
-
-        string res="";
-        stack<char> st;
+        string res ="";
         
+        stack<char> st;
         for(int i=0;i<t.length();i++){
             if(st.empty() || t[i] == st.top()){
                 st.push(t[i]);
             } else {
-                res += st.size() + '0';
-                res += st.top();
+                char c = st.size() + '0';
+                char top = st.top();
+                res += c;
+                res += top;
                 while(!st.empty()){
                     st.pop();
                 }
                 st.push(t[i]);
             }
         }
+        
         if(!st.empty()){
-            res += st.size() + '0';
-            res += st.top();
+            char c = st.size() + '0';
+            char top = st.top();
+            res += c;
+            res += top;
         }
         return res;
     }
