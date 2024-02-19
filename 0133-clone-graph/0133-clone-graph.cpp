0877d3270774ffbd566765745d52d82589fb81cf
Purva Singh <singhpurva2906@gmail.com>
2024-02-19 10:05:34 -0500

    Time: 8 ms (11.29%), Space: 11.6 MB (17.98%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -22,39 +22,35 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
-        if(!node){
-            return NULL;
-        }
+        if(!node) return NULL;
         unordered_map<Node*, Node*> m;
-        unordered_map<Node*, bool> visited;
         queue<Node*> q;
         q.push(node);
+        unordered_map<Node*, bool> visited;
         visited[node] = true;
         
-        while(q.size()!=0){
+        while(!q.empty()){
+            auto f = q.front();
+            q.pop();
             
-                auto f = q.front();
-                q.pop();
-                
-                if(m.find(f)==m.end()){
-                    m[f] = new Node(f->val);
+            if(m.find(f)==m.end()){
+                m[f] = new Node(f->val);
+            }
+            
+            for(auto c:f->neighbors){
+                if(m.find(c)==m.end()){
+                    m[c] = new Node(c->val);
+                }
+                m[f]->neighbors.push_back(m[c]);
+                if(visited[c]==false){
+                    visited[c] = true; 
+                    q.push(c);    
                 }
                 
-                for(auto c:f->neighbors){
-                    if(m.find(c)==m.end()){
-                        m[c] = new Node(c->val);
-                    }
-                    m[f]->neighbors.push_back(m[c]);
-                    if(visited[c] == false){
-                        visited[c] = true;
-                        q.push(c);    
-                    }
-                    
-                }
-            
+            }
         }
         
         return m[node];
-        
+            
     }
 };
