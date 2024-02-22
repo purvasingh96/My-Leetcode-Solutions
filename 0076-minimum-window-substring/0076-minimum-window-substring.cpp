0031eced328bb5eeec1cffc009c96d818794c59c
Purva Singh <singhpurva2906@gmail.com>
2024-02-22 10:13:04 -0500

    Time: 32 ms (19.21%), Space: 11.2 MB (8.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int c=0;
-        int l=0, r=0;
-        string str="";
-        int len = INT_MAX;
-        
         unordered_map<char, int> ms;
         unordered_map<char, int> mt;
+        string str;
+        int len=INT_MAX;
+        int l=0, r=0, c=0;
         
-        for(auto c:t) mt[c] += 1;
+        for(auto c:t){
+            mt[c]+=1;
+        }
         
         for(;r<s.length();r++){
-            
             if(mt.find(s[r])!=mt.end()){
                 ms[s[r]]+=1;
                 if(ms[s[r]] <= mt[s[r]]){
@@ -21,25 +20,19 @@ public:
             }
             
             if(c == t.length()){
-                
-                while(l<r && (mt.find(s[l])==mt.end() || (ms[s[l]] > mt[s[l]]))){
+                while(l<r && mt.find(s[l]) == mt.end() || ms[s[l]]>mt[s[l]] ){
                     ms[s[l]]-=1;
                     l+=1;
-                    
                 }
                 
                 if(r-l+1 < len){
                     len = r-l+1;
-                    str = s.substr(l,len);
+                    str = s.substr(l, len);
                 }
-                
             }
-            
-            
             
         }
         
         return str;
-        
     }
 };
