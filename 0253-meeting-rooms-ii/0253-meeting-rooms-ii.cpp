86979f33238969a7bf4b9da61766824da2b7600a
Purva Singh <singhpurva2906@gmail.com>
2024-02-22 23:06:28 -0500

    Time: 8 ms (92.47%), Space: 15.3 MB (36.83%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -1,24 +1,18 @@
-class MyComp{
- public:
-    bool operator()(int& a, int& b){
-        return a > b;
-    }
-};
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        priority_queue<int, vector<int>, MyComp> pq;
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
         
         for(int i=0;i<intervals.size();i++){
-            if(pq.size()==0 || intervals[i][0]<pq.top()){
+            if(pq.empty() || pq.top() > intervals[i][0]){
                 pq.push(intervals[i][1]);
             } else {
                 pq.pop();
                 pq.push(intervals[i][1]);
             }
         }
-        
         return pq.size();
     }
 };
