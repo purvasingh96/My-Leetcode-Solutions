33c941f3b789f73dbb6b77ee594722cc6f7cbddc
Purva Singh <singhpurva2906@gmail.com>
2024-02-22 22:05:01 -0500

    Time: 23 ms (73.52%), Space: 22.3 MB (22.70%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,35 +1,31 @@
 class Solution {
-private:
-    bool hasIntersection(vector<int> s1, vector<int> s2){
-        int start1 = s1[0], end1 = s1[1];
-        int start2 = s2[0], end2 = s2[1];
-        
-        // 2 intersects 1
-        // complete
-        if(end1 < start2 || end2 < start1){
-            return false;
-        }
-        return true;
-    }
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& s1, vector<vector<int>>& s2) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& n1, vector<vector<int>>& n2) {
+     
+        vector<vector<int>> ans;
+        
         int i=0, j=0;
-        vector<vector<int>> res;
-        while(i<s1.size() && j<s2.size()){
+        
+        while(i<n1.size() && j<n2.size()){
+            int start = max(n1[i][0], n2[j][0]);
+            int end = min(n1[i][1], n2[j][1]);
             
-                // intersection
-                vector<int> ans = {max(s1[i][0], s2[j][0]), min(s1[i][1], s2[j][1])};
-                if(ans[0] <= ans[1]){
-                    res.push_back(ans);
-                }
-                    if(s1[i][1] < s2[j][1]) {
-                        i+=1;
-                    } else{
-                        j+=1;
-                    }
-                
-             
+            if(start<=end){
+                ans.push_back({start, end});
+            }
+            
+            if(n1[i][1] < n2[j][1]){
+                i+=1;
+            } else {
+                j+=1;
+            }
         }
-        return res;
+        
+       
+        
+        
+        return ans;
+        
+        
     }
 };
