f6bda1044688b1e1d6348cba2ab0d5883eccf5da
Purva Singh <singhpurva2906@gmail.com>
2024-02-22 23:01:52 -0500

    Time: 374 ms (85.94%), Space: 207.9 MB (48.07%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> findRLEArray(vector<vector<int>>& e1, vector<vector<int>>& e2) {
-        int i=0, j=0;
+        
         vector<vector<int>> res;
         
+        int i=0, j=0;
+        
         while(i<e1.size() && j<e2.size()){
-            int val, count;
-            val = e1[i][0] * e2[j][0];
+            int prod = e1[i][0] * e2[j][0];
+            int freq;
             
             if(e1[i][1] < e2[j][1]){
-                count = e1[i][1];
+                freq = e1[i][1];
                 e2[j][1] -= e1[i][1];
-                if(e2[j][1]==0){
-                    j+=1;
-                }
                 i+=1;
             } else if(e2[j][1] < e1[i][1]){
-                count = e2[j][1];
+                freq = e2[j][1];
                 e1[i][1] -= e2[j][1];
-                if(e1[i][1] == 0){
-                    i+=1;
-                }
                 j+=1;
-            } else if(e1[i][1] == e2[j][1]){
-                count = e2[j][1];
+            } else {
+                freq =  e1[i][1];
+                e1[i][1]=0;
+                e2[j][1]=0;
                 i+=1;
                 j+=1;
             }
             
-            if(res.size()!=0 && res.back()[0] == val){
-                count += res.back()[1];
+            if(res.size() > 0 && res.back()[0] == prod){
+                freq += res.back()[1];
                 res.pop_back();
             }
-            res.push_back({val, count});
+            res.push_back({prod, freq});
         }
-        
         
         return res;
         
