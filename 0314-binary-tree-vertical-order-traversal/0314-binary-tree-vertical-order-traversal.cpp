dda0ba09af8668da596be036274e5a4869aad748
Purva Singh <singhpurva2906@gmail.com>
2024-02-24 23:14:30 -0500

    Time: 0 ms (100.00%), Space: 15.7 MB (6.30%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
@@ -12,39 +12,35 @@
 class Solution {
 private:
     void dfs(TreeNode* root, int row, int col, map<int, vector<pair<int, int>>>& m){
-        if(!root) {
+        if(!root){
             return;
         }
-        m[col].push_back({row, root->val});
         
-        if(root->left){
-            dfs(root->left, row+1, col-1, m);    
-        }
-        if(root->right){
-            dfs(root->right, row+1, col+1, m);
-        }
+        m[col].push_back({root->val, row});
         
+        dfs(root->left, row+1, col-1, m);
+        dfs(root->right, row+1, col+1, m);
     }
 public:
     vector<vector<int>> verticalOrder(TreeNode* root) {
         map<int, vector<pair<int, int>>> m;
-        dfs(root, 0, 0,m);
-        vector<vector<int>> res;
+        dfs(root, 0, 0, m);
+        vector<vector<int>> ans;
+        
         for(auto x:m){
             sort(x.second.begin(), x.second.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-               if(a.first!=b.first){
-                   return a.first < b.first;
-               }
-                return false;
+                return a.second <b.second;
             });
+            
             vector<int> temp;
             for(auto y:x.second){
-                temp.push_back(y.second);
+                temp.push_back(y.first);
             }
-            res.push_back(temp);
+                
+            
+            ans.push_back(temp);
         }
         
-       
-        return res;
+        return ans;
     }
 };
