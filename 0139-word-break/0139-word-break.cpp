91df510830cf0c34a9bc4fe3d8ad7d915f554202
Purva Singh <singhpurva2906@gmail.com>
2024-02-25 16:13:01 -0500

    Time: 18 ms (33.58%), Space: 23.5 MB (7.41%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,64 +1,32 @@
-class TrieNode{
-public:
-    vector<TrieNode*> children;
-    bool eow;
-    TrieNode(){
-        this->children.resize(26, NULL);
-        this->eow= false;
-    }
-};
-
 class Solution {
 private:
-    TrieNode* head;
-    void insert(TrieNode* root, string& word){
-        TrieNode* temp = root;
-        
-        for(int i=0;i<word.length();i++){
-            int idx = word[i] - 'a';
-            if(temp->children[idx]==NULL){
-                temp->children[idx] = new TrieNode();
+    bool helper(string s, int i, string temp, vector<string>& dict, map<pair<int, string>, bool>& dp){
+        if(i>=s.length()){
+            if(temp=="" || find(dict.begin(), dict.end(), temp)!=dict.end()){
+                return dp[{i, temp}] = true;
             }
-            temp =temp->children[idx];
-        }
-        temp->eow=true;
-    }
-    
-    bool dfs(int pos, TrieNode* root, string& s, map<pair<int, TrieNode*>, bool>& dp){
-        int idx = s[pos] - 'a';
-        
-        if(dp.find({pos,root})!=dp.end()) return dp[{pos,root}];
-        
-        if(pos == s.length()) {
-            return dp[{pos,root}] = root->eow;
+            return dp[{i, temp}] = false;
         }
         
-        if(root->children[idx] == NULL) {
-            if(root->eow){
-                return dp[{pos,root}] = dfs(pos, head, s, dp);
-            }
-            return dp[{pos,root}] = false;
+        if(dp.find({i, temp})!=dp.end()){
+            return dp[{i, temp}];
         }
         
-        bool beginAgain = false, nextWord=false;
-        if(root->eow){
-            // can explore a diff word
-            beginAgain= dfs(pos, head, s, dp);
-        }
+        temp += s[i];
         
-        nextWord = dfs(pos+1, root->children[idx], s, dp);
-        return dp[{pos,root}] = beginAgain || nextWord;
+        
+        if(find(dict.begin(), dict.end(), temp)!=dict.end()){
+            // element found
+            return dp[{i, temp}] = helper(s, i+1, "", dict, dp) || helper(s, i+1, temp, dict, dp);
+        } else {
+            return dp[{i, temp}] = helper(s, i+1, temp, dict, dp);
+        }
         
     }
-    
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        map<pair<int, TrieNode*>, bool> dp;
-        head = new TrieNode();
-        TrieNode* root = head;
-        for(auto word:wordDict) insert(root, word);
-        
-        return dfs(0, root, s, dp);
-        
+        map<pair<int, string>, bool> dp;
+       
+        return helper(s, 0, "", wordDict, dp);
     }
 };
