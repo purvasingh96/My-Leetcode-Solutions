b142f692f207e9a03663b49a918aecf498d77d7c
Purva Singh <singhpurva2906@gmail.com>
2024-02-25 10:02:12 -0500

    Time: 4 ms (18.58%), Space: 8.8 MB (8.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,23 +1,29 @@
 class Solution {
 private:
-    double helper(double x, int n, map<int, double>& dp){
-        if(n==0) return 1;
-        if(n==1) return x;
-        if(dp.find(n)!=dp.end()) return dp[n];
+    double power(double x, int n, unordered_map<int, double>& dp){
+        if(n==0){
+            return 1.0;
+        }
+        if(n==1){
+            return x;
+        }
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
         
-        double prod1 = helper(x, n/2, dp);
-        return dp[n] = n%2==0 ? prod1 * prod1 : prod1*prod1*x;
+        if(n%2==0){
+            return dp[n] = power(x, n/2, dp) * power(x, n/2, dp);
+        }
+        
+        return dp[n] = power(x, n/2, dp) * power(x, n/2, dp) * x;
     }
 public:
     double myPow(double x, int n) {
-        map<int, double> dp;
-        int power = n/2;
-        if(power < 0) power = power*-1;
-        
-        double ans = helper(x, power, dp);
-        double res = n%2==0 ? ans*ans : ans*ans*x;
-        
-        return n<0 ? 1/res : res;
-        
+        unordered_map<int, double> dp;
+        if(n<0){
+            double ans = power(x, n, dp);
+            return 1/ans;
+        }
+        return power(x, n, dp);
     }
 };
