13ede0d504d32443dbb689798a867854df2446ef
Purva Singh <singhpurva2906@gmail.com>
2024-02-26 08:46:41 -0500

    Time: 41 ms (88.06%), Space: 22.2 MB (80.83%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -8,36 +8,39 @@ private:
     }
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        queue<vector<int>> q;
-        if(grid[0][0]!=0) return -1;
-        grid[0][0]=1;
-        
-        q.push({0, 0, 1});
-        int ans = INT_MAX, m = grid.size(), n = grid[0].size();
-        
-        while(!q.empty()){
-            
-            auto f = q.front();
-            q.pop();
-            
-            int x = f[0], y = f[1], steps=f[2];
-            if(x == m-1 && y==n-1){
-                ans = min(ans, steps);
-            }
-            
-            for(int i=0;i<8;i++){
-                int new_x = x+dx[i];
-                int new_y = y+dy[i];
-                
-                if(isValid(new_x, new_y, grid) && grid[new_x][new_y]==0){
-                    grid[new_x][new_y]=1;
-                    q.push({new_x, new_y, steps+1});
-                }
-            }
-            
+        // BFS?
+        queue<pair<int, int>> q;
+        int d=1;
+        q.push({0, 0});
+        int n = grid.size();
+        if(grid[n-1][n-1]==1 || grid[0][0] == 1){
+            return -1;
         }
         
-        return ans==INT_MAX?-1:ans;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                auto f = q.front();
+                q.pop();
+                int x = f.first, y = f.second;
+                
+                if(x==n-1 && y==n-1){
+                    return d;
+                }
+                
+                for(int i=0;i<8;i++){
+                    int new_x = x+dx[i];
+                    int new_y = y+dy[i];
+                    if(isValid(new_x, new_y, grid) && grid[new_x][new_y]==0){
+                        grid[new_x][new_y]=1;
+                        q.push({new_x, new_y});
+                    }
+                }
+            }
+            d+=1;
+        }
         
+        return -1;
+            
     }
 };
