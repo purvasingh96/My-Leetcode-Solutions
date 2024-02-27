b13872598bd5fc2f7cf8a586f8261feec8d42069
Purva Singh <singhpurva2906@gmail.com>
2024-02-27 09:13:39 -0500

    Time: 19 ms (79.86%), Space: 12.8 MB (46.59%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,19 +1,34 @@
 class Solution {
+
 public:
     string minRemoveToMakeValid(string s) {
-  stack<int> st;
-  for (auto i = 0; i < s.size(); ++i) {
-    if (s[i] == '(') st.push(i);
-    if (s[i] == ')') {
-      if (!st.empty()) st.pop();
-      else s[i] = '*';
+        stack<int> st;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '('){
+                st.push(i);
+            } else if(s[i] == ')'){
+                if(!st.empty()){
+                    st.pop();
+                } else {
+                    s[i] = '*';
+                }
+            }
+        }
+        
+        while(!st.empty()){
+            s[st.top()]='*';
+            st.pop();
+        }
+        
+        string res="";
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]!='*'){
+                res += s[i];
+            }
+        }
+        
+       return res; 
     }
-  }
-  while (!st.empty()) {
-    s[st.top()] = '*';
-    st.pop();
-  }
-  s.erase(remove(s.begin(), s.end(), '*'), s.end());
-  return s;
-}
 };
