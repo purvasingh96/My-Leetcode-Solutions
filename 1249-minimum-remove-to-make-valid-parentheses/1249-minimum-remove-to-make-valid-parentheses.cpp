e41d8511dd0c9aa7658e55f6094feb5b3d05e5cb
Purva Singh <singhpurva2906@gmail.com>
2024-02-27 09:13:08 -0500

    Time: 11 ms (99.25%), Space: 11.7 MB (79.59%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,42 +1,19 @@
 class Solution {
-private:
-    int eraseInvalidClosingBrackets(string& s){
-        int balance=0;
-        string res="";
-        for(int i=0;i<s.length();i++){
-            if(s[i] == ')'){
-                balance-=1;
-                if(balance < 0){
-                    balance+=1;   
-                } else {
-                    res += s[i];
-                }
-            } else {
-                if(s[i] == '('){
-                    balance+=1;
-                }
-                res += s[i];
-            }
-        }
-        
-        s=res;
-        
-        return balance;
-    }
 public:
     string minRemoveToMakeValid(string s) {
-        int balance = eraseInvalidClosingBrackets(s);
-        cout<<"balance:: "<<balance<<"\n";
-        cout<<"s:: "<<s<<"\n";
-        for(int i=s.length()-1;i>=0;i--){
-            if(s[i] == '(') {
-                if(balance>0){
-                    balance-=1;
-                    s.erase(i, 1);
-                }
-            }
-        }
-        
-        return s;
+  stack<int> st;
+  for (auto i = 0; i < s.size(); ++i) {
+    if (s[i] == '(') st.push(i);
+    if (s[i] == ')') {
+      if (!st.empty()) st.pop();
+      else s[i] = '*';
     }
+  }
+  while (!st.empty()) {
+    s[st.top()] = '*';
+    st.pop();
+  }
+  s.erase(remove(s.begin(), s.end(), '*'), s.end());
+  return s;
+}
 };
