aec35d0384235b7ba9349f0fce4b1be1b7646ec6
Purva Singh <singhpurva2906@gmail.com>
2024-02-28 11:50:49 -0500

    Time: 201 ms (22.74%), Space: 41.6 MB (34.92%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,57 +1,57 @@
 class Solution {
 private:
-    bool isvalid(string s){
-        stack<char> st;
-        for(auto c:s){
-            if(c=='('){
-                st.push(c);
-            } else if(c == ')'){
-                if(!st.empty() && st.top() == '('){
-                    st.pop();
-                } else {
-                    st.push(c);
+    bool isValid(string s){
+        int balance=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                balance+=1;
+            } else if(s[i] == ')') {
+                balance-=1;
+                if(balance < 0){
+                    return false;
                 }
-            }   
+            }
         }
-        return st.empty();
+        
+        return balance==0;
     }
 public:
     vector<string> removeInvalidParentheses(string s) {
         queue<string> q;
         q.push(s);
-        vector<string> res;
         unordered_map<string, bool> visited;
-        visited[s]=true;
         bool found=false;
+        vector<string> res;
+        visited[s] = true;
         
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
                 auto f = q.front();
                 q.pop();
-                //cout<<"f:: "<<f;
-                if(isvalid(f)){
-                    //cout<<"f:: "<<f<<"\n";
-                    found=true;
+                
+                if(isValid(f)){
+                    found = true;
                     res.push_back(f);
-                } else{
-                    for(int i=0;i<f.length();i++){
-                        string left = f.substr(0, i);
-                        string right = f.substr(i+1);
-                        string temp = left+right;
-                        if(visited[temp] == false){
-                            visited[temp] = true;
-                            q.push(temp);    
-                        }
-                        
-                    }
                 }
+                
+                for(int i=0;i<f.length();i++){
+                    string left = f.substr(0, i);
+                    string right = f.substr(i+1);
+                    string temp = left+right;
+                    if(!visited[temp]){
+                        visited[temp] = true;
+                        q.push(temp);
+                    }
                     
+                }
+                
             }
             if(found){
                 break;
             }
         }
+        
         return res;
     }
 };
