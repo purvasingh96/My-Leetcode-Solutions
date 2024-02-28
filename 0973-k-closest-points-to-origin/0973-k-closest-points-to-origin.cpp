ff5d7fc731da67daccaeeee22a198dc52715ac10
Purva Singh <singhpurva2906@gmail.com>
2024-02-28 08:55:42 -0500

    Time: 613 ms (9.75%), Space: 60.5 MB (94.37%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,55 +1,38 @@
 class Solution {
 private:
-    int partition(vector<pair<int, int>>& nums, int start, int end){
-        int pivot = nums[end].second;
-        int i=start-1, j=start;
-        
-        while(j<end){
-            if(nums[j].second <= pivot){
+    int dist(vector<int>& points){
+        return pow(points[0], 2) + pow(points[1], 2);
+    }
+    
+    int partition(vector<vector<int>>& points, int start, int end){
+        int pivot = dist(points[end]);
+        int i=start-1;
+        for(int j=start;j<end;j++){
+            if(dist(points[j]) < pivot){
                 i+=1;
-                swap(nums[i], nums[j]);
+                swap(points[i], points[j]);
             }
-            j+=1;
         }
-        
         i+=1;
-        swap(nums[i], nums[end]);
+        swap(points[i], points[end]);
         return i;
     }
     
-    void quickSort(vector<pair<int, int>>& nums, int start, int end, int k){
-        if(start <= end){
-            int pivot = partition(nums, start, end);
-
-            if(pivot == k){
-                return;
-            } else if(k < pivot){
-                quickSort(nums, start, pivot-1, k);
-            } else if(k > pivot){
-                quickSort(nums, pivot+1, end, k);
+    void quickSort(vector<vector<int>>& points, int start, int end, int k){
+        if(start<=end){
+            int pivot = partition(points, start, end);
+            if(k<pivot){
+                quickSort(points, start, pivot-1, k);
+            } else {
+                quickSort(points, pivot+1, end, k);
             }
         }
-        
-    }
-    
-    int dist(vector<int>& points){
-        int x1 = points[0], y1 = points[1];
-        return pow(x1, 2) + pow(y1, 2);
     }
     
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int, int>> nums;
-        for(int i=0;i<points.size();i++){
-            nums.push_back({i, dist(points[i])});
-        }
-        quickSort(nums, 0, nums.size()-1, k);
-        vector<vector<int>> ans;
-        for(int i=0;i<k;i++){
-            int idx = nums[i].first;
-            ans.push_back(points[idx]);
-        }
-        
+        quickSort(points, 0, points.size()-1, k);
+        vector<vector<int>> ans(points.begin(), points.begin()+k);
         return ans;
     }
 };
