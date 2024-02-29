341429833e771928e984769737dc05438bde8373
Purva Singh <singhpurva2906@gmail.com>
2024-02-29 08:20:48 -0500

    Time: 3 ms (66.57%), Space: 11.1 MB (54.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,44 +1,48 @@
 class Solution {
 private:
     bool isPeak(vector<int>& nums, int idx){
-        if(idx == 0){
-            if(nums[1]<nums[0]){
-                return true;
+        if(idx==0){
+            if(nums[1]<nums[idx]){
+                return true;    
             }
             return false;
         }
         
         if(idx == nums.size()-1){
-            if(nums[nums.size()-2] < nums[nums.size()-1]){
-                return true;
+            if(nums[nums.size()-2] < nums[idx]){
+                return true;    
             }
             return false;
+        } 
+        if(nums[idx-1] < nums[idx] && nums[idx] > nums[idx+1]){
+            return true;
         }
         
-        return nums[idx-1] < nums[idx] && nums[idx] > nums[idx+1];
+        return false;
     }
 public:
     int findPeakElement(vector<int>& nums) {
-        int low=0, high=nums.size()-1;
         if(nums.size()==1){
             return 0;
         }
         
+        int low=0, high=nums.size()-1;
+        
+        
         while(low<high){
-            int mid = low +(high-low)/2;
+            int mid = low + (high-low)/2;
             
             if(isPeak(nums, mid)){
                 return mid;
             }
-            
-            if(nums[mid+1] > nums[mid]){
+            else if(nums[mid+1] > nums[mid]){
                 low = mid+1;
-            } else {
-                high=mid-1;
+            }
+            else {
+                high = mid-1;
             }
         }
-        
-        return isPeak(nums, low) ? low:-1;
-        
+            
+        return isPeak(nums, low) ? low : -1;
     }
 };
