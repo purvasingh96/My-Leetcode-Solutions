4199cacf35c0625682db3be0bd8f4d0379ec03b4
Purva Singh <singhpurva2906@gmail.com>
2024-03-02 18:11:40 -0500

    Time: 57 ms (20.89%), Space: 33.5 MB (46.07%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,41 @@
 class Solution {
 private:
-    bool canShip(vector<int>& weights, int maxCap, int days){
-        int d = 0;
-        int sum=0;
+    bool canShip(vector<int>& weights, int days, int target){
+        int sum=0, d=0;
         for(int i=0;i<weights.size();i++){
             sum += weights[i];
-            if(sum > maxCap){
-                sum = weights[i];
+            if(sum > target){
                 d+=1;
                 if(d > days){
                     return false;
                 }
-            } 
+                sum = weights[i];
+            }
         }
-        // ship last batch
         d+=1;
-        return d<=days;
+        return d <= days;
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        cout<<high;
+        int l = *max_element(weights.begin(), weights.end());
+        int h = accumulate(weights.begin(), weights.end(), 0);
         
-        while(low<high){
-            int maxCap = low+(high-low)/2;
-            //cout<<"mnaxcap: "<<maxCap<<" ship?: "<<canShip(weights, maxCap, days)<<"\n";
-            if(canShip(weights, maxCap, days)){
-                high = maxCap;
+        int ans=h;
+        
+        while(l<h){
+            int mid = l + (h-l)/2;
+            cout<<"mid: "<<mid<<" canship: "<<canShip(weights, days, mid)<<"\n";
+            if(canShip(weights, days, mid)){
+                ans = min(ans, mid);
+                h=mid-1;
             } else {
-                low = maxCap+1;
+                l = mid+1;
             }
-            
         }
-        return low;    
+        
+        if(canShip(weights, days, l)){
+            ans = min(ans, l);
+        }
+        return ans;
     }
-    
 };
