2fca89fb1cc427364c8c97f9982223f225db2dd3
Purva Singh <singhpurva2906@gmail.com>
2024-03-03 09:34:06 -0500

    Time: 12 ms (63.90%), Space: 17.1 MB (44.81%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,16 +1,16 @@
 class Solution {
 private:
-    // number, freq
+    // val, freq
     int partition(vector<pair<int, int>>& nums, int start, int end){
         int pivot = nums[end].second;
-        int i=start-1, j=start;
-        
-        for(;j<end;j++){
+        int i=start-1;
+        for(int j=start;j<end;j++){
             if(nums[j].second >= pivot){
                 i+=1;
                 swap(nums[i], nums[j]);
             }
         }
+        
         i+=1;
         swap(nums[i], nums[end]);
         return i;
@@ -18,28 +18,31 @@ private:
     
     void quickSort(vector<pair<int, int>>& nums, int start, int end, int k){
         if(start<=end){
-            int pivot = partition(nums, start, end);
-            if(k <= pivot){
-                quickSort(nums, start, pivot-1, k);
+            int n = partition(nums, start, end);
+            
+            if(n<k){
+                quickSort(nums, n+1, end, k);
             } else {
-                quickSort(nums, pivot+1, end, k);
+                quickSort(nums, start, n-1, k);
             }
+            
         }
     }
+        
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
+        unordered_map<int, int> count;
         for(auto x:nums){
-            m[x]+=1;
+            count[x]+=1;
         }
         vector<pair<int, int>> res;
-        for(auto x:m){
+        
+        for(auto x:count){
             res.push_back({x.first, x.second});
         }
+        
         quickSort(res, 0, res.size()-1, k);
-        for(auto x:res){
-            cout<<x.first<<" ";
-        }
+        
         vector<int> ans;
         for(int i=0;i<k;i++){
             ans.push_back(res[i].first);
