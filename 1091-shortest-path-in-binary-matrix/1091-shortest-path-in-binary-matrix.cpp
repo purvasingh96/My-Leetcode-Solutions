60e48007f23fa7b0de683d4f51f0509aa6ce8973
Purva Singh <singhpurva2906@gmail.com>
2024-03-03 17:20:08 -0500

    Time: 41 ms (85.90%), Space: 22.3 MB (85.62%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -8,20 +8,23 @@ private:
     }
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        // BFS?
-        queue<pair<int, int>> q;
-        int d=1;
-        q.push({0, 0});
+        //bfs
         int n = grid.size();
-        if(grid[n-1][n-1]==1 || grid[0][0] == 1){
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
+        queue<pair<int, int>> q;
+        q.push({0, 0});
+        grid[0][0]=1;
+        
+        int d=1;
         
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
                 auto f = q.front();
                 q.pop();
+                
                 int x = f.first, y = f.second;
                 
                 if(x==n-1 && y==n-1){
@@ -31,7 +34,9 @@ public:
                 for(int i=0;i<8;i++){
                     int new_x = x+dx[i];
                     int new_y = y+dy[i];
-                    if(isValid(new_x, new_y, grid) && grid[new_x][new_y]==0){
+                    
+                    if(isValid(new_x, new_y, grid) &&
+                       grid[new_x][new_y]==0){
                         grid[new_x][new_y]=1;
                         q.push({new_x, new_y});
                     }
@@ -41,6 +46,6 @@ public:
         }
         
         return -1;
-            
+        
     }
 };
