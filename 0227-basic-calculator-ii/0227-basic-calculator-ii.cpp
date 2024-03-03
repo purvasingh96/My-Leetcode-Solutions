bc8de5398f1e3d1f03e4335d65683e55de1727a0
Purva Singh <singhpurva2906@gmail.com>
2024-03-03 09:03:30 -0500

    Time: 15 ms (82.02%), Space: 15.1 MB (14.11%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> st;
         char op = '+';
-        int d=0;
+        stack<long> st;
         
-        for(auto x:s){    
-            if(x == ' '){
+        long d=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i] == ' '){
                 continue;
             }
-            if(isdigit(x)){
-                d = d*10 + (x-'0');
+            if(isdigit(s[i])){
+                d=d*10 + s[i]-'0';
             } else {
                 if(op == '+'){
                     st.push(d);
                 } else if(op == '-'){
-                    st.push(-1*d);
+                    st.push(-d);
                 } else if(op == '*'){
                     int top = st.top();
                     st.pop();
@@ -23,26 +24,27 @@ public:
                 } else if(op == '/'){
                     int top = st.top();
                     st.pop();
-                    st.push(top / d);    
+                    st.push(top/d);
                 }
+                op = s[i];
                 d=0;
-                op = x;
-            }    
+            }
         }
         
         if(op == '+'){
-                    st.push(d);
-                } else if(op == '-'){
-                    st.push(-1*d);
-                } else if(op == '*'){
-                    int top = st.top();
-                    st.pop();
-                    st.push(top * d);
-                } else if(op == '/'){
-                    int top = st.top();
-                    st.pop();
-                    st.push(top / d);    
-                }
+            st.push(d);
+        } else if(op == '-'){
+            st.push(-d);
+        } else if(op == '*'){
+           int top = st.top();
+            st.pop();
+            st.push(top * d);
+        } else if(op == '/'){
+            int top = st.top();
+            st.pop();
+            st.push(top/d);
+        }
+        
         int ans=0;
         while(!st.empty()){
             ans += st.top();
@@ -50,5 +52,6 @@ public:
         }
         
         return ans;
+        
     }
 };
