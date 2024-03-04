399cae185789ca37c09f004912d422d001b97039
Purva Singh <singhpurva2906@gmail.com>
2024-03-04 08:18:47 -0500

    Time: 47 ms (70.82%), Space: 38.7 MB (38.87%) - LeetHub

--- 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
+++ 0691-stickers-to-spell-word/0691-stickers-to-spell-word.cpp
@@ -4,63 +4,68 @@ public:
         int n = stickers.size();
         vector<int> t_freq(26, 0);
         vector<vector<int>> s_freq(n, vector<int>(26, 0));
-        int c=0;
-        unordered_set<string> visited;
-        queue<vector<int>> q;
+        unordered_map<string, bool> visited;
+        visited[target] = true;
         
-        for(auto c:target){
-            t_freq[c-'a']+=1;
+        for(int i=0;i<target.length();i++){
+            t_freq[target[i]-'a']+=1;
         }
-        q.push(t_freq);
-        
+            
         for(int i=0;i<stickers.size();i++){
             for(auto c:stickers[i]){
                 s_freq[i][c-'a']+=1;
             }
         }
+            
+        
+        queue<vector<int>> q;
+        q.push(t_freq);
+        
+        int c=0;
         
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
-                auto t_f = q.front();
+                auto f= q.front();
                 q.pop();
                 
                 string temp="";
-                for(int i=0;i<t_f.size();i++){
-                    if(t_f[i]>0){
-                        string j(t_f[i], i+'a');
-                        temp += j;
+                for(int i=0;i<f.size();i++){
+                    if(f[i]>0){
+                        temp += string(f[i], i+'a');
                     }
                 }
-                cout<<"temp: "<<temp<<"\n";
+                
                 if(temp == ""){
                     return c;
                 }
                 
-                if(visited.count(temp)){
-                    continue;
+                if(visited.find(temp) == visited.end()){
+                    visited[temp] = true;
                 } else {
-                    visited.insert(temp);
+                    continue;
                 }
                 
-                // create neighbors
                 char seek = temp[0];
-                for(int i=0;i<s_freq.size();i++){
+                
+                for(int i=0;i<n;i++){
                     if(s_freq[i][seek-'a']!=0){
-                        // explore
-                        q.push(t_f);
-                        for(int j=0;j<s_freq[i].size();j++){
-                            if(s_freq[i][j]!=0){
+                        q.push(f);
+                       for(int j=0;j<s_freq[i].size();j++){
+                            if(s_freq[i][j]>0){
                                 q.back()[j] -= s_freq[i][j];
                             }
-                        }
+                        } 
                     }
+                    
                 }
+                
                 
             }
             c+=1;
         }
         
         return -1;
+        
     }
 };
