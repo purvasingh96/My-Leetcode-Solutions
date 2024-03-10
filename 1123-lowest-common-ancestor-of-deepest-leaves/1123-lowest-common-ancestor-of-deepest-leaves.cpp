fa4949987207a4b5d7ca8b296f784eac6856f6a4
Purva Singh <singhpurva2906@gmail.com>
2024-03-10 09:25:42 -0400

    Time: 15 ms (22.23%), Space: 19.9 MB (58.66%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,55 +11,21 @@
  */
 class Solution {
 private:
-    TreeNode* res;
-    int lca(TreeNode* root, vector<int> leaves){
-        if(!root){
-            return 0;
-        }
+    TreeNode* ans;
+    int leafDepth(TreeNode* root, int d, int& maxD){
+        if(!root) return d - 1;
+        int l = leafDepth(root -> left, d + 1, maxD);
+        int r = leafDepth(root -> right, d + 1, maxD);
+        int leafD = max(l, r);
+        maxD = max(maxD, leafD);
+        if(maxD == l && l == r) ans = root;
+        return leafD;
         
-        // leaf nodes
-        if(find(leaves.begin(), leaves.end(), root->val)!=leaves.end()){
-            if(leaves.size() == 1 && !res){
-                res=root;
-            }
-            return 1;
-        }
-        
-        int l = lca(root->left, leaves);
-        int r = lca(root->right,leaves);
-        //cout<<"root->val:: "<<root->val<<" l: "<<l<<" r: "<<r<<"\n";
-        
-        if(l+r == leaves.size() && !res){
-            res = root;
-        }
-        
-        return l+r;
-       
     }
-    
-    void collectDeepestLeaves(TreeNode* root, int d, map<int, vector<int>>& leaves){
-        if(!root){
-            return;
-        }
-        if(!root->left && !root->right){
-            leaves[d].push_back(root->val);
-        }
-        
-        collectDeepestLeaves(root->left, d+1, leaves);
-        collectDeepestLeaves(root->right, d+1, leaves);
-    }
-    
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        res = NULL;
-        if(!root){
-            return res;
-        }
-        
-        map<int, vector<int>> leaves;
-        collectDeepestLeaves(root, 0, leaves);
-        
-        lca(root, leaves.rbegin()->second);
-        return res;
+        int maxD = 0;
+        leafDepth(root, 0, maxD);
+        return ans;
     }
 };
