bd48661141f6cac0899cf772dcaaca82e6acce1f
Purva Singh <singhpurva2906@gmail.com>
2024-03-10 11:48:12 -0400

    Time: 23 ms (82.55%), Space: 24.1 MB (87.76%) - LeetHub

---
+++ 0536-construct-binary-tree-from-string/0536-construct-binary-tree-from-string.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* str2tree(string s) {
+        if(s.length() == 0){
+            return NULL;
+        }
+        stack<TreeNode*> st;
+        char op='(';
+        int d=0;
+        
+        for(int i=0;i<s.length();i++){    
+            if(isdigit(s[i])){
+                int j=i;
+                while(j<s.length() && isdigit(s[j])){
+                    d=d*10+(s[j]-'0');
+                    j+=1;
+                }
+               if(op == '-'){
+                   d = d*-1;
+               }
+                i=j-1;
+                
+                TreeNode* node = new TreeNode(d);
+                
+                
+                
+                    if(st.empty()){
+                        st.push(node);
+                    } else {
+                        if(st.top()->left==NULL){
+                            st.top()->left = node;
+                        } else {
+                            st.top()->right=node;
+                        }
+                        st.push(node);
+                    }
+                
+                
+                d=0;
+                
+            } else {
+                op = s[i];
+                if(op == ')'){
+                    if(!st.empty()){
+                        st.pop();
+                    }
+                }
+            }
+        }
+        
+        return st.size()>0 ? st.top() : NULL;
+        
+    }
+};
