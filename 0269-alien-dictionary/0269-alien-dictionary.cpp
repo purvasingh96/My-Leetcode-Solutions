77c1246e62ac850d0f02734d9b054f26f69d8bae
Purva Singh <singhpurva2906@gmail.com>
2024-03-15 08:12:07 -0400

    Time: 0 ms (100.00%), Space: 12.8 MB (23.30%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,64 +1,79 @@
 class Solution {
 private:
-    bool startsWith(string& a, string& prefix){
+    bool startsWith(string a, string prefix){
         for(int i=0;i<prefix.length();i++){
-            if(a[i]!=prefix[i]) return false;
+            if(a[i]!=prefix[i]){
+                return false;
+            }
         }
+        
         return true;
     }
 public:
     string alienOrder(vector<string>& words) {
-        unordered_map<char, unordered_set<char>> st;
-        int unq=0;
-        int n = words.size();
+        unordered_map<char, unordered_set<char>> adj;
         unordered_map<char, int> indegree;
         
         for(auto word:words){
             for(auto c:word){
-                st[c]={};
+                adj[c] = {};
+                indegree[c]=0;
             }
         }
         
-        for(int i=0;i<n-1;i++){
-            string a = words[i], b = words[i+1];
-            int j=0, k=0;
-            if(b.length() < a.length() && startsWith(a, b)) return "";
+        for(int i=0;i<words.size()-1;i++){
+            string a = words[i];
+            string b = words[i+1];
             
-            while(j<a.length() && k<b.length() && a[j] == b[k]){
+            if(b.length() < a.length() && startsWith(a, b)){
+                return "";
+            }
+            
+            int j=0, k=0;
+            while(j<a.length() && k<b.length() && a[j]==b[k]){
                 j+=1;
                 k+=1;
             }
-            if(j<a.length() && k<b.length()){
-                if(st[a[j]].count(b[k])==0){
-                    st[a[j]].insert(b[k]);
-                    indegree[b[k]]+=1;     
-                }
-               
-            }
-        }
-        
-        queue<char> q;
-        
-        for(auto it:st){
-            if(indegree[it.first]==0){
-                q.push(it.first);
-            }
-        }
-        
-        cout<<q.size();
-        string res="";
-        while(!q.empty()){
-            auto f = q.front();q.pop();
-            string t(1, f);
-            res+=t;
             
-            for(auto x:st[f]){
-                
-                indegree[x]-=1;
-                if(indegree[x]==0) q.push(x);
+            if(j<a.length() && k<b.length()){
+                if(adj[a[j]].count(b[k]) == 0){
+                adj[a[j]].insert(b[k]);
+                indegree[b[k]]+=1;
+            }
+    
+            }
+                        
+        }
+        
+        
+        string order="";
+        queue<char> q;
+        for(auto x:indegree){
+            if(x.second==0){
+                q.push(x.first);
             }
         }
-        return res.length()!=st.size() ? "":res;
+        
+        if(q.size()==0){
+            return "";
+        }
+        
+        while(!q.empty()){
+            auto f = q.front();
+            q.pop();
+            order+=f;
+            for(auto x:adj[f]){
+                indegree[x]-=1;
+                if(indegree[x]==0){
+                    q.push(x);
+                }
+            }
+            
+        }
+        cout<<"order: "<<order<<" "<<adj.size();
+        return order.length() == adj.size() ? order : "";
+        
+        
         
     }
 };
