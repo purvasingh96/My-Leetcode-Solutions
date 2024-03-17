8cc2f02440ab0da77e1eb0440fa73de26dcbde3b
Purva Singh <singhpurva2906@gmail.com>
2024-03-17 17:51:27 -0400

    Time: 3 ms (67.78%), Space: 15.7 MB (9.65%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.cpp
@@ -17,30 +17,31 @@ private:
         }
         
         m[col].push_back({root->val, row});
-        
         dfs(root->left, row+1, col-1, m);
         dfs(root->right, row+1, col+1, m);
     }
 public:
     vector<vector<int>> verticalOrder(TreeNode* root) {
+        
         map<int, vector<pair<int, int>>> m;
         dfs(root, 0, 0, m);
         vector<vector<int>> ans;
-        
+        if(!root){
+            return ans;
+        }
         for(auto x:m){
+            vector<int> temp;
             sort(x.second.begin(), x.second.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-                return a.second <b.second;
+                return a.second < b.second;
             });
             
-            vector<int> temp;
             for(auto y:x.second){
                 temp.push_back(y.first);
             }
-                
-            
             ans.push_back(temp);
         }
         
         return ans;
+        
     }
 };
