05282d9d28fc0558e66433b26d284daec3b34549
Purva Singh <singhpurva2906@gmail.com>
2024-03-18 21:56:22 -0400

    Time: 25 ms (47.86%), Space: 22.2 MB (68.70%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& n1, vector<vector<int>>& n2) {
-     
-        vector<vector<int>> ans;
-        
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& a, vector<vector<int>>& b) {
         int i=0, j=0;
-        
-        while(i<n1.size() && j<n2.size()){
-            int start = max(n1[i][0], n2[j][0]);
-            int end = min(n1[i][1], n2[j][1]);
+        vector<vector<int>> ans;
+        while(i<a.size() && j <b.size()){
+            int start = max(a[i][0], b[j][0]);
+            int end = min(a[i][1], b[j][1]);
             
             if(start<=end){
                 ans.push_back({start, end});
             }
             
-            if(n1[i][1] < n2[j][1]){
+            if(a[i][1] < b[j][1]){
                 i+=1;
             } else {
                 j+=1;
             }
         }
         
-       
-        
-        
         return ans;
-        
-        
     }
 };
