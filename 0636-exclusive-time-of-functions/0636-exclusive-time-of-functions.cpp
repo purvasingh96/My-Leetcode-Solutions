f0691e984b7fa2b45f00b4275a5c7e743bef6016
Purva Singh <singhpurva2906@gmail.com>
2024-03-19 22:32:33 -0400

    Time: 37 ms (11.58%), Space: 25.5 MB (16.50%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.cpp
@@ -1,45 +1,47 @@
 class Solution {
 private:
-    vector<string> split(string s){
+    vector<string> split(string log){
         string res="";
+        log+=':';
         vector<string> ans;
-        for(int i=0;i<s.length();i++){
-            if(s[i]==':'){
+        for(int i=0;i<log.length();i++){
+            if(log[i] == ':'){
                 ans.push_back(res);
-                res = "";
-            } else {
-                res += s[i];
+                res="";
+            } else{
+                res += log[i];
             }
         }
-        ans.push_back(res);
         return ans;
     }
-    
 public:
     vector<int> exclusiveTime(int n, vector<string>& logs) {
         stack<int> st;
         vector<int> ans(n, 0);
-        int prevTime=0;
+        int pt=0;
         
-        for(int i=0;i<logs.size();i++){
-            vector<string> data = split(logs[i]);
-            int idx = stoi(data[0]);
-            string op = data[1];
-            int currTime = stoi(data[2]);
-            
+        for(auto log:logs){
+            vector<string> temp = split(log);
+            int i = stoi(temp[0]);
+            string op = temp[1];
+            int ct = stoi(temp[2]);
             if(op == "start"){
-                int top = st.empty() ? idx:st.top();
-                ans[top] += currTime - prevTime;
-                prevTime = currTime;
-                st.push(idx);
+                if(st.empty()){
+                    st.push(i);
+                } else {
+                    ans[st.top()] += ct-pt;
+                    pt = ct;
+                    st.push(i);
+                }
             } else {
+                int top = st.top();
                 st.pop();
-                ans[idx] += currTime - prevTime+1;
-                prevTime = currTime+1;
+                ans[top] += (ct -pt+1);
+                pt = ct+1;
             }
-            
         }
         
         return ans;
+        
     }
 };
