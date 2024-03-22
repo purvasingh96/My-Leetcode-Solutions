c4df2cfe1aa15510de2e205fab7a83b2384badad
Purva Singh <singhpurva2906@gmail.com>
2024-03-21 22:16:27 -0400

    Time: 0 ms (100.00%), Space: 14.7 MB (19.42%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         vector<int> res;
-        int start=-1, end=-1;
         bool found=false;
-        
+        int start=-1, end=-1;
         for(int i=nums.size()-1;i>=0;i--){
             if(res.size()==0 || nums[res.back()] <= nums[i]){
                 res.push_back(i);
-            }
-            else {
-                while(res.size()!=0 && nums[res.back()] > nums[i]){
-                    end = res.back();
-                    res.pop_back();
-                }
-                found = true;
-                start = i;
+            } else {
+                found=true;
+               while(res.size()!=0 && nums[res.back()] > nums[i]) {
+                   end=res.back();
+                   res.pop_back();
+               }
+                start=i;
                 break;
-            }            
+            }
         }
-        
+        cout<<found;
         if(!found){
             sort(nums.begin(), nums.end());
         } else {
             swap(nums[start], nums[end]);
             sort(nums.begin()+start+1, nums.end());
         }
+        
+        
     }
 };
