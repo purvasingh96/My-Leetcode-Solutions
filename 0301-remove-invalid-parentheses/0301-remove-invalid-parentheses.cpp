dd78224164f4acbded67cc0fc437f7c3b3282d59
Purva Singh <singhpurva2906@gmail.com>
2024-03-21 21:26:17 -0400

    Time: 83 ms (57.13%), Space: 24.5 MB (41.86%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.cpp
@@ -1,57 +1,62 @@
 class Solution {
 private:
     bool isValid(string s){
-        int balance=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                balance+=1;
-            } else if(s[i] == ')') {
-                balance-=1;
-                if(balance < 0){
+        int b=0;
+        for(auto c:s){
+            if(c=='('){
+                b+=1;
+            } else if(c == ')'){
+                b-=1;
+                if(b<0){
                     return false;
                 }
             }
         }
-        
-        return balance==0;
+        return b==0;
     }
 public:
     vector<string> removeInvalidParentheses(string s) {
         queue<string> q;
         q.push(s);
         unordered_map<string, bool> visited;
-        bool found=false;
-        vector<string> res;
-        visited[s] = true;
         
+        vector<string> ans;
+        visited[s] = true;
+        bool found=false;
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
-                auto f = q.front();
+                
+                auto f= q.front();
                 q.pop();
                 
                 if(isValid(f)){
+                    //cout<<"valid";
+                    ans.push_back(f);
                     found = true;
-                    res.push_back(f);
                 }
                 
                 for(int i=0;i<f.length();i++){
-                    string left = f.substr(0, i);
-                    string right = f.substr(i+1);
-                    string temp = left+right;
-                    if(!visited[temp]){
-                        visited[temp] = true;
-                        q.push(temp);
+                    if(f[i] == '(' || f[i] == ')'){
+                         string temp = f.substr(0, i) + f.substr(i+1);
+                            if(!visited[temp]){
+                                visited[temp] = true;
+                                q.push(temp);
+                            }
                     }
-                    
+                   
                 }
                 
+                
             }
+            
             if(found){
-                break;
+                
+                return ans;
             }
         }
         
-        return res;
+        return ans;
+        
     }
 };
