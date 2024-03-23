aaaef41c191d4f060358a86f84b72ac010b71748
Purva Singh <singhpurva2906@gmail.com>
2024-03-23 16:31:48 -0400

    Time: 64 ms (78.24%), Space: 37 MB (84.35%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,18 +1,19 @@
 class DSU{
- public:
+public:
     vector<int> root;
-    vector<int> size;
+    vector<int> rank;
+    
     DSU(int n){
         root.resize(n);
-        size.resize(n);
+        rank.resize(n, 1);
+        
         for(int i=0;i<n;i++){
             root[i]=i;
-            size[i]=1;
         }
     }
     
     int find(int x){
-        if(root[x] == x) {
+        if(root[x] == x){
             return x;
         }
         return root[x] = find(root[x]);
@@ -23,55 +24,53 @@ class DSU{
         int ry = find(y);
         
         if(rx!=ry){
-            if(size[rx] >= size[ry]){
+            if(rank[rx] >= rank[ry]){
                 root[ry] = rx;
-                size[rx] += size[ry];
+                rank[rx]+=rank[ry];
             } else {
-                root[rx] = ry;
-                size[ry]+=size[rx];
+                root[rx]  = ry;
+                rank[ry] += rank[rx];
             }
         }
-    }
         
+    }
 };
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
-        DSU dsu(n);
+    vector<vector<string>> accountsMerge(vector<vector<string>>& nums) {
         
+        int n = nums.size();
+        DSU dsu(n);
         
         unordered_map<string, int> m;
         
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-                if(m.find(accounts[i][j]) == m.end()){
-                    m[accounts[i][j]] = i;
+        for(int i=0;i<n;i++){
+            
+            for(int j=1;j<nums[i].size();j++){
+                if(m.find(nums[i][j]) != m.end()){
+                    dsu.merge(m[nums[i][j]], i);
                 } else {
-                    dsu.merge(i, m[accounts[i][j]]);
+                    m[nums[i][j]] = i;
                 }
             }
         }
         
-        cout<<"working";
-        
-        vector<vector<string>> ans(n);
+        unordered_map<int, vector<string>> res;
         
         for(auto x:m){
-            int idx = dsu.find(x.second);
-            ans[idx].push_back(x.first);
+            res[dsu.find(x.second)].push_back(x.first);
         }
-        vector<vector<string>> res;
-        for(int i=0;i<ans.size();i++){
-            if(ans[i].size()!=0){
-                vector<string> temp;
-                temp.push_back(accounts[i][0]);
-                sort(ans[i].begin(), ans[i].end());
-                temp.insert(temp.end(), ans[i].begin(), ans[i].end());
-                res.push_back(temp);
-            }
-            
+        
+        vector<vector<string>> ans;
+        for(auto x:res){
+            vector<string> temp;
+            temp.push_back(nums[x.first][0]);
+            sort(x.second.begin(), x.second.end());
+            temp.insert(temp.end(), x.second.begin(), x.second.end());
+            ans.push_back(temp);
         }
-        return res;
+        
+        return ans;
+        
     }
 };
