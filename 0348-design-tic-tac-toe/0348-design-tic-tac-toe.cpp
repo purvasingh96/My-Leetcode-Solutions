51ab3029b47730bbdab9cb54bdb8905ec2569d94
Purva Singh <singhpurva2906@gmail.com>
2024-03-23 18:41:03 -0400

    Time: 30 ms (9.95%), Space: 22.8 MB (74.88%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
@@ -1,58 +1,58 @@
 class TicTacToe {
 public:
-    vector<int> row_1;
-    vector<int> col_1;
-    vector<int> row_2;
-    vector<int> col_2;
     int diag_1;
-    int anti_diag_1;
     int diag_2;
+    int anti_diag_1;
     int anti_diag_2;
-    int sz;
+    vector<int> rows_1;
+    vector<int> rows_2;
+    vector<int> cols_1;
+    vector<int> cols_2;
+    int m;
     TicTacToe(int n) {
         diag_1=0;
         diag_2=0;
         anti_diag_1=0;
         anti_diag_2=0;
-        sz=n;
-        row_1.resize(n, 0);
-        col_1.resize(n, 0);
-        row_2.resize(n, 0);
-        col_2.resize(n, 0);
+        rows_1.resize(n, 0);
+        rows_2.resize(n, 0);
+        cols_1.resize(n, 0);
+        cols_2.resize(n, 0);
+        m=n;
     }
-    
-    
     
     int move(int row, int col, int player) {
         if(player==1){
-            row_1[row] +=1;
-            col_1[col]+=1;
+            rows_1[row]+=1;
+            cols_1[col]+=1;
             
             if(row == col){
                 diag_1+=1;
-            }
-            if(row+col == sz-1){
-                anti_diag_1+=1;
-            }
+            } 
             
-            if(row_1[row]==sz || col_1[col]==sz || diag_1 == sz || anti_diag_1 == sz){
+            if(row + col == m-1){
+                anti_diag_1 +=1;
+            } 
+            
+            if(rows_1[row]==m || cols_1[col] == m || diag_1 == m || anti_diag_1 == m){
                 return 1;
             }
         } else {
-           row_2[row] +=1;
-            col_2[col]+=1;
+            rows_2[row]+=1;
+            cols_2[col] += 1;
             
             if(row == col){
-                diag_2+=1;
+                diag_2 += 1;
             }
-            if(row+col == sz-1){
-                anti_diag_2+=1;
+            if(row + col == m-1){
+                anti_diag_2 += 1;
             }
             
-            if(row_2[row]==sz || col_2[col]==sz || diag_2 == sz || anti_diag_2 == sz){
+            if(rows_2[row] == m || cols_2[col] == m || diag_2 == m || anti_diag_2 == m){
                 return 2;
-            } 
+            }
         }
+        
         return 0;
     }
 };
