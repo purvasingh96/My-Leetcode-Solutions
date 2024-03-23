a0b29fab47ab68b5e93cb3899a07c54d09c39ed5
Purva Singh <singhpurva2906@gmail.com>
2024-03-23 10:51:31 -0400

    Time: 0 ms (100.00%), Space: 8.2 MB (51.31%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -4,20 +4,23 @@ public:
         int expected=lower;
         vector<vector<int>> ans;
         
+        // if(nums.size()==0){
+        //     ans.push_back({lower, upper});
+        //     return ans;
+        // }
+        
         for(int i=0;i<nums.size();i++){
-            if(nums[i] == expected){
-                expected+=1;
-                continue;
-            } else {
+            if(nums[i] != expected){
                 ans.push_back({expected, nums[i]-1});
-                expected=nums[i]+1;
+                expected = nums[i]+1;
+            } else {
+                expected+=1;
             }
         }
         
         if(expected<=upper){
             ans.push_back({expected, upper});
         }
-        
         return ans;
     }
 };
