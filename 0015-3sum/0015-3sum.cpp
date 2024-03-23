d6befbab26e5ef3d42b71a8f29c4135d7904f14a
Purva Singh <singhpurva2906@gmail.com>
2024-03-23 17:03:48 -0400

    Time: 140 ms (40.28%), Space: 39 MB (35.90%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,51 +1,47 @@
 class Solution {
 private:
-   void twoSum(vector<int>& nums, int start, int end, int target, set<vector<int>>& res){
-         
+    void twoSum(vector<int>& nums, int start, int end, int target, set<vector<int>>& ans){
         while(start<end){
             int sum = nums[start] + nums[end];
             if(sum == target){
-                res.insert({-target, nums[start], nums[end]});
-                
-                while(start < end && nums[start] == nums[start+1]){
+                ans.insert({-target, nums[start], nums[end]});
+                while(start<end && nums[start] == nums[start+1]){
                     start+=1;
                 }
-                
-                while(start<end && nums[end] == nums[end-1]){
+                while(end>start && nums[end] == nums[end-1]){
                     end-=1;
                 }
-                
                 start+=1;
                 end-=1;
-                
-            }
-            
-            else if(sum > target){
-                while(start<end && nums[end] == nums[end-1]){
+            } else if(sum > target){
+                while(end>start && nums[end] == nums[end-1]){
                     end-=1;
                 }
                 end-=1;
-            } else {
+            } else if(sum < target) {
                 while(start<end && nums[start] == nums[start+1]){
                     start+=1;
                 }
                 start+=1;
             }
         }
-        
     }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> res;
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> res;
         
-        for(int i=1;i<nums.size();i++){
-            int start = i, end = nums.size()-1;
-            int target = -nums[start-1];
-            twoSum(nums, start, end, target, res);
+        sort(nums.begin(), nums.end());
+        set<vector<int>> ans;
+        
+        for(int i=0;i<n-1;i++){
+            twoSum(nums, i+1, n-1, -nums[i], ans);
         }
         
-        vector<vector<int>> ans(res.begin(), res.end());
-        return ans;
+        for(auto x:ans){
+            res.push_back(x);
+        }
+        
+        return res;
     }
 };
