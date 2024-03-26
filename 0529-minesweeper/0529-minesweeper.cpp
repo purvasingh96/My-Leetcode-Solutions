1e5c60a04b643233baa981a32879ddd9354fc6ee
Purva Singh <singhpurva2906@gmail.com>
2024-03-25 23:21:38 -0400

    Time: 20 ms (35.10%), Space: 16 MB (31.84%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.cpp
+++ 0529-minesweeper/0529-minesweeper.cpp
@@ -1,58 +1,54 @@
 class Solution {
 private:
-    
     int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
     int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 
     bool isValid(int x, int y, vector<vector<char>>& grid){
             return x>=0 && y>=0 && x<grid.size() && y<grid[0].size();
     }
-    char returnMines(int x,int y, vector<vector<char>>& board){
-        int mines=0;
-        for(int i=0;i<8;i++){
-            int newX = x+dx[i];
-            int newY = y+dy[i];
-            if(isValid(newX, newY, board) && board[newX][newY]=='M'){
-                mines+=1;
+    int count(int x, int y, vector<vector<char>>& board){
+        int c=0;
+        for(int k=0;k<8;k++){
+            int new_x = x+dx[k];
+            int new_y = y+dy[k];
+            if(isValid(new_x, new_y, board) && board[new_x][new_y]=='M'){
+                c+=1;
             }
         }
-        return mines+'0';
+        
+        return c;
     }
-    
     void dfs(int x, int y, vector<vector<char>>& board, vector<vector<bool>>& visited){
+        if(board[x][y] == 'M'){
+            board[x][y] = 'X';
+            return;
+        }
         
         if(board[x][y] == 'E'){
-            char mines = returnMines(x, y, board);
-            //cout<<"x: "<<x<<" y: "<<y<<"mines: "<<mines<<"\n";
-            if(mines=='0'){
-                board[x][y]='B';
-                // recrusve reveal  
-                for(int i=0;i<8;i++){
-                    int newX = x+dx[i];
-                    int newY = y+dy[i];
-                    if(isValid(newX, newY, board) && !visited[newX][newY] && board[newX][newY]!='B'){
-                        visited[newX][newY]=true;
-                        dfs(newX, newY, board, visited);
+            int c = count(x, y, board);
+            if(c == 0){
+                board[x][y] = 'B';
+                for(int k=0;k<8;k++){
+                    int new_x = x+dx[k];
+                    int new_y = y+dy[k];
+                    if(isValid(new_x, new_y, board) && 
+                       !visited[new_x][new_y]){
+                        visited[new_x][new_y] = true;
+                        dfs(new_x, new_y, board, visited);
                     }
                 }
             } else {
-               board[x][y] = mines; 
+                board[x][y] = c+'0';
             }
-            
-        } 
+        }
+        
     }
-    
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
-        int x =click[0], y = click[1];
-        if(board[x][y]=='M'){
-            board[x][y]='X';
-            return board;
-        }
         int m = board.size(), n = board[0].size();
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        visited[x][y]=true;
-        dfs(x, y, board, visited);
+        dfs(click[0], click[1], board, visited);
         return board;
+        
     }
 };
