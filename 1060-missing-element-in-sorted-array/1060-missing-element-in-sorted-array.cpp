e7326b6ebdd72650e3e4fe6f97fd2e65c142552a
Purva Singh <singhpurva2906@gmail.com>
2024-04-01 21:27:01 -0400

    Time: 52 ms (9.03%), Space: 33.8 MB (84.87%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int missingElement(vector<int>& nums, int k) {
-        int l=0, r=nums.size()-1;
+        int l = 0, r=nums.size()-1;
         
         while(l<=r){
-            // mid represents number of missing elements
             int mid = l + (r-l)/2;
-            // missing elements
             int missing = nums[mid] - nums[0] - mid;
+            
             if(missing < k){
                 l=mid+1;
             } else {
@@ -15,7 +14,6 @@ public:
             }
         }
         
-        return nums[0] + l + k-1;
-        
+        return nums[0]+l+k-1;
     }
 };
