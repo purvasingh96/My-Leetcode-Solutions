ec46a4fd8262f743e985872a896007101b946162
Purva Singh <singhpurva2906@gmail.com>
2024-04-01 20:02:22 -0400

    Time: 17 ms (13.36%), Space: 24.6 MB (12.49%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,18 +4,21 @@ public:
         vector<int> temp = {0};
         temp.insert(temp.end(), nums.begin(), nums.end());
         temp.push_back(0);
-        nums = temp;
+        int k =nums.size();
         
-        
-        for(int i=1;i<nums.size()-1;i++){
-            if(nums[i-1] == nums[i] && nums[i] == nums[i+1] && nums[i+1]==0){
-                n-=1;
-                nums[i]=1;
+        for(int i=1;i<temp.size()-1;i++){
+            
+                if(temp[i] ==0 && temp[i-1] !=1 && temp[i+1]!=1){
+                    n-=1;
+                    temp[i]=1;
+                }
+            
+            
+            if(n<=0){
+                return true;
             }
-            if(n<=0) return true;
         }
         
         return n<=0;
-        
     }
 };
