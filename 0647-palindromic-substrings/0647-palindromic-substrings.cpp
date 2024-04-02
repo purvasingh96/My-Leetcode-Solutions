7adb2a11b98078f4d1d1f3546559d59f925d412a
Purva Singh <singhpurva2906@gmail.com>
2024-04-01 21:06:10 -0400

    Time: 47 ms (26.28%), Space: 10.1 MB (43.25%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int ans=0;
+        
         for(int i=0;i<n;i++){
-            dp[i][i] = 1;
+            dp[i][i] = true;            
         }
         
-        int ans = s.length();
+        for(int i=0;i<n-1;i++){
+            dp[i][i+1] = s[i] == s[i+1];
+            ans += dp[i][i+1];
+        }
         
-        for(int d=1;d<n;d++){
-            for(int i=0;i+d<n;i++){
-                int j = i+d;
-                //cout<<"i: "<<i<<"j: "<<j<<"\n";
-                if(s[i] == s[j]){
-                    if((i+1 <= j-1 && dp[i+1][j-1]) || (j-i+1==2)){
-                        ans+=1;
-                        dp[i][j]=1;
-                    } 
-                }
+        for(int len=3;len<=n;len++){
+            for(int i=0, j=i+len-1;j<n;i++, j++){
+                dp[i][j] = dp[i+1][j-1] && s[i]==s[j];
+                ans += dp[i][j];
             }
         }
         
-//         for(auto x:dp){
-//             for(auto y:x){
-//                 cout<<y<<" ";
-//             }
-//             cout<<"\n";
-//         }
-        
-        return ans;
-        
+        return ans+n;
     }
+    
 };
