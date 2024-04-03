c9ee4ba8b9ca814962b5bd8b90b93a3b8dd13e01
Purva Singh <singhpurva2906@gmail.com>
2024-04-02 21:34:01 -0400

    Time: 29 ms (65.38%), Space: 34.1 MB (69.94%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        // find left and right bounds
-        int n = arr.size();
-        int l = 0, r=n-1;
+        int l=0, r=arr.size()-k;
         
         while(l<r){
             int mid = l + (r-l)/2;
-            if(mid+k < n && x-arr[mid] > arr[mid+k]-x){
+            if(x - arr[mid] > arr[mid+k] - x){
                 l=mid+1;
             } else {
-                r = mid;
+                r=mid;
             }
         }
         
+        cout<<"here";
+        
         vector<int> ans;
-        for(int i=l;i<l+k;i++){
-            ans.push_back(arr[i]);
+        
+        for(int j=l;j<l+k;j++){
+            ans.push_back(arr[j]);
         }
         
         return ans;
-        
+            
     }
 };
